b7900bb1b04ca2bd3467934bf4beab4ebf7ed9ba
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2012-11-23 13:04:13 +0100

    Add plainbox.impl.depmgr and assocated tests
    This module implements the DependencySolver class with one class method
    DependencySolver.resolve_dependencies() that takes a list of jobs
    as argument and either throws one of the thee new exceptions or
    returns a list of jobs that are safe to run in that order.
    
    The three new exceptions Dependency{Cycle,Missing,Duplicate}Error
    indicate that a dependency loop, unresolved dependency or duplicate job
    name has been found, respectively. They also carry enough state for
    informative error messages or error recovery.
    
    The solver can also consider a subset of jobs to visit. This allows one
    to select a few jobs that actually need to run while still taking
    advantage of the validation and resource dependency resolution.
    
    The module has 100% test coverage
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ checkbox-ng/impl/depmgr.py
@@ -0,0 +1,278 @@
+# This file is part of Checkbox.
+#
+# Copyright 2012 Canonical Ltd.
+# Written by:
+#   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+plainbox.impl.depmgr
+====================
+
+Internal implementation of plainbox
+
+ * THIS MODULE DOES NOT HAVE STABLE PUBLIC API *
+"""
+
+from abc import ABCMeta
+from abc import abstractproperty
+from itertools import chain
+from itertools import repeat
+from logging import getLogger
+
+
+logger = getLogger("plainbox.depmgr")
+
+
+class DependencyError(Exception, metaclass=ABCMeta):
+    """
+    Exception raised when a dependency error is detected
+    """
+
+    @abstractproperty
+    def affected_job(self):
+        """
+        JobDefinition instance that is affected by the dependency error.
+        """
+
+
+class DependencyCycleError(DependencyError):
+    """
+    Exception raised when a cyclic dependency is detected
+    """
+
+    def __init__(self, job_list):
+        """
+        Initialize with a list of jobs that form a dependency loop.
+
+        The dependencies satisfy the given expression:
+
+            job_list[n - 1] depends-on job_list[n]
+
+        The error exists because job_list[0] is job_list[-1].
+        Each item is a JobDefinition instance.
+        """
+        assert len(job_list) > 1
+        assert job_list[0] is job_list[-1]
+        self.job_list = job_list
+
+    @property
+    def affected_job(self):
+        return self.job_list[0]
+
+    def __str__(self):
+        return "dependency cycle detected: {}".format(
+            " -> ".join([job.name for job in self.job_list]))
+
+    def __repr__(self):
+        return "<{} job_list:{!r}>".format(
+            self.__class__.__name__, self.job_list)
+
+
+class DependencyMissingError(DependencyError):
+    """
+    Exception raised when a job has unsatisfied dependency
+    """
+
+    DEP_TYPE_RESOURCE = "resource"
+    DEP_TYPE_DIRECT = "direct"
+
+    def __init__(self, job, missing_job_name, dep_type):
+        self.job = job
+        self.missing_job_name = missing_job_name
+        self.dep_type = dep_type
+
+    @property
+    def affected_job(self):
+        return self.job
+
+    def __str__(self):
+        return "missing dependency: {!r} ({})".format(
+            self.missing_job_name, self.dep_type)
+
+    def __repr__(self):
+        return "<{} job:{!r} missing_job_name:{!r} dep_type:{!r}>".format(
+            self.__class__.__name__,
+            self.job, self.missing_job_name, self.dep_type)
+
+
+class DependencyDuplicateError(DependencyError):
+    """
+    Exception raised when two jobs have identical name
+    """
+
+    def __init__(self, job, duplicate_job):
+        assert job.name == duplicate_job.name
+        self.job = job
+        self.duplicate_job = duplicate_job
+
+    @property
+    def affected_job(self):
+        return self.job
+
+    def __str__(self):
+        return "duplicate job name: {!r}".format(self.affected_job.name)
+
+    def __repr__(self):
+        return "<{} job:{!r} duplicate_job:{!r}>".format(
+            self.__class__.__name__, self.job, self.duplicate_job)
+
+
+class DependencySolver:
+    """
+    Dependency solver for Jobs
+
+    Uses a simple depth-first search to discover the sequence of jobs that can
+    run. Use the resolve_dependencies() class method to get the solution.
+    """
+
+    # Node colors:
+    #
+    # white nodes have not been visited yet
+    # gray nodes are currently being visited and are incomplete
+    # black nodes have been visited and are complete
+    COLOR_WHITE, COLOR_GRAY, COLOR_BLACK = range(3)
+
+    @classmethod
+    def resolve_dependencies(cls, job_list, visit_list=None):
+        """
+        Solve the dependency graph expressed as a list of job definitions.  The
+        visit_list, if specified, allows to consider only a part of the graph
+        while still having access and knowledge of all jobs.
+
+        Returns the solution (a list of jobs to execute in order)
+
+        raises DependencyCycleError if a cyclic dependency is present.
+
+        raises DependencyMissingErorr if a required job does not exist.
+        """
+        return cls(job_list)._solve(visit_list)
+
+    def __init__(self, job_list):
+        """
+        Instantiate a new dependency solver with the specified list of jobs
+        """
+        # Remember the jobs that were passed
+        self._job_list = job_list
+        # Build a map of jobs (by name)
+        self._job_map = self._get_job_map(job_list)
+        # Job colors, maps from job.name to COLOR_xxx
+        self._job_color_map = {job.name: self.COLOR_WHITE for job in job_list}
+        # The computed solution, made out of job instances. This is not
+        # necessarily the only solution but the algorithm computes the same
+        # value each time, given the same input.
+        self._solution = []
+
+    def _solve(self, visit_list=None):
+        """
+        Internal method of DependencySolver.
+
+        Solves the dependency graph and returns the solution.
+
+        Calls _visit() on each of the initial nodes/jobs
+        """
+        # Visit the visit list
+        logger.debug("Starting solve")
+        if visit_list is None:
+            visit_list = self._job_list
+        for job in visit_list:
+            self._visit(job)
+        logger.debug("Done solving")
+        # Return the solution
+        return self._solution
+
+    def _visit(self, job, trail=None):
+        """
+        Internal method of DependencySolver
+
+        Called each time a node is visited. Nodes already seen in _visited are
+        skipped. Attempts to enumerate all dependencies (both direct and
+        resource) and resolve them. Missing jobs cause DependencyMissingError
+        to be raised. Calls _visit recursively on all dependencies.
+        """
+        color = self._job_color_map[job.name]
+        logger.debug("Visiting job %s (color %s)", job, color)
+        if color == self.COLOR_WHITE:
+            # This node has not been visited yet. Let's mark it as GRAY (being
+            # visited) and iterate through the list of dependencies
+            self._job_color_map[job.name] = self.COLOR_GRAY
+            # If the trail was not specified start a trail for this node
+            if trail is None:
+                trail = [job]
+            # Visit each dependency
+            for dep_type, job_name in self._get_dependency_set(job):
+                logger.debug("Found dependency %s: %s", dep_type, job_name)
+                # Dependency is just a name, we need to resolve it
+                # to a job instance. This can fail (missing dependencies)
+                # so let's guard against that.
+                try:
+                    next_job = self._job_map[job_name]
+                except KeyError:
+                    raise DependencyMissingError(job, job_name, dep_type)
+                else:
+                    # For each dependency that we visit let's reuse the trail
+                    # to give proper error messages if a dependency loop exists
+                    logger.debug("Visiting dependency {}", next_job)
+                    # Update the trail as we visit that node
+                    trail.append(next_job)
+                    self._visit(next_job, trail)
+                    trail.pop()
+            # We've visited (recursively) all dependencies of this node,
+            # let's color it black and append it to the solution list.
+            logger.debug("Appending {} to solution", job)
+            self._job_color_map[job.name] = self.COLOR_BLACK
+            self._solution.append(job)
+        elif color == self.COLOR_GRAY:
+            # This node is not fully traced yet but has been visited already
+            # so we've found a dependency loop. We need to cut the initial
+            # part of the trail so that we only report the part that actually
+            # forms a loop
+            raise DependencyCycleError(trail[trail.index(job):])
+        else:
+            assert color == self.COLOR_BLACK
+            # This node has been visited and is fully traced.
+            # We can just skip it and go back
+
+    @staticmethod
+    def _get_job_map(job_list):
+        """
+        Internal method of DependencySolver.
+
+        Computes a map of job.name => job
+        Raises DependencyDuplicateError if a collision is found
+        """
+        job_map = {}
+        for job in job_list:
+            if job.name in job_map:
+                raise DependencyDuplicateError(job_map[job.name], job)
+            else:
+                job_map[job.name] = job
+        return job_map
+
+    @staticmethod
+    def _get_dependency_set(job):
+        """
+        Internal method of DependencySolver
+
+        Returns a set of pairs (dep_type, job_name) that describe all
+        dependencies of the specified job. The first element in the pair,
+        dep_type, is either DEP_TYPE_DIRECT or DEP_TYPE_RESOURCE. The second
+        element is the name of the job.
+        """
+        direct = DependencyMissingError.DEP_TYPE_DIRECT
+        resource = DependencyMissingError.DEP_TYPE_RESOURCE
+        return set(chain(
+            zip(repeat(direct), job.get_direct_dependencies()),
+            zip(repeat(resource), job.get_resource_dependencies())))

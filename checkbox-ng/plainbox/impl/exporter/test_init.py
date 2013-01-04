c4396daede489d8fa1823e80610465a4613028e5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-01-04 20:41:33 +0100

    Add initial unit tests for SessionStateExporterBase
    The only tested thing are the default settings of the exporter's
    only function, the get_session_data_subset()
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/exporter/test_init.py
+++ checkbox-ng/plainbox/impl/exporter/test_init.py
@@ -26,7 +26,10 @@ Test definitions for plainbox.impl.exporter module
 
 from unittest import TestCase
 
+from plainbox.impl.exporter import SessionStateExporterBase
 from plainbox.impl.exporter import classproperty
+from plainbox.impl.session import SessionState
+from plainbox.impl.testing_utils import make_job, make_job_result
 
 
 class ClassPropertyTests(TestCase):
@@ -50,3 +53,42 @@ class ClassPropertyTests(TestCase):
         cls = self.get_C()
         obj = cls()
         self.assertEqual(obj.prop, obj.attr)
+
+
+class SessionStateExporterBaseTests(TestCase):
+
+    class TestSessionStateExporter(SessionStateExporterBase):
+
+        def dump(self, data, stream):
+            """
+            Dummy implementation of a method required by the base class.
+            """
+
+    def make_test_session(self):
+        # Create a small session with two jobs and two results
+        job_a = make_job('job_a')
+        job_b = make_job('job_b')
+        session = SessionState([job_a, job_b])
+        session.update_desired_job_list([job_a, job_b])
+        result_a = make_job_result(job_a, 'pass')
+        result_b = make_job_result(job_b, 'fail')
+        session.update_job_result(job_a, result_a)
+        session.update_job_result(job_b, result_b)
+        return session
+
+    def test_defaults(self):
+        # Test all defaults, with all options unset
+        exporter = self.TestSessionStateExporter()
+        session = self.make_test_session()
+        data = exporter.get_session_data_subset(session)
+        expected_data = {
+            'result_map': {
+                'job_a': {
+                    'outcome': 'pass'
+                },
+                'job_b': {
+                    'outcome': 'fail'
+                }
+            }
+        }
+        self.assertEqual(data, expected_data)

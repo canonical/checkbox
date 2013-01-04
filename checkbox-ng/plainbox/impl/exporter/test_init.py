b10066ced59b07a6bc0216f3f37dd838f58a322b
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-01-04 22:56:32 +0100

    Add realistic tests for get_session_data_subset()
    This patch adds a number of tests that check how this complex method
    behaves. There is no distinction on what each option enables or not
    but it is already much better then previous naive test that was present
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/exporter/test_init.py
+++ checkbox-ng/plainbox/impl/exporter/test_init.py
@@ -29,6 +29,8 @@ from unittest import TestCase
 from plainbox.impl.exporter import SessionStateExporterBase
 from plainbox.impl.exporter import classproperty
 from plainbox.impl.session import SessionState
+from plainbox.impl.job import JobDefinition
+from plainbox.impl.result import JobResult, IOLogRecord
 from plainbox.impl.testing_utils import make_job, make_job_result
 
 
@@ -92,3 +94,103 @@ class SessionStateExporterBaseTests(TestCase):
             }
         }
         self.assertEqual(data, expected_data)
+
+    def make_realistic_test_session(self):
+        # Create a more realistic session with two jobs but with richer set
+        # of data in the actual jobs and results.
+        job_a = JobDefinition({
+            'plugin': 'shell',
+            'name': 'job_a',
+            'command': 'echo testing && true',
+            'requires': 'job_b.ready == "yes"'
+        })
+        job_b = JobDefinition({
+            'plugin': 'resource',
+            'name': 'job_b',
+            'command': 'echo ready: yes'
+        })
+        session = SessionState([job_a, job_b])
+        session.update_desired_job_list([job_a, job_b])
+        result_a = JobResult({
+            'job': job_a,
+            'outcome': 'pass',
+            'return_code': 0,
+            'io_log': (
+                IOLogRecord(0, 'stdout', 'testing\n'),
+            )
+        })
+        result_b = JobResult({
+            'job': job_b,
+            'outcome': 'pass',
+            'return_code': 0,
+            'io_log': (
+                IOLogRecord(0, 'stdout', 'ready: yes\n'),
+            )
+        })
+        session.update_job_result(job_a, result_a)
+        session.update_job_result(job_b, result_b)
+        return session
+
+    def test_all_at_once(self):
+        # Test every option set, all at once
+        # Currently this sets both OPTION_WITH_IO_LOG and
+        # one of the two mutually exclusive options:
+        #   - OPTION_SQUASH_IO_LOG
+        #   - OPTION_FLATTEN_IO_LOG
+        # The implementation favours SQUASH_IO_LOG
+        # and thus the code below tests that option
+        exporter = self.TestSessionStateExporter(
+            self.TestSessionStateExporter.supported_option_list)
+        session = self.make_realistic_test_session()
+        data = exporter.get_session_data_subset(session)
+        expected_data = {
+            'job_list': ['job_a', 'job_b'],
+            'run_list': ['job_b', 'job_a'],
+            'desired_job_list': ['job_a', 'job_b'],
+            'resource_map': {
+                'job_b': [{
+                    'ready': 'yes'
+                }]
+            },
+            'result_map': {
+                'job_a': {
+                    'outcome': 'pass',
+                    'plugin': 'shell',
+                    'command': 'echo testing && true',
+                    'io_log': ['testing'],
+                    'requires': 'job_b.ready == "yes"'
+                },
+                'job_b': {
+                    'outcome': 'pass',
+                    'plugin': 'resource',
+                    'command': 'echo ready: yes',
+                    'io_log': ['ready: yes'],
+                }
+            }
+        }
+        # This is just to make debugging easier
+        self.assertEqual(expected_data.keys(), data.keys())
+        for key in data.keys():
+            self.assertEqual(expected_data[key], data[key],
+                             msg="wrong data in %r" % key)
+        # This is to make sure we didn't miss anything by being too smart
+        self.assertEqual(data, expected_data)
+
+    def test_io_log_processors(self):
+        # Test all of the io_log processors that are built into
+        # the base SessionStateExporter class
+        cls = self.TestSessionStateExporter
+        io_log = (
+            IOLogRecord(0, 'stdout', 'foo\n'),
+            IOLogRecord(1, 'stderr', 'bar\n'),
+            IOLogRecord(2, 'stdout', 'quxx\n')
+        )
+        self.assertEqual(
+            cls._squash_io_log(io_log), ['foo', 'bar', 'quxx'])
+        self.assertEqual(
+            cls._flatten_io_log(io_log), 'foo\nbar\nquxx\n')
+        self.assertEqual(
+            cls._io_log(io_log), [
+                (0, 'stdout', 'foo'),
+                (1, 'stderr', 'bar'),
+                (2, 'stdout', 'quxx')])

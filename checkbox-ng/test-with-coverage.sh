dadf90983d11ed288a7d8f80b10b8619dbff4e13
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-01-17 11:56:47 +0100

    Run integration tests when testing for coverage
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/test-with-coverage.sh
+++ checkbox-ng/test-with-coverage.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # Requires activated virtualenv with coverage
-coverage run --branch setup.py test && coverage report ; coverage html
+coverage run --branch setup.py test && \
+    coverage run --append --branch $(which plainbox) self-test --verbose && \
+    coverage report && \
+    coverage html

0acbe667d487357e3acd31c6675782c3476b0648
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-02-06 13:56:07 +0100

    runner: add tests for CommandIOLogBuilder
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/test_runner.py
+++ checkbox-ng/plainbox/impl/test_runner.py
@@ -3,6 +3,7 @@
 # Copyright 2013 Canonical Ltd.
 # Written by:
 #   Sylvain Pineau <sylvain.pineau@canonical.com>
+#   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
 #
 # Checkbox is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,6 +27,7 @@ Test definitions for plainbox.impl.runner module
 
 from unittest import TestCase
 
+from plainbox.impl.runner import CommandIOLogBuilder
 from plainbox.impl.runner import slugify
 
 
@@ -39,3 +41,22 @@ class SlugifyTests(TestCase):
         self.assertEqual(slugify("\z"), "_z")
         self.assertEqual(slugify("/z"), "_z")
         self.assertEqual(slugify("1k"), "1k")
+
+
+class CommandIOLogBuilderTests(TestCase):
+
+    def test_smoke(self):
+        builder = CommandIOLogBuilder()
+        # Calling on_begin() resets internal state
+        builder.on_begin(None, None)
+        self.assertEqual(builder.io_log, [])
+        # Calling on_line accumulates records
+        builder.on_line('stdout', b'text\n')
+        builder.on_line('stdout', b'different text\n')
+        builder.on_line('stderr', b'error message\n')
+        self.assertEqual(builder.io_log[0].stream_name, 'stdout')
+        self.assertEqual(builder.io_log[0].data, b'text\n')
+        self.assertEqual(builder.io_log[1].stream_name, 'stdout')
+        self.assertEqual(builder.io_log[1].data, b'different text\n')
+        self.assertEqual(builder.io_log[2].stream_name, 'stderr')
+        self.assertEqual(builder.io_log[2].data, b'error message\n')

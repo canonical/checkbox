2c8cee17404af0a5528809a7da5f8277098a31de
roadmr <roadmr@ubuntu.com>
2013-03-07 11:08:41 -0500

    Added checkbox-specific tests for resource program syntax.
    A test will be generated for each checkbox built-in job, to
    and ensure their resource programs make sense. This will catch,
    for instance, resource programs which refer to two resources in the
    same line, which is unsupported by plainbox.

---
+++ checkbox-ng/plainbox/impl/test_checkbox.py
@@ -0,0 +1,41 @@
+# This file is part of Checkbox.
+#
+# Copyright 2012 Canonical Ltd.
+# Written by:
+#   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+plainbox.impl.test_checkbox
+===========================
+
+Test definitions for plainbox.impl.checkbox module
+"""
+
+from plainbox.testing_utils.testcases import TestCaseWithParameters
+
+from plainbox.impl.checkbox import CheckBox
+
+
+class TestCheckBox(TestCaseWithParameters):
+    parameter_names = ('job',)
+
+    @classmethod
+    def get_parameter_values(cls):
+        for job in CheckBox().get_builtin_jobs():
+            yield (job,)
+
+    def test_job_resource_expression(self):
+        resource_program = self.parameters.job.get_resource_program()

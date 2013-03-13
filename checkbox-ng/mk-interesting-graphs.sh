b581ae9bd40ef421aaff88359b264f7798dc4cbb
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-03-13 13:09:47 +0100

    mk-interesting-graphs.sh: update to use new command line syntax
    There are two changes:
     - use -i 'pattern' instead of -r 'pattern'
     - use regexp patterns rather than glob patterns
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/mk-interesting-graphs.sh
+++ checkbox-ng/mk-interesting-graphs.sh
@@ -26,8 +26,8 @@ mkdir -p graphs
 # The grep / discards stuff that does not have category/name pattern
 # The rest just gets the category string
 for pattern in $(plainbox special --list-jobs | grep '/' | cut -d '/' -f 1 | sort | uniq); do
-    plainbox special -r $pattern'*' --dot | dot -Tsvg -o graphs/$pattern.svg
-    plainbox special -r $pattern'*' --dot --dot-resources | dot -Tsvg -o graphs/$pattern-with-resources.svg
+    plainbox special -i $pattern'.*' --dot | dot -Tsvg -o graphs/$pattern.svg
+    plainbox special -i $pattern'.*' --dot --dot-resources | dot -Tsvg -o graphs/$pattern-with-resources.svg
 done
 plainbox special --dot | dot -Tsvg -o graphs/everything-at-once.svg
 plainbox special --dot --dot-resources | dot -Tsvg -o graphs/everything-at-once-with-resources.svg

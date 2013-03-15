30cee3afad79c63dff3188766415146b227c02c7
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-03-15 16:46:20 +0100

    test-with-coverage.sh: use coverage3 instead of coverage
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/test-with-coverage.sh
+++ checkbox-ng/test-with-coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Requires activated virtualenv with coverage
-coverage run --branch $(which plainbox) self-test --unit-tests --verbose && \
-coverage run --append --branch $(which plainbox) self-test --integration-tests --verbose && \
-coverage report && \
-coverage html
+coverage3 run --branch $(which plainbox) self-test --unit-tests --verbose && \
+coverage3 run --append --branch $(which plainbox) self-test --integration-tests --verbose && \
+coverage3 report && \
+coverage3 html

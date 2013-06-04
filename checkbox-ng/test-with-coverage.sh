4751f3b3ad318fd1dfbf2102782453e9387f78c5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-06-04 16:16:26 +0200

    plainbox:test-with-coverage.sh: add support for --skip-integration
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/test-with-coverage.sh
+++ checkbox-ng/test-with-coverage.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Requires activated virtualenv with coverage
-coverage3 run --branch $(which plainbox) self-test --unit-tests --verbose && \
-coverage3 run --append --branch $(which plainbox) self-test --integration-tests --verbose && \
-coverage3 report && \
+coverage3 run --branch $(which plainbox) self-test --unit-tests --verbose
+if [ "$1" != "--skip-integration" ]; then
+    coverage3 run --append --branch $(which plainbox) self-test --integration-tests --verbose
+fi
+coverage3 report
 coverage3 html

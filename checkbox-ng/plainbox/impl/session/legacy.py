2977c97997673dff41f58c935dce83f8f03a7ee3
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-08-30 21:25:18 +0200

    plainbox:session:legacy: add adapter using SessionManager via legacy API
    This patch adds a new subclass of SessionState that implements all of
    the legacy methods necessary to create, suspend, resume and destroy
    sessions. The API is implemented using a mostly straightforward
    pass-through to the SessionManager APIs except for the "create a new
    session" vs "open an existing session" that uses delayed decision to
    properly map that onto the SessionManager calls.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/session/legacy.py
+++ checkbox-ng/plainbox/impl/session/legacy.py
@@ -35,6 +35,8 @@ from plainbox.impl.result import DiskJobResult
 from plainbox.impl.result import MemoryJobResult
 from plainbox.impl.session.jobs import JobState
 from plainbox.impl.session.state import SessionState
+from plainbox.impl.session.manager import SessionManager
+from plainbox.impl.session.storage import SessionStorageRepository
 
 logger = logging.getLogger("plainbox.session.legacy")
 
@@ -275,6 +277,177 @@ class SessionStateEncoder(json.JSONEncoder):
             return cls.from_json_record(data)
         else:
             return data
+
+
+class SessionStateLegacyAPICompatImpl(SessionState, ISessionStateLegacyAPI):
+    """
+    Compatibility wrapper to use new suspend/resume implementation via the
+    original (legacy) suspend/resume API.
+
+    This subclass of SessionState implements the ISessionStateLegacyAPI
+    interface thus allowing applications to keep using suspend/resume as they
+    did before, without adjusting their code.
+
+    :ivar _manager:
+        Instance of SessionManager (this is a bit insane because
+        the manager actually knows about the session too)
+
+    :ivar _commit_hint:
+        Either None or a set of flags (strings) that determine what kind of
+        actions should take place before the next time the 'manager' property
+        gets accessed. This is used to implement lazy decision on how to
+        map the open/resume/clean methods onto the SessionManager API
+    """
+
+    def __init__(self, job_list):
+        super(SessionStateLegacyAPICompatImpl, self).__init__(job_list)
+        self._manager = None
+        self._commit_hint = None
+
+    def open(self):
+        """
+        Open session state for running jobs.
+        """
+        logger.debug("SessionState.open()")
+        self._add_hint('open')
+        return self
+
+    def resume(self):
+        """
+        Erase the job_state_map and desired_job_list with the saved ones
+        """
+        logger.debug("SessionState.resume()")
+        self._add_hint('resume')
+        self._commit_manager()
+
+    def clean(self):
+        """
+        Clean the session directory.
+        """
+        logger.debug("SessionState.clean()")
+        self._add_hint('clean')
+        self._commit_manager()
+
+    def close(self):
+        """
+        Close the session.
+
+        Legacy API, this function does absolutely nothing
+        """
+        logger.debug("SessionState.close()")
+        self._manager = None
+        self._commit_hint = None
+
+    def _add_hint(self, hint):
+        if self._commit_hint is None:
+            self._commit_hint = set()
+        self._commit_hint.add(hint)
+
+    @property
+    def manager(self):
+        logger.debug(".manager accessed")
+        if self._commit_hint is not None:
+            self._commit_manager()
+        if self._manager is None:
+            raise AttributeError("Session not ready, did you call open()?")
+        return self._manager
+
+    def _commit_manager(self):
+        """
+        Commit the new value of the '_manager' instance attribute.
+
+        This method looks at '_commit_hint' to figure out if the semantics
+        of open(), resume() or clean() should be applied on the SessionManager
+        instance that this class is tracking.
+        """
+        logger.debug("_commit_manager(), _commit_hint: %r", self._commit_hint)
+        assert isinstance(self._commit_hint, set)
+        if 'open' in self._commit_hint:
+            if 'resume' in self._commit_hint:
+                self._commit_resume()
+            elif 'clean' in self._commit_hint:
+                self._commit_clean()
+            else:
+                self._commit_open()
+        self._commit_hint = None
+
+    def _commit_open(self):
+        logger.debug("_commit_open()")
+        self._manager = SessionManager.create_session(
+            self.job_list, legacy_mode=True)
+        # Compatibility hack. Since session manager is supposed to
+        # create and manage both session state and session storage
+        # we need to inject ourselves into its internal attribute.
+        # This way it will keep operating on this instance in the
+        # essential checkpoint() method.
+        self._manager._state = self
+
+    def _commit_clean(self):
+        logger.debug("_commit_clean()")
+        if self._manager:
+            self._manager.destroy()
+            self._manager.create_session(self.job_list)
+        self._manager = SessionManager.create_session(
+            self.job_list, legacy_mode=True)
+        self._manager._state = self
+
+    def _commit_resume(self):
+        logger.debug("_commit_resume()")
+        last_storage = SessionStorageRepository().get_last_storage()
+        assert last_storage is not None, "no saved session to resume"
+        self._manager = SessionManager.load_session(
+            self.job_list, last_storage)
+        # Copy over the resumed state to this instance
+        self._job_list = self._manager.state._job_list
+        self._job_state_map = self._manager.state._job_state_map
+        self._run_list = self._manager.state._run_list
+        self._desired_job_list = self._manager.state._desired_job_list
+        self._resource_map = self._manager.state._resource_map
+        self._metadata = self._manager.state._metadata
+        # Copy this instance over what the manager manages
+        self._manager._state = self
+
+    @property
+    def session_dir(self):
+        """
+        pathname of a temporary directory for this session
+
+        This is not None only between calls to open() / close().
+        """
+        return self.manager.storage.location
+
+    @property
+    def jobs_io_log_dir(self):
+        """
+        pathname of the jobs IO logs directory
+
+        This is not None only between calls to open() / close().
+        """
+        # TODO: use well-known dir helper
+        return os.path.join(self.manager.storage.location, 'io-logs')
+
+    def previous_session_file(self):
+        """
+        Check the filesystem for previous session data
+        Returns the full pathname to the session file if it exists
+        """
+        last_storage = SessionStorageRepository().get_last_storage()
+        if last_storage:
+            return last_storage.location
+        else:
+            return None
+
+    def persistent_save(self):
+        """
+        Save to disk the minimum needed to resume plainbox where it stopped
+        """
+        self.manager.checkpoint()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
 
 
 SessionStateLegacyAPI = SessionStateLegacyAPIOriginalImpl

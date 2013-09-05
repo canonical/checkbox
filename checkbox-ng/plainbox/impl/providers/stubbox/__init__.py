b14f9866ef30d47a1cd7723d5f580c70eca46379
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-09-06 00:24:31 +0200

    plainbox:providers:stubbox: fix the location of the stubbox provider
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/providers/stubbox/__init__.py
+++ checkbox-ng/plainbox/impl/providers/stubbox/__init__.py
@@ -39,5 +39,5 @@ class StubBoxProvider(Provider1):
 
     def __init__(self):
         super(StubBoxProvider, self).__init__(
-            os.path.join(get_plainbox_dir(), "stubbox"),
+            os.path.join(get_plainbox_dir(), "impl/providers/stubbox"),
             "stubbox", "StubBox (dummy data for development)")

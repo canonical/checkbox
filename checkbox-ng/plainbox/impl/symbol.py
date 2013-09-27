cbc72e793643695486f2e680b6f7ee716a4a701c
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-09-27 19:21:50 +0200

    plainbox:symbol: add the new symbol module
    The symbol module can be used to create unique-instance symbols that
    wrap a string. In addition it provides a convenient syntax to define
    many symbols at once (similar to the python3.4 Enum class)
    
    Symbols provide a slight improvement over upper-case constants with
    strings assigned to them as they are more DRY.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ checkbox-ng/plainbox/impl/symbol.py
@@ -0,0 +1,196 @@
+# This file is part of Checkbox.
+#
+# Copyright 2013 Canonical Ltd.
+# Written by:
+#   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+:mod:`plainbox.impl.symbol` -- Symbol Type
+==========================================
+
+Symbols are special values that evaluate back to themselves. They are global,
+unlike enumeration values, and are not bound to any container that defined
+them. Symbols can be easily converted to strings and back and are a useful way
+to store constants for use inside applications or libraries.
+
+Applications can use Symbol class directly or use the SymbolDef helper to
+quickly construct symbols without syntax overhead.
+"""
+
+__all__ = ['Symbol', 'SymbolDef']
+
+import inspect
+
+
+class Symbol:
+    """
+    Symbol type.
+
+    Instances of this class behave as self-interning strings. All instances are
+    tracked and at most one instance with a given symbol name can be
+    constructed. The name is immutable.
+    """
+
+    __symbols = {}
+
+    def __new__(cls, name):
+        """
+        Create a new symbol instance.
+
+        If the name was already used in another symbol then that object is
+        returned directly. If the name was not used before then construct a new
+        Symbol instance and return it.
+        """
+        try:
+            return cls.__symbols[name]
+        except KeyError:
+            symbol = object.__new__(cls)
+            cls.__symbols[name] = symbol
+            return symbol
+
+    def __init__(self, name):
+        """
+        Initialize a symbol with the given name
+        """
+        self._name = name
+
+    @property
+    def name(self):
+        """
+        name of the symbol
+        """
+        return self._name
+
+    def __str__(self):
+        """
+        Convert the symbol object to its name
+        """
+        return self._name
+
+    def __repr__(self):
+        """
+        Convert the symbol object to its representation in python
+        """
+        return "{}({!r})".format(self.__class__.__name__, self._name)
+
+    def __eq__(self, other):
+        """
+        Compare two symbols or a string and a symbol for equality
+        """
+        if isinstance(other, Symbol):
+            return self is other
+        elif isinstance(other, str):
+            return self._name == other
+        else:
+            return False
+
+    def __ne__(self, other):
+        """
+        Compare two symbols or a string and a symbol for inequality
+        """
+        if isinstance(other, Symbol):
+            return self is not other
+        elif isinstance(other, str):
+            return self._name != other
+        else:
+            return False
+
+    def __hash__(self):
+        """
+        Has the name of the symbol
+        """
+        return hash(self._name)
+
+
+class SymbolDefNs:
+    """
+    Internal implementation detail of the symbol module.
+
+    A special namespace used by :class:`SymbolDefMeta` to keep track of names
+    that were being accessed. Each accessed name is converted to a
+    :class:`Symbol` and added to the nanespace.
+    """
+
+    PASSTHRU = frozenset(('__name__', '__qualname__', '__doc__', '__module__'))
+
+    def __init__(self):
+        self.data = {}
+
+    def __setitem__(self, name, value):
+        if name in self.PASSTHRU:
+            self.data[name] = value
+        elif isinstance(value, Symbol):
+            self.data[name] = value
+        elif isinstance(value, str):
+            self.data[name] = Symbol(value)
+        else:
+            raise ValueError("Only Symbol() instances can be assigned here")
+
+    def __getitem__(self, name):
+        if name in self.PASSTHRU:
+            return self.data[name]
+        elif name in self.data:
+            return self.data[name]
+        elif name == 'Symbol':
+            return Symbol
+        else:
+            symbol = Symbol(name)
+            self.data[name] = symbol
+            return symbol
+
+
+class SymbolDefMeta(type):
+    """
+    Metaclass for :class:`SymbolDef` which helps to construct multiple symbol
+    objects easily. Uses :class:`SymbolDefNs` to keep track of all the symbol
+    definitions inside the class and convert them to a list of candidate
+    symbols to define.
+    """
+
+    @classmethod
+    def __prepare__(mcls, name, bases, **kwargs):
+        return SymbolDefNs()
+
+    def __new__(mcls, name, bases, ns):
+        classdict = ns.data
+        classdict['get_all_symbols'] = classmethod(mcls.get_all_symbols)
+        return type.__new__(mcls, name, bases, classdict)
+
+    # This is inserted via a simple trick because it's very hard to do any
+    # normal method definition inside SymbolDef blocks.
+    @staticmethod
+    def get_all_symbols(cls):
+        """
+        Get all symbols defined by this symbol definition block
+        """
+        # NOTE: This feels a bit like Enum and the extra property that it
+        # carries which holds all values. I don't know if we should have that
+        # as symbols are not 'bound' to any 'container' like enumeration values
+        # are.
+        return [value for name, kind, defcls, value
+                in inspect.classify_class_attrs(cls)
+                if kind == 'data' and isinstance(value, Symbol)]
+
+
+class SymbolDef(metaclass=SymbolDefMeta):
+    """
+    Helper class that allows to easily define symbols.
+
+    All sub-classes of SymbolDef are evaluated specially. Each word used inside
+    the class definition becomes a Symbol() instance. In addition explicit
+    assignment can create new symbols. This can be used to create symbols with
+    value different from their identifiers.
+    """
---
+++ checkbox-ng/plainbox/impl/test_symbol.py
@@ -0,0 +1,176 @@
+# This file is part of Checkbox.
+#
+# Copyright 2013 Canonical Ltd.
+# Written by:
+#   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+plainbox.impl.test_symbol
+=========================
+
+Test definitions for plainbox.impl.symbol module
+"""
+
+import unittest
+
+from plainbox.impl.symbol import SymbolDef, Symbol
+
+
+class SymbolTests(unittest.TestCase):
+    """
+    Tests for Symbol class
+    """
+
+    def test_symbol_str(self):
+        """
+        verify that str() produces the symbol name
+        """
+        self.assertEqual(str(Symbol('foo')), 'foo')
+
+    def test_symbol_repr(self):
+        """
+        verify that repr() produces the symbol object
+        """
+        self.assertEqual(repr(Symbol('foo')), "Symbol('foo')")
+
+    def test_symbol_hash(self):
+        """
+        verify that hash() hashes the symbol name
+        """
+        self.assertEqual(hash(Symbol('foo')), hash('foo'))
+
+    def test_symbol_name(self):
+        """
+        verify that Symbol.name returns the symbol name
+        """
+        self.assertEqual(Symbol('foo').name, 'foo')
+
+    def test_symbol_uniqueness(self):
+        """
+        verify that two symbols with the same name are indeed a single object
+        """
+        self.assertIs(Symbol('foo'), Symbol('foo'))
+
+    def test_different_symbols_are_not_same(self):
+        """
+        verify that two symbols with different names are not the same object
+        """
+        self.assertIsNot(Symbol('foo'), Symbol('bar'))
+
+    def test_symbol_symbol_comparison(self):
+        """
+        verify that comparing symbols to symbols works
+        """
+        self.assertEqual(Symbol('foo'), Symbol('foo'))
+        self.assertNotEqual(Symbol('foo'), Symbol('bar'))
+
+    def test_symbol_string_comparison(self):
+        """
+        verify that comparing symbols to strings works
+        """
+        self.assertEqual(Symbol('foo'), 'foo')
+        self.assertNotEqual(Symbol('foo'), 'bar')
+
+    def test_string_symbol_comparison(self):
+        """
+        verify that comparing strings to symbols works
+        """
+        self.assertEqual('foo', Symbol('foo'))
+        self.assertNotEqual('bar', Symbol('foo'))
+
+    def test_symbol_other_comparison(self):
+        """
+        verify that comparing symbols to other types (or vice
+        versa) is always False
+        """
+        self.assertFalse(
+            Symbol('foo') == 1,
+            "Symbol compared equal to integer")
+        self.assertFalse(
+            1 == Symbol('foo'),
+            "integer compared equal to Symbol")
+        self.assertFalse(
+            Symbol('foo') != 1,
+            "Symbol compared unequal to integer")
+        self.assertFalse(
+            1 != Symbol('foo'),
+            "integer compared unequal to Symbol")
+
+
+class SymbolDefTests(unittest.TestCase):
+    """
+    Tests for SymbolDef class
+    """
+
+    def test_implicit_symbols(self):
+        """
+        verify that referencing names inside SymbolDef creates symbols
+        """
+        class S(SymbolDef):
+            a
+            b
+            c
+        self.assertIs(S.a, Symbol('a'))
+        self.assertIs(S.b, Symbol('b'))
+        self.assertIs(S.c, Symbol('c'))
+
+    def test_custom_symbols(self):
+        """
+        verify that assigning symbols to variables works
+        """
+        class S(SymbolDef):
+            a = Symbol("the-a-symbol")
+        self.assertIs(S.a, Symbol('the-a-symbol'))
+
+    def test_custom_string_symbols(self):
+        """
+        verify that assigning strings to variables creates symbols
+        """
+        class S(SymbolDef):
+            a = "the-a-symbol"
+        self.assertIs(S.a, Symbol('the-a-symbol'))
+
+    def test_repeated_symbol(self):
+        """
+        verify that repeating a symbol doesn't break anything
+        """
+        class S(SymbolDef):
+            a
+            a
+        self.assertIs(S.a, Symbol('a'))
+        self.assertEqual(S.get_all_symbols(), [Symbol('a')])
+
+    def test_invalid_assignment(self):
+        """
+        verify that assigning other values is rejected
+        """
+        with self.assertRaises(ValueError) as boom:
+            class S(SymbolDef):
+                a = 1
+        self.assertEqual(
+            str(boom.exception),
+            "Only Symbol() instances can be assigned here")
+
+    def test_get_all_symbols(self):
+        """
+        verify that get_all_symbols() works as intended
+        """
+        class S(SymbolDef):
+            a
+            b
+            c
+        self.assertEqual(
+            S.get_all_symbols(), [Symbol('a'), Symbol('b'), Symbol('c')])

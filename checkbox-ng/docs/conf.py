dde9378672d7f8f6d715cf94b137266061084c59
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-10-03 10:08:27 +0200

    plainbox:docs: change copyright to 'Canonical Ltd'
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/docs/conf.py
+++ checkbox-ng/docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'PlainBox'
-copyright = '2013, Zygmunt Krynicki'
+copyright = '2012, 2013, Canonical Ltd'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the

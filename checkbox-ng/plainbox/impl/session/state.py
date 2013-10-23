acd010c9c0f74c8d403faa9f403c23c5ee574dc3
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-10-24 00:25:16 +0200

    plainbox:session:state: use session controller for processing job results
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/session/state.py
+++ checkbox-ng/plainbox/impl/session/state.py
@@ -481,17 +481,7 @@ class SessionState:
         local jobs don't replace anything. They cannot replace an existing job
         with the same name.
         """
-        assert job in self._job_list
-        # Store the result in job_state_map
-        self._job_state_map[job.name].result = result
-        self.on_job_state_map_changed()
-        self.on_job_result_changed(job, result)
-        # Treat some jobs specially and interpret their output
-        if job.plugin == "resource":
-            self._process_resource_result(job, result)
-        elif job.plugin == "local":
-            self._process_local_result(job, result)
-        # Update all job readiness state
+        job.controller.observe_result(self, job, result)
         self._recompute_job_readiness()
 
     def add_job(self, new_job, recompute=True):
@@ -554,86 +544,6 @@ class SessionState:
         Resources silently overwrite any old resources with the same name.
         """
         self._resource_map[resource_name] = resource_list
-
-    def _process_resource_result(self, job, result):
-        """
-        Analyze a result of a CheckBox "resource" job and generate
-        or replace resource records.
-        """
-        new_resource_list = []
-        for record in self._gen_rfc822_records_from_io_log(job, result):
-            # XXX: Consider forwarding the origin object here.  I guess we
-            # should have from_frc822_record as with JobDefinition
-            resource = Resource(record.data)
-            logger.info("Storing resource record %r: %s", job.name, resource)
-            new_resource_list.append(resource)
-        # Replace any old resources with the new resource list
-        self._resource_map[job.name] = new_resource_list
-
-    def _process_local_result(self, job, result):
-        """
-        Analyze a result of a CheckBox "local" job and generate
-        additional job definitions
-        """
-        # TODO: refactor using add_job() but make sure we compute
-        # job state map at most once
-
-        # First parse all records and create a list of new jobs (confusing
-        # name, not a new list of jobs)
-        new_job_list = []
-        for record in self._gen_rfc822_records_from_io_log(job, result):
-            new_job = job.create_child_job_from_record(record)
-            new_job_list.append(new_job)
-        # Then for each new job, add it to the job_list, unless it collides
-        # with another job with the same name.
-        for new_job in new_job_list:
-            try:
-                existing_job = self._job_state_map[new_job.name].job
-            except KeyError:
-                self._job_state_map[new_job.name] = JobState(new_job)
-                self._job_list.append(new_job)
-                self.on_job_state_map_changed()
-                self.on_job_added(new_job)
-            else:
-                # XXX: there should be a channel where such errors could be
-                # reported back to the UI layer. Perhaps update_job_result()
-                # could simply return a list of problems in a similar manner
-                # how update_desired_job_list() does.
-                if new_job != existing_job:
-                    logging.warning(
-                        ("Local job %s produced job %r that collides with"
-                         " an existing job %s (from %s), the new job was"
-                         " discarded"),
-                        job, new_job, existing_job, existing_job.origin)
-                else:
-                    # Patch the origin of the existing job so that it traces
-                    # back to the job that "generated" it again. This is
-                    # basically required to get __category__ jobs to associate
-                    # themselves with their children.
-                    existing_job._origin = new_job.origin
-
-    def _gen_rfc822_records_from_io_log(self, job, result):
-        """
-        Convert io_log from a job result to a sequence of rfc822 records
-        """
-        logger.debug("processing output from a job: %r", job)
-        # Select all stdout lines from the io log
-        line_gen = (record[2].decode('UTF-8', errors='replace')
-                    for record in result.get_io_log()
-                    if record[1] == 'stdout')
-        # Allow the generated records to be traced back to the job that defined
-        # the command which produced (printed) them.
-        source = JobOutputTextSource(job)
-        try:
-            # Parse rfc822 records from the subsequent lines
-            for record in gen_rfc822_records(line_gen, source=source):
-                yield record
-        except RFC822SyntaxError as exc:
-            # When this exception happens we will _still_ store all the
-            # preceding records. This is worth testing
-            logger.warning(
-                "local script %s returned invalid RFC822 data: %s",
-                job, exc)
 
     @property
     def job_list(self):

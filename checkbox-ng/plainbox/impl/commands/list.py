e2a33a096d8fabc8808662183dfe80cbfda97902
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-11-11 19:40:00 +0100

    plainbox:commands:list: add the list command
    The `plainbox dev list` command can be used to display a list of objects
    that the Explorer class knows about. It supports some simple filtering
    but the main idea is to explore internal data as a hierarchy of objects.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ checkbox-ng/plainbox/impl/commands/list.py
@@ -0,0 +1,74 @@
+# This file is part of Checkbox.
+#
+# Copyright 2013 Canonical Ltd.
+# Written by:
+#   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+:mod:`plainbox.impl.commands.list` -- list sub-command
+======================================================
+"""
+
+import logging
+
+from plainbox.impl.commands import PlainBoxCommand
+from plainbox.impl.highlevel import Explorer
+
+
+logger = logging.getLogger("plainbox.commands.list")
+
+
+class ListInvocation:
+
+    def __init__(self, provider_list, ns):
+        self.explorer = Explorer(provider_list)
+        self.group = ns.group
+
+    def run(self):
+        obj = self.explorer.get_object_tree()
+        self._show(obj)
+
+    def _show(self, obj, indent=None):
+        if indent is None:
+            indent = ""
+        # Apply optional filtering
+        if self.group is None or obj.group == self.group:
+            # Display the object name and group
+            print("{}{} {!r}".format(indent, obj.group, obj.name))
+            # It would be cool if this would draw an ASCI-art tree
+            indent += "  "
+        for child in obj.children:
+            self._show(child, indent)
+
+
+class ListCommand(PlainBoxCommand):
+    """
+    Implementation of ``$ plainbox dev list <object>``
+    """
+
+    def __init__(self, provider_list):
+        self.provider_list = provider_list
+
+    def invoked(self, ns):
+        self.autopager()
+        return ListInvocation(self.provider_list, ns).run()
+
+    def register_parser(self, subparsers):
+        parser = subparsers.add_parser(
+            "list", help="list and describe various objects")
+        parser.add_argument(
+            'group', nargs='?', help="list objects from the specified group")
+        parser.set_defaults(command=self)

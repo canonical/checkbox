5c7db092cbe34aa9cdc4fe2b75cdbca48d04532d
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-11-22 23:54:53 +0100

    plainbox: bump version to 0.4b1
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -44,7 +44,7 @@ else:
 
 setup(
     name="plainbox",
-    version="0.4.dev",
+    version="0.4b1",
     url="https://launchpad.net/checkbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

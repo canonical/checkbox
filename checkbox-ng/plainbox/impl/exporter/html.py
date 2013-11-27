d59518789d927ecdf3ce17bcaa49eb689dc7072e
Daniel Manrique <roadmr@ubuntu.com>
2013-11-27 15:40:19 -0500

    plainbox: Changed header for all files mentioning GPLv3 or later to just GPLv3

--- checkbox-ng/plainbox/impl/exporter/html.py
+++ checkbox-ng/plainbox/impl/exporter/html.py
@@ -7,9 +7,9 @@
 #   Daniel Manrique <daniel.manrique@canonical.com>
 #
 # Checkbox is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+
 #
 # Checkbox is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
--- checkbox-ng/plainbox/impl/exporter/test_init.py
+++ checkbox-ng/plainbox/impl/exporter/test_init.py
@@ -6,9 +6,9 @@
 #   Daniel Manrique  <roadmr@ubuntu.com>
 #
 # Checkbox is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+
 #
 # Checkbox is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of

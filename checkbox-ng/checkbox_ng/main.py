3d54ff5c36d8df0ac3fc2256f8040edb152d5d7a
Sylvain Pineau <sylvain.pineau@canonical.com>
2013-12-03 15:51:28 +0100

    checkbox_ng:main: Define the CLI commands settings
    Move common parameters to dedicated mappings providing the following keys:
    
    - subparser_name and subparser_help: name/help of checkbox-ng subcommands
    - default_whitelist: Only used when run non-interactively
    - default_providers: list of provider to consider for suite selection
    - welcome_text: text used by the splash screen

--- checkbox-ng/checkbox_ng/main.py
+++ checkbox-ng/checkbox_ng/main.py
@@ -42,6 +42,58 @@ from checkbox_ng.config import CertificationConfig, CheckBoxConfig, CDTSConfig
 
 logger = logging.getLogger("checkbox.ng.main")
 
+checkbox_cli_settings = {
+    'subparser_name': 'checkbox-cli',
+    'subparser_help': 'application for system testing',
+    'default_whitelist': 'default',
+    'default_providers': ['2013.com.canonical:checkbox'],
+    'welcome_text': """\
+Welcome to System Testing!
+Checkbox provides tests to confirm that your system is working properly. \
+Once you are finished running the tests, you can view a summary report for \
+your system.
+Warning: Some tests could cause your system to freeze or become \
+unresponsive. Please save all your work and close all other running \
+applications before beginning the testing process."""
+}
+
+cdts_cli_settings = {
+    'subparser_name': 'driver-test-suite-cli',
+    'subparser_help': 'driver test suite application',
+    'default_whitelist': 'ihv-firmware',
+    'default_providers': ['2013.com.canonical:canonical-driver-test-suite'],
+    'welcome_text': """\
+Welcome to the Canonical Driver Test Suite.
+This program contains automated and manual tests to help you discover issues \
+that will arise when running your device drivers on Ubuntu.
+This application will step the user through these tests in a predetermined \
+order and automatically collect both system information as well as test \
+results. It will also prompt the user for input when manual testing is \
+required.
+The run time for the tests is determined by which tests you decide to \
+execute. The user will have the opportunity to customize the test run to \
+accommodate the driver and the amount of time available for testing.
+If you have any questions during or after completing your test run, please \
+do not hesitate to contact your Canonical account representative.
+To begin, simply press the Continue button below and follow the onscreen \
+instructions."""
+}
+
+cert_cli_settings = {
+    'subparser_name': 'certification-server',
+    'subparser_help': 'application for server certification',
+    'default_whitelist': 'server-cert',
+    'default_providers': ['2013.com.canonical:certification-server'],
+    'welcome_text': """\
+Welcome to System Certification!
+This application will gather information from your system. Then you will be \
+asked manual tests to confirm that the system is working properly. Finally, \
+you will be asked for the Secure ID of the computer to submit the \
+information to the certification.canonical.com database.
+To learn how to create or locate the Secure ID, please see here:
+https://certification.canonical.com/"""
+}
+
 
 class CheckBoxNGTool(PlainBoxToolBase):
 

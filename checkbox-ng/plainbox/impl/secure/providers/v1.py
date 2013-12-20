d5a9e23604eecbe5e76843405e8e8fcd2c875779
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2013-12-20 19:02:34 +0100

    plainbox:secure:providers: update docstrings
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/secure/providers/v1.py
+++ checkbox-ng/plainbox/impl/secure/providers/v1.py
@@ -237,6 +237,13 @@ class Provider1(IProvider1, IProviderBackend1):
     def get_all_executables(self):
         """
         Discover and return all executables offered by this provider
+
+        :returns:
+            list of executable names (without the full path)
+        :raises IOError, OSError:
+            if there were any problems accessing files or directories. Note
+            that OSError is silently ignored when the `bin_dir` directory is
+            missing.
         """
         executable_list = []
         try:

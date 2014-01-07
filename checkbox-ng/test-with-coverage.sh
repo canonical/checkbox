6bbedfef1364d9908d4aa2e424b2c9bab3d68a5b
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-01-07 19:04:33 +0100

    plainbox: adapt test-in-coverage to python3.4
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/test-with-coverage.sh
+++ checkbox-ng/test-with-coverage.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
+# On recent enough Debian/Ubuntu you can run this with python3-coverage (or
+# python3.4-coverage) to run coverage with standard packages, without
+# virtualenv. Everywhere else the default 'coverage3' should work fine.
+#
 # Because python3-coverage is still broken
 rm -rf htmlcov
+coverage=${1:-coverage3}
 # Requires activated virtualenv with coverage
-coverage3 run --branch $(which plainbox) self-test --unit-tests --fail-fast --quiet
+$coverage run --branch $(which plainbox) self-test --unit-tests --fail-fast --quiet
 if [ "$1" != "--skip-integration" ]; then
-    coverage3 run --append --branch $(which plainbox) self-test --integration-tests --fail-fast --quiet
+    $coverage run --append --branch $(which plainbox) self-test --integration-tests --fail-fast --quiet
 fi
 # coverage3 report
-coverage3 html
+$coverage html

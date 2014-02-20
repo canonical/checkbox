dee091c300826ac37b0339d22d337d9870ff8784
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-02-20 22:43:55 +0100

    plainbox:session:manager: sprinkle gettext, ngettetx and translator hints
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/session/manager.py
+++ checkbox-ng/plainbox/impl/session/manager.py
@@ -34,7 +34,7 @@ import errno
 import logging
 import os
 
-from plainbox.i18n import gettext as _
+from plainbox.i18n import gettext as _, ngettext
 from plainbox.impl.session.resume import SessionResumeHelper
 from plainbox.impl.session.state import SessionState
 from plainbox.impl.session.storage import LockedStorageError
@@ -111,6 +111,8 @@ class SessionManager:
         self._state = state
         self._storage = storage
         logger.debug(
+            # TRANSLATORS: please don't translate 'SessionManager' 'state' and
+            # 'storage'
             _("Created SessionManager with state:%r and storage:%r"),
             state, storage)
 
@@ -160,7 +162,7 @@ class SessionManager:
         :return:
             fresh :class:`SessionManager` instance
         """
-        logger.debug(_("SessionManager.create_session()"))
+        logger.debug("SessionManager.create_session()")
         if job_list is None:
             job_list = []
         state = SessionState(job_list)
@@ -209,7 +211,7 @@ class SessionManager:
         :returns:
             Fresh instance of :class:`SessionManager`
         """
-        logger.debug(_("SessionManager.load_session()"))
+        logger.debug("SessionManager.load_session()")
         try:
             data = storage.load_checkpoint()
         except IOError as exc:
@@ -228,11 +230,13 @@ class SessionManager:
         After calling this method you can later reopen the same session with
         :meth:`SessionManager.open_session()`.
         """
-        logger.debug(_("SessionManager.checkpoint()"))
+        logger.debug("SessionManager.checkpoint()")
         data = SessionSuspendHelper().suspend(self.state)
         logger.debug(
-            _("Saving %d bytes of checkpoint data to %r"),
-            len(data), self.storage.location)
+            ngettext(
+                "Saving %d byte of checkpoint data to %r",
+                "Saving %d bytes of checkpoint data to %r", len(data)
+            ), len(data), self.storage.location)
         try:
             self.storage.save_checkpoint(data)
         except LockedStorageError:
@@ -246,5 +250,5 @@ class SessionManager:
         This basically calls
         :meth:`~plainbox.impl.session.storage.SessionStorage.remove()`
         """
-        logger.debug(_("SessionManager.destroy()"))
+        logger.debug("SessionManager.destroy()")
         self.storage.remove()

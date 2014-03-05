3dd8ffd92899ecf4f353b06bdc7bc386e21ec804
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-03-05 16:38:33 +0100

    plainbox:ctrl: make PLAINBOX_PROVIDER_DATA optional
    This patch makes the special PLAINBOX_PROVIDER_DATA environment variable
    optional. It will no longer be injected into job execution environment
    if the provider does not define it.
    
    In addition, it is no longer defined as CHECKBOX_SHARE + '/data'.
    Instead it is a separate variable that may or may not exist.
    PLAINBOX_PROVIDER_DATA is now being implemented with the
    IProviderBackend1.data_dir property.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/ctrl.py
+++ checkbox-ng/plainbox/impl/ctrl.py
@@ -524,8 +524,8 @@ class CheckBoxExecutionController(IExecutionController):
         # Add CHECKBOX_DATA (temporary checkbox data)
         env['CHECKBOX_DATA'] = self.CHECKBOX_DATA
         # Add plainbox equivalents of the two above
-        env['PLAINBOX_PROVIDER_DATA'] = os.path.join(
-            job.provider.CHECKBOX_SHARE, 'data')
+        if job.provider.data_dir is not None:
+            env['PLAINBOX_PROVIDER_DATA'] = job.provider.data_dir
         env['PLAINBOX_SESSION_SHARE'] = self.CHECKBOX_DATA
         # Inject additional variables that are requested in the config
         if config is not None and config.environment is not Unset:

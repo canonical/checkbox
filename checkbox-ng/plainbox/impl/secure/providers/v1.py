7e9ca631c8bba5ae481b6f7c460835281aefbcd4
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-03-05 12:53:32 +0100

    plainbox:secure:providers: fix Provider1.form_definition()
    The Provider1.from_definition() method was broken and would ignore
    directories explicitly specified in the definition object *if* the
    location field was specified. That was never the intention.
    
    Given a provider definition:
    
      [PlainBox Provider]
      location = /some/place
      bin_dir = /extra/special/place
    
    With the old code, bin_dir would be always None since the code never had
    any else paths to go through. The new code is significantly easier to
    understand and arguable more correct since it builds upon the added set
    of properties like definition.effective_$thing which encapsulate the
    logic of selecting the right value.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/secure/providers/v1.py
+++ checkbox-ng/plainbox/impl/secure/providers/v1.py
@@ -232,61 +232,14 @@ class Provider1(IProvider1, IProviderBackend1):
         providers, is *locale_dir*, by default it would be ``location/locale``
         but ``manage.py i18n`` creates ``location/build/mo``
         """
-        # Compute all the directories, depending on the value of
-        # definition.location and presence, or lack of thereof, of each
-        # directory definition entry.
-        jobs_dir = None
-        whitelists_dir = None
-        data_dir = None
-        bin_dir = None
-        locale_dir = None
-        if definition.location is not Unset:
-            # When location is not Unset, all the relevant directories _may_ be
-            # subdirectories of location (unless overridden by explicit value).
-            # Since all of the directories are validated we take special care
-            # not to assign directories that may not exist.
-            if definition.jobs_dir is Unset:
-                jobs_dir = os.path.join(definition.location, "jobs")
-                if not os.path.isdir(jobs_dir):
-                    jobs_dir = None
-            if definition.whitelists_dir is Unset:
-                whitelists_dir = os.path.join(definition.location,
-                                              "whitelists")
-                if not os.path.isdir(whitelists_dir):
-                    whitelists_dir = None
-            if definition.data_dir is Unset:
-                data_dir = os.path.join(definition.location, "data")
-                if not os.path.isdir(data_dir):
-                    data_dir = None
-            if definition.bin_dir is Unset:
-                bin_dir = os.path.join(definition.location, "bin")
-                if not os.path.isdir(bin_dir):
-                    bin_dir = None
-            if definition.locale_dir is Unset:
-                locale_dir = os.path.join(definition.location, "locale")
-                if not os.path.isdir(locale_dir):
-                    locale_dir = None
-        else:
-            if definition.jobs_dir is not Unset:
-                jobs_dir = definition.jobs_dir
-            if definition.whitelists_dir is not Unset:
-                whitelists_dir = definition.whitelists_dir
-            if definition.data_dir is not Unset:
-                data_dir = definition.data_dir
-            if definition.bin_dir is not Unset:
-                bin_dir = definition.bin_dir
-            if definition.locale_dir is not Unset:
-                locale_dir = definition.locale_dir
-        # Get gettext domain
-        if definition.gettext_domain is not Unset:
-            gettext_domain = definition.gettext_domain
-        else:
-            gettext_domain = None
+        logger.debug("Loading provider from definition %r", definition)
         # Initialize the provider object
         return cls(
             definition.name, definition.version, definition.description,
-            secure, gettext_domain, jobs_dir, whitelists_dir, data_dir,
-            bin_dir, locale_dir)
+            secure, definition.effective_gettext_domain,
+            definition.effective_jobs_dir, definition.effective_whitelists_dir,
+            definition.effective_data_dir, definition.effective_bin_dir,
+            definition.effective_locale_dir)
 
     def __repr__(self):
         return "<{} name:{!r}>".format(self.__class__.__name__, self.name)

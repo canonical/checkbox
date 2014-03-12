b4d791f961cb4f94766e01768a29d91ea284dc70
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-03-12 21:04:25 +0100

    plainbox:commands:startprovider: escape some markdown syntax
    The generated README.md should escape underscores as those have special
    meaning.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/commands/startprovider.py
+++ checkbox-ng/plainbox/impl/commands/startprovider.py
@@ -412,7 +412,7 @@ class ProviderSkeleton(Skeleton):
              ============================================
 
              You can refer to files from this directory, in your scripts, using
-             the $PLAINBOX_PROVIDER_DATA environment variable. See the job
+             the $PLAINBOX\\_PROVIDER\\_DATA environment variable. See the job
              examples/data-access for details.
 
              You should delete this file as anything here is automatically

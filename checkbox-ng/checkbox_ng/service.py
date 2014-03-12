719800699c1223a5e0c400575963e50132488037
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-03-12 10:52:00 +0100

    checkbox-ng:service: replace job.name with job.id
    This is mostly automatic except for the JobDefinition.name property
    that still is called name for backwards compatibility reasons. It does
    return self.native.id though.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/checkbox_ng/service.py
+++ checkbox-ng/checkbox_ng/service.py
@@ -283,7 +283,9 @@ class JobDefinitionWrapper(PlainBoxObjectWrapper):
 
     @dbus.service.property(dbus_interface=JOB_IFACE, signature="s")
     def name(self):
-        return self.native.name
+        # XXX: name should be removed but for now it should just return the
+        # full id instead of the old name (name may be very well gone)
+        return self.native.id
 
     @dbus.service.property(dbus_interface=JOB_IFACE, signature="s")
     def description(self):
@@ -660,7 +662,7 @@ class JobStateWrapper(PlainBoxObjectWrapper):
         return dbus.types.Array([
             (inhibitor.cause,
              inhibitor.cause_name,
-             (inhibitor.related_job.name
+             (inhibitor.related_job.id
               if inhibitor.related_job is not None else ""),
              (inhibitor.related_expression.text
               if inhibitor.related_expression is not None else ""))
@@ -847,7 +849,7 @@ class SessionWrapper(PlainBoxObjectWrapper):
         """
         logger.debug("_job_added(%r)", job)
         # Get references to the three key objects, job, state and result
-        state = self.native.job_state_map[job.name]
+        state = self.native.job_state_map[job.id]
         result = state.result
         assert job is state.job
         # Wrap them in the right order (state has to be last)
@@ -855,7 +857,7 @@ class SessionWrapper(PlainBoxObjectWrapper):
         self.add_result(result)
         state_wrapper = self.add_state(state)
         # Update the job_state_map wrapper that we have here
-        self._job_state_map_wrapper[job.name] = state_wrapper
+        self._job_state_map_wrapper[job.id] = state_wrapper
         # Send the signal that the 'job_state_map' property has changed
         self.PropertiesChanged(SESSION_IFACE, {
             self.__class__.job_state_map._dbus_property:
@@ -874,7 +876,7 @@ class SessionWrapper(PlainBoxObjectWrapper):
         """
         logger.debug("_job_removed(%r)", job)
         # Get references to the three key objects, job, state and result
-        state_wrapper = self._job_state_map_wrapper[job.name]
+        state_wrapper = self._job_state_map_wrapper[job.id]
         result_wrapper = state_wrapper._result_wrapper
         job_wrapper = state_wrapper._job_wrapper
         # Remove result and state from our managed object list
@@ -890,7 +892,7 @@ class SessionWrapper(PlainBoxObjectWrapper):
         if job_wrapper._is_generated:
             job_wrapper.remove_from_connection()
         # Update the job_state_map wrapper that we have here
-        del self._job_state_map_wrapper[job.name]
+        del self._job_state_map_wrapper[job.id]
         # Send the signal that the 'job_state_map' property has changed
         self.PropertiesChanged(SESSION_IFACE, {
             self.__class__.job_state_map._dbus_property:

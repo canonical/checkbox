6dac977574a7edf38139d287f2588701e0198a68
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-04-07 18:24:47 +0200

    providers:checkbox: add manage.py script
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

---
+++ providers/base/manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from plainbox.provider_manager import setup
+from plainbox.provider_manager import N_
+
+setup(
+    name='2013.com.canonical.certification:checkbox',
+    version="0.4",
+    description=N_("Checkbox provider"),
+    gettext_domain='2013.com.canonical.certification.checkbox',
+)

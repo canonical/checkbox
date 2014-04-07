875428e430c92c235f98a0298e440ad7cceb2e99
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-04-07 10:41:49 +0200

    plainbox: increment version to 0.6.dev

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -50,7 +50,7 @@ else:
 
 setup(
     name="plainbox",
-    version="0.5.3",
+    version="0.6.dev",
     url="https://launchpad.net/checkbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

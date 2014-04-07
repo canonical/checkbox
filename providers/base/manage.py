842134e7a0bd85d3c7b48749c43bb72fadc44d19
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-04-07 22:12:41 +0200

    providers:checkbox: bump version to 0.5
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- providers/base/manage.py
+++ providers/base/manage.py
@@ -4,7 +4,7 @@ from plainbox.provider_manager import N_
 
 setup(
     name='2013.com.canonical.certification:checkbox',
-    version="0.4",
+    version="0.5",
     description=N_("Checkbox provider"),
     gettext_domain='2013.com.canonical.certification.checkbox',
 )

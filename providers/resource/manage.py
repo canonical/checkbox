a29f5980e20148c17416b49a3b2d9ee627cd3391
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-04-07 13:07:14 +0200

    providers:resource: handle sdist specially
    This patch overrides the `manage.py sdist` command to run autoreconf -i in the
    src/ directory. We need to ship a number of files but they are automatically
    generated and we don't want them to be a part of the version control system so
    they are generated by the updated sdist command.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- providers/resource/manage.py
+++ providers/resource/manage.py
@@ -1,18 +1,46 @@
 #!/usr/bin/env python3
-from plainbox.provider_manager import setup
+import os
+import shutil
+import subprocess
+
 from plainbox.provider_manager import N_
+from plainbox.provider_manager import SourceDistributionCommand
+from plainbox.provider_manager import manage_py_extension
+from plainbox.provider_manager import setup
 
-# You can inject other stuff here but please don't go overboard.
-#
-# In particular, if you need comprehensive compilation support to get
-# your bin/ populated then please try to discuss that with us in the
-# upstream project IRC channel #checkbox on irc.freenode.net.
 
-# NOTE: one thing that you could do here, that makes a lot of sense,
-# is to compute version somehow. This may vary depending on the
-# context of your provider. Future version of PlainBox will offer git,
-# bzr and mercurial integration using the versiontools library
-# (optional)
+@manage_py_extension
+class SourceDistributionCommandExt(SourceDistributionCommand):
+    # Overridden version of SourceDistributionCommand that handles autotools
+    __doc__ = SourceDistributionCommand.__doc__
+    _INCLUDED_ITEMS = SourceDistributionCommand._INCLUDED_ITEMS + ['COPYING']
+    _GENERATED_ITEMS = [
+        'INSTALL',
+        'Makefile.in',
+        'aclocal.m4',
+        'compile',
+        'config.h.in',
+        'configure',
+        'depcomp',
+        'install-sh',
+        'missing',
+    ]
+
+    @property
+    def src_dir(self):
+        return os.path.join(self.definition.location, "src")
+
+    def invoked(self, ns):
+        # Update the configure script
+        subprocess.check_call(['autoreconf', '-i'], cwd=self.src_dir)
+        # Remove autom4te.cache, we don't need it in our source tarballs
+        # http://www.gnu.org/software/autoconf/manual/autoconf-2.64/html_node/Autom4te-Cache.html
+        shutil.rmtree(os.path.join(self.src_dir, 'autom4te.cache'))
+        # Generate the source tarball
+        super().invoked(ns)
+        # Remove generated autotools stuff
+        for item in self._GENERATED_ITEMS:
+            os.remove(os.path.join(self.src_dir, item))
 
 setup(
     name='2013.com.canonical.certification:plainbox-resources',

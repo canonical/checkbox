27935fc68296619c7e24885da62a391ec13a36d0
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-05-20 20:41:20 +0200

    checkbox-support:setup: depend on configparser on python2.7
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-support/setup.py
+++ checkbox-support/setup.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with CloudBox.  If not, see <http://www.gnu.org/licenses/>.
 
+import sys
 from io import open
 
 from setuptools import setup, find_packages
@@ -39,7 +40,7 @@ setup(
     install_requires=[
         'lxml >= 2.3',
         'pyparsing >= 2.0.0',
-    ],
+    ] + (['configparser'] if sys.version_info.major == 2 else []),
     entry_points={
         'plainbox.parsers': [
             "pactl-list=checkbox_support.parsers.pactl:parse_pactl_output",

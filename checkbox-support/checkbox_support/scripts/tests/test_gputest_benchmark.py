9cb994cf0c8c9d939673c20846d68585129489ae
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-05-20 20:54:34 +0200

    checkbox-support:scripts:gputest_benchmark: use io for open() (tests)
    The version of open() from the io module is more akin the python3.x
    counterpart and supports the encoding= argument
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-support/checkbox_support/scripts/tests/test_gputest_benchmark.py
+++ checkbox-support/checkbox_support/scripts/tests/test_gputest_benchmark.py
@@ -21,6 +21,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from io import open
 from tempfile import NamedTemporaryFile
 import os
 import unittest

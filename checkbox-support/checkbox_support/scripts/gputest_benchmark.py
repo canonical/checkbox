4db42d37b489ff84740757cc1107c3db22170607
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-05-20 20:54:24 +0200

    checkbox-support:scripts:gputest_benchmark: add future imports
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-support/checkbox_support/scripts/gputest_benchmark.py
+++ checkbox-support/checkbox_support/scripts/gputest_benchmark.py
@@ -17,6 +17,10 @@
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 from subprocess import check_output, CalledProcessError
 from tempfile import TemporaryDirectory
 from zipfile import ZipFile

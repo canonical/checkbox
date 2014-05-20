54c42f7ddff5e3c9947b0f9accc89a17cb6af6b3
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-05-20 20:54:21 +0200

    checkbox-support:parsers:efi: add future imports (tests)
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-support/checkbox_support/parsers/tests/test_efi.py
+++ checkbox-support/checkbox_support/parsers/tests/test_efi.py
@@ -15,9 +15,13 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
-#
-from io import StringIO
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from io import StringIO
 from unittest import TestCase
 
 from checkbox_support.parsers.efi import EfiParser

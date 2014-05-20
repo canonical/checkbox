ae72e8808cb7a97294ab9dc702bd9f3ef6cb244f
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-05-20 20:54:24 +0200

    checkbox-support:parsers:submission: add future imports (tests)
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-support/checkbox_support/parsers/tests/test_submission.py
+++ checkbox-support/checkbox_support/parsers/tests/test_submission.py
@@ -16,10 +16,14 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
-#
-import os
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 from unittest import TestCase
+import os
 
 from checkbox_support.parsers.submission import SubmissionParser
 

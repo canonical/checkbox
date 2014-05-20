be136e6e5324005836cdb9ed69234ef87cefeebc
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-05-20 20:54:19 +0200

    checkbox-support:lib:input: fix PEP-8 issue
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-support/checkbox_support/lib/input.py
+++ checkbox-support/checkbox_support/lib/input.py
@@ -15,7 +15,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
-#
+
 
 # See linux/input.h
 class Input:

c683082fa08a1adbdf6a0b7408f9464b0642d54b
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-05-20 20:54:18 +0200

    checkbox-support:contrib:xrandr: add future imports
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-support/checkbox_support/contrib/xrandr.py
+++ checkbox-support/checkbox_support/contrib/xrandr.py
@@ -35,6 +35,11 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA  02110-1301  USA
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 from ctypes import (
     POINTER,
     Structure,

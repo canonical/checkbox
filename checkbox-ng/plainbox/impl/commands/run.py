92ed0c58605480e56366dc3d090e98bcf50a9e48
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-05-30 03:01:01 +0200

    plainbox:commands:run: add Colorizer
    This patch adds the Colorizer, a small helper utility to make working
    with colors less annoying.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/commands/run.py
+++ checkbox-ng/plainbox/impl/commands/run.py
@@ -58,6 +58,69 @@ from plainbox.impl.transport import get_all_transports
 logger = getLogger("plainbox.commands.run")
 
 
+class Colorizer:
+    """
+    Colorizing helper for various kinds of content we need to handle
+    """
+
+    # NOTE: Ideally result and all would be handled by multi-dispatch __call__
+
+    def __init__(self, color):
+        self.c = color
+
+    def result(self, result):
+        outcome_color = {
+            IJobResult.OUTCOME_PASS: "GREEN",
+            IJobResult.OUTCOME_FAIL: "RED",
+            IJobResult.OUTCOME_SKIP: "YELLOW",
+            IJobResult.OUTCOME_UNDECIDED: "MAGENTA",
+            IJobResult.OUTCOME_NOT_SUPPORTED: "YELLOW",
+        }.get(result.outcome, "RESET")
+        return self(result.tr_outcome(), outcome_color)
+
+    def header(self, text, color_name='WHITE', bright=True, fill='='):
+        return self("[ {} ]".format(text).center(80, fill), color_name, bright)
+
+    def f(self, color_name):
+        return getattr(self.c.f, color_name.upper())
+
+    def b(self, color_name):
+        return getattr(self.c.b, color_name.upper())
+
+    def s(self, style_name):
+        return getattr(self.c.s, style_name.upper())
+
+    def __call__(self, text, color_name="WHITE", bright=True):
+        return ''.join([
+            self.f(color_name),
+            self.c.s.BRIGHT if bright else '', str(text),
+            self.c.s.RESET_ALL])
+
+    def BLACK(self, text, bright=True):
+        return self(text, "BLACK", bright)
+
+    def RED(self, text, bright=True):
+        return self(text, "RED", bright)
+
+    def GREEN(self, text, bright=True):
+        return self(text, "GREEN", bright)
+
+    def YELLOW(self, text, bright=True):
+        return self(text, "YELLOW", bright)
+
+    def BLUE(self, text, bright=True):
+        return self(text, "BLUE", bright)
+
+    def MAGENTA(self, text, bright=True):
+        return self(text, "MAGENTA", bright)
+
+    def CYAN(self, text, bright=True):
+        return self(text, "CYAN", bright)
+
+    def WHITE(self, text, bright=True):
+        return self(text, "WHITE", bright)
+
+
 class RunInvocation(CheckBoxInvocationMixIn):
     """
     Invocation of the 'plainbox run' command.

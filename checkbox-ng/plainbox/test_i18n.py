2ea818c0aa448f1bc13071f34bc4514f2f2bf721
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-06-02 17:04:48 +0200

    plainbox:i18n: add tests for NoOpTranslator
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/test_i18n.py
+++ checkbox-ng/plainbox/test_i18n.py
@@ -26,6 +26,7 @@ Test definitions for plainbox.i18n module
 from unittest import TestCase
 
 from plainbox.i18n import gettext_noop
+from plainbox.i18n import NoOpTranslator
 from plainbox.i18n import docstring
 
 
@@ -45,3 +46,50 @@ class FunctionTests(TestCase):
         def foo():
             pass
         self.assertEqual(foo.__doc__, "text")
+
+
+class NoOpTranslatorTests(TestCase):
+
+    DOMAIN = 'domain'
+
+    def setUp(self):
+        self.t = NoOpTranslator()
+
+    def test_gettext(self):
+        self.assertEqual(self.t.gettext("msgid"), "msgid")
+
+    def test_ngettext(self):
+        self.assertEqual(self.t.ngettext("msgid1", "msgid2", 1), "msgid1")
+        self.assertEqual(self.t.ngettext("msgid1", "msgid2", 2), "msgid2")
+
+    def test_dgettext(self):
+        self.assertEqual(self.t.dgettext("other-domain", "msgid"), "msgid")
+
+    def test_dngettext(self):
+        self.assertEqual(
+            self.t.dngettext("other-domain", "msgid1", "msgid2", 1), "msgid1")
+        self.assertEqual(
+            self.t.dngettext("other-domain", "msgid1", "msgid2", 2), "msgid2")
+
+    def test_pgettext(self):
+        self.assertEqual(self.t.pgettext("msgctxt", "msgid"), "msgid")
+
+    def test_pdgettext(self):
+        self.assertEqual(
+            self.t.pdgettext("msgctxt", "other-domain", "msgid"), "msgid")
+
+    def test_pngettext(self):
+        self.assertEqual(
+            self.t.pngettext("msgctxt", "msgid1", "msgid2", 1), "msgid1")
+        self.assertEqual(
+            self.t.pngettext("msgctxt", "msgid1", "msgid2", 2), "msgid2")
+
+    def test_pdngettext(self):
+        self.assertEqual(
+            self.t.pdngettext(
+                "msgctxt", "other-domain", "msgid1", "msgid2", 1),
+            "msgid1")
+        self.assertEqual(
+            self.t.pdngettext(
+                "msgctxt", "other-domain", "msgid1", "msgid2", 2),
+            "msgid2")

f610fff966517f0f4a4c0365529f238aabc77b87
Sylvain Pineau <sylvain.pineau@canonical.com>
2014-06-16 23:34:43 +0200

    plainbox: increment version to 0.6

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -50,7 +50,7 @@ else:
 
 setup(
     name="plainbox",
-    version="0.6c1",
+    version="0.6",
     url="https://launchpad.net/plainbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

3abae54ea1a68b1c376111f3fd5b1263026f75ee
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-07-22 14:43:05 +0200

    plainbox:buildsystems: add copyright/docstring header
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/buildsystems.py
+++ checkbox-ng/plainbox/impl/buildsystems.py
@@ -1,3 +1,26 @@
+# This file is part of Checkbox.
+#
+# Copyright 2014 Canonical Ltd.
+# Written by:
+#   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+:mod:`plainbox.impl.buildsystems` -- build system interfaces
+============================================================
+"""
+
 import glob
 import shlex
 import os

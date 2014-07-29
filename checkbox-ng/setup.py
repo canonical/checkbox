27867b36a249123afedb272289ccac3ab1879bd3
Sylvain Pineau <sylvain.pineau@canonical.com>
2014-07-29 18:05:54 +0200

    checkbox-ng:setup.py: Install the checkbox-cli launcher script

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -64,6 +64,7 @@ setup(
     description="Checkbox - Command Line Test Runner",
     long_description=long_description,
     install_requires=install_requires,
+    scripts = ['launchers/checkbox-cli'],
     entry_points={
         'console_scripts': [
             'checkbox=checkbox_ng.main:main',

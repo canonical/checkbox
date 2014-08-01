c582a1547325509d6ecb367c0f7d3c0e9499cb9d
Sylvain Pineau <sylvain.pineau@canonical.com>
2014-08-01 14:58:12 +0200

    checkbox-ng: increment version to 0.7

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -54,7 +54,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="0.7c1",
+    version="0.7",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

bfd56e22ceed2eb5003694757cf8ac428bbb3344
Daniel Manrique <roadmr@ubuntu.com>
2014-08-15 16:13:54 -0400

    providers/plainbox-provider-resource-generic: increment version to 0.8.dev

--- providers/resource/manage.py
+++ providers/resource/manage.py
@@ -44,7 +44,7 @@ class SourceDistributionCommandExt(SourceDistributionCommand):
 
 setup(
     name='2013.com.canonical.certification:plainbox-resources',
-    version="0.7",
+    version="0.8",
     description=N_("PlainBox resources provider"),
     gettext_domain='2013.com.canonical.certification.plainbox-resources',
 )

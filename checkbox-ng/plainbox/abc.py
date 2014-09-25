2f86f52bde46971208b0b844f48ca687d2811304
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2014-09-25 15:31:26 +0000

    plainbox:abc: add IJobDefinition.provider
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/abc.py
+++ checkbox-ng/plainbox/abc.py
@@ -149,6 +149,17 @@ class IJobDefinition(metaclass=ABCMeta):
         This value can be None
         """
 
+    @abstractproperty
+    def provider(self):
+        """
+        The provider this job definition belongs to
+
+        .. note::
+            Technically this still can be None (a provider-less job may exist) but
+            it can only happen in testing. This mode is discouraged and will be eventually
+            forbidden. All job definition units must belong to a provider.
+        """
+
 
 class IJobResult(metaclass=ABCMeta):
     """

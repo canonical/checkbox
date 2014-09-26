5587293adfc8cb497bc908172d4452912611b1e0
Daniel Manrique <roadmr@ubuntu.com>
2014-09-26 17:10:05 -0400

    plainbox: increment version to 0.13

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -50,7 +50,7 @@ else:
 
 setup(
     name="plainbox",
-    version="0.13c2",
+    version="0.13",
     url="https://launchpad.net/plainbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

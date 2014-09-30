20febf344435d88de8d4074b112aa0061de06f1f
Daniel Manrique <roadmr@ubuntu.com>
2014-09-30 17:46:46 -0400

    plainbox: Increment version to 0.13.1

--- checkbox-ng/plainbox/__init__.py
+++ checkbox-ng/plainbox/__init__.py
@@ -35,4 +35,4 @@ if sys.version_info[0:2] < (3, 2):
 # PEP386 compliant version declaration.
 #
 # This is used by @public decorator to enforce our public API guarantees.
-__version__ = (0, 13, 0, "final", 0)
+__version__ = (0, 13, 1, "final", 0)
--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -50,7 +50,7 @@ else:
 
 setup(
     name="plainbox",
-    version="0.13",
+    version="0.13.1",
     url="https://launchpad.net/plainbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

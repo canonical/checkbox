9d24cd7610616b9af9b1d54482ef07cae7ac2a6d
Sylvain Pineau <sylvain.pineau@canonical.com>
2014-10-20 22:51:52 +0200

    plainbox: increment version to 0.15c1

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -40,7 +40,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 setup(
     name="plainbox",
-    version="0.15.dev",
+    version="0.15c1",
     url="https://launchpad.net/plainbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

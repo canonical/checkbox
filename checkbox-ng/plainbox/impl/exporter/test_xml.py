cf859d4a52a6c0e1ca57f5958147eb9f0f46dc55
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-01-08 11:02:14 +0100

    plainbox:exporter:xml: remove obsolete STATUS_MAP test
    This patch removes test that uses _ALLOWED_STATUS and _STATUS_MAP that
    are subsequently removed by the next two patches.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/exporter/test_xml.py
+++ checkbox-ng/plainbox/impl/exporter/test_xml.py
@@ -91,24 +91,6 @@ class XMLSessionStateExporterTests(TestCaseWithParameters):
         self.assertEqual(actual, expected)
 
 
-class XMLExporterStatusMappingTests(TestCaseWithParameters):
-
-    parameter_names = ('checkbox_status',)
-    parameter_values = [(outcome, ) for outcome in IJobResult.ALL_OUTCOME_LIST]
-
-    def test_status_mapping(self):
-        """
-        Ensure that all possible plainbox statuses are mapped to
-        one of the possible ALLOWED_STATUS permitted by checkbox
-        legacy infrastructure.
-        """
-        pb_outcome = self.parameters.checkbox_status
-        self.assertIn(pb_outcome, XMLSessionStateExporter._STATUS_MAP)
-        mapped_status = XMLSessionStateExporter._STATUS_MAP[pb_outcome]
-        self.assertIn(mapped_status,
-                      XMLSessionStateExporter._ALLOWED_STATUS)
-
-
 class XMLExporterTests(TestCase):
 
     def setUp(self):

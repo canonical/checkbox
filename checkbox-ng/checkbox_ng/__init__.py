45561300a33fd60fb5b13b4b076ca178808f3f13
Sylvain Pineau <sylvain.pineau@canonical.com>
2015-01-26 16:25:17 +0100

    checkbox-ng: increment version to 0.17

--- checkbox-ng/checkbox_ng/__init__.py
+++ checkbox-ng/checkbox_ng/__init__.py
@@ -24,4 +24,4 @@
 CheckBoxNG is a new version of CheckBox built on top of PlainBox
 """
 
-__version__ = (0, 17, 0, "candidate", 1)
+__version__ = (0, 17, 0, "final", 0)
--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -54,7 +54,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="0.17c1",
+    version="0.17",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

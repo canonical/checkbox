9f7f785c24ab86cbbf9acfa50866e95a7c0ecc10
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2015-01-30 00:32:47 +0100

    plainbox:qml_shell: add standalone qml-native shell
    This patch adds standalone qml-shell that can be run without a need to run
    'plainbox run (...)', which would require provider to be created and loaded.
    
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

---
+++ checkbox-ng/plainbox/qml_shell/qml_shell.py
@@ -0,0 +1,125 @@
+# This file is part of Checkbox.
+#
+# Copyright 2014 Canonical Ltd.
+# Written by:
+#  Maciej Kisielewski <maciej.kisielewski@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+import json
+import os
+import subprocess
+import sys
+
+from plainbox import __version__ as plainbox_version
+from plainbox.i18n import docstring
+from plainbox.i18n import gettext as _
+from plainbox.i18n import gettext_noop as N_
+from plainbox.impl import get_plainbox_dir
+from plainbox.impl.applogic import PlainBoxConfig
+from plainbox.impl.clitools import SingleCommandToolMixIn
+from plainbox.impl.commands import PlainBoxCommand
+from plainbox.impl.commands import PlainBoxToolBase
+
+
+@docstring(
+    # TRANSLATORS: please leave various options (both long and short forms),
+    # environment variables and paths in their original form. Also keep the
+    # special @EPILOG@ string. The first line of the translation is special and
+    # is used as the help message. Please keep the pseudo-statement form and
+    # don't finish the sentence with a dot. Pay extra attention to whitespace.
+    # It must be correctly preserved or the result won't work. In particular
+    # the leading whitespace *must* be preserved and *must* have the same
+    # length on each line.
+    N_("""
+    run qml job in standalone shell
+
+    Runs specified file as it would be a plainbox' qml job.
+    Returns 0 if job returned 'pass', 1 if job returned 'fail', or
+    other value in case of an error.
+
+    @EPILOG@
+
+    Provided QML file must conform to the specification described in CEP-5.
+    """))
+class QmlShellCommand(PlainBoxCommand):
+    def register_parser(self, subparsers):
+        parser = subparsers.add_parser(
+            "plainbox-qml-shell",
+            help=_("run qml-native test in a standalone shell"))
+
+        self.register_arguments(parser)
+
+    def register_arguments(self, parser):
+        parser.set_defaults(command=self)
+        parser.add_argument('QML_FILE', help=_("qml file with job to be run"),
+                            metavar='QML-FILE')
+
+    def invoked(self, ns):
+        QML_SHELL_PATH = os.path.join(get_plainbox_dir(), 'qml_shell',
+                                      'qml_shell.qml')
+
+        test_result_object_prefix = "qml: __test_result_object:"
+        test_res = None
+        p = subprocess.Popen(['qmlscene', '--job',
+                             os.path.abspath(ns.QML_FILE), QML_SHELL_PATH],
+                             stderr=subprocess.PIPE)
+        for line in iter(p.stderr.readline, ''):
+            line = line.decode(sys.stderr.encoding)
+            if not line:
+                break
+            if line.startswith(test_result_object_prefix):
+                obj_json = line[len(test_result_object_prefix):]
+                test_res = json.loads(obj_json)
+            else:
+                print(line)
+
+        if not test_res:
+            return _("Job did not return any result")
+
+        print(_("Test outcome: {}").format(test_res['outcome']))
+        return test_res['outcome'] != "pass"
+
+
+class PlainboxQmlShellTool(SingleCommandToolMixIn, PlainBoxToolBase):
+    def get_command(self):
+        return QmlShellCommand()
+
+    @classmethod
+    def get_exec_name(cls):
+        return "plainbox-qml-shell"
+
+    @classmethod
+    def get_exec_version(cls):
+        """
+        Get the version reported by this executable
+        """
+        return cls.format_version_tuple(plainbox_version)
+
+    @classmethod
+    def get_config_cls(cls):
+        """
+        Get the Config class that is used by this implementation.
+
+        This can be overridden by subclasses to use a different config
+        class that is suitable for the particular application.
+        """
+        return PlainBoxConfig
+
+
+def main(argv=None):
+    raise SystemExit(PlainboxQmlShellTool().main(argv))
+
+
+def get_parser_for_sphinx():
+    return PlainboxQmlShellTool().construct_parser()
---
+++ checkbox-ng/plainbox/qml_shell/qml_shell.qml
@@ -0,0 +1,75 @@
+/*
+ * This file is part of Checkbox
+ *
+ * Copyright 2014 Canonical Ltd.
+ *
+ * Authors:
+ * - Maciej Kisielewski <maciej.kisielewski@canonical.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*! \brief QML standalone shell
+
+    This component serves as QML shell that embeds native QML plainbox jobs.
+    The job it loads is specified by --job argument passed to qmlscene
+    launching this component.
+    Job to be run must have testingShell property that be used as a hook to
+    'outside world'. The job should also define testDone signal that gets
+    signalled once test is finished. The signal should pass result object
+    containing following fields:
+    'outcome' (mandatory): outcome of a test, e.g. 'pass', 'fail', 'undecided'.
+    'suggestedOutcome': if outcome is 'undecided', than this suggestion will be
+    presented to the user, letting them decide the final outcome of a test.
+*/
+import QtQuick 2.0
+import Ubuntu.Components 1.1
+
+MainView {
+    id: mainView
+    width: units.gu(100)
+    height: units.gu(75)
+
+    // information and functionality passed to qml job component
+    property var testingShell: {
+        "name": "Standalone testing shell",
+        "pageStack": pageStack
+    }
+
+    Arguments {
+        id: args
+        Argument {
+            name: "job"
+            help: "QML-native job to run"
+            required: true
+            valueNames: ["PATH"]
+        }
+    }
+
+    Loader {
+        id: loader
+        anchors.fill: parent
+        onLoaded: loader.item.testDone.connect(testDone)
+    }
+    PageStack {
+        id: pageStack
+    }
+
+    function testDone(res) {
+        console.error("__test_result_object:"+JSON.stringify(res));
+        Qt.quit();
+    }
+
+    Component.onCompleted: {
+        loader.setSource(args.values.job, {'testingShell': testingShell});
+    }
+}

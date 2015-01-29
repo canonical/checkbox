83adc087160ebb9d3849968efff065c40e8f63f6
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-01-29 02:55:49 +0100

    plainbox:parsers: bump copyright
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/parsers.py
+++ checkbox-ng/plainbox/impl/parsers.py
@@ -1,13 +1,12 @@
 # This file is part of Checkbox.
 #
-# Copyright 2013 Canonical Ltd.
+# Copyright 2013-2015 Canonical Ltd.
 # Written by:
 #   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
 #
 # Checkbox is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3,
 # as published by the Free Software Foundation.
-
 #
 # Checkbox is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,7 +15,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
-
 """
 :mod:`plainbox.impl.parsers` -- generic parser interface
 ========================================================
@@ -27,7 +25,6 @@ to create abstract syntax trees of compatible inputs. For convenience
 and scriptability any parser is expected to be able to dump its AST
 as JSON.
 """
-
 import abc
 import inspect
 import json

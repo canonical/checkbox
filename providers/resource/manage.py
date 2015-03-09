f6ee9b1137d741660a5afc48115f7d379db29950
Sylvain Pineau <sylvain.pineau@canonical.com>
2015-03-09 11:57:17 +0100

    providers/plainbox-provider-resource-generic: increment version to 0.18.dev

--- providers/resource/manage.py
+++ providers/resource/manage.py
@@ -44,7 +44,7 @@ class SourceDistributionCommandExt(SourceDistributionCommand):
 
 setup(
     name='2013.com.canonical.certification:plainbox-resources',
-    version="0.17",
+    version="0.18",
     description=N_("PlainBox resources provider"),
     gettext_domain='plainbox-provider-resource',
     deprecated=False, strict=False,

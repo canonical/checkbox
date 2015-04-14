2f67f08174b20f80d16f72aada143a30097cc3de
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-04-14 21:43:58 +0200

    checkbox-ng:tools: bump copyright
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/checkbox_ng/tools.py
+++ checkbox-ng/checkbox_ng/tools.py
@@ -1,6 +1,6 @@
 # This file is part of Checkbox.
 #
-# Copyright 2012-2014 Canonical Ltd.
+# Copyright 2012-2015 Canonical Ltd.
 # Written by:
 #   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
 #

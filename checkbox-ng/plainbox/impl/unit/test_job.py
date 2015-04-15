74955c75aeee8395dd962d8fe7774d6d0b03c7d2
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-04-15 18:40:52 +0200

    plainbox:unit:job: materialize imports before passing to resource program
    This patch fixes an interesting bug where a "imports" line would only work
    for the first out of all the requirements. The reason for this is that
    the implementation of the imports system uses a generator and the
    resource program iterates over that iterator more than once.
    
    Fixes: https://bugs.launchpad.net/plainbox/+bug/1444242
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/plainbox/impl/unit/test_job.py
+++ checkbox-ng/plainbox/impl/unit/test_job.py
@@ -27,10 +27,12 @@ Test definitions for plainbox.impl.unit.job module
 from unittest import TestCase
 import warnings
 
+from plainbox.impl.providers.v1 import Provider1
 from plainbox.impl.secure.origin import FileTextSource
 from plainbox.impl.secure.origin import Origin
 from plainbox.impl.secure.rfc822 import RFC822Record
 from plainbox.impl.unit.job import JobDefinition
+from plainbox.impl.unit.job import propertywithsymbols
 from plainbox.impl.unit.test_unit_with_id import UnitWithIdFieldValidationTests
 from plainbox.impl.unit.unit_with_id import UnitWithId
 from plainbox.impl.unit.validators import UnitValidationContext
@@ -39,7 +41,6 @@ from plainbox.impl.validation import Severity
 from plainbox.impl.validation import ValidationError
 from plainbox.testing_utils.testcases import TestCaseWithParameters
 from plainbox.vendor import mock
-from plainbox.impl.unit.job import propertywithsymbols
 
 
 class DecoratorTests(TestCase):
@@ -1338,3 +1339,23 @@ class JobParsingTests(TestCaseWithParameters):
         expected = set({'foo', 'bar', 'froz'})
         observed = job.get_direct_dependencies()
         self.assertEqual(expected, observed)
+
+
+class RegressionTests(TestCase):
+
+    """ Regression tests. """
+
+    def test_1444242(self):
+        """ Regression test for http://pad.lv/1444242/. """
+        provider = mock.Mock(spec_set=Provider1, name='provider')
+        provider.namespace = '2013.com.canonical.certification'
+        job = JobDefinition({
+            'id': 'audio/playback_thunderbolt',
+            'imports': 'from 2013.com.canonical.plainbox import manifest',
+            'requires': (
+                "device.category == 'AUDIO'\n"
+                "manifest.has_thunderbolt == 'True'\n"),
+        }, provider=provider)
+        prog = job.get_resource_program()
+        self.assertEqual(prog.expression_list[-1].resource_id,
+                         '2013.com.canonical.plainbox::manifest')

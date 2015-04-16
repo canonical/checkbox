ec2e96d209d36d4e79d472da90d09e5593ae8b21
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2015-04-16 14:45:37 +0200

    plainbox:commands:inv_run: fix wrong reporting of estimated_time
    This patch fixes reporting of time left in the session.
    
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-ng/plainbox/impl/commands/inv_run.py
+++ checkbox-ng/plainbox/impl/commands/inv_run.py
@@ -726,7 +726,7 @@ class RunInvocation(CheckBoxInvocationMixIn):
                 print(self.C.header(
                     _('Running job {} / {}. Estimated time left: {}').format(
                         job_no, len(jobs_to_run),
-                        seconds_to_human_duration(min(0, estimated_time))),
+                        seconds_to_human_duration(max(0, estimated_time))),
                     fill='-'))
                 self.run_single_job(job)
                 estimated_time -= job.estimated_duration

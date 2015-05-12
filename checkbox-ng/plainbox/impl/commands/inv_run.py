1209f89ccdad05b951719c74791d86f3ed1b5a49
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2015-05-12 09:23:25 +0200

    plainbox:commands: print field headers
    This patch makes CLI front-ends print 'Purpose:', 'Steps:' and 'Verificaiton:'
    text before printing contents of the corresponding fields. This should make
    interaction with plainbox more digestable.
    
    Fixes: https://bugs.launchpad.net/plainbox/+bug/1453070
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-ng/plainbox/impl/commands/inv_run.py
+++ checkbox-ng/plainbox/impl/commands/inv_run.py
@@ -214,16 +214,19 @@ class NormalUI(IJobRunnerUI):
 
     def notify_about_purpose(self, job):
         if job.tr_purpose() is not None:
+            print(self.C.CYAN(_("Purpose:")))
             print(self.C.CYAN(job.tr_purpose()))
         else:
             self.notify_about_description(job)
 
     def notify_about_steps(self, job):
         if job.tr_steps() is not None:
+            print(self.C.CYAN(_("Steps:")))
             print(self.C.CYAN(job.tr_steps()))
 
     def notify_about_verification(self, job):
         if job.tr_verification() is not None:
+            print(self.C.CYAN(_("Verification:")))
             print(self.C.CYAN(job.tr_verification()))
 
     def job_cannot_start(self, job, job_state, result):

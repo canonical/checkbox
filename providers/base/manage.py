202960365af915dcb1a19050d1714e3e684d7cb1
Sylvain Pineau <sylvain.pineau@canonical.com>
2015-05-21 22:22:24 +0200

    providers/plainbox-provider-checkbox: increment version to 0.20.1

--- providers/base/manage.py
+++ providers/base/manage.py
@@ -4,7 +4,7 @@ from plainbox.provider_manager import N_
 
 setup(
     name='2013.com.canonical.certification:checkbox',
-    version="0.21",
+    version="0.20.1",
     description=N_("Checkbox provider"),
     gettext_domain='plainbox-provider-checkbox',
     strict=False, deprecated=False,

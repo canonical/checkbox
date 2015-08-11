92f00ee5f56781d22bdf501e9d6d773658c5e504
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-08-11 15:55:31 +0200

    Correct the version to 0.18
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- providers/certification-client/manage.py
+++ providers/certification-client/manage.py
@@ -78,7 +78,7 @@ class InstallCommandExt(InstallCommand):
 
 setup(
     name='2013.com.canonical.certification:certification-client',
-    version="1.0",
+    version="0.18",
     description=N_("Client Certification provider"),
     gettext_domain="2013_com_canonical_certification_certification-client",
     deprecated=False,

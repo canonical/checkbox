23ad99547abd69536f27f6bb884b45b7ccac0f9f
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2015-09-17 15:32:38 +0200

    plainbox,checkbox-ng: move JobTreeNode to checkbox-ng
    This patch moves the JobTreeNode class from plainbox to checkbox-ng.
    This code is likely to change significantly as checkbox-ng adopts the
    SessionAssistant APIs so it'd be better to keep it close to where it is
    going to be used. This also removes another lock-step dependency between
    checkbox-ng and plainbox.
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- checkbox-ng/checkbox_ng/test_misc.py
+++ checkbox-ng/checkbox_ng/test_misc.py
@@ -26,11 +26,49 @@ Test definitions for checkbox_ng.commands.cli module
 
 from unittest import TestCase
 
-from plainbox.impl.job import JobDefinition
-from plainbox.impl.testing_utils import make_job
 from plainbox.impl.session import SessionState
+from plainbox.impl.testing_utils import make_job
+from plainbox.impl.unit.job import JobDefinition
 
+from checkbox_ng.misc import JobTreeNode
 from checkbox_ng.misc import SelectableJobTreeNode
+
+
+class TestJobTreeNode(TestCase):
+
+    def setUp(self):
+        A = make_job('A')
+        B = make_job('B', plugin='local', description='foo')
+        C = make_job('C')
+        D = make_job('D', plugin='shell')
+        E = make_job('E', plugin='local', description='bar')
+        F = make_job('F', plugin='shell')
+        G = make_job('G', plugin='local', description='baz')
+        R = make_job('R', plugin='resource')
+        Z = make_job('Z', plugin='local', description='zaz')
+        state = SessionState([A, B, C, D, E, F, G, R, Z])
+        # D and E are a child of B
+        state.job_state_map[D.id].via_job = B
+        state.job_state_map[E.id].via_job = B
+        # F is a child of E
+        state.job_state_map[F.id].via_job = E
+        self.tree = JobTreeNode.create_tree(
+            state, [R, B, C, D, E, F, G, A, Z])
+
+    def test_create_tree(self):
+        self.assertIsInstance(self.tree, JobTreeNode)
+        self.assertEqual(len(self.tree.categories), 3)
+        [self.assertIsInstance(c, JobTreeNode) for c in self.tree.categories]
+        self.assertEqual(len(self.tree.jobs), 3)
+        [self.assertIsInstance(j, JobDefinition) for j in self.tree.jobs]
+        self.assertIsNone(self.tree.parent)
+        self.assertEqual(self.tree.depth, 0)
+        node = self.tree.categories[1]
+        self.assertEqual(node.name, 'foo')
+        self.assertEqual(len(node.categories), 1)
+        [self.assertIsInstance(c, JobTreeNode) for c in node.categories]
+        self.assertEqual(len(node.jobs), 1)
+        [self.assertIsInstance(j, JobDefinition) for j in node.jobs]
 
 
 class TestSelectableJobTreeNode(TestCase):

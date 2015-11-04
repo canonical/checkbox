689f191e6c1651633ca42a76d4065d88497bcd3b
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2015-11-04 09:36:33 +0100

    plainbox:commands: reuse EmptyProviderSkeleton in ProviderSkeleton
    This patch reuses EmptyProviderSkeleton class (that generates manage.py) in the
    full ProviderSkeleton class.
    
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-ng/plainbox/impl/commands/inv_startprovider.py
+++ checkbox-ng/plainbox/impl/commands/inv_startprovider.py
@@ -167,12 +167,14 @@ class EmptyProviderSkeleton(Skeleton):
         """))
 
 
-class ProviderSkeleton(Skeleton):
+class ProviderSkeleton(EmptyProviderSkeleton):
     """
     A skeleton with various content created for the startprovider command.
     """
 
-    things = []
+    # we're using a copy of empty skeleton's things, so we don't modify
+    # the original one (in EmptyProviderSkeleton)
+    things = EmptyProviderSkeleton.things[:]
 
     units_dir = Directory("units")
     things.append(units_dir)
@@ -496,30 +498,6 @@ class ProviderSkeleton(Skeleton):
              #!/bin/sh
              echo "Custom script executed"
              """))
-
-    things.append(File("manage.py", executable=True, full_text="""
-        #!/usr/bin/env python3
-        from plainbox.provider_manager import setup, N_
-
-        # You can inject other stuff here but please don't go overboard.
-        #
-        # In particular, if you need comprehensive compilation support to get
-        # your bin/ populated then please try to discuss that with us in the
-        # upstream project IRC channel #checkbox on irc.freenode.net.
-
-        # NOTE: one thing that you could do here, that makes a lot of sense,
-        # is to compute version somehow. This may vary depending on the
-        # context of your provider. Future version of PlainBox will offer git,
-        # bzr and mercurial integration using the versiontools library
-        # (optional)
-
-        setup(
-            name={name!r},
-            version="1.0",
-            description=N_("The {name} provider"),
-            gettext_domain="{gettext_domain}",
-        )
-        """))
 
     things.append(File(".gitignore", full_text="dist/*.tar.gz\nbuild/mo/*\n"))
 

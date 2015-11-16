5ca808c82c79e3faec02db04feb07ca68ad20256
Pierre Equoy <pierre.equoy@canonical.com>
2015-11-16 17:17:35 +0800

    checkbox-support: increment version to 0.23.dev0

--- checkbox-support/setup.py
+++ checkbox-support/setup.py
@@ -38,7 +38,7 @@ with open(os.path.join(base_dir, "README.rst"), encoding="UTF-8") as stream:
 
 setup(
     name="checkbox-support",
-    version="0.22",
+    version="0.23.dev0",
     url="https://launchpad.net/checkbox/",
     packages=find_packages(),
     test_suite='checkbox_support.tests.test_suite',

d86b8bb0a4930054b047faada4d17515768f0811
Pierre Equoy <pierre.equoy@canonical.com>
2015-12-15 10:28:35 +0800

    providers/plainbox-provider-resource-generic: increment version to 0.23.dev0

--- providers/resource/manage.py
+++ providers/resource/manage.py
@@ -44,7 +44,7 @@ class SourceDistributionCommandExt(SourceDistributionCommand):
 
 setup(
     name='2013.com.canonical.certification:plainbox-resources',
-    version="0.22",
+    version="0.23",
     description=N_("PlainBox resources provider"),
     gettext_domain='plainbox-provider-resource',
     deprecated=False, strict=False,

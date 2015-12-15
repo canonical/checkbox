ee955e2055a44a3cf52d5d3629744ad9092595af
Pierre Equoy <pierre.equoy@canonical.com>
2015-12-15 10:28:11 +0800

    plainbox: increment version to 0.25

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -39,7 +39,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 setup(
     name="plainbox",
-    version="0.25c1",
+    version="0.25",
     url="https://launchpad.net/plainbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

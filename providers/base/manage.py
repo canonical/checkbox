27f867a6c452942b0b471f1f7fb2523d8552e1c5
Sylvain Pineau <sylvain.pineau@canonical.com>
2016-01-12 09:32:33 +0100

    providers:checkbox: Distinguish name and namespace

--- providers/base/manage.py
+++ providers/base/manage.py
@@ -3,7 +3,8 @@ from plainbox.provider_manager import setup
 from plainbox.provider_manager import N_
 
 setup(
-    name='2013.com.canonical.certification:checkbox',
+    name='plainbox-provider-checkbox',
+    namespace='2013.com.canonical.certification',
     version="0.25",
     description=N_("Checkbox provider"),
     gettext_domain='plainbox-provider-checkbox',

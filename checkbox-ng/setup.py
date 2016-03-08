2b5443d11fe1735d3b3d5ae993f295f4c70d2262
Pierre Equoy <pierre.equoy@canonical.com>
2016-03-08 13:55:22 +0800

    plainbox: increment version to 0.26c1

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -51,7 +51,7 @@ else:
 
 setup(
     name="plainbox",
-    version="0.26.dev0",
+    version="0.26c1",
     url="https://launchpad.net/plainbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

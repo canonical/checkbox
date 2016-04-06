5e7fc8c047cd614f1cae8b50be198474abd10d01
Pierre Equoy <pierre.equoy@canonical.com>
2016-04-06 15:02:02 +0800

    Bump to v0.26

--- providers/certification-client/manage.py
+++ providers/certification-client/manage.py
@@ -79,7 +79,7 @@ class InstallCommandExt(InstallCommand):
 setup(
     name='certification-client',
     namespace='2013.com.canonical.certification',
-    version="0.23",
+    version="0.26",
     description=N_("Client Certification provider"),
     gettext_domain="certification-client",
     deprecated=False,

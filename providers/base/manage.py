64b178e352250f0dce142f9caf00192094e89cd2
Pierre Equoy <pierre.equoy@canonical.com>
2016-08-11 11:40:28 +0200

    providers:checkbox: Extend manage command to copy non-executable bits to bin
    A non-executable Python module named `bt_helper.py` needs to be copied along
    with executable scripts to the `bin` directory because it's required by the
    `bt_connect` script.
    
    By default, manage.py only copies executable bits into `bin`, so we add an
    extend method to copy the needed (non executable) script along.
    
    Fixes: https://bugs.launchpad.net/plainbox-provider-checkbox/+bug/1612080

--- providers/base/manage.py
+++ providers/base/manage.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
+from plainbox.provider_manager import InstallCommand
+from plainbox.provider_manager import manage_py_extension
 from plainbox.provider_manager import setup
 from plainbox.provider_manager import N_
+
+import os, shutil
+
+@manage_py_extension
+class InstallPyModules(InstallCommand):
+    """
+    copy additional (non executable) Python module needed by the bin scripts
+    
+    @EPILOG@
+
+    This command copies non executable Python module needed by a script written
+    to test Bluetooth connectivity.
+    """
+    # Fixes lp:1612080
+    name = 'install'
+
+    def invoked(self, ns):
+        super().invoked(ns)
+        dest_map = self._get_dest_map(ns.layout, ns.prefix)
+        provider = self.get_provider()
+        shutil.copy(
+            os.path.join(provider.bin_dir, 'bt_helper.py'),
+            ns.root + dest_map['bin'])
 
 setup(
     name='plainbox-provider-checkbox',

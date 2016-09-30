f387bf07ddd1265a9bbfe3cab5a8efa04d2a4ff1
Pierre Equoy <pierre.equoy@canonical.com>
2016-09-30 15:19:11 +0800

    checkbox-ng: increment version to 0.30.dev0

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -55,7 +55,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="0.29",
+    version="0.30.dev0",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

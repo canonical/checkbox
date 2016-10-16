384313e419cc69bed4330374650ddc9082a63d0a
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2016-10-16 15:47:23 +0200

    commands:cmd_session: don't get_exporter_names() when not needed
    So that SessionManager class is not instantiated, and no 'spooling-up' is
    required.
    
    This makes invocation of 'plainbox --version' soooo much faster (~4x faster)
    
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-ng/plainbox/impl/commands/cmd_session.py
+++ checkbox-ng/plainbox/impl/commands/cmd_session.py
@@ -112,7 +112,7 @@ class SessionCommand(PlainBoxCommand):
         group = export_parser.add_argument_group(_("output options"))
         group.add_argument(
             '-f', '--output-format', default='text',
-            metavar=_('FORMAT'), choices=[_('?')] + get_all_exporter_names(),
+            metavar=_('FORMAT'), choices=[_('?')],
             help=_('save test results in the specified FORMAT'
                    ' (pass ? for a list of choices)'))
         group.add_argument(

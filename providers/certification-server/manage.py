21ce5a0ed3aa94fabc4f51331d402bc5160a32f5
Sylvain Pineau <sylvain.pineau@canonical.com>
2017-02-03 11:27:32 +0100

    Bump to v0.31.0

--- providers/certification-server/manage.py
+++ providers/certification-server/manage.py
@@ -79,7 +79,7 @@ class InstallCommandExt(InstallCommand):
 setup(
     name='certification-server',
     namespace='2013.com.canonical.certification',
-    version="0.31.0rc2",
+    version="0.31.0",
     description=N_("Server Certification provider"),
     gettext_domain="certification-server",
     strict=False, deprecated=False,

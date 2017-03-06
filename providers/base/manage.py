52a8aee864446f76d3a227c59f1b3fd850c61842
Pierre Equoy <pierre.equoy@canonical.com>
2017-03-06 16:22:33 +0800

    Bump to v0.35.0rc1

--- providers/base/manage.py
+++ providers/base/manage.py
@@ -30,7 +30,7 @@ class InstallPyModules(InstallCommand):
 setup(
     name='plainbox-provider-checkbox',
     namespace='2013.com.canonical.certification',
-    version="0.35.0.dev0",
+    version="0.35.0rc1",
     description=N_("Checkbox provider"),
     gettext_domain='plainbox-provider-checkbox',
     strict=False, deprecated=False,

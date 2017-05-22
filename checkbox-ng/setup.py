ea12395bbccec450f9ce90f052ca19cf8b3f3832
Sylvain Pineau <sylvain.pineau@canonical.com>
2017-05-22 19:17:52 +0200

    Bump to v0.33.0

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -55,7 +55,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="0.33.0rc2",
+    version="0.33.0",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

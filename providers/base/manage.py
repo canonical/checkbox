086c20e38f0c5edf2c4aefa129d0348a10373cf8
Sylvain Pineau <sylvain.pineau@canonical.com>
2017-07-18 10:56:30 +0200

    Bump to v0.37.0

--- providers/base/manage.py
+++ providers/base/manage.py
@@ -5,7 +5,7 @@ from plainbox.provider_manager import N_
 setup(
     name='plainbox-provider-checkbox',
     namespace='com.canonical.certification',
-    version="0.37.0rc4",
+    version="0.37.0",
     description=N_("Checkbox provider"),
     gettext_domain='plainbox-provider-checkbox',
     strict=False, deprecated=False,

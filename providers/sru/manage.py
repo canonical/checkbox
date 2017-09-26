b1ccfdde5a094aeea849a6101668a61fce4a9960
Sylvain Pineau <sylvain.pineau@canonical.com>
2017-09-26 10:27:51 +0200

    Bump to v1.9.0rc2

--- providers/sru/manage.py
+++ providers/sru/manage.py
@@ -16,7 +16,7 @@ from plainbox.provider_manager import setup, N_
 setup(
     name='plainbox-provider-sru',
     namespace='com.canonical.certification',
-    version="1.9.0.dev0",
+    version="1.9.0rc2",
     description=N_("The SRU provider"),
     gettext_domain="plainbox-provider-sru",
 )

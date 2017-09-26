806d258e57fbfe3228678847b782eafd61e37198
Sylvain Pineau <sylvain.pineau@canonical.com>
2017-09-26 10:52:27 +0200

    Bump to v0.35.0rc4

--- checkbox-ng/checkbox_ng/__init__.py
+++ checkbox-ng/checkbox_ng/__init__.py
@@ -24,4 +24,4 @@
 CheckBoxNG is a new version of CheckBox built on top of PlainBox
 """
 
-__version__ = '0.35.0rc3'
+__version__ = '0.35.0rc4'
--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -55,7 +55,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="0.35.0rc3",
+    version="0.35.0rc4",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

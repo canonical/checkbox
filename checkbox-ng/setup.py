707f9655a7b12ccac47599f85e35819b90b0c1bf
Sylvain Pineau <sylvain.pineau@canonical.com>
2017-10-24 13:29:00 +0200

    Bump to v0.37.0

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -39,7 +39,7 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
 setup(
     name="plainbox",
-    version="0.37.0rc2",
+    version="0.37.0",
     url="https://launchpad.net/plainbox/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

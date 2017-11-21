2eb123dae8c08635424dab68b7194142bf7615bc
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2017-11-21 12:29:38 +0100

    move RootViaSudoWithPass ctrl from converged
    this will no longer be used only in checkbox-converged, and IMHO it could live
    in plainbox code all along since the controller is front-end agnostic.
    
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-ng/plainbox/impl/ctrl.py
+++ checkbox-ng/plainbox/impl/ctrl.py
@@ -49,7 +49,9 @@ try:
 except ImportError:
     posix = None
 import tempfile
+import subprocess
 import sys
+import threading
 from subprocess import check_output, CalledProcessError, STDOUT
 
 from plainbox.abc import IExecutionController
@@ -1526,3 +1528,188 @@ class RootViaSudoExecutionController(
             A callable to do the warm-up
         """
         return self.warm_up_func
+
+
+class RootViaSudoWithPassExecutionController(
+        CheckBoxDifferentialExecutionController):
+    """
+    Execution controller that gains root by using sudo, automatically
+    supplying password to the sudo process.
+
+    This controller should be used for jobs that need root in front-ends that
+    are not CLI based.
+    """
+
+    def __init__(self, provider_list, password_provider_cls):
+        """
+        Initialize a new RootViaSudoWithPassExecutionController
+
+        :param provider_list:
+            A list of Provider1 objects that will be available for script
+            dependency resolutions. Currently all of the scripts are makedirs
+            available but this will be refined to the minimal set later.
+        :param password_provider_cls:
+            A callable that will be used to obtain user's password.
+            It is called when the controller runs a sudo command.
+        """
+        super().__init__(provider_list)
+        try:
+            in_sudo_group = grp.getgrnam("sudo").gr_gid in posix.getgroups()
+        except KeyError:
+            in_sudo_group = False
+        try:
+            in_admin_group = grp.getgrnam("admin").gr_gid in posix.getgroups()
+        except KeyError:
+            in_admin_group = False
+        self.user_can_sudo = in_sudo_group or in_admin_group
+        self._password_provider = password_provider_cls
+
+    def get_execution_command(self, job, job_state, config, session_dir,
+                              nest_dir):
+        """
+        Get the command to execute the specified job
+
+        :param job:
+            job definition with the command and environment definitions
+        :param job_state:
+            The JobState associated to the job to execute.
+        :param config:
+            A PlainBoxConfig instance which can be used to load missing
+            environment definitions that apply to all jobs. It is used to
+            provide values for missing environment variables that are required
+            by the job (as expressed by the environ key in the job definition
+            file).
+        :param session_dir:
+            Base directory of the session this job will execute in.
+            This directory is used to co-locate some data that is unique to
+            this execution as well as data that is shared by all executions.
+        :param nest_dir:
+            A directory with a nest of symlinks to all executables required to
+            execute the specified job. This argument may or may not be used,
+            depending on how PATH is passed to the command (via environment or
+            via the commant line)
+        :returns:
+            List of command arguments
+        """
+        # Run env(1) as the required user
+        # --prompt is used to set sudo promp to an empty string so we don't
+        # pollute command output
+        # --reset-timestamp makes sudo ask for password even if it has been
+        # supplied recently
+        cmd = ['sudo', '--prompt=', '--reset-timestamp', '--stdin',
+               '--user', job.user, 'env']
+        # Append all environment data
+        env = self.get_differential_execution_environment(
+            job, job_state, config, session_dir, nest_dir)
+        cmd += ["{key}={value}".format(key=key, value=value)
+                for key, value in sorted(env.items())]
+        # Lastly use job.shell -c, to run our command
+        cmd += [job.shell, '-c', job.command]
+        return cmd
+
+    def execute_job(self, job, job_state, config, session_dir, extcmd_popen):
+        """
+        Execute the job using standard subprocess.Popen
+
+        :param job:
+            The JobDefinition to execute
+        :param job_state:
+            The JobState associated to the job to execute.
+        :param config:
+            A PlainBoxConfig instance which can be used to load missing
+            environment definitions that apply to all jobs. It is used to
+            provide values for missing environment variables that are required
+            by the job (as expressed by the environ key in the job definition
+            file).
+        :param session_dir:
+            Base directory of the session this job will execute in.
+            This directory is used to co-locate some data that is unique to
+            this execution as well as data that is shared by all executions.
+        :param extcmd_popen:
+            A subprocess.Popen like object
+        :returns:
+            The return code of the command, as returned by subprocess.call()
+        """
+
+        def call(extcmd_popen, *args, **kwargs):
+            """Invoke the desired sub-process and intercept the output."""
+            # Notify that the process is about to start
+            extcmd_popen._delegate.on_begin(args, kwargs)
+            # Setup stdout/stderr redirection
+            kwargs['stdout'] = subprocess.PIPE
+            kwargs['stderr'] = subprocess.PIPE
+            # Start the process
+            proc = extcmd_popen._popen(*args, **kwargs)
+            # Setup all worker threads. By now the pipes have been created and
+            # proc.stdout/proc.stderr point to open pipe objects.
+            stdout_reader = threading.Thread(
+                target=extcmd_popen._read_stream, args=(proc.stdout, "stdout"))
+            stderr_reader = threading.Thread(
+                target=extcmd_popen._read_stream, args=(proc.stderr, "stderr"))
+            queue_worker = threading.Thread(target=extcmd_popen._drain_queue)
+            # Start all workers
+            queue_worker.start()
+            stdout_reader.start()
+            stderr_reader.start()
+            try:
+                while True:
+                    try:
+                        # sudo manpage explicitly states that \n should be
+                        # appended
+                        pass_bytes = bytes(self._password_provider() + '\n',
+                                           'UTF-8')
+                        proc.communicate(input=pass_bytes)
+                        # Break out of the endless loop if it does
+                        break
+                    except KeyboardInterrupt:
+                        # On interrupt send a signal to the process
+                        extcmd_popen._on_keyboard_interrupt(proc)
+                        # And send a notification about this
+                        extcmd_popen._delegate.on_interrupt()
+            finally:
+                # Wait until all worker threads shut down
+                stdout_reader.join()
+                stderr_reader.join()
+                # Tell the queue worker to shut down
+                extcmd_popen._queue.put(None)
+                queue_worker.join()
+            # Notify that the process has finished
+            extcmd_popen._delegate.on_end(proc.returncode)
+            return proc.returncode
+
+        if not os.path.isdir(self.get_CHECKBOX_DATA(session_dir)):
+            os.makedirs(self.get_CHECKBOX_DATA(session_dir))
+        # Setup the executable nest directory
+        with self.configured_filesystem(job, config) as nest_dir:
+            # Get the command and the environment.
+            # of this execution controller
+            cmd = self.get_execution_command(
+                job, job_state, config, session_dir, nest_dir)
+            env = self.get_execution_environment(
+                job, job_state, config, session_dir, nest_dir)
+            with self.temporary_cwd(job, config) as cwd_dir:
+                # run the command
+                # TRANSLATORS: Please leave %(CMD)r, %(ENV)r, %(DIR)r as-is
+                logger.debug(_("job[%(ID)s] executing %(CMD)r with env %(ENV)r"
+                               " in cwd %(DIR)r"),
+                             {"ID": job.id, "CMD": cmd,
+                             "ENV": env, "DIR": cwd_dir})
+                return_code = call(extcmd_popen, cmd, stdin=subprocess.PIPE,
+                                   env=env, cwd=cwd_dir)
+                if 'noreturn' in job.get_flag_set():
+                    self._halt()
+                return return_code
+
+    def get_checkbox_score(self, job):
+        """
+        Compute how applicable this controller is for the specified job.
+
+        :returns:
+            -1 if the job does not have a user override or the user cannot use
+            sudo and 2 otherwise
+        """
+        # Only makes sense with jobs that need to run as another user
+        if job.user is not None and self.user_can_sudo:
+            return 2
+        else:
+            return -1

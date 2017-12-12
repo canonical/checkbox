2853bafbee7835094baa4a51061bf42fe5f431b6
Jonathan Cave <jonathan.cave@canonical.com>
2017-12-12 19:41:51 +0000

    Fix consuming configs from SNAP_DATA

--- checkbox-ng/checkbox_ng/launcher/checkbox_cli.py
+++ checkbox-ng/checkbox_ng/launcher/checkbox_cli.py
@@ -96,17 +96,21 @@ class LauncherIngredient(Ingredient):
             generic_launcher.read_string(text)
             config_filename = os.path.expandvars(
                 generic_launcher.config_filename)
+            # if wrapper specifies just the basename
             if not os.path.split(config_filename)[0]:
-                configs += [
-                    '/etc/xdg/{}'.format(config_filename),
-                    os.path.expanduser('~/.config/{}'.format(config_filename))]
+                if "SNAP_DATA" in os.environ:
+                    configs = [context.args.launcher]
+                    configs.append(os.path.join(
+                        os.path.expandvars('$SNAP_DATA'), config_filename))
+                else:
+                    configs += [
+                        '/etc/xdg/{}'.format(config_filename),
+                        os.path.expanduser('~/.config/{}'.format(
+                            config_filename))]
+            # if wrapper specifies an absolute file
             else:
                 configs.append(config_filename)
             launcher = generic_launcher.get_concrete_launcher()
-        if "SNAP_DATA" in os.environ:
-            # configs[0] contains the filename that should be used
-            configs.insert(1, os.path.join(
-                os.path.expandvars('$SNAP_DATA'), configs[0]))
         launcher.read(configs)
         if launcher.problem_list:
             _logger.error(_("Unable to start launcher because of errors:"))

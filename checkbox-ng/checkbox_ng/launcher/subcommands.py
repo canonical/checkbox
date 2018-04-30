5939c1a0c6c1995571f1c6dd2d6c5c8525ca9d78
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2018-04-30 11:59:20 +0200

    add --version option to checkbox-cli
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-ng/checkbox_ng/launcher/subcommands.py
+++ checkbox-ng/checkbox_ng/launcher/subcommands.py
@@ -184,6 +184,10 @@ class Launcher(Command, MainLoopStage, ReportsStage):
         return self.launcher.api_flags
 
     def invoked(self, ctx):
+        if ctx.args.version:
+            from checkbox_ng import __version__
+            print(__version__)
+            return
         if ctx.args.verify:
             # validation is always run, so if there were any errors the program
             # exited by now, so validation passed
@@ -655,6 +659,8 @@ class Launcher(Command, MainLoopStage, ReportsStage):
             'print debug messages from checkbox'))
         parser.add_argument('--clear-cache', action='store_true', help=_(
             'remove cached results from the system'))
+        parser.add_argument('--version', action='store_true', help=_(
+            "show program's version information and exit"))
 
 
 

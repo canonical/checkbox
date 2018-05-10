d3d19a839fb59e61b01755ad44256bc43c9cdca2
Jeff Lane <jeffrey.lane@canonical.com>
2018-05-10 12:38:03 -0400

    rollback change to get_udev_xhci_devices that was not necessary

--- checkbox-support/checkbox_support/udev.py
+++ checkbox-support/checkbox_support/udev.py
@@ -109,8 +109,7 @@ def get_udev_xhci_devices(udev_client):
     enumerator.add_match_subsystem('pci')
     devices = [
         device for device in enumerator.execute()
-        if device.get_device_file() is not None and
-        (device.get_driver() == 'xhci_hcd')]
+        if (device.get_driver() == 'xhci_hcd')]
     # Sort the list, this is not needed but makes various debugging dumps
     # look better.
     devices.sort(key=lambda device: device.get_property('PCI_SLOT_NAME'))

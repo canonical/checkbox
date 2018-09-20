6a17a8bc89d2ffaff825f7987038d98fff7e2374
Sylvain Pineau <sylvain.pineau@canonical.com>
2018-09-20 10:05:42 +0200

    increment version to v0.41.0.dev0

--- providers/certification-server/manage.py
+++ providers/certification-server/manage.py
@@ -79,7 +79,7 @@ class InstallCommandExt(InstallCommand):
 setup(
     name='certification-server',
     namespace='com.canonical.certification',
-    version="0.40.0",
+    version="0.41.0.dev0",
     description=N_("Server Certification provider"),
     gettext_domain="certification-server",
     strict=False, deprecated=False,

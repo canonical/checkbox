ed44e752b7ff3e904532c35982171147d1f3ed07
Sylvain Pineau <sylvain.pineau@canonical.com>
2018-10-29 13:00:30 +0100

    checkbox-cli: Refactor merge-reports/submissions code
    MergeSubmissions inherits MergeReports

--- checkbox-ng/checkbox_ng/launcher/merge_reports.py
+++ checkbox-ng/checkbox_ng/launcher/merge_reports.py
@@ -48,70 +48,76 @@ class MergeReports(Command):
             '-o', '--output-file', metavar='FILE',
             help='save combined test results to the specified FILE')
 
-    def invoked(self, ctx):
-        manager_list = []
-        for submission in ctx.args.submission:
-            tmpdir = TemporaryDirectory()
-            job_list = []
-            category_list = []
-            try:
-                with tarfile.open(submission) as tar:
-                    tar.extractall(tmpdir.name)
-                    with open(os.path.join(
-                              tmpdir.name, 'submission.json')) as f:
-                        data = json.load(f)
-                for result in data['results']:
-                    result['plugin'] = 'shell'  # Required so default to shell
-                    result['summary'] = result['name']
-                    job_list.append(JobDefinition(result))
-                for result in data['resource-results']:
-                    result['plugin'] = 'resource'
-                    result['summary'] = result['name']
-                    job_list.append(JobDefinition(result))
-                for result in data['attachment-results']:
-                    result['plugin'] = 'attachment'
-                    result['summary'] = result['name']
-                    job_list.append(JobDefinition(result))
-                for cat_id, cat_name in data['category_map'].items():
-                    category_list.append(
+    def _parse_submission(self, submission, tmpdir, mode="list"):
+        try:
+            with tarfile.open(submission) as tar:
+                tar.extractall(tmpdir.name)
+                with open(os.path.join(
+                          tmpdir.name, 'submission.json')) as f:
+                    data = json.load(f)
+            for result in data['results']:
+                result['plugin'] = 'shell'  # Required so default to shell
+                result['summary'] = result['name']
+                if mode == "list":
+                    self.job_list.append(JobDefinition(result))
+                elif mode == "dict":
+                    self.job_dict[result['id']] = JobDefinition(result)
+            for result in data['resource-results']:
+                result['plugin'] = 'resource'
+                result['summary'] = result['name']
+                if mode == "list":
+                    self.job_list.append(JobDefinition(result))
+                elif mode == "dict":
+                    self.job_dict[result['id']] = JobDefinition(result)
+            for result in data['attachment-results']:
+                result['plugin'] = 'attachment'
+                result['summary'] = result['name']
+                if mode == "list":
+                    self.job_list.append(JobDefinition(result))
+                elif mode == "dict":
+                    self.job_dict[result['id']] = JobDefinition(result)
+            for cat_id, cat_name in data['category_map'].items():
+                if mode == "list":
+                    self.category_list.append(
                         CategoryUnit({'id': cat_id, 'name': cat_name}))
-                manager = SessionManager.create_with_unit_list(
-                    job_list + category_list)
-                manager.state.metadata.title = data['title']
-            except OSError as e:
-                print(e)
-                return 1
-            except KeyError as e:
-                print("Invalid JSON submission, missing key:", e)
-                return 1
-            for job in job_list:
-                io_log = [
-                    IOLogRecord(count, 'stdout', line.encode('utf-8'))
-                    for count, line in enumerate(
-                        job.get_record_value('io_log').splitlines(
-                            keepends=True))
-                ]
-                result = MemoryJobResult({
-                    'outcome': job.get_record_value('status'),
-                    'comments': job.get_record_value('comments'),
-                    'execution_duration': job.get_record_value('duration'),
-                    'io_log': io_log,
-                })
-                manager.state.update_job_result(job, result)
-                if job.plugin == 'resource':
-                    new_resource_list = []
-                    for record in gen_rfc822_records_from_io_log(job, result):
-                        resource = Resource(record.data)
-                        new_resource_list.append(resource)
-                    if not new_resource_list:
-                        new_resource_list = [Resource({})]
-                    manager.state.set_resource_list(
-                        "com.canonical.certification::" + job.id,
-                        new_resource_list)
-                job_state = manager.state.job_state_map[job.id]
-                job_state.effective_category_id = job.get_record_value(
-                    'category_id', 'com.canonical.plainbox::uncategorised')
-            manager_list.append(manager)
+                elif mode == "dict":
+                    self.category_dict[cat_id] = CategoryUnit(
+                        {'id': cat_id, 'name': cat_name})
+        except OSError as e:
+            raise SystemExit(e)
+        except KeyError as e:
+            raise SystemExit(e)
+        return data['title']
+
+    def _populate_session_state(self, job, state):
+        io_log = [
+            IOLogRecord(count, 'stdout', line.encode('utf-8'))
+            for count, line in enumerate(
+                job.get_record_value('io_log').splitlines(
+                    keepends=True))
+        ]
+        result = MemoryJobResult({
+            'outcome': job.get_record_value('status'),
+            'comments': job.get_record_value('comments'),
+            'execution_duration': job.get_record_value('duration'),
+            'io_log': io_log,
+        })
+        state.update_job_result(job, result)
+        if job.plugin == 'resource':
+            new_resource_list = []
+            for record in gen_rfc822_records_from_io_log(job, result):
+                resource = Resource(record.data)
+                new_resource_list.append(resource)
+            if not new_resource_list:
+                new_resource_list = [Resource({})]
+            state.set_resource_list(
+                "com.canonical.certification::" + job.id,
+                new_resource_list)
+        job_state = state.job_state_map[job.id]
+        job_state.effective_category_id = job.get_record_value(
+            'category_id', 'com.canonical.plainbox::uncategorised')
+
+    def _create_exporter(self, exporter_id):
         exporter_map = {}
         exporter_units = get_exporters().unit_list
         for unit in exporter_units:
@@ -119,10 +125,25 @@ class MergeReports(Command):
                 support = unit.support
                 if support:
                     exporter_map[unit.id] = support
-        exporter_support = exporter_map[
-            'com.canonical.plainbox::html-multi-page']
-        exporter = exporter_support.exporter_cls(
+        exporter_support = exporter_map[exporter_id]
+        return exporter_support.exporter_cls(
             [], exporter_unit=exporter_support)
+
+    def invoked(self, ctx):
+        manager_list = []
+        for submission in ctx.args.submission:
+            tmpdir = TemporaryDirectory()
+            self.job_list = []
+            self.category_list = []
+            session_title = self._parse_submission(submission, tmpdir)
+            manager = SessionManager.create_with_unit_list(
+                self.job_list + self.category_list)
+            manager.state.metadata.title = session_title
+            for job in self.job_list:
+                self._populate_session_state(job, manager.state)
+            manager_list.append(manager)
+        exporter = self._create_exporter(
+            'com.canonical.plainbox::html-multi-page')
         with open(ctx.args.output_file, 'wb') as stream:
             exporter.dump_from_session_manager_list(manager_list, stream)
         print(ctx.args.output_file)

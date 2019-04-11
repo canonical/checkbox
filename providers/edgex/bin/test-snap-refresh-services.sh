7426bccb066965d9a87382b477a344c9eb714d64
Ian Johnson <ian.johnson@canonical.com>
2019-04-10 21:38:34 -0500

    bin/test-snap-refresh-services.sh: add test for refreshes
    This test script checks we can upgrade from the stable and delhi stable
    channels to this revision by ensuring that the services startup properly
    after the refresh.
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

---
+++ providers/edgex/bin/test-snap-refresh-services.sh
@@ -0,0 +1,55 @@
+#!/bin/bash -e
+
+# get the directory of this script
+# snippet from https://stackoverflow.com/a/246128/10102404
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# load the utils
+# shellcheck source=/dev/null
+source "$SCRIPT_DIR/utils.sh"
+
+# remove the snap if it's already installed
+snap_remove
+
+for channel in delhi stable; do 
+    # first make sure that the snap installs correctly from the channel
+    case "$channel" in 
+        delhi)
+            if [ -n "$EDGEX_DELHI_SNAP_FILE" ]; then
+                snap_install "$EDGEX_DELHI_SNAP_FILE"
+            else
+                snap_install edgexfoundry delhi
+            fi
+            ;;
+        stable)
+            if [ -n "$EDGEX_STABLE_SNAP_FILE" ]; then
+                snap_install "$EDGEX_STABLE_SNAP_FILE"
+            else
+                snap_install edgexfoundry stable
+            fi
+            ;;
+        *)
+            snap_install edgexfoundry "$channel"
+            ;;
+    esac
+    # wait for services to come online
+    # NOTE: this may have to be significantly increased on arm64 or low RAM platforms
+    # to accomodate time for everything to come online
+    sleep 120
+    snap_check_svcs
+
+    # now install the snap version we are testing and check again
+    if [ -n "$REVISION_TO_TEST" ]; then
+        snap_install "$REVISION_TO_TEST" "$REVISION_TO_TEST_CHANNEL" "$REVISION_TO_TEST_CONFINEMENT"
+    else
+        snap_install edgexfoundry beta 
+    fi
+    # wait for services to come online
+    # NOTE: this may have to be significantly increased on arm64 or low RAM platforms
+    # to accomodate time for everything to come online
+    sleep 120
+    snap_check_svcs
+
+    # remove the snap to run the next channel upgrade
+    snap_remove
+done

4163e9d5d4ccbb321615e4c092c2fe5731a42a57
Ian Johnson <ian.johnson@canonical.com>
2019-05-13 10:46:52 -0500

    bin/test-device-random.sh: add test for device-random
    This test ensures that:
    * device-random starts up and stays running after 10 seconds
    * device-random creates a device in core-metadata called
      Random-Integer-Generator01
    * device-random sends readings to core-data for the
      Random-Integer-Generator01 device
    
    Signed-off-by: Ian Johnson <ian.johnson@canonical.com>

---
+++ providers/edgex/bin/test-device-random.sh
@@ -0,0 +1,91 @@
+#!/bin/bash -e
+
+# get the directory of this script
+# snippet from https://stackoverflow.com/a/246128/10102404
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ "$(id -u)" = "0" ]; then
+    SUDO=""
+else
+    SUDO="sudo"
+fi
+
+# load the utils
+# shellcheck source=/dev/null
+source "$SCRIPT_DIR/utils.sh"
+
+snap_remove
+
+# install the snap to make sure it installs
+if [ -n "$REVISION_TO_TEST" ]; then
+    snap_install "$REVISION_TO_TEST" "$REVISION_TO_TEST_CHANNEL" "$REVISION_TO_TEST_CONFINEMENT"
+else
+    snap_install edgexfoundry beta 
+fi
+
+# wait for services to come online
+# NOTE: this may have to be significantly increased on arm64 or low RAM platforms
+# to accomodate time for everything to come online
+sleep 120
+
+# start device-random
+$SUDO snap start edgexfoundry.device-random
+
+# wait 10 seconds - check to make sure it's still running
+sleep 10
+if [ -n "$(snap services edgexfoundry.device-random | grep edgexfoundry.device-random | grep inactive)" ]; then
+    echo "failed to start device-random"
+    exit 1
+fi
+
+echo -n "finding jq... "
+
+set +e
+if command -v edgexfoundry.jq > /dev/null; then
+    JQ=$(command -v edgexfoundry.jq)
+elif command -v jq > /dev/null; then
+    JQ=$(command -v jq)
+else
+    echo "NOT FOUND"
+    echo "install with snap install jq"
+    exit 1
+fi
+
+echo "found at $JQ"
+
+# check to see if we can find the device created by device-random
+while true; do
+    if ! (edgexfoundry.curl -s localhost:48081/api/v1/device | $JQ '.'); then
+        # not json - something's wrong
+        echo "invalid JSON response from core-metadata"
+        exit 1
+    elif [ "$(edgexfoundry.curl -s localhost:48081/api/v1/device | $JQ 'map(select(.name == "Random-Integer-Generator01")) | length')" -lt 1 ]; then
+        # no devices yet, keep waiting
+        sleep 1
+    else
+        # got the device, break out
+        break
+    fi
+done
+
+# check to see if we can get a reading from the Random-Integer-Generator
+while true; do
+    if ! (edgexfoundry.curl -s localhost:48080/api/v1/reading/device/Random-Integer-Generator01/10 | $JQ '.'); then
+        # not json - something's wrong
+        echo "invalid JSON response from core-data"
+        exit 1
+    elif [ "$(edgexfoundry.curl -s localhost:48080/api/v1/reading/device/Random-Integer-Generator01/10 | $JQ 'length')" -le 1 ]; then
+        # no readings yet, keep waiting
+        sleep 1
+    else
+        # got at least one reading, break out
+        break
+    fi
+done
+
+
+
+set -e
+
+# remove the snap to run the next test
+snap_remove

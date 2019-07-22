dbea4aa9b98a2479609cb547c3c976cebf4ac010
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2019-07-22 14:10:46 +0200

    config: outline loading of file configs to a function
    Signed-off-by: Maciej Kisielewski <maciej.kisielewski@canonical.com>

--- checkbox-ng/checkbox_ng/config.py
+++ checkbox-ng/checkbox_ng/config.py
@@ -1,7 +1,8 @@
 # This file is part of Checkbox.
 #
-# Copyright 2013 Canonical Ltd.
+# Copyright 2013-2019 Canonical Ltd.
 # Written by:
+#   Maciej Kisielewski <maciej.kisielewski@canonical.com>
 #   Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
 #
 # Checkbox is free software: you can redistribute it and/or modify
@@ -21,12 +22,19 @@
 :mod:`checkbox_ng.config` -- CheckBoxNG configuration
 =====================================================
 """
-
+import gettext
 import itertools
+import logging
 import os
 
 from plainbox.impl.applogic import PlainBoxConfig
+from plainbox.impl.launcher import DefaultLauncherDefinition
+from plainbox.impl.launcher import LauncherDefinition
 
+
+_ = gettext.gettext
+
+_logger = logging.getLogger("config")
 
 class CheckBoxConfig(PlainBoxConfig):
     """
@@ -47,3 +55,49 @@ class CheckBoxConfig(PlainBoxConfig):
                     PlainBoxConfig.Meta.filename_list, (
                         '/etc/xdg/checkbox.conf',
                         os.path.expanduser('~/.config/checkbox.conf')))))
+
+def load_configs(launcher_file=None):
+    if not launcher_file:
+        # launcher not supplied from cli - using the default one
+        launcher = DefaultLauncherDefinition()
+        configs = [
+            '/etc/xdg/{}'.format(launcher.config_filename),
+            os.path.expanduser(
+                '~/.config/{}'.format(launcher.config_filename))]
+    else:
+        configs = [launcher_file]
+        try:
+            with open(launcher_file, 'rt', encoding='UTF-8') as stream:
+                first_line = stream.readline()
+                if not first_line.startswith("#!"):
+                    stream.seek(0)
+                text = stream.read()
+        except IOError as exc:
+            _logger.error(_("Unable to load launcher definition: %s"), exc)
+            raise SystemExit(1)
+        generic_launcher = LauncherDefinition()
+        generic_launcher.read_string(text)
+        config_filename = os.path.expandvars(
+            generic_launcher.config_filename)
+        # if wrapper specifies just the basename
+        if not os.path.split(config_filename)[0]:
+            if "SNAP_DATA" in os.environ:
+                configs = [launcher_file]
+                configs.append(os.path.join(
+                    os.path.expandvars('$SNAP_DATA'), config_filename))
+            else:
+                configs += [
+                    '/etc/xdg/{}'.format(config_filename),
+                    os.path.expanduser('~/.config/{}'.format(
+                        config_filename))]
+        # if wrapper specifies an absolute file
+        else:
+            configs.append(config_filename)
+        launcher = generic_launcher.get_concrete_launcher()
+    launcher.read(configs)
+    if launcher.problem_list:
+        _logger.error(_("Unable to start launcher because of errors:"))
+        for problem in launcher.problem_list:
+            _logger.error("%s", str(problem))
+        raise SystemExit(1)
+    return launcher

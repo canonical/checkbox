9d8cfdcc096b71534aeb1a8ab77205ca0c48d05a
PMR <pmr@pmr-lander>
2020-02-19 07:56:15 +0000

    increment version to v1.8.0.dev0

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -54,7 +54,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="1.7.0",
+    version="1.8.0.dev0",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

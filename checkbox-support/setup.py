f4b784288ef9bd8944e67b4b3d54b959c0404c7a
Jonathan Cave <jonathan.cave@canonical.com>
2020-04-20 16:32:45 +0100

    setup.py: relax distro package version requirement

--- checkbox-support/setup.py
+++ checkbox-support/setup.py
@@ -51,7 +51,7 @@ setup(
     install_requires=[
         'pyparsing >= 2.0.0',
         'requests >= 1.0',
-        'distro >= 1.3.0'
+        'distro >= 1.0'
     ] + (['configparser'] if sys.version_info.major == 2 else []) + (
         ['requests_unixsocket >= 0.1.2']
         if sys.version_info >= (3, 5) else []),

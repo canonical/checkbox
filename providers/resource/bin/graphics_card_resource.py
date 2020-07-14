9d6ace749ad10054a3dd715a1619ae8e7f0ce541
Sylvain Pineau <sylvain.pineau@canonical.com>
2020-07-14 14:57:33 +0200

    bin:graphics_card_resource -> graphics_card_resource.py

--- providers/resource/bin/graphics_card_resource.py
+++ providers/resource/bin/graphics_card_resource.py
@@ -101,7 +101,7 @@ def bus_ordering(record):
 
 def main():
     """
-    graphics_card_resource was done as a script to be able to reuse it in
+    graphics_card_resource.py was done as a script to be able to reuse it in
     graphics tests that need to be generated per-card.
     It does two things in addition to what filtering the resources by
     category=VIDEO would achieve:

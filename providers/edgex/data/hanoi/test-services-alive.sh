30de1913e09638bf2b6dc6d3d5bf0accdb12ea8e
Tony Espy <espy@canonical.com>
2020-08-24 11:45:06 -0400

    Update tests for EdgeX Hanoi release
    This commit introduces new versions of the test cases
    for the current development release of EdgeX (Hanoi).
    Also as mongo was deprecated and removed from the snap
    for Hanoi, both dbtype tests were removed.

---
+++ providers/edgex/data/hanoi/test-services-alive.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+# get the directory of this script
+# snippet from https://stackoverflow.com/a/246128/10102404
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# load the utils
+# shellcheck source=/dev/null
+source "$SCRIPT_DIR/utils.sh"
+
+DEFAULT_TEST_CHANNEL=${DEFAULT_TEST_CHANNEL:-beta}
+
+snap_remove
+
+# install the snap to make sure it installs
+if [ -n "$REVISION_TO_TEST" ]; then
+    snap_install "$REVISION_TO_TEST" "$REVISION_TO_TEST_CHANNEL" "$REVISION_TO_TEST_CONFINEMENT"
+else
+    snap_install edgexfoundry "$DEFAULT_TEST_CHANNEL"  
+fi
+
+# wait for services to come online
+# NOTE: this may have to be significantly increased on arm64 or low RAM platforms
+# to accomodate time for everything to come online
+sleep 120
+
+# check services
+snap_check_hanoi_svcs
+
+# remove the snap to run the next test
+snap_remove

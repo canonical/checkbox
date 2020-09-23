de639176d509854ecc086d75850e725c940dff82
Jonathan Cave <jonathan.cave@canonical.com>
2020-09-23 17:19:26 +0100

    bootloader_resource: update paths for UC20
    Some updates to identifying the active bootloader for UC20.
    Handle kernel path identifciation for grub.

--- providers/resource/bin/bootloader_resource.py
+++ providers/resource/bin/bootloader_resource.py
@@ -15,67 +15,82 @@
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
+import contextlib
 import os
 import re
 
 from checkbox_support.parsers.kernel_cmdline import parse_kernel_cmdline
-from checkbox_support.snap_utils.system import (
-    get_lk_bootimg_path, add_hostfs_prefix)
+from checkbox_support.snap_utils.system import get_lk_bootimg_path
+from checkbox_support.snap_utils.system import add_hostfs_prefix
+from checkbox_support.snap_utils.system import get_series
+
 
 # Supported bootloaders and detected files taken from:
 # https://github.com/snapcore/snapd/blob/master/bootloader/bootloader.go
 # name = [root dir, config file]
-bootloaders = {
-    'uboot': ['/boot/uboot', 'uboot.env'],
-    'grub': ['/boot/grub', 'grub.cfg'],
-    'androidboot': ['/boot/androidboot', 'androidboot.env'],
-    'lk': ['/dev/disk/by-partlabel', 'snapbootsel']
-}
+if int(get_series()) >= 20:
+    bootloaders = {
+        'uboot': ['/boot/uboot', 'boot.sel'],
+        'grub': ['/boot/grub', 'grub.cfg'],
+        'androidboot': ['/boot/androidboot', 'androidboot.env'],
+        'lk': ['/dev/disk/by-partlabel', 'snapbootsel']
+    }
+else:
+    bootloaders = {
+        'uboot': ['/boot/uboot', 'uboot.env'],
+        'grub': ['/boot/grub', 'grub.cfg'],
+        'androidboot': ['/boot/androidboot', 'androidboot.env'],
+        'lk': ['/dev/disk/by-partlabel', 'snapbootsel']
+    }
 
 
 def detect_bootloader():
     for name, config in bootloaders.items():
-        if os.path.exists(
-                os.path.join(add_hostfs_prefix(config[0]), config[1])):
+        path = os.path.join(add_hostfs_prefix(config[0]), config[1])
+        if os.path.exists(path):
             return name
     return 'unknown'
 
 
 def booted_kernel_location(bl_name):
-    path = 'unknown'
-    type = 'unknown'
-    if bl_name == 'uboot':
-        pass
-    elif bl_name == 'grub':
-        # what about force-kernel-extract true/false ?
-        type = 'fs'
+    """Get kernel location returning (path, type) tuple.
+
+    Path: either file path or partition name (see type)
+    Type: how the kernel is stored on the storage medium. Either: 'fs' for file
+      on a regular filesystem, or 'raw' for directly at a partition location
+
+    If either is not indentifiable currently use value 'unknown'.
+    """
+    if bl_name == 'grub':
+        if int(get_series()) >= 20:
+            symlink = '/boot/grub/kernel.efi'
+            prefixed = add_hostfs_prefix(symlink)
+            path = os.path.realpath(prefixed)
+            if symlink != prefixed:
+                path = path[len(prefixed.rsplit(symlink, 1)[0]):]
+            return (path, 'fs')
+
+        # The BOOT_IMAGE kernmel cmdline parameter is tricky to decipher. It
+        # can be an absolute path or refer or contain a variable expanded by
+        # ... initramfs? Tested on fortknox, vasteras projects.
         with open('/proc/cmdline', 'r') as f:
             cmdline = f.readline()
         result = parse_kernel_cmdline(cmdline)
-        grub_path = result.params['BOOT_IMAGE']
-
-        # The BOOT_IMAGE parameter is tricky to decipher. It can be absolute
-        # path or refer or contain a variable expanded by ... initramfs?
-        if os.path.isabs(grub_path):
-            path = grub_path
-        else:
-            try:
-                # if we know where the parameter is referencing, replace it
-                if '(loop)' in grub_path:
-                    # implies of out of the kernel snap (so no FDE)
-                    pass
-                elif re.search(r'\(hd\d,gpt\d\)', grub_path, re.M):
-                    # used by fortknox, vasteras,...
+        grub_path = result.params.get('BOOT_IMAGE')
+        if grub_path:
+            if os.path.isabs(grub_path):
+                return(grub_path, 'fs')
+            with contextlib.suppress(ValueError):
+                # if we know where the parameter is referencing,
+                # replace it
+                if re.search(r'\(hd\d,gpt\d\)', grub_path, re.M):
                     path = os.path.join(
                         '/boot/efi', grub_path[grub_path.index(')')+2:])
-            except ValueError:
-                pass
-    elif bl_name == 'androidboot':
-        pass
+                    return(path, 'fs')
+        return ('unknown', 'fs')
     elif bl_name == 'lk':
-        type = 'raw'
-        path = get_lk_bootimg_path()
-    return (path, type)
+        return(get_lk_bootimg_path(), 'raw')
+    return ('unknown', 'unknown')  # bl_name in ('uboot', 'androidboot')
 
 
 if __name__ == "__main__":

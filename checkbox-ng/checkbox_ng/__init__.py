f5b6eb62ab2a8c1e300d319f76b9ba21096bb9c6
PMR <pmr@pmr-lander>
2020-09-24 13:23:31 +0000

    increment version to v1.11.0.dev0

--- checkbox-ng/checkbox_ng/__init__.py
+++ checkbox-ng/checkbox_ng/__init__.py
@@ -24,4 +24,4 @@
 CheckBoxNG is a new version of CheckBox built on top of PlainBox
 """
 
-__version__ = '1.10.0'
+__version__ = '1.11.0.dev0'
--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -53,7 +53,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="1.10.0",
+    version="1.11.0.dev0",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

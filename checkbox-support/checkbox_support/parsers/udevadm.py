93a3eaac89986f7d42a4585c28ff3dff27af3ea8
Sylvain Pineau <sylvain.pineau@canonical.com>
2020-09-28 09:12:34 +0200

    parsers:udev: Consider all /dev/dri/card* as valid VIDEO devices
    On arm devices, identifying VIDEO controllers can't be done from usb/pci
    modaliases or classes. And most of the time none is reported.
    
    Though, when the kernel creates a /dev/dri/card, it means a video controller
    is enabled and can then be categorized as a VIDEO device.
    
    To avoid creating both a VIDEO and a DRI device, the following rules are applied:
    
    1. When there's a least one VIDEO category found by the udev parser, just ignore
    all DRI devices and remove them from the list of known devices.
    
    2. If no VIDEO devices are found but some DRI are, then re-categorize them
    as VIDEO. It will allow existing graphic jobs in the checkbox provider to
    work properly and jobs using templates to be generated.
    
    Note, as a product name the ID_FOR_SEAT property is used.

--- checkbox-support/checkbox_support/parsers/udevadm.py
+++ checkbox-support/checkbox_support/parsers/udevadm.py
@@ -134,6 +134,7 @@ class UdevadmDevice(object):
         "_bits",
         "_stack",
         "_bus",
+        "_category",
         "_interface",
         "_mac",
         "_product",
@@ -155,6 +156,7 @@ class UdevadmDevice(object):
         self._bits = bits
         self._stack = stack
         self._bus = None
+        self._category = None
         self._interface = None
         self._mac = None
         self._product = None
@@ -234,6 +236,8 @@ class UdevadmDevice(object):
 
     @property
     def category(self):
+        if self._category is not None:
+            return self._category
         if "IFINDEX" in self._environment:
             if "DEVTYPE" in self._environment:
                 devtype = self._environment["DEVTYPE"]
@@ -560,6 +564,9 @@ class UdevadmDevice(object):
                 if self._environment["RFKILL_NAME"].startswith('hci'):
                     return 'BLUETOOTH'
 
+        if "/dev/dri/card" in self._environment.get("DEVNAME", ""):
+            return "DRI"
+
         # Any devices that have a product name and proper vendor and product
         # IDs, but had no other category, are lumped together in OTHER.
         # A few devices may have no self.product but carry PRODUCT data in
@@ -574,6 +581,10 @@ class UdevadmDevice(object):
         # up to downstream users of this class to decide what to do with
         # those devices.
         return None
+
+    @category.setter
+    def category(self, value):
+        self._category = value
 
     @property
     def major(self):
@@ -910,6 +921,10 @@ class UdevadmDevice(object):
                     return decode_id(self._environment["ID_MODEL_ENC"])
             if "/dev/mapper" in self._environment["DEVLINKS"]:
                 return self.name
+
+        if "/dev/dri/card" in self._environment.get("DEVNAME", ""):
+            if "ID_FOR_SEAT" in self._environment:
+                return self._environment["ID_FOR_SEAT"]
 
         return None
 
@@ -1276,6 +1291,18 @@ class UdevadmParser(object):
                 if d._stack:
                     parent = d._stack[-1]
                     HID_devices_path_list.append(parent._raw_path)
+
+        video_devices = list(self.devices.values())
+        for device in video_devices:
+            if device.category == "VIDEO":
+                for d in video_devices:
+                    if d.category == "DRI":
+                        self.devices.pop(d._raw_path, None)
+                break
+        else:
+            for d in self.devices.values():
+                if d.category == "DRI":
+                    d.category = "VIDEO"
 
         for device in list(self.devices.values()):
             if device.category == 'HIDRAW' and device._stack:

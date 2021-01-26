45f553e1affc9f54d9250cfe97bb9af725fccaab
PMR <pmr@pmr-lander>
2021-01-26 08:32:30 +0000

    Bump to v0.3.0

--- providers/gpgpu/manage.py
+++ providers/gpgpu/manage.py
@@ -79,7 +79,7 @@ class InstallCommandExt(InstallCommand):
 setup(
     name='checkbox-provider-gpgpu',
     namespace='com.canonical.certification',
-    version="0.3.0rc1",
+    version="0.3.0",
     description=N_("Checkbox Provider for GPGPU Testing"),
     gettext_domain='checkbox-provider-gpgpu',
 )

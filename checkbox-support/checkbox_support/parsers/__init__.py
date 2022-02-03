0f94bcd90d1201b1107f6b25efdc605c25a23d87
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2022-02-03 21:12:34 +0000

    Remove: old versioning scheme

--- checkbox-support/checkbox_support/parsers/__init__.py
+++ checkbox-support/checkbox_support/parsers/__init__.py
@@ -39,13 +39,6 @@ from checkbox_support.parsers import pactl
 from checkbox_support.parsers import pci_config
 from checkbox_support.parsers import udevadm
 
-# The version is an incrementing number, if the version changes it means
-# one of the parser components has changed and may produce different
-# results for a given parsable input.
-# This should be updated manually when a parser component changes, note
-# that this includes changes to any module under checkbox.parsers.
-__version__ = '3'
-
 AVAILABLE_PARSERS = {
     'bto': image_info.parse_bto_attachment_output,
     'buildstamp': image_info.parse_buildstamp_attachment_output,

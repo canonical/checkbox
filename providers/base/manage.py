5babfe40d525d6a785430c274b1d74699a2ecec6
Jenkins <jenkins@cert-jenkins-master-201506-18558.maas>
2022-04-15 09:06:09 +0000

    Bump to v0.64.0

--- providers/base/manage.py
+++ providers/base/manage.py
@@ -5,7 +5,7 @@ from plainbox.provider_manager import N_
 setup(
     name='plainbox-provider-checkbox',
     namespace='com.canonical.certification',
-    version="0.64.0rc1",
+    version="0.64.0",
     description=N_("Checkbox provider"),
     gettext_domain='plainbox-provider-checkbox',
     strict=False, deprecated=False,

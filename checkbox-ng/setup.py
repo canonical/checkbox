597b27620134bd096a7154efc0d3b80390bf0b4d
Jenkins <jenkins@cert-jenkins-master-201506-18558.maas>
2022-04-15 09:06:08 +0000

    Bump to v1.17.0

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -53,7 +53,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="1.17.0rc2",
+    version="1.17.0",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

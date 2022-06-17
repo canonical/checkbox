74c0f97596e1c7f6013b80c2701cecd3c56b7589
Bin Li <bin.li@canonical.com>
2022-06-17 16:37:30 +0800

    Using ignore to fix the paring error. (LP: #1979036)

--- providers/base/bin/network_device_info.py
+++ providers/base/bin/network_device_info.py
@@ -343,7 +343,7 @@ class UdevDevices():
     def _collect_devices(self):
         cmd = ['udevadm', 'info', '--export-db']
         try:
-            output = check_output(cmd).decode(sys.stdout.encoding)
+            output = check_output(cmd).decode(sys.stdout.encoding, errors='ignore')
         except CalledProcessError as err:
             sys.stderr.write(err)
             return

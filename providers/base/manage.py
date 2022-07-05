5b56e6f0430710a3290a51103ebfd8d1df67b4b5
PMR <pmr@pmr-lander>
2022-07-05 07:31:22 +0000

    Bump to v0.65.0

--- providers/base/manage.py
+++ providers/base/manage.py
@@ -5,7 +5,7 @@ from plainbox.provider_manager import N_
 setup(
     name='plainbox-provider-checkbox',
     namespace='com.canonical.certification',
-    version="0.65.0rc3",
+    version="0.65.0",
     description=N_("Checkbox provider"),
     gettext_domain='plainbox-provider-checkbox',
     strict=False, deprecated=False,

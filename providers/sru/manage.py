d7f63eac444019c29561bb03c86d9aa8d62882ad
PMR <pmr@pmr-lander>
2022-07-05 07:31:23 +0000

    Bump to v1.23.0

--- providers/sru/manage.py
+++ providers/sru/manage.py
@@ -16,7 +16,7 @@ from plainbox.provider_manager import setup, N_
 setup(
     name='plainbox-provider-sru',
     namespace='com.canonical.certification',
-    version="1.23.0rc1",
+    version="1.23.0",
     description=N_("The SRU provider"),
     gettext_domain="plainbox-provider-sru",
 )

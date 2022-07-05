7868260c9b022562d4180836a008be9cf2ab3787
PMR <pmr@pmr-lander>
2022-07-05 07:31:21 +0000

    Bump to v1.18.0

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -53,7 +53,7 @@ else:
 
 setup(
     name="checkbox-ng",
-    version="1.18.0rc1",
+    version="1.18.0",
     url="https://launchpad.net/checkbox-ng/",
     packages=find_packages(),
     author="Zygmunt Krynicki",

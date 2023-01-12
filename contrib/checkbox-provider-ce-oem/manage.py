cbbb8ebeaa7294604a557ca40eca99e3b54061b0
rickwu666666 <98441647+rickwu666666@users.noreply.github.com>
2023-01-12 09:47:21 +0800

    Change name space and README.md (#9)
    * Change: name space
    
    * Change: README.md
    
    * Fix: wording of checkbox interface snap

--- contrib/checkbox-provider-ce-oem/manage.py
+++ contrib/checkbox-provider-ce-oem/manage.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 # Copyright 2022 Canonical Ltd.
-# All rights reserved.
-
-"""Management script for the arm provider."""
 
 from plainbox.provider_manager import setup
 from plainbox.provider_manager import N_
 
 setup(
-    name='checkbox-provider-arm',
-    namespace='com.canonical.qa.arm',
+    name='checkbox-provider-ce-oem',
+    namespace='com.canonical.qa.ceoem',
     version="0.1",
-    description=N_("Checkbox Provider for ARM platforms"),
-    gettext_domain='checkbox-provider-arm',
+    description=N_("Checkbox provider for both IoT and PC devices"),
+    gettext_domain='checkbox-provider-ce-oem',
 )

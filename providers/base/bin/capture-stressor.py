b9c0e69306f8d31b60584a4c5ec421f6de25c56c
LiaoU3 <58060146+LiaoU3@users.noreply.github.com>
2023-02-20 15:10:14 +0800

    Separate stress-ng tests from class-based to stressor-based (#323)
    * Add capture-stressor.py to bin/ to get stressor name
    
    * Modified 'stress-ng-classes' into 'stress-ng-stressors', so we could get the list of stressors
    
    * Modified 'stress-ng-test-for-stressor-{stress-ng-classes}' into 'stress-ng-test-for-stressor-{stressor}' to create jobs for each stressor
    
    * Modified jobs name in 'stress-ng-automated' to fit the jobs in 'stress-ng.pxu'

---
+++ providers/base/bin/capture-stressor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import subprocess
+
+classes_list = ['cpu', 'cpu-cache', 'memory', 'os', 'pipe', 'scheduler', 'vm']
+
+stressors_list = []
+
+for stress_ng_class in classes_list:
+    cmd_get_stressors = ['stress-ng', '--class', '{}?'.format(stress_ng_class)]
+    try:
+        output = subprocess.run(cmd_get_stressors, capture_output=True,
+                                universal_newlines=True, check=True).stdout
+    except Exception as err:
+        raise SystemExit(err)
+
+    class_stressor_list = output.split(': ')[1].split()
+
+    for class_stressor in class_stressor_list:
+        if class_stressor in stressors_list:
+            continue
+        stressors_list.append(class_stressor)
+
+stressors_list.sort()
+for stressor in stressors_list:
+    print('stressor: {}'.format(stressor))
+    print()

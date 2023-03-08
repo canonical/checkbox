bd0c8269fffbaac6627c1199de820eea06a4e237
Pierre Equoy <pierre.equoy@canonical.com>
2023-03-08 16:34:40 +0800

    Create dedicated Checkbox source configs per mode and Ubuntu release

---
+++ metabox/configs/remote-source-18.04.py
@@ -0,0 +1,12 @@
+configuration = {
+    'remote': {
+        # Metabox can run tests from a local directory containing a copy of
+        # the Checkbox source code repository.
+        'origin': 'source',
+        'releases': ['bionic'],
+    },
+    'service': {
+        'origin': 'source',
+        'releases': ['bionic'],
+    },
+}
---
+++ metabox/configs/remote-source-20.04.py
@@ -0,0 +1,12 @@
+configuration = {
+    'remote': {
+        # Metabox can run tests from a local directory containing a copy of
+        # the Checkbox source code repository.
+        'origin': 'source',
+        'releases': ['focal'],
+    },
+    'service': {
+        'origin': 'source',
+        'releases': ['focal'],
+    },
+}

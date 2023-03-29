6b23a03f482cb7b9b6fe34afc727ac8422264d13
Ivan Hu <ivan.hu@canonical.com>
2023-03-29 14:11:28 -0600

    add ACPI OEM _OSI test
    OEM _OSI strings were for workarounds in 2016 and 2017. They shouldn't be
    used in BIOS/firmware anymore.
    
    Signed-off-by: Alex Hung <alex.hung@canonical.com>
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

---
+++ providers/base/bin/acpi_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+OEM_OSI_LIST=( Linux-Dell-Video Linux-Lenovo-NV-HDMI-Audio Linux-HPI-Hybrid-Graphics )
+for osi in "${OEM_OSI_LIST[@]}"
+do
+	grep -q -r $osi /sys/firmware/acpi/* && exit 1
+done
+
+exit 0

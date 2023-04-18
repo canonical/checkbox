977a25b6938f340fbb964e931f4b5a559bcf5729
Ivan Hu <ivan.hu@canonical.com>
2023-04-18 14:35:25 +0800

    add description for fail explaination
    Signed-off-by: Ivan Hu <ivan.hu@canonical.com>

--- providers/base/bin/acpi_tests.sh
+++ providers/base/bin/acpi_tests.sh
@@ -3,7 +3,9 @@
 OEM_OSI_LIST=( Linux-Dell-Video Linux-Lenovo-NV-HDMI-Audio Linux-HPI-Hybrid-Graphics )
 for osi in "${OEM_OSI_LIST[@]}"
 do
-	grep -q -r "$osi" /sys/firmware/acpi/* && exit 1
+	grep -q -r "$osi" /sys/firmware/acpi/* && \
+	echo "Found ACPI OEM _OSI strings are used as workaround, they shouldn't be present in current firmware implementation." && \
+	exit 1
 done
 
 exit 0

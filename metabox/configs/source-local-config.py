3c9ed34632a4a5a1e728d6a2765be63a424ac660
Sylvain Pineau <sylvain.pineau@canonical.com>
2023-05-11 09:33:31 +0200

    Remove the hardcoded uri in source*config.py files

--- metabox/configs/source-local-config.py
+++ metabox/configs/source-local-config.py
@@ -3,8 +3,6 @@ configuration = {
         # Metabox can run tests from a local directory containing a copy of
         # the Checkbox source code repository.
         'origin': 'source',
-        # Path to the Checkbox source code repository
-        'uri': '~/dev/work/checkbox',
         'releases': ['focal'],
     },
 }
--- metabox/configs/source-remote-config.py
+++ metabox/configs/source-remote-config.py
@@ -1,12 +1,10 @@
 configuration = {
     'remote': {
         'origin': 'source',
-        'uri': '~/dev/work/checkbox',
         'releases': ['focal'],
     },
     'service': {
         'origin': 'source',
-        'uri': '~/dev/work/checkbox',
         'releases': ['focal'],
     },
 }

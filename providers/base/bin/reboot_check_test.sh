2348c4b3b9ab5d0c155c20651f910361f0f42bba
isaacyang <isaac.yang@canonical.com>
2023-05-19 11:44:14 +0800

    Modify:reboot_check_test.sh support FWTS

--- providers/base/bin/reboot_check_test.sh
+++ providers/base/bin/reboot_check_test.sh
@@ -2,6 +2,36 @@
 
 device_check="true"
 service_check="true"
+fwts_supported="false"
+fwts_check="true"
+
+
+fwts_support_check(){
+    echo -e "\n## Check if support fwts.."
+    if  type -p fwts > /dev/null ; then
+        echo "FWTS supported"
+        fwts_supported="true"
+    fi
+}
+
+run_fwts(){
+    echo -e "\n## Run fwts of test: $*"
+    fwts -r "$output_dir"/fwts_klog_oops.log "$@"
+}
+
+fwts_log_check(){
+    echo -e "\n## Check fwts log failures.."
+    fwts_log_path="$output_dir"/fwts_klog_oops.log
+    if [[ -f "$fwts_log_path" ]]; then
+        sleep_test_log_check.py -v --ignore-warning -t all "$output_dir"/fwts_klog_oops.log
+        return_code=$?
+        if [[ $return_code != 0 ]]; then
+            fwts_check="false"
+        fi
+    else
+        fwts_check="false"
+    fi
+}
 
 service_check() {
     echo -e "\n## Check system service.."
@@ -45,6 +75,11 @@ compare() {
 main() {
     if  [[ -n "$output_dir" ]]; then
         dump
+        fwts_support_check
+        if "$fwts_supported"; then
+            run_fwts "klog" "oops"
+            fwts_log_check
+        fi
     fi
 
     if [[ -n "$compare_dir" ]]; then
@@ -61,7 +96,8 @@ main() {
     fi
 
     if [[ -n "$service_opt" && "$service_check" == "false" ]] || \
-       [[ -n "$compare_dir" && "$device_check" == "false" ]]; then
+       [[ -n "$compare_dir" && "$device_check" == "false" ]] || \
+       [[ -n "$fwts_opt" && "$fwts_check" == "false" ]]; then
         exit 1
     fi
 }
@@ -74,16 +110,19 @@ help_function() {
     echo -e "\t-d    Output directory."
     echo -e "\t-c    The target directory for comparing device"
     echo -e "\t-s    Do service check"
+    echo -e "\t-f    Do fwts log failures check"
 }
 
 service_opt=""
+fwts_opt=""
 output_dir=""
 compare_dir=""
-while getopts "d:c:s" opt; do
+while getopts "d:c:sf" opt; do
     case "$opt" in
         d) output_dir="$OPTARG" ;;
         c) compare_dir="$OPTARG" ;;
         s) service_opt="True" ;;
+        f) fwts_opt="True" ;;
         ?) help_function ;;
     esac
 done

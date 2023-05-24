2672b6552cac729133a777678e50ff5b0599b03a
Hector CAO <hector.cao@canonical.com>
2023-05-24 20:38:04 +0200

    Show values of GROUP argument for list command help
    It can be helpful to have the list of all possible values for GROUP
    argument
    
    [Review#1] Fix regression on special value all-jobs
    [Review#2] all_units does not cover all objects types, switch to a simple constant list

--- checkbox-ng/checkbox_ng/launcher/subcommands.py
+++ checkbox-ng/checkbox_ng/launcher/subcommands.py
@@ -51,7 +51,6 @@ from plainbox.impl.session.storage import WellKnownDirsHelper
 from plainbox.impl.transport import TransportError
 from plainbox.impl.transport import get_all_transports
 from plainbox.impl.transport import SECURE_ID_PATTERN
-from plainbox.impl.unit import all_units
 
 from checkbox_ng.config import load_configs
 from checkbox_ng.launcher.stages import MainLoopStage, ReportsStage
@@ -798,9 +797,8 @@ class Run(MainLoopStage):
 
 class List():
     def register_arguments(self, parser):
-        all_units.load()
         parser.add_argument(
-            'GROUP', nargs='?', choices=all_units.get_all_names() + ['all-jobs'],
+            'GROUP', nargs='?', choices=Explorer.OBJECT_TYPES,
             help=_("list objects from the specified group"))
         parser.add_argument(
             '-a', '--attrs', default=False, action="store_true",

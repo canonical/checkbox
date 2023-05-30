b96393a6bcb4b59d5e3d5a49fcdb1501939d917f
Hook25 <massimiliano.girardi@canonical.com>
2023-05-30 16:18:37 +0200

    Update to check after starting all builds

--- tools/daily-builds/deb_daily_builds.py
+++ tools/daily-builds/deb_daily_builds.py
@@ -18,14 +18,19 @@
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import atexit
+import tempfile
 import subprocess
 
-CONFIG_PPA_PATH = "/tmp/ppa_tools_config.yaml"
-CONFIG_PPA_DEV_TOOLS = """{
-    'wait_max_age_hours' : 24,
+from functools import partial
+from contextlib import suppress
+
+CONFIG_PPA_DEV_TOOLS = """{{
+    'wait_max_age_hours' : 10,
     'exit_on_only_build_failure' : True,
-    'name' : 'checkbox'
-}
+    'wait_seconds' : 60,
+    'name' : '{}'
+}}
 """
 
 def run(*args, **kwargs):
@@ -38,6 +43,32 @@ def run(*args, **kwargs):
     except subprocess.CalledProcessError as e:
         print('{}\n{}'.format(e, e.output.decode()))
         raise SystemExit(1)
+
+def _del_file(path):
+    with suppress(FileNotFoundError):
+        os.remove(path)
+
+def check_build(name) -> bool:
+    """
+    Checks if a build was succesful, returns true if it was
+    """
+    handle, path = tempfile.mkstemp(text=True)
+    # try to remove the file before exit
+    atexit.register(partial(_del_file, path))
+    with os.fdopen(handle, "w") as f:
+        f.write(CONFIG_PPA_DEV_TOOLS.format(name))
+    with suppress(subprocess.CalledProcessError):
+        subprocess.check_call(
+            [
+                "/tmp/ppa-dev-tools/scripts/ppa",
+                "wait",
+                "ppa:checkbox-dev/ppa",
+                "-C",
+                path
+            ]
+        )
+        return True
+    return False
 
 def main():
     """Parse the checkbox monorepo to trigger deb daily builds in Launchpad."""
@@ -56,6 +87,7 @@ def main():
             if project_name.startswith('provider'):
                 project_name = "checkbox-"+project_name
             projects[project_name] = project_path
+    to_check = []
     # Find projects new commits from the last 24 hours
     for name, path in sorted(projects.items(), key=lambda i: i[1]):
         new_commits = int(run(
@@ -70,14 +102,17 @@ def main():
                 "--recipe {}".format(name+'-daily'),
                 shell=True, check=True).stdout.decode().rstrip()
             print(output)
-            # this assumes ppa-dev-tools was cloned in ~
-            with open(CONFIG_PPA_PATH, "w+") as f:
-                f.write(CONFIG_PPA_DEV_TOOLS)
-            output = run([
-                "/tmp/ppa-dev-tools/scripts/ppa",
-                "wait", "ppa:checkbox-dev/ppa", "-C", CONFIG_PPA_PATH
-            ], check=True)
-            print(output)
+            # We have started the build, store it here so it can
+            # be checked after.
+            to_check.append(name)
+
+    checked = [(name, check_build(name)) for name in to_check]
+    for name, ok in checked:
+        if not ok:
+            print("Failed to build:", name)
+    if any(not ok for (_,ok) in to_check):
+        raise SystemExit(1)
+
 
 if __name__ == "__main__":
     main()

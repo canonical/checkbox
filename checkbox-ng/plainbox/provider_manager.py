cdbceba14130b5c931c0d986b85ae9c27c5c62e1
Hook25 <massimiliano.girardi@canonical.com>
2023-05-31 10:50:27 +0200

    Dump PROVIDERPATH in warning when it is substituted to -d

--- checkbox-ng/plainbox/provider_manager.py
+++ checkbox-ng/plainbox/provider_manager.py
@@ -552,7 +552,10 @@ class DevelopCommand(ManageCommand):
     def invoked(self, ns):
         pp_env = os.getenv("PROVIDERPATH")
         if pp_env and not os.path.samefile(pp_env, ns.directory):
-            _logger.warning("$PROVIDERPATH is defined, ignoring -d/--directory and developing there")
+            _logger.warning(
+                "$PROVIDERPATH is defined, ignoring -d/--directory"
+                " and developing in: %s", pp_env
+            )
             ns.directory = pp_env
         pathname = os.path.join(
             ns.directory, "{}.provider".format(

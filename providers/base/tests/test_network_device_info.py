51ab38d8ca8fc486fe6139d7f608058f375a4ab2
diohe <dio.he@canonical.com>
2023-06-01 16:34:21 +0800

    remove old comments

--- providers/base/tests/test_network_device_info.py
+++ providers/base/tests/test_network_device_info.py
@@ -24,11 +24,9 @@ class GetIpv4AddressTests(unittest.TestCase):
     # There are 2 output we could get from fcntl.ioctl():
     # 1. No WiFi connected 
     #    (It pulls out bytes data directly from kernel, so if the given SIOCGIFADDR doesn't exist, it raise a OSError)
-    #    "None"
 
     # 2. Connected with WiFi
     #    It will pulls out a 256 bytes data into buffer, then we get to parse the IP address
-    #    "192.168.68.101"
     def test_get_ipv4_address_without_connection(self):
         test_input = b""
         mock_ioctl = Mock(return_value=test_input)
@@ -65,13 +63,10 @@ class GetIpv4AddressTests(unittest.TestCase):
 class GetIpv6AddressTests(unittest.TestCase):
     # There are 3 output we could get, but only 2 cases will happen due to the way we command it
     # 1. No WiFi connected 
-    #    ""
 
     # 2. Connected with WiFi
-    #    "2: wlo1    inet6 fe80::d9eb:3f93:c7b2:86ba/64 scope link noprefixroute \       valid_lft forever preferred_lft forever"
 
     # 3. Connected with WiFi, but we ask the wrong interface name (This would not happen in our case, because the name is given by NetworkManager)
-    #    "Device "wlan0" does not exist."
     def test_get_ipv6_address_without_connection(self):
         test_input = ""
         mock_check_output = Mock(return_value=test_input)
@@ -83,7 +78,7 @@ class GetIpv6AddressTests(unittest.TestCase):
     def test_get_ipv6_address_with_connection(self):
         test_input = "2: wlo1    inet6 fe80::d9eb:3f93:c7b2:86ba/64 scope link noprefixroute \       valid_lft forever preferred_lft forever"
         mock_check_output = Mock(return_value=test_input)
-        with patch("network_device_info.check_output", mock_check_output): # somehow it don't regonize check_output() if I don't put subprocess in front
+        with patch("network_device_info.check_output", mock_check_output):
             interface = "wlo1"
             addr = Utils.get_ipv6_address(interface)
             self.assertEqual(addr, "fe80::d9eb:3f93:c7b2:86ba/64")

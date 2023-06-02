4d9cc8c50e18c58db6343ede952b516bf625bb3f
Massimiliano <massimiliano.girardi@canonical.com>
2023-06-02 14:50:22 +0200

    Integrate ppa-dev-tools in deb-daily-builds (#512)
    * Integrate ppa-dev-tools in deb-daily-builds
    
    This integrates the ppa-dev-tool both in the job yaml and
    the python script
    
    * Update to check after starting all builds
    
    * Minor: fixed wrong var usage

--- tools/daily-builds/deb_daily_builds.py
+++ tools/daily-builds/deb_daily_builds.py
@@ -18,8 +18,20 @@
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import atexit
+import tempfile
 import subprocess
 
+from functools import partial
+from contextlib import suppress
+
+CONFIG_PPA_DEV_TOOLS = """{{
+    'wait_max_age_hours' : 10,
+    'exit_on_only_build_failure' : True,
+    'wait_seconds' : 60,
+    'name' : '{}'
+}}
+"""
 
 def run(*args, **kwargs):
     """wrapper for subprocess.run."""
@@ -32,6 +44,31 @@ def run(*args, **kwargs):
         print('{}\n{}'.format(e, e.output.decode()))
         raise SystemExit(1)
 
+def _del_file(path):
+    with suppress(FileNotFoundError):
+        os.remove(path)
+
+def check_build(name) -> bool:
+    """
+    Checks if a build was succesful, returns true if it was
+    """
+    handle, path = tempfile.mkstemp(text=True)
+    # try to remove the file before exit
+    atexit.register(partial(_del_file, path))
+    with os.fdopen(handle, "w") as f:
+        f.write(CONFIG_PPA_DEV_TOOLS.format(name))
+    with suppress(subprocess.CalledProcessError):
+        subprocess.check_call(
+            [
+                "/tmp/ppa-dev-tools/scripts/ppa",
+                "wait",
+                "ppa:checkbox-dev/ppa",
+                "-C",
+                path
+            ]
+        )
+        return True
+    return False
 
 def main():
     """Parse the checkbox monorepo to trigger deb daily builds in Launchpad."""
@@ -50,6 +87,7 @@ def main():
             if project_name.startswith('provider'):
                 project_name = "checkbox-"+project_name
             projects[project_name] = project_path
+    to_check = []
     # Find projects new commits from the last 24 hours
     for name, path in sorted(projects.items(), key=lambda i: i[1]):
         new_commits = int(run(
@@ -64,6 +102,16 @@ def main():
                 "--recipe {}".format(name+'-daily'),
                 shell=True, check=True).stdout.decode().rstrip()
             print(output)
+            # We have started the build, store it here so it can
+            # be checked after.
+            to_check.append(name)
+
+    checked = [(name, check_build(name)) for name in to_check]
+    for name, ok in checked:
+        if not ok:
+            print("Failed to build:", name)
+    if any(not ok for (_,ok) in checked):
+        raise SystemExit(1)
 
 
 if __name__ == "__main__":

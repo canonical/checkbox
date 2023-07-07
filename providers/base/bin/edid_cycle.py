7dda208945e6b73ee2c238acb9bf434525cd97ba
Paolo Gentili <paolo.gentili@canonical.com>
2023-07-07 12:22:34 +0200

    Change: match-dedicated function with doctests

--- providers/base/bin/edid_cycle.py
+++ providers/base/bin/edid_cycle.py
@@ -20,11 +20,69 @@ from checkbox_support.scripts.zapper_proxy import (             # noqa: E402
 PATTERN = r"^HDMI-1.*\n.*^\s+(\d+x\d+).*\*"
 
 
+def _match(output):
+    """
+    Match the given randr output with a pattern to grab
+    the current resolution on HDMI-1.
+
+    Both gnome-randr and xrandr highlight the currently
+    selected resolution for each monitor with a `*`.
+
+    xrandr
+    >>> _match("Screen 0: minimum 320 x 200, current 1920 x 1080,"
+    ... " maximum 16384 x 16384i\\n"
+    ... "DPI-1 connected primary 1920x1080+0+0 (normal left inverted"
+    ... "   1920x1080     60.00*+  59.97    59.96    59.93    40.00\\n"
+    ... "HDMI-1 connected primary 1920x1080+0+0 (normal left inverted"
+    ... " right x axis y axis) 309mm x 174mm\\n"
+    ... "   1920x1080     60.00+  59.97    59.96    59.93    40.00\\n"
+    ... "   1680x1050     59.95    59.88\\n"
+    ... "   1600x1024     60.17*\\n")
+    '1600x1024'
+
+    xrandr, no output on HDMI-1
+    >>> _match("Screen 0: minimum 320 x 200, current 1920 x 1080,"
+    ... " maximum 16384 x 16384i\\n"
+    ... "DPI-1 connected primary 1920x1080+0+0 (normal left inverted"
+    ... "   1920x1080     60.00*+  59.97    59.96    59.93    40.00\\n"
+    ... "HDMI-1 connected primary 1920x1080+0+0 (normal left inverted"
+    ... " right x axis y axis) 309mm x 174mm\\n"
+    ... "   1920x1080     60.00+  59.97    59.96    59.93    40.00\\n"
+    ... "   1680x1050     59.95    59.88\\n")
+
+    xrandr, no HDMI-1 available
+    >>> _match("Screen 0: minimum 320 x 200, current 1920 x 1080,"
+    ... " maximum 16384 x 16384i\\n"
+    ... "DPI-1 connected primary 1920x1080+0+0 (normal left inverted"
+    ... "   1920x1080     60.00*+  59.97    59.96    59.93    40.00\\n")
+
+    gnome-randr
+    >>> _match("supports-mirroring: true\\n"
+    ... "layout-mode: physical\\n"
+    ... "supports-changing-layout-mode: false\\n"
+    ... "global-scale-required: false\\n"
+    ... "legacy-ui-scaling-factor: 1\\n"
+    ... 'renderer: "native"\\n\\n'
+    ... "logical monitor 0:\\n"
+    ... "x: 0, y: 0, scale: 1, rotation: normal, primary: yes\\n"
+    ... "associated physical monitors:\\n"
+    ... "	HDMI-1 DEL DELL U2722DE 4M9KFH3\\n\\n"
+    ... "HDMI-1 DEL DELL U2722DE 4M9KFH3\\n"
+    ... "              2560x1440@59.951	2560x1440 	59.95+"
+    ... "[x1.00+, x2.00, x3.00]\\n"
+    ... "              2048x1080@23.997	2048x1080 	24.00*"
+    ... "[x1.00+, x2.00]\\n")
+    '2048x1080'
+    """
+    match = re.search(PATTERN, output, re.MULTILINE | re.DOTALL)
+    if match:
+        return match.group(1)
+    return None
+
+
 def check_resolution():
     """
     Check output resolution on HDMI using randr.
-    Both gnome-randr and xrandr highlight the currently
-    selected resolution for each monitor with a `*`.
     """
     if os.getenv('XDG_SESSION_TYPE') == 'wayland':
         cmd = "gnome-randr"
@@ -35,10 +93,7 @@ def check_resolution():
         [cmd],
         universal_newlines=True, encoding="utf-8")
 
-    match = re.search(PATTERN, randr_output, re.MULTILINE | re.DOTALL)
-    if match:
-        return match.group(1)
-    return None
+    return _match(randr_output)
 
 
 def change_edid(host, edid_file):

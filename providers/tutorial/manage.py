a533c578b09b1864709ee1a5c42a9590f8680e57
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2023-07-26 23:57:39 +0200

    add empty tutorial provider

---
+++ providers/tutorial/manage.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from plainbox.provider_manager import setup, N_
+
+setup(
+    name='com.canonical.certification:tutorial',
+    version="1.0",
+    description=N_("The Checkbox Tutorial provider"),
+    gettext_domain="com_canonical_certification_tutorial",
+)

bc4a4fcf6d24f3b451cb8ff604ff10ca9dec0a72
Hook25 <massimiliano.girardi@canonical.com>
2023-08-11 15:53:17 +0200

    Add metabox scenario to validate installation

---
+++ metabox/metabox/scenarios/installation/validation.py
@@ -0,0 +1,41 @@
+# This file is part of Checkbox.
+#
+# Copyright 2023 Canonical Ltd.
+# Written by:
+#   Massimiliano Girardi <massimiliano.girardi@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+import textwrap
+
+from metabox.core.actions import AssertPrinted, Start
+from metabox.core.scenario import Scenario
+
+
+class VerifyInstallation(Scenario):
+    """
+    This verifies that the checkbox installation installed
+    all that is needed
+    """
+
+    modes = ["local"]
+    launcher = textwrap.dedent(
+        """
+        [test plan]
+        unit = 2021.com.canonical.certification::installation_verify
+        forced = yes
+        [test selection]
+        forced = yes
+        """
+    )
+    steps = [Start(), AssertPrinted("Outcome: job passed")]

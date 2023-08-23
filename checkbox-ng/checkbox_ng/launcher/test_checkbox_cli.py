faf2a484ca6bbee8febcde8d966460b4319eba7f
Hook25 <massimiliano.girardi@canonical.com>
2023-08-23 13:45:59 +0200

    Add unit test to checkbox_cli

---
+++ checkbox-ng/checkbox_ng/launcher/test_checkbox_cli.py
@@ -0,0 +1,48 @@
+# This file is part of Checkbox.
+#
+# Copyright 2023 Canonical Ltd.
+# Written by:
+#   Massimiliano Girardi <massimiliano.girardi@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections import namedtuple
+from unittest import TestCase, mock
+
+from checkbox_ng.launcher.checkbox_cli import main
+
+
+class CheckboxCliTests(TestCase):
+    @mock.patch("sys.argv")
+    @mock.patch("argparse.ArgumentParser.parse_args")
+    @mock.patch("checkbox_ng.launcher.checkbox_cli.Launcher")
+    def test_launcher_ok(
+        self,
+        launcher_mock,
+        parse_args_mock,
+        sys_argv_mock,
+    ):
+        # Add here necessary fake args to provide
+        ns_parse_args_type = namedtuple(
+            "ParseArgsNamespace", ["subcommand", "debug", "verbose"]
+        )
+        parse_args_mock.return_value = ns_parse_args_type(
+            subcommand="launcher", debug=False, verbose=False
+        )
+        # for simplicitys sake, launcher_mock returns itself when constructed
+        launcher_mock.return_value = launcher_mock
+
+        main()
+
+        launcher_mock.assert_called_once()
+        launcher_mock.invoked.assert_called_once()

3386e6fb5cbfd949fdf8fb631635da6356b699bc
Pierre Equoy <pierre.equoy@canonical.com>
2023-08-30 17:39:40 +0800

    Split tutorial provider name and namespace

--- providers/tutorial/manage.py
+++ providers/tutorial/manage.py
@@ -2,7 +2,8 @@
 from plainbox.provider_manager import setup, N_
 
 setup(
-    name='com.canonical.certification:tutorial',
+    name="tutorial",
+    namespace="com.canonical.certification",
     version="1.0",
     description=N_("The Checkbox Tutorial provider"),
     gettext_domain="com_canonical_certification_tutorial",

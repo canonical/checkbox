56b3088505fb35c995e9461aaf5139098abe6854
Massimiliano <massimiliano.girardi@canonical.com>
2023-10-09 09:33:42 +0200

    Add launcher cli arg to check_config (New) (#757)
    * Add launcher cli arg to check_config
    
    * Updated the documentation
    
    * Fixed wrong import from unittest to unittest.mock
    
    * Update check_config test

--- checkbox-ng/checkbox_ng/launcher/test_check_config.py
+++ checkbox-ng/checkbox_ng/launcher/test_check_config.py
@@ -17,6 +17,7 @@
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
 from unittest import TestCase, mock
+from unittest.mock import MagicMock
 
 from checkbox_ng.launcher.check_config import CheckConfig
 from plainbox.impl.config import Configuration
@@ -26,10 +27,12 @@ class CheckConfigTests(TestCase):
     @mock.patch("builtins.print")
     @mock.patch("checkbox_ng.launcher.check_config.load_configs")
     def test_invoked_ok(self, mock_load_configs, mock_print):
+        args_mock = MagicMock()
+        args_mock.args.launcher = None
         # this is the default configuration
         mock_load_configs.return_value = Configuration()
 
-        ret_val = CheckConfig.invoked(...)
+        ret_val = CheckConfig.invoked(args_mock)
 
         mock_print.assert_any_call("Configuration files:")
         mock_print.assert_any_call("No problems with config(s) found!")
@@ -38,14 +41,23 @@ class CheckConfigTests(TestCase):
     @mock.patch("builtins.print")
     @mock.patch("checkbox_ng.launcher.check_config.load_configs")
     def test_invoked_has_problems(self, mock_load_configs, mock_print):
+        args_mock = MagicMock()
+        args_mock.args.launcher = None
         # this is the default configuration
         conf = Configuration()
         conf.notice_problem("Test problem")
         mock_load_configs.return_value = conf
 
-        ret_val = CheckConfig.invoked(...)
+        ret_val = CheckConfig.invoked(args_mock)
 
         mock_print.assert_any_call("Configuration files:")
         mock_print.assert_any_call("Problems:")
         mock_print.assert_any_call("- ", "Test problem")
         self.assertEqual(ret_val, 1)
+
+    def test_register_arguments(self):
+        self_mock = MagicMock()
+        parser_mock = MagicMock()
+        CheckConfig.register_arguments(self_mock, parser_mock)
+
+        self.assertTrue(parser_mock.add_argument.called)

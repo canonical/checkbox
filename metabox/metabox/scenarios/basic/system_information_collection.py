b0bc8070ef0eecb9d72266b31ecfc8fa59af9d2d
Hook25 <massimiliano.girardi@canonical.com>
2023-10-11 17:42:46 +0200

    Metabox tests to check system_information
    Minor: also port the fix (no reconnecting message) to inxi job

--- metabox/metabox/scenarios/basic/system_information_collection.py
+++ metabox/metabox/scenarios/basic/system_information_collection.py
@@ -2,8 +2,6 @@
 #
 # Copyright 2023 Canonical Ltd.
 # Written by:
-#   Maciej Kisielewski <maciej.kisielewski@canonical.com>
-#   Sylvain Pineau <sylvain.pineau@canonical.com>
 #   Massimiliano Girardi <massimiliano.girardi@canonical.com>
 #
 # Checkbox is free software: you can redistribute it and/or modify
@@ -24,52 +22,18 @@ from metabox.core.scenario import Scenario
 from metabox.core.utils import tag
 
 
-@tag("reboot")
-class Reboot(Scenario):
-    modes = ["remote"]
-    launcher = textwrap.dedent(
-        """
-        [launcher]
-        launcher_version = 1
-        stock_reports = text
-        [test plan]
-        unit = com.canonical.certification::power-automated
-        forced = yes
-        [test selection]
-        forced = yes
-        exclude = .*cold.*
-        [ui]
-        type = silent
-        """
-    )
-    steps = [
-        AssertPrinted("Connection lost!"),
-        AssertPrinted("job passed   : Warm reboot"),
-    ]
-
-
 @tag("system_information")
-class SystemInfoPersistency(Scenario):
-    """
-    System Information should be persistent across reboots
-    """
-
-    modes = ["remote"]
+class SystemInformationCollection(Scenario):
     launcher = textwrap.dedent(
         """
         [launcher]
         launcher_version = 1
         stock_reports = text
         [test plan]
-        unit = com.canonical.certification::power-automated
+        unit = 2021.com.canonical.certification::basic-automated-passing
         forced = yes
         [test selection]
         forced = yes
-        exclude = .*cold.*
-        [ui]
-        type = silent
-        # Use json exporter to dump the submission.json file to the screen
-        # so that we can read it with AssertPrinted
         [exporter:json]
         unit = com.canonical.plainbox::json
         [transport:out]
@@ -81,9 +45,8 @@ class SystemInfoPersistency(Scenario):
         forced = yes
         """
     )
+
     steps = [
-        AssertPrinted("Connection lost!"),
-        AssertPrinted("job passed   : Warm reboot"),
         AssertPrinted('"system_information"'),
         AssertPrinted('"inxi"'),
     ]

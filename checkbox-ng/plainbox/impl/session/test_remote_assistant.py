5624ad98e094a38bb3ed270cbacc08fb3b3309cc
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2023-10-20 14:43:41 +0200

    add unit tests

--- checkbox-ng/plainbox/impl/session/test_remote_assistant.py
+++ checkbox-ng/plainbox/impl/session/test_remote_assistant.py
@@ -18,6 +18,7 @@
 
 from unittest import TestCase, mock
 
+from plainbox.abc import IJobResult
 from plainbox.impl.session import remote_assistant
 
 
@@ -27,7 +28,8 @@ class RemoteAssistantTests(TestCase):
         allowed_when = remote_assistant.RemoteSessionAssistant.allowed_when
 
         @allowed_when(remote_assistant.Idle)
-        def allowed(self, *args): ...
+        def allowed(self, *args):
+            ...
 
         self_mock._state = remote_assistant.Idle
         allowed(self_mock)
@@ -37,8 +39,33 @@ class RemoteAssistantTests(TestCase):
         allowed_when = remote_assistant.RemoteSessionAssistant.allowed_when
 
         @allowed_when(remote_assistant.Idle)
-        def not_allowed(self, *args): ...
+        def not_allowed(self, *args):
+            ...
 
         self_mock._state = remote_assistant.Started
         with self.assertRaises(AssertionError):
             not_allowed(self_mock)
+
+
+class RemoteAssistantFinishJobTests(TestCase):
+    def setUp(self):
+        self.rsa = remote_assistant.RemoteSessionAssistant("")
+        self.rsa._sa = mock.Mock()
+        self.rsa._be = None
+
+    @mock.patch("plainbox.impl.session.remote_assistant.JobResultBuilder")
+    def test_no_result_after_auto_resume(self, MockJobResultBuilder):
+        self.rsa._currently_running_job = "job_id"
+        mock_job = mock.Mock()
+        mock_job.plugin = "shell"
+        mock_builder = MockJobResultBuilder.return_value
+        mock_builder.get_result.return_value = IJobResult.OUTCOME_PASS
+
+        result = self.rsa.finish_job()
+
+        self.rsa._sa.use_job_result.assert_called_with("job_id", "pass")
+        self.assertEqual(result, IJobResult.OUTCOME_PASS)
+        MockJobResultBuilder.assert_called_with(
+            outcome=IJobResult.OUTCOME_PASS,
+            comments="Automatically passed while resuming",
+        )

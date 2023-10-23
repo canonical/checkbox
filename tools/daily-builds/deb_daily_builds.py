19948927d94acbb27edb9e718fa06bfaa2baf7da
Hook25 <massimiliano.girardi@canonical.com>
2023-10-23 13:44:53 +0200

    black deb_daily_build

--- tools/daily-builds/deb_daily_builds.py
+++ tools/daily-builds/deb_daily_builds.py
@@ -34,20 +34,22 @@ CONFIG_PPA_DEV_TOOLS = """{{
 }}
 """
 
+
 def run(*args, **kwargs):
     """wrapper for subprocess.run."""
     try:
         return subprocess.run(
-            *args, **kwargs,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
+            *args, **kwargs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+        )
     except subprocess.CalledProcessError as e:
-        print('{}\n{}'.format(e, e.output.decode()))
+        print("{}\n{}".format(e, e.output.decode()))
         raise SystemExit(1)
+
 
 def _del_file(path):
     with suppress(FileNotFoundError):
         os.remove(path)
+
 
 def check_build(name) -> bool:
     """
@@ -65,44 +67,58 @@ def check_build(name) -> bool:
                 "wait",
                 "ppa:checkbox-dev/ppa",
                 "-C",
-                path
+                path,
             ]
         )
         return True
     return False
 
+
 def main():
     """Parse the checkbox monorepo to trigger deb daily builds in Launchpad."""
     # First request code import (GitHub -> Launchpad)
     run(
-       "./tools/release/lp-request-import.py "
-       "~checkbox-dev/checkbox/+git/checkbox",
-       shell=True, check=True)
+        "./tools/release/lp-request-import.py "
+        "~checkbox-dev/checkbox/+git/checkbox",
+        shell=True,
+        check=True,
+    )
     projects = {}
-    for path, dirs, files in os.walk('.'):
+    for path, dirs, files in os.walk("."):
         if "debian" in dirs:
             project_path = os.path.relpath(path)
             # Tweak the provider paths to get names in the following form:
             # providers/base -> checkbox-provider-base
-            project_name = project_path.replace('s/', '-')
-            if project_name.startswith('provider'):
-                project_name = "checkbox-"+project_name
+            project_name = project_path.replace("s/", "-")
+            if project_name.startswith("provider"):
+                project_name = "checkbox-" + project_name
             projects[project_name] = project_path
     to_check = []
     # Find projects new commits from the last 24 hours
     for name, path in sorted(projects.items(), key=lambda i: i[1]):
-        new_commits = int(run(
-            'git rev-list --count HEAD --not '
-            '$(git rev-list -n1 --before="24 hours" '
-            '--first-parent HEAD) -- :{}'.format(path),
-            shell=True, check=True).stdout.decode().rstrip())
+        new_commits = int(
+            run(
+                "git rev-list --count HEAD --not "
+                '$(git rev-list -n1 --before="24 hours" '
+                "--first-parent HEAD) -- :{}".format(path),
+                shell=True,
+                check=True,
+            )
+            .stdout.decode()
+            .rstrip()
+        )
         # Kick off daily builds if the new commits got merged into main
         if new_commits:
-            output = run(
-                "./tools/release/lp-recipe-update-build.py checkbox "
-                "--recipe {} -n {}".format(
-                    name+'-daily', get_version()),
-                shell=True, check=True).stdout.decode().rstrip()
+            output = (
+                run(
+                    "./tools/release/lp-recipe-update-build.py checkbox "
+                    "--recipe {} -n {}".format(name + "-daily", get_version()),
+                    shell=True,
+                    check=True,
+                )
+                .stdout.decode()
+                .rstrip()
+            )
             print(output)
             # We have started the build, store it here so it can
             # be checked after.
@@ -112,7 +128,7 @@ def main():
     for name, ok in checked:
         if not ok:
             print("Failed to build:", name)
-    if any(not ok for (_,ok) in checked):
+    if any(not ok for (_, ok) in checked):
         raise SystemExit(1)
 
 

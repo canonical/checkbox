d0510781b2f578ba425215fe38c5492471b3c684
Hook25 <massimiliano.girardi@canonical.com>
2023-11-22 10:23:55 +0100

    Test finalize_session

--- checkbox-ng/checkbox_ng/launcher/test_subcommands.py
+++ checkbox-ng/checkbox_ng/launcher/test_subcommands.py
@@ -399,6 +399,7 @@ class TestLListBootstrapped(TestCase):
         self.launcher.invoked(self.ctx)
         self.assertEqual(stdout.getvalue(), expected_out)
 
+
 class TestUtilsFunctions(TestCase):
     @patch("checkbox_ng.launcher.subcommands.Colorizer", new=MagicMock())
     @patch("builtins.print")

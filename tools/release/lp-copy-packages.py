b143159c72850a7786209746a9d0d78be189679f
Hook25 <massimiliano.girardi@canonical.com>
2023-11-30 10:26:40 +0100

    Add main to lp-copy-packages.py

--- tools/release/lp-copy-packages.py
+++ tools/release/lp-copy-packages.py
@@ -23,51 +23,56 @@ import os
 from launchpadlib.credentials import Credentials
 from launchpadlib.launchpad import Launchpad
 
-# Authenticate with Launchpad
-credentials = Credentials.from_string(os.getenv("LP_CREDENTIALS"))
-lp = Launchpad(
-    credentials, None, None, service_root="production", version="devel"
-)
 
-# Define the source and destination PPAs
-source_ppa_name = "beta"
-source_owner_name = "checkbox-dev"
-dest_ppa_name = "stable"
-dest_owner_name = "checkbox-dev"
-
-# Load the source and destination PPA owners
-source_owner = lp.people[source_owner_name]
-dest_owner = lp.people[dest_owner_name]
-
-# Get the source and destination PPAs
-source_ppa = source_owner.getPPAByName(name=source_ppa_name)
-dest_ppa = dest_owner.getPPAByName(name=dest_ppa_name)
-
-# Define the time period for package publication
-one_week_ago = datetime.datetime.utcnow().replace(
-    tzinfo=datetime.timezone.utc
-) - datetime.timedelta(weeks=4)
-
-# Get the packages in the source PPA that were published within the last week
-# and start with "checkbox"
-packages = [
-    p
-    for p in source_ppa.getPublishedSources(created_since_date=one_week_ago)
-    if p.source_package_name.startswith("checkbox")
-]
-
-# Copy each package from the source PPA to the destination PPA,
-# without rebuilding them
-for package in packages:
-    dest_ppa.copyPackage(
-        from_archive=source_ppa,
-        include_binaries=True,
-        to_pocket=package.pocket,
-        source_name=package.source_package_name,
-        version=package.source_package_version,
+def main():
+    # Authenticate with Launchpad
+    credentials = Credentials.from_string(os.getenv("LP_CREDENTIALS"))
+    lp = Launchpad(
+        credentials, None, None, service_root="production", version="devel"
     )
-    print(
-        f"Copied {package.source_package_name} "
-        f"version {package.source_package_version} "
-        f"from {source_ppa_name} to {dest_ppa_name} (without rebuilding)"
-    )
+
+    # Define the source and destination PPAs
+    source_ppa_name = "beta"
+    source_owner_name = "checkbox-dev"
+    dest_ppa_name = "stable"
+    dest_owner_name = "checkbox-dev"
+
+    # Load the source and destination PPA owners
+    source_owner = lp.people[source_owner_name]
+    dest_owner = lp.people[dest_owner_name]
+
+    # Get the source and destination PPAs
+    source_ppa = source_owner.getPPAByName(name=source_ppa_name)
+    dest_ppa = dest_owner.getPPAByName(name=dest_ppa_name)
+
+    # Define the time period for package publication
+    one_week_ago = datetime.datetime.utcnow().replace(
+        tzinfo=datetime.timezone.utc
+    ) - datetime.timedelta(weeks=4)
+
+    # Get the packages in the source PPA that were published within the last week
+    # and start with "checkbox"
+    packages = [
+        p
+        for p in source_ppa.getPublishedSources(created_since_date=one_week_ago)
+        if p.source_package_name.startswith("checkbox")
+    ]
+
+    # Copy each package from the source PPA to the destination PPA,
+    # without rebuilding them
+    for package in packages:
+        dest_ppa.copyPackage(
+            from_archive=source_ppa,
+            include_binaries=True,
+            to_pocket=package.pocket,
+            source_name=package.source_package_name,
+            version=package.source_package_version,
+        )
+        print(
+            f"Copied {package.source_package_name} "
+            f"version {package.source_package_version} "
+            f"from {source_ppa_name} to {dest_ppa_name} (without rebuilding)"
+        )
+
+if __name__ == "__main__":
+    main()

ff142aeb9a7dad5783690a4ca874d663259f915b
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2023-12-01 15:26:25 +0100

    fix restart logic requiring SA_RESTARTABLE

--- checkbox-ng/plainbox/impl/session/assistant.py
+++ checkbox-ng/plainbox/impl/session/assistant.py
@@ -201,18 +201,16 @@ class SessionAssistant:
             self.get_old_sessions: ("get previously created sessions"),
             self.delete_sessions: ("delete previously created sessions"),
             self.finalize_session: "to finalize session",
+            self.configure_application_restart: (
+                "configure automatic restart capability"
+            ),
+            self.use_alternate_restart_strategy: (
+                "configure automatic restart capability"
+            ),
         }
         # Restart support
         self._restart_cmd_callback = None
         self._restart_strategy = None  # None implies auto-detection
-        if SA_RESTARTABLE in self._flags:
-            allowed_calls = UsageExpectation.of(self).allowed_calls
-            allowed_calls[
-                self.configure_application_restart
-            ] = "configure automatic restart capability"
-            allowed_calls[
-                self.use_alternate_restart_strategy
-            ] = "configure automatic restart capability"
 
     @property
     def config(self):
@@ -519,15 +517,13 @@ class SessionAssistant:
             self.get_session_id: "to get the id of currently running session",
             self.hand_pick_jobs: "select jobs to run (w/o a test plan)",
             self.finalize_session: "to finalize session",
+            self.configure_application_restart: (
+                "configure automatic restart capability"
+            ),
+            self.use_alternate_restart_strategy: (
+                "configure automatic restart capability"
+            ),
         }
-        if SA_RESTARTABLE in self._flags:
-            allowed_calls = UsageExpectation.of(self).allowed_calls
-            allowed_calls[
-                self.configure_application_restart
-            ] = "configure automatic restart capability"
-            allowed_calls[
-                self.use_alternate_restart_strategy
-            ] = "configure automatic restart capability"
 
     @raises(KeyError, UnexpectedMethodCall)
     def resume_session(

d0c41eb5947499b0822995941b63e62bc4e0a7d4
Massimiliano <massimiliano.girardi@canonical.com>
2023-12-05 09:24:10 +0100

    New tool to update ppa recipes (infra) (#867)
    * Update launchpad recipe tool
    
    * Shard utils module
    
    * Move and integrate lp_copy_packages and utils
    
    * Type hints for lp_update_recipe
    
    * Updated docstring with the actual new purpose

--- tools/release/test_lp_copy_packages.py
+++ tools/release/test_lp_copy_packages.py
@@ -4,29 +4,6 @@ from unittest.mock import patch, MagicMock
 import lp_copy_packages
 
 
-class TestLpCopyPackages(unittest.TestCase):
-    @patch("lp_copy_packages.Credentials")
-    @patch("lp_copy_packages.Launchpad")
-    @patch("os.getenv")
-    def test_get_launchpad_client_ok(
-        self, getenv_mock, launchpad_mock, credentials_mock
-    ):
-        getenv_mock.return_value = "some credential"
-
-        lp_client = lp_copy_packages.get_launchpad_client()
-
-        # credentials are built from the env
-        self.assertTrue(credentials_mock.from_string.called)
-        self.assertEqual(lp_client, launchpad_mock())
-
-    @patch("os.getenv")
-    def test_get_launchpad_client_no_cred(self, getenv_mock):
-        getenv_mock.return_value = None
-
-        with self.assertRaises(SystemExit):
-            lp_copy_packages.get_launchpad_client()
-
-
 class TestMain(unittest.TestCase):
     @patch("lp_copy_packages.get_launchpad_client")
     def test_main(self, get_launchpad_client_mock):
---
+++ tools/release/test_lp_update_recipe.py
@@ -0,0 +1,53 @@
+import unittest
+
+from unittest.mock import MagicMock, patch
+
+import lp_update_recipe
+
+
+class TestUpdateBuildRecipe(unittest.TestCase):
+    def test_get_build_path_provider(self):
+        provider_recipe_name = "checkbox-provider-name-risk"
+        self.assertEqual(
+            lp_update_recipe.get_build_path(provider_recipe_name),
+            "providers/name",
+        )
+
+    def test_get_build_path_checkbox_ng(self):
+        checkbox_ng_recipe_name = "checkbox-ng-risk"
+        self.assertEqual(
+            lp_update_recipe.get_build_path(checkbox_ng_recipe_name),
+            "checkbox-ng",
+        )
+
+    def test_get_updated_build_recipe(self):
+        new_recipe = lp_update_recipe.get_updated_build_recipe(
+            "checkbox-provider-base-edge", "X.Y.Z~dev13", "aaabbb"
+        )
+        # part path is correctly calculated
+        self.assertIn("providers/base", new_recipe)
+        # version is included in the recipe
+        self.assertIn("X.Y.Z~dev13", new_recipe)
+        # revision is also included in the recipe
+        self.assertIn("aaabbb", new_recipe)
+
+    @patch("lp_update_recipe.get_build_recipe")
+    @patch("lp_update_recipe.print")
+    def test_main(self, print_mock, get_build_path_mock):
+        lp_recipe = get_build_path_mock()
+
+        lp_update_recipe.main(
+            [
+                "checkbox",
+                "--recipe",
+                "checkbox-support-edge",
+                "--new-version",
+                "X.Y.Z~dev2",
+                "--revision",
+                "abcd",
+            ]
+        )
+        self.assertIn("X.Y.Z~dev2", lp_recipe.recipe_text)
+        self.assertIn("checkbox-support", lp_recipe.recipe_text)
+        self.assertIn("abcd", lp_recipe.recipe_text)
+        self.assertTrue(lp_recipe.lp_save.called)

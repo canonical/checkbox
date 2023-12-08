23d6751cfb8a6ae19acd2708f478e0325a2d5268
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2023-12-08 15:21:47 +0100

    more agent unit tests

--- checkbox-ng/checkbox_ng/launcher/test_agent.py
+++ checkbox-ng/checkbox_ng/launcher/test_agent.py
@@ -26,16 +26,16 @@ from plainbox.impl.session.assistant import ResumeCandidate
 from plainbox.impl.session.state import SessionMetaData
 
 
+@mock.patch("checkbox_ng.launcher.agent._")
+@mock.patch("checkbox_ng.launcher.agent._logger")
+@mock.patch("checkbox_ng.launcher.agent.is_passwordless_sudo")
+@mock.patch("checkbox_ng.launcher.agent.SessionAssistantAgent")
+@mock.patch("checkbox_ng.launcher.agent.RemoteDebRestartStrategy")
+@mock.patch("checkbox_ng.launcher.agent.RemoteSessionAssistant")
+@mock.patch("checkbox_ng.launcher.agent.ThreadedServer")
+@mock.patch("os.geteuid")
+@mock.patch("os.getenv")
 class AgentTests(TestCase):
-    @mock.patch("checkbox_ng.launcher.agent._")
-    @mock.patch("checkbox_ng.launcher.agent._logger")
-    @mock.patch("checkbox_ng.launcher.agent.is_passwordless_sudo")
-    @mock.patch("checkbox_ng.launcher.agent.SessionAssistantAgent")
-    @mock.patch("checkbox_ng.launcher.agent.RemoteDebRestartStrategy")
-    @mock.patch("checkbox_ng.launcher.agent.RemoteSessionAssistant")
-    @mock.patch("checkbox_ng.launcher.agent.ThreadedServer")
-    @mock.patch("os.geteuid")
-    @mock.patch("os.getenv")
     # used to load an empty launcher with no error
     def test_invoked_ok(
         self,
@@ -66,6 +66,80 @@ class AgentTests(TestCase):
         server = threaded_server_mock.return_value
         # the server was started
         self.assertTrue(server.start.called)
+
+    # used to load an empty launcher with no error
+    def test_invoked_with_resume_warns(
+        self,
+        getenv_mock,
+        geteuid_mock,
+        threaded_server_mock,
+        remote_assistant_mock,
+        remote_strategy_mock,
+        session_assistant_mock,
+        pwdless_sudo_mock,
+        logger_mock,
+        gettext_mock,
+    ):
+        ctx_mock = mock.MagicMock()
+        ctx_mock.args.resume = True
+
+        self_mock = mock.MagicMock()
+
+        geteuid_mock.return_value = 0  # agent will not run as non-root
+        pwdless_sudo_mock.return_value = True  # agent needs pwd-less sudo
+        getenv_mock.return_value = None
+
+        with mock.patch("builtins.open"):
+            RemoteAgent.invoked(self_mock, ctx_mock)
+
+        # agent server was created
+        self.assertTrue(threaded_server_mock.called)
+        server = threaded_server_mock.return_value
+        # the server was started
+        self.assertTrue(server.start.called)
+        logger_mock.warning.assert_called()
+
+    def test_invoked_with_session(
+        self,
+        getenv_mock,
+        geteuid_mock,
+        threaded_server_mock,
+        remote_assistant_mock,
+        remote_strategy_mock,
+        sa_mock,
+        pwdless_sudo_mock,
+        logger_mock,
+        gettext_mock,
+    ):
+        ctx_mock = mock.MagicMock()
+        ctx_mock.args.resume = False
+
+        self_mock = mock.MagicMock()
+
+        geteuid_mock.return_value = 0
+
+        ctx_mock.sa.get_resumable_sessions.return_value = [
+            ResumeCandidate("an_id", SessionMetaData())
+        ]
+        with mock.patch(
+            "checkbox_ng.launcher.agent.is_the_session_noninteractive"
+        ) as itni:
+            itni.return_value = True
+
+            RemoteAgent.invoked(self_mock, ctx_mock)
+            remote_assistant_mock.return_value.resume_by_id.assert_called()
+
+        remote_assistant_mock.reset_mock()
+
+        with mock.patch(
+            "checkbox_ng.launcher.agent.is_the_session_noninteractive"
+        ) as itni:
+            itni.return_value = False
+
+            RemoteAgent.invoked(self_mock, ctx_mock)
+            self.assertFalse(
+                remote_assistant_mock.return_value.resume_by_id.called
+            )
 
 
 class IsTheSessionNonInteractiveTests(TestCase):

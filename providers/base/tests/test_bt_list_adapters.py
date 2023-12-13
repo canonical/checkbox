cb0658c74e826c2e4809386d1144ab76ea6df1f2
Pierre Equoy <pierre.equoy@canonical.com>
2023-12-13 18:37:38 +0800

    Add unit tests to cover the new main function

--- providers/base/tests/test_bt_list_adapters.py
+++ providers/base/tests/test_bt_list_adapters.py
@@ -1,6 +1,7 @@
 import unittest
 from unittest.mock import patch, mock_open
 from pathlib import Path
+from io import StringIO
 
 import bt_list_adapters
 
@@ -20,8 +21,8 @@ class BTTests(unittest.TestCase):
         self.assertFalse(bt_list_adapters.is_bluetooth_adapter(Path("test")))
 
     def test_bt_adapter_not_found(self):
-        with self.assertRaises(SystemExit):
-            bt_list_adapters.get_bluetooth_devices([])
+        list_bt_devices = bt_list_adapters.get_bluetooth_devices([])
+        self.assertEqual(list_bt_devices, [])
 
     @patch("bt_list_adapters.is_bluetooth_adapter")
     @patch("bt_list_adapters.get_node_content")
@@ -30,3 +31,35 @@ class BTTests(unittest.TestCase):
         mock_is_bt_adapter.return_value = True
         fake_devices = bt_list_adapters.get_bluetooth_devices([Path("/test")])
         self.assertEqual(fake_devices, [("test", "test")])
+
+    @patch("bt_list_adapters.get_bluetooth_devices")
+    @patch("bt_list_adapters.Path.iterdir")
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_main(self, mock_stdout, mock_path, mock_bt_devices):
+        mock_path.return_value = [
+            "test",
+        ]
+        btdev = bt_list_adapters.BTDevice("test", "test")
+        mock_bt_devices.return_value = [
+            btdev,
+        ]
+        bt_list_adapters.main()
+        self.assertEqual(mock_stdout.getvalue().strip(), "test test")
+
+    @patch("bt_list_adapters.get_bluetooth_devices")
+    @patch("bt_list_adapters.Path.iterdir")
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_main_no_bt_devices(self, mock_stdout, mock_path, mock_bt_devices):
+        mock_path.return_value = []
+        mock_bt_devices.return_value = []
+        with self.assertRaises(SystemExit):
+            bt_list_adapters.main()
+
+    @patch("bt_list_adapters.get_bluetooth_devices")
+    @patch("bt_list_adapters.Path.iterdir")
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_main_no_rfkill_path(self, mock_stdout, mock_path, mock_bt_devices):
+        mock_path.side_effect = FileNotFoundError
+        mock_bt_devices.return_value = []
+        with self.assertRaises(SystemExit):
+            bt_list_adapters.main()

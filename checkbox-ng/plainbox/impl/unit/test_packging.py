f25eb1122a1b83840abf0437e1d7aaa031e6b780
Fernando Bravo <fernando.bravo.hernandez@canonical.com>
2023-12-14 17:44:33 +0100

    Added test to read os version id form text
    Testing that the reading an comparison of the os-version-id is done
    properly.
    Formatted test_packaging.py

--- checkbox-ng/plainbox/impl/unit/test_packging.py
+++ checkbox-ng/plainbox/impl/unit/test_packging.py
@@ -19,6 +19,7 @@
 """Tests for the PackagingMetaDataUnit and friends."""
 
 from unittest import TestCase
+import textwrap
 
 from plainbox.impl.unit.packaging import DebianPackagingDriver
 from plainbox.impl.unit.packaging import PackagingMetaDataUnit
@@ -26,6 +27,7 @@ from plainbox.impl.unit.packaging import _strategy_id
 from plainbox.impl.unit.packaging import _strategy_id_like
 from plainbox.impl.unit.packaging import _strategy_id_version
 from plainbox.impl.unit.packaging import _compare_versions
+from plainbox.impl.secure.rfc822 import load_rfc822_records
 
 
 class DebianPackagingDriverTests(TestCase):
@@ -33,60 +35,81 @@ class DebianPackagingDriverTests(TestCase):
     """Tests for the DebianPackagingDriver class."""
 
     DEBIAN_JESSIE = {
-        'PRETTY_NAME': "Debian GNU/Linux 8 (jessie)",
-        'NAME': "Debian GNU/Linux",
-        'VERSION_ID': "8",
-        'VERSION': "8 (jessie)",
-        'ID': 'debian',
-        'HOME_URL': "http://www.debian.org/",
-        'SUPPORT_URL': "http://www.debian.org/support/",
-        'BUG_REPORT_URL': "https://bugs.debian.org/",
+        "PRETTY_NAME": "Debian GNU/Linux 8 (jessie)",
+        "NAME": "Debian GNU/Linux",
+        "VERSION_ID": "8",
+        "VERSION": "8 (jessie)",
+        "ID": "debian",
+        "HOME_URL": "http://www.debian.org/",
+        "SUPPORT_URL": "http://www.debian.org/support/",
+        "BUG_REPORT_URL": "https://bugs.debian.org/",
     }
 
     DEBIAN_SID = {
-        'PRETTY_NAME': "Debian GNU/Linux stretch/sid",
-        'NAME': "Debian GNU/Linux",
-        'ID': 'debian',
-        'HOME_URL': "https://www.debian.org/",
-        'SUPPORT_URL': "https://www.debian.org/support/",
-        'BUG_REPORT_URL': "https://bugs.debian.org/",
+        "PRETTY_NAME": "Debian GNU/Linux stretch/sid",
+        "NAME": "Debian GNU/Linux",
+        "ID": "debian",
+        "HOME_URL": "https://www.debian.org/",
+        "SUPPORT_URL": "https://www.debian.org/support/",
+        "BUG_REPORT_URL": "https://bugs.debian.org/",
     }
 
     UBUNTU_VIVID = {
-        'NAME': "Ubuntu",
-        'VERSION': "15.04 (Vivid Vervet)",
-        'ID': 'ubuntu',
-        'ID_LIKE': 'debian',
-        'PRETTY_NAME': "Ubuntu 15.04",
-        'VERSION_ID': "15.04",
-        'HOME_URL': "http://www.ubuntu.com/",
-        'SUPPORT_URL': "http://help.ubuntu.com/",
-        'BUG_REPORT_URL': "http://bugs.launchpad.net/ubuntu/",
+        "NAME": "Ubuntu",
+        "VERSION": "15.04 (Vivid Vervet)",
+        "ID": "ubuntu",
+        "ID_LIKE": "debian",
+        "PRETTY_NAME": "Ubuntu 15.04",
+        "VERSION_ID": "15.04",
+        "HOME_URL": "http://www.ubuntu.com/",
+        "SUPPORT_URL": "http://help.ubuntu.com/",
+        "BUG_REPORT_URL": "http://bugs.launchpad.net/ubuntu/",
+    }
+
+    UBUNTU_FOCAL = {
+        "NAME": "Ubuntu",
+        "ID": "ubuntu",
+        "ID_LIKE": "debian",
+        "VERSION_ID": "20.04",
+    }
+
+    UBUNTU_JAMMY = {
+        "NAME": "Ubuntu",
+        "ID": "ubuntu",
+        "ID_LIKE": "debian",
+        "VERSION_ID": "22.04",
     }
 
     def test_fix_1476678(self):
         """Check https://bugs.launchpad.net/plainbox/+bug/1476678."""
         driver = DebianPackagingDriver({})
-        driver.collect(PackagingMetaDataUnit({
-            'Depends': (
-                'python3-checkbox-support (>= 0.2),\n'
-                'python3 (>= 3.2),\n'),
-            'Recommends': (
-                'dmidecode,\n'
-                'dpkg (>= 1.13),\n'
-                'lsb-release,\n'
-                'wodim')
-        }))
-        self.assertEqual(driver._depends, [
-            'python3-checkbox-support (>= 0.2)',
-            'python3 (>= 3.2)',
-        ])
-        self.assertEqual(driver._recommends, [
-            'dmidecode',
-            'dpkg (>= 1.13)',
-            'lsb-release',
-            'wodim'
-        ])
+        driver.collect(
+            PackagingMetaDataUnit(
+                {
+                    "Depends": (
+                        "python3-checkbox-support (>= 0.2),\n"
+                        "python3 (>= 3.2),\n"
+                    ),
+                    "Recommends": (
+                        "dmidecode,\n"
+                        "dpkg (>= 1.13),\n"
+                        "lsb-release,\n"
+                        "wodim"
+                    ),
+                }
+            )
+        )
+        self.assertEqual(
+            driver._depends,
+            [
+                "python3-checkbox-support (>= 0.2)",
+                "python3 (>= 3.2)",
+            ],
+        )
+        self.assertEqual(
+            driver._recommends,
+            ["dmidecode", "dpkg (>= 1.13)", "lsb-release", "wodim"],
+        )
         self.assertEqual(driver._suggests, [])
 
     def test_fix_1477095(self):
@@ -94,9 +117,11 @@ class DebianPackagingDriverTests(TestCase):
         # This unit is supposed to for Debian (any version) and derivatives.
         # Note below that id match lets both Debian Jessie and Debian Sid pass
         # and that id_like match also lets Ubuntu Vivid pass.
-        unit = PackagingMetaDataUnit({
-            'os-id': 'debian',
-        })
+        unit = PackagingMetaDataUnit(
+            {
+                "os-id": "debian",
+            }
+        )
         # Using id and version match
         self.assertFalse(_strategy_id_version(unit, {}))
         self.assertFalse(_strategy_id_version(unit, self.DEBIAN_SID))
@@ -115,10 +140,7 @@ class DebianPackagingDriverTests(TestCase):
         # This unit is supposed to for Debian Jessie only.  Note below that
         # only Debian Jessie is passed and only by id and version match.
         # Nothing else is allowed.
-        unit = PackagingMetaDataUnit({
-            'os-id': 'debian',
-            'os-version-id': '8'
-        })
+        unit = PackagingMetaDataUnit({"os-id": "debian", "os-version-id": "8"})
         # Using id and version match
         self.assertFalse(_strategy_id_version(unit, {}))
         self.assertFalse(_strategy_id_version(unit, self.DEBIAN_SID))
@@ -139,9 +161,11 @@ class DebianPackagingDriverTests(TestCase):
         # version that is allowed here is the one Vivid version we test for.
         # (If there was an Elementary test here it would have passed as well, I
         # hope).
-        unit = PackagingMetaDataUnit({
-            'os-id': 'ubuntu',
-        })
+        unit = PackagingMetaDataUnit(
+            {
+                "os-id": "ubuntu",
+            }
+        )
         # Using id and version match
         self.assertFalse(_strategy_id_version(unit, {}))
         self.assertFalse(_strategy_id_version(unit, self.DEBIAN_SID))
@@ -160,10 +184,9 @@ class DebianPackagingDriverTests(TestCase):
         # This unit is supposed to for Ubuntu Vivid only.  Note that it behaves
         # exactly like the Debian Jessie test above.  Only Ubuntu Vivid is
         # passed and only by the id and version match.
-        unit = PackagingMetaDataUnit({
-            'os-id': 'ubuntu',
-            'os-version-id': '15.04'
-        })
+        unit = PackagingMetaDataUnit(
+            {"os-id": "ubuntu", "os-version-id": "15.04"}
+        )
         # Using id and version match
         self.assertFalse(_strategy_id_version(unit, {}))
         self.assertFalse(_strategy_id_version(unit, self.DEBIAN_SID))
@@ -181,18 +204,49 @@ class DebianPackagingDriverTests(TestCase):
         self.assertFalse(_strategy_id_like(unit, self.UBUNTU_VIVID))
 
     def test_package_with_comparision(self):
-        unit = PackagingMetaDataUnit({
-            'os-id': 'ubuntu',
-            'os-version-id': '>=14.04'
-        })
+        unit = PackagingMetaDataUnit(
+            {"os-id": "ubuntu", "os-version-id": ">=14.04"}
+        )
         # Using id and version match
         self.assertFalse(_strategy_id_version(unit, {}))
         self.assertFalse(_strategy_id_version(unit, self.DEBIAN_SID))
         self.assertFalse(_strategy_id_version(unit, self.DEBIAN_JESSIE))
         self.assertTrue(_strategy_id_version(unit, self.UBUNTU_VIVID))
 
-    def test_compare_versions(self):
+    def test_read_os_version_id_from_text(self):
+        file_content_1 = textwrap.dedent(
+            """\
+        unit: packaging meta-data
+        os-id: ubuntu
+        os-version-id: >=20.04
+        Depends: python3-opencv
+        """
+        )
 
+        record_1 = load_rfc822_records(file_content_1)[0]
+        unit_1 = PackagingMetaDataUnit.from_rfc822_record(record_1)
+
+        self.assertFalse(_strategy_id_version(unit_1, self.UBUNTU_VIVID))
+        self.assertTrue(_strategy_id_version(unit_1, self.UBUNTU_FOCAL))
+        self.assertTrue(_strategy_id_version(unit_1, self.UBUNTU_JAMMY))
+
+        file_content = textwrap.dedent(
+            """\
+        unit: packaging meta-data
+        os-id: ubuntu
+        os-version-id: 20.04
+        Depends: python3-opencv
+        """
+        )
+
+        record_2 = load_rfc822_records(file_content)[0]
+        unit_2 = PackagingMetaDataUnit.from_rfc822_record(record_2)
+
+        self.assertFalse(_strategy_id_version(unit_2, self.UBUNTU_VIVID))
+        self.assertTrue(_strategy_id_version(unit_2, self.UBUNTU_FOCAL))
+        self.assertFalse(_strategy_id_version(unit_2, self.UBUNTU_JAMMY))
+
+    def test_compare_versions(self):
         # equal operator
         self.assertTrue(_compare_versions("==1.0.0", "1.0.0"))
         self.assertFalse(_compare_versions("==1.0.1", "1.0.0"))

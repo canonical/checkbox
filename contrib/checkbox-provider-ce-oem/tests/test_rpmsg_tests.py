60c42b1a0964baa28a78797a2b8eda20546aa661
stanley31huang <stanley.huang@canonical.com>
2023-12-18 11:49:27 +0800

    Add: implement the RPMSG-tty tests
    implement the RPMSG-tty tests for i.MX series

--- contrib/checkbox-provider-ce-oem/tests/test_rpmsg_tests.py
+++ contrib/checkbox-provider-ce-oem/tests/test_rpmsg_tests.py
@@ -2,6 +2,9 @@ import unittest
 import sys
 from unittest.mock import patch, MagicMock
 sys.modules["systemd"] = MagicMock()
+sys.modules["serial_test"] = MagicMock()
+sys.modules["serial_test"].client_mode = MagicMock(
+                                    side_effect=SystemExit(0))
 import rpmsg_tests
 
 
@@ -60,7 +63,7 @@ class RpmsgTests(unittest.TestCase):
 
     def test_arm_processor_unexpected(self):
         """
-        /sys/devices/soc0/family and machine fils not exists
+        /sys/devices/soc0/family and machine files not exists
         """
         cpu_family = ""
         with patch("rpmsg_tests.get_soc_machine") as mock_machine:
@@ -70,3 +73,48 @@ class RpmsgTests(unittest.TestCase):
                 cpu_family = rpmsg_tests.detect_arm_processor_type()
 
         self.assertEqual("unknown", cpu_family)
+
+    def test_rpmsg_test_supported_with_imx(self):
+        """
+        RPMSG TTY test is supported for i.MX
+        """
+        pattern, cmd = rpmsg_tests.rpmsg_tty_test_supported("imx")
+        self.assertEqual(pattern, r"ttyRPMSG[0-9]*")
+        self.assertEqual(cmd, "modprobe imx_rpmsg_tty")
+
+    def test_rpmsg_test_supported_with_ti(self):
+        """
+        RPMSG TTY test is not supported for TI
+        """
+        with self.assertRaisesRegex(
+                SystemExit, "Unsupported method for TI."):
+            rpmsg_tests.rpmsg_tty_test_supported("ti")
+
+    def test_rpmsg_test_supported_with_other(self):
+        """
+        RPMSG TTY test is not supported for Other SoC
+        """
+        with self.assertRaisesRegex(
+                SystemExit, "Unexpected CPU type."):
+            rpmsg_tests.rpmsg_tty_test_supported("mtk")
+
+    @patch("rpmsg_tests.check_rpmsg_tty_devices")
+    def test_no_rpmsg_devices(self, mock_check_rpmsg_tty_devices):
+        """
+        No RPMSG TTY devices found and raise SystemExit
+        """
+        mock_check_rpmsg_tty_devices.return_value = []
+        with self.assertRaisesRegex(
+                SystemExit, "No RPMSG TTY devices found."):
+            rpmsg_tests.serial_tty_test("imx", 64)
+
+    @patch("rpmsg_tests.check_rpmsg_tty_devices")
+    def test_rpmsg_test_passed(self,
+                               mock_check_rpmsg_tty_devices):
+        """
+        String-ECHO test passed through RPMSG TTY device
+        """
+        mock_check_rpmsg_tty_devices.return_value = ["/dev/ttyRPMSG30"]
+
+        with self.assertRaises(SystemExit):
+            rpmsg_tests.serial_tty_test("imx", 64)

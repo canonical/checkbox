3a08ef91d9fb3e0497154f5e8fd4f89f684ce080
liaou3 <vincent.liao@canonical.com>
2024-01-04 13:56:45 +0800

    add_something

--- checkbox-support/checkbox_support/tests/test_image_checker.py
+++ checkbox-support/checkbox_support/tests/test_image_checker.py
@@ -22,9 +22,12 @@ from checkbox_support.image_checker import get_type, get_source, main
 
 
 class ImageCheckerTest(unittest.TestCase):
-    mock_info = ("Sun, 20 June 1999 66:66:66 +7777\n"
-                 "Somethingcool\n"
-                 "iot-vincent-core-24-x24-2023-10-31-24\n")
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self.mock_oem_info = ("Sun, 20 June 1999 66:66:66 +7777\n"
+                              "Somethingcool\n"
+                              "iot-vincent-core-24-x24-2023-10-31-24\n")
+        self.mock_stock_info = "20231031.6"
 
     @patch("checkbox_support.image_checker.on_ubuntucore")
     def test_get_type(self, mock_on_ubuntu_core):
@@ -39,8 +42,11 @@ class ImageCheckerTest(unittest.TestCase):
     def test_get_source(self, mock_get_type, mock_exists):
         # Test when it is core image
         mock_get_type.return_value = "core"
-        with patch("builtins.open", mock_open(read_data=self.mock_info)):
+        with patch("builtins.open", mock_open(read_data=self.mock_oem_info)):
             self.assertEqual(get_source(), "oem")
+
+        with patch("builtins.open", mock_open(read_data=self.mock_stock_info)):
+            self.assertEqual(get_source(), "stock")
 
         with patch("builtins.open", side_effect=FileNotFoundError):
             self.assertEqual(get_source(), "unknown")

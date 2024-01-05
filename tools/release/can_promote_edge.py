a8adf526f974036ba04f0389dcace66e9b8e8fbd
Hook25 <massimiliano.girardi@canonical.com>
2024-01-05 18:03:59 +0100

    Added/Updated docstrings

--- tools/release/can_promote_edge.py
+++ tools/release/can_promote_edge.py
@@ -14,6 +14,11 @@ import subprocess
 
 
 def get_gh_daily_builds_array() -> list[dict]:
+    """
+    Query github for the latest 20 runs of the daily-builds.yml workflow
+    returning a parsed array of dicts containing their headSha (hash the run
+    was triggered on) and conclusion.
+    """
     return json.loads(
         subprocess.check_output(
             [
@@ -35,6 +40,9 @@ def get_gh_daily_builds_array() -> list[dict]:
 
 
 def get_latest_ok_head(builds: list[dict]) -> str:
+    """
+    Returns the headSha of the most recent run of a build that was successful
+    """
     try:
         return next(
             x["headSha"] for x in builds if x["conclusion"] == "success"
@@ -44,13 +52,26 @@ def get_latest_ok_head(builds: list[dict]) -> str:
 
 
 def get_head_beta_validated() -> str:
+    """
+    Returns the full hash of origin/beta_validation's HEAD
+    """
     return subprocess.check_output(
-        ["git", "show", "origin/beta_validation", "--pretty=format:%H", "--no-patch"],
+        [
+            "git",
+            "show",
+            "origin/beta_validation",
+            "--pretty=format:%H",
+            "--no-patch",
+        ],
         text=True,
     )
 
 
-def beta_validation_matches_successful_daily():
+def beta_validation_matches_successful_daily() -> bool:
+    """
+    Returns true if the most recent daily build was run on the same commit
+    that the head of beta_validation points to, else false
+    """
     builds = get_gh_daily_builds_array()
     last_daily_build_commit_hash = get_latest_ok_head(builds)
     last_beta_validated_head = get_head_beta_validated()

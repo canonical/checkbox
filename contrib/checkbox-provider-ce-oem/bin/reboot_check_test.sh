aba4ff768f1c9e1746d30ebaa70de5042dbcaba5
rickwu666666 <98441647+rickwu666666@users.noreply.github.com>
2024-01-09 10:58:59 +0800

    Add dbus reboot stress (#70)
    * Add script for installing test snap and connect interfaces
    
    * Add reboot via dbus command
    
    This test is for snap strict confinement mode

--- contrib/checkbox-provider-ce-oem/bin/reboot_check_test.sh
+++ contrib/checkbox-provider-ce-oem/bin/reboot_check_test.sh
@@ -2,9 +2,39 @@
 
 device_check="true"
 service_check="true"
+fwts_supported="false"
+fwts_check="true"
+
+
+fwts_support_check(){
+    echo -e "\n## Checking if support fwts..."
+    if  type -p fwts > /dev/null ; then
+        echo "FWTS supported"
+        fwts_supported="true"
+    fi
+}
+
+run_fwts(){
+    echo -e "\n## Running FWTS of test: $*..."
+    fwts -r "$output_dir"/fwts_klog_oops.log "$@"
+}
+
+fwts_log_check(){
+    echo -e "\n## Checking FWTS log failures..."
+    fwts_log_path="$output_dir"/fwts_klog_oops.log
+    if [[ -f "$fwts_log_path" ]]; then
+        sleep_test_log_check.py -v --ignore-warning -t all "$output_dir"/fwts_klog_oops.log
+        return_code=$?
+        if [[ $return_code != 0 ]]; then
+            fwts_check="false"
+        fi
+    else
+        fwts_check="false"
+    fi
+}
 
 service_check() {
-    echo -e "\n## Check system service.."
+    echo -e "\n## Checking system services..."
     COUNT=$(systemctl --system --no-ask-password --no-pager --no-legend list-units --state=failed | wc -l)
     printf "Found %s failed units\n" "$COUNT"
     if [ "$COUNT" != 0 ]; then
@@ -15,27 +45,30 @@ service_check() {
 }
 
 dump() {
-    echo "## Dump the devices information to $output_dir.."
+    echo "## Dumping the devices information to $output_dir..."
     mkdir -p "$output_dir"
+    echo "Gathering information about PCI devices (lspci)..."
     lspci -i "$SNAP"/usr/share/misc/pci.ids > "$output_dir"/lspci_log
+    echo "Gathering information about WiFi connections (iw)..."
     iw dev | grep "Interface\|addr\|ssid" > "$output_dir"/wifi_conn
+    echo "Gathering information about USB devices (lsusb)..."
     checkbox-support-lsusb -f "$CHECKBOX_RUNTIME"/var/lib/usbutils/usb.ids -s | sort > "$output_dir"/lsusb_log
     echo "dump devices complete"
     sync
 }
 
 compare() {
-    echo -e "\n## Compare the devices.."
+    echo -e "\n## Comparing the devices..."
     if ! diff -u "$compare_dir"/lspci_log "$output_dir"/lspci_log; then
-        echo "lspci mismatch during cycle"
+        echo "The list of PCI devices (lspci) is different from the original list gathered at the beginning of the session!"
         device_check="false"
     fi
     if ! diff -u "$compare_dir"/wifi_conn "$output_dir"/wifi_conn; then
-        echo "wifi mismatch during cycle"
+        echo "The list of WiFi connections (iw) is different from the original list gathered at the beginning of the session!"
         device_check="false"
     fi
     if ! diff -u "$compare_dir"/lsusb_log "$output_dir"/lsusb_log; then
-        echo "lsusb mismatch during cycle"
+        echo "The list of USB devices (lsusb) is different from the original list gathered at the beginning of the session!"
         device_check="false"
     fi
 
@@ -45,6 +78,11 @@ compare() {
 main() {
     if  [[ -n "$output_dir" ]]; then
         dump
+        fwts_support_check
+        if "$fwts_supported"; then
+            run_fwts "klog" "oops"
+            fwts_log_check
+        fi
     fi
 
     if [[ -n "$compare_dir" ]]; then
@@ -61,29 +99,34 @@ main() {
     fi
 
     if [[ -n "$service_opt" && "$service_check" == "false" ]] || \
-       [[ -n "$compare_dir" && "$device_check" == "false" ]]; then
+       [[ -n "$compare_dir" && "$device_check" == "false" ]] || \
+       [[ -n "$fwts_opt" && "$fwts_check" == "false" ]]; then
         exit 1
     fi
 }
 
 
 help_function() {
-    echo "This script is uses for collect device and compare the difference of device during every reboot iteration"
+    echo "This script is used to collect device information and to check for differences between reboots."
     echo
-    echo "Usage: cold_reboot_by_pdu.sh -t type -p pud-ip:outlet-port"
+    echo "Usage: reboot_check_test.sh -d Output_directory"
+    echo "       reboot_check_test.sh -d Output_directory -c Comparing_directory -s -f"
     echo -e "\t-d    Output directory."
     echo -e "\t-c    The target directory for comparing device"
     echo -e "\t-s    Do service check"
+    echo -e "\t-f    Do fwts log failures check"
 }
 
 service_opt=""
+fwts_opt=""
 output_dir=""
 compare_dir=""
-while getopts "d:c:s" opt; do
+while getopts "d:c:sf" opt; do
     case "$opt" in
         d) output_dir="$OPTARG" ;;
         c) compare_dir="$OPTARG" ;;
         s) service_opt="True" ;;
+        f) fwts_opt="True" ;;
         ?) help_function ;;
     esac
 done
---
+++ contrib/checkbox-provider-ce-oem/tests/test_env_setup.py
@@ -0,0 +1,103 @@
+import unittest
+from unittest.mock import patch, mock_open
+import json
+import env_setup
+
+
+class TestGetConfig(unittest.TestCase):
+
+    @patch('os.path.exists', return_value=True)
+    def test_valid_config_file(self, mock_exists):
+        config_path = '/path/to/valid_config.json'
+        mock_file_content = '{"key": "value"}'
+        config_file = {}
+        with patch('builtins.open', mock_open(
+                read_data=mock_file_content)) as mock_file:
+            config = env_setup.get_config(config_file, config_path)
+
+        mock_file.assert_called_once_with(config_path)
+        self.assertEqual(config, {"key": "value"})
+
+    def test_invalid_config_file(self):
+        config_path = '/path/to/invalid_config.json'
+        config_file = {
+                        "test-strict-confinement": {
+                            "channel": "edge",
+                            "plugs": {
+                                "jace-bt-led": {
+                                    "gadget": "bt-led",
+                                },
+                            },
+                        },
+        }
+
+        with patch('builtins.open', side_effect=json.JSONDecodeError):
+            config = env_setup.get_config(config_file, config_path)
+
+        self.assertEqual(config,
+                         {
+                            "test-strict-confinement": {
+                                "channel": "edge",
+                                "plugs": {
+                                    "jace-bt-led": {
+                                        "gadget": "bt-led",
+                                    },
+                                },
+                            },
+                         })
+
+    def test_nonexistent_config_file(self):
+        config_path = '/path/to/nonexistent_config.json'
+        config_file = {
+                        "test-strict-confinement": {
+                            "channel": "edge",
+                            "plugs": {
+                                "jace-bt-led": {
+                                    "gadget": "bt-led",
+                                },
+                            },
+                        },
+        }
+
+        with patch('builtins.open', side_effect=FileNotFoundError):
+            config = env_setup.get_config(config_file, config_path)
+
+        self.assertEqual(config,
+                         {
+                            "test-strict-confinement": {
+                                "channel": "edge",
+                                "plugs": {
+                                    "jace-bt-led": {
+                                        "gadget": "bt-led",
+                                        },
+                                        },
+                            },
+                         })
+        # Additional checks/assertions if needed
+
+    def test_empty_config_path(self):
+        config_path = ''
+        config_file = {
+                        "test-strict-confinement": {
+                            "channel": "edge",
+                            "plugs": {
+                                "jace-bt-led": {
+                                    "gadget": "bt-led",
+                                },
+                            },
+                        },
+        }
+
+        config = env_setup.get_config(config_file, config_path)
+
+        self.assertEqual(config,
+                         {
+                            "test-strict-confinement": {
+                                "channel": "edge",
+                                "plugs": {
+                                    "jace-bt-led": {
+                                        "gadget": "bt-led",
+                                        },
+                                        },
+                            },
+                         })

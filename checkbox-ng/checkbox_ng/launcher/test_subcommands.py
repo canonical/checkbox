ae96bdbed4564e94b4202043f9dcc727ea4922da
Maciej Kisielewski <maciej.kisielewski@canonical.com>
2024-01-10 14:47:55 +0100

    remove another unused import

--- checkbox-ng/checkbox_ng/launcher/test_subcommands.py
+++ checkbox-ng/checkbox_ng/launcher/test_subcommands.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
 from unittest import TestCase
 
 from unittest.mock import patch, Mock, MagicMock

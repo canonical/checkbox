44b337cbb6c6066dc9ccdfa4d9ec5ebdc2391ac5
Hook25 <massimiliano.girardi@canonical.com>
2024-01-29 16:38:31 +0100

    Additional tests to hit the coverage quota

--- providers/base/tests/test_gateway_ping_test.py
+++ providers/base/tests/test_gateway_ping_test.py
@@ -1,13 +1,25 @@
 import unittest
 from unittest.mock import patch, MagicMock
-from gateway_ping_test import main
+from gateway_ping_test import main, parse_args
 
 
 class TestMainFunction(unittest.TestCase):
     @patch("gateway_ping_test.get_host_to_ping")
     @patch("gateway_ping_test.ping")
-    def test_no_internet_connection(self, mock_ping, mock_get_host_to_ping):
-        mock_ping.return_value = {"received": 0}
+    def test_no_internet_connection_no_cause(
+        self, mock_ping, mock_get_host_to_ping
+    ):
+        mock_get_host_to_ping.return_value = None
+        mock_ping.return_value = None
+        result = main(["1.1.1.1"])
+        self.assertEqual(result, 1)
+
+    @patch("gateway_ping_test.get_host_to_ping")
+    @patch("gateway_ping_test.ping")
+    def test_no_internet_connection_cause(
+        self, mock_ping, mock_get_host_to_ping
+    ):
+        mock_ping.return_value = {"received": 0, "cause": "Test cause"}
         result = main(["1.1.1.1"])
         self.assertEqual(result, 1)
 
@@ -66,3 +78,13 @@ class TestMainFunction(unittest.TestCase):
     ):
         with self.assertRaises(SystemExit):
             main(["-c", "10", "-d", "8"])
+
+
+    def test_adjust_count_based_on_non_default_deadline(self):
+        # Assuming default_delay is 4
+        args = parse_args(["-d", "1", "-v"])
+        self.assertEqual(
+            args.count,
+            1,
+            "Count should be adjusted based on the non-default deadline",
+        )

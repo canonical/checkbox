6a445285e2b9c6a3588edadec99889ca12fb77ff
Hook25 <massimiliano.girardi@canonical.com>
2024-02-21 16:21:04 +0100

    Bump the version number

--- checkbox-ng/plainbox/impl/session/remote_assistant.py
+++ checkbox-ng/plainbox/impl/session/remote_assistant.py
@@ -143,7 +143,7 @@ class BackgroundExecutor(Thread):
 class RemoteSessionAssistant:
     """Remote execution enabling wrapper for the SessionAssistant"""
 
-    REMOTE_API_VERSION = 12
+    REMOTE_API_VERSION = 13
 
     def __init__(self, cmd_callback):
         _logger.debug("__init__()")

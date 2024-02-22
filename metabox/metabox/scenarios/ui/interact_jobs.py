a88e5d01285e3946d7cc684bea191d0ead55e329
Massimiliano <massimiliano.girardi@canonical.com>
2024-02-22 16:08:29 +0100

    Add tests for resume menu (infra) (#1008)
    * Add tests for resume menu
    
    * Remove random newline everywhere in license
    
    * Removed reduntant test tag

--- metabox/metabox/scenarios/ui/interact_jobs.py
+++ metabox/metabox/scenarios/ui/interact_jobs.py
@@ -7,7 +7,6 @@
 # Checkbox is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3,
 # as published by the Free Software Foundation.
-
 #
 # Checkbox is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of

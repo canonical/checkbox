71f0d20e8f2d0bb380de236a519965909cc0f356
Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
2024-03-13 18:50:47 +0100

    Cover smaller genio tests (New) (#1060)
    * Formatted dvfs test and removed typos
    
    * Added test for dvfs_gpu_check_governors
    
    * Raise SystemExit only on fail
    
    * Added serialcheck tests
    
    * Fixed Error raise when stdout is empty
    
    * Added spidev test
    
    * Added pmic regulator tests
    
    * Fixed errors in linux_ccf
    
     - Moved check_env_variables to function so the module can be imported without requiring them
     - Added choices for devices considered in the test
     - Test failing with wrong verify output
    
    * Added linux ccf tests
    
    * Update contrib/genio/bin/spidev_test.py
    
    Co-authored-by: kissiel <maciej.kisielewski@canonical.com>
    
    ---------
    
    Co-authored-by: kissiel <maciej.kisielewski@canonical.com>

--- contrib/genio/bin/dvfs_gpu_check_governors.py
+++ contrib/genio/bin/dvfs_gpu_check_governors.py
@@ -2,41 +2,43 @@
 
 import argparse
 
-GOVERNORS = ['userspace', 'powersave', 'performance', 'simple_ondemand']
-print(f'Expected Governos: {GOVERNORS}')
+GOVERNORS = ["userspace", "powersave", "performance", "simple_ondemand"]
+print(f"Expected Governors: {GOVERNORS}")
 
 
 def test_sysfs_attrs_read(soc):
     fail = 0
-    mail_type = '13000000.mali'
-    if soc == 'mt8365':
-        mail_type = '13040000.mali'
+    mail_type = "13000000.mali"
+    if soc == "mt8365":
+        mail_type = "13040000.mali"
     node_path = (
-        f'/sys/devices/platform/soc/{mail_type}/devfreq/{mail_type}/'
-        f'available_governors'
+        f"/sys/devices/platform/soc/{mail_type}/devfreq/{mail_type}/"
+        f"available_governors"
     )
 
     with open(node_path) as f:
         for node in f.read().strip().split():
             if node not in GOVERNORS:
                 fail = 1
-                print(f"Failed: found governor '{node}' out of expextation")
+                print(
+                    f"Failed: found governor '{node}' out of expectation"
+                )
     return fail
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'soc',
-        help='SoC type. e.g mt8395',
-        choices=['mt8395', 'mt8390', 'mt8365']
+        "soc",
+        help="SoC type. e.g mt8395",
+        choices=["mt8395", "mt8390", "mt8365"],
     )
     args = parser.parse_args()
     ret = test_sysfs_attrs_read(args.soc)
     if ret:
         exit(1)
-    print('Pass')
+    print("Pass")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ contrib/genio/tests/test_dvfs_gpu_check_governors.py
@@ -0,0 +1,55 @@
+import unittest
+from unittest.mock import mock_open, patch
+import dvfs_gpu_check_governors as dvfs
+
+
+class TestDvfsGpuCheckGovernors(unittest.TestCase):
+
+    def test_all_expected_governors(self):
+        # Test when all expected governors are present
+        governors = "userspace powersave performance simple_ondemand"
+        with patch("builtins.open", mock_open(read_data=governors)):
+            result = dvfs.test_sysfs_attrs_read("mt8365")
+        self.assertEqual(result, 0)
+
+        with patch("builtins.open", mock_open(read_data=governors)):
+            result = dvfs.test_sysfs_attrs_read("mt8364")
+        self.assertEqual(result, 0)
+
+    def test_unexpected_governor(self):
+        # Test when an unexpected governor is present
+        governors = "userspace powersave performance unexpected_governor"
+        with patch("builtins.open", mock_open(read_data=governors)):
+            result = dvfs.test_sysfs_attrs_read("mt8365")
+        self.assertEqual(result, 1)
+
+    @patch("builtins.open", mock_open(read_data=""))
+    def test_empty_file(self):
+        # Test when the file is empty
+        governors = ""
+        with patch("builtins.open", mock_open(read_data=governors)):
+            result = dvfs.test_sysfs_attrs_read("mt8365")
+        self.assertEqual(result, 0)
+
+    @patch("dvfs_gpu_check_governors.test_sysfs_attrs_read")
+    def test_main(self, mock_attrs_read):
+        mock_attrs_read.return_value = 0
+        with patch("sys.argv", ["script_name", "mt8395"]):
+            result = dvfs.main()
+        self.assertEqual(mock_attrs_read.call_count, 1)
+        self.assertEqual(result, None)
+
+    @patch("dvfs_gpu_check_governors.test_sysfs_attrs_read")
+    def test_main_bad_args(self, mock_attrs_read):
+        with patch("sys.argv", ["script_name", "bad_soc"]):
+            with self.assertRaises(SystemExit):
+                dvfs.main()
+        self.assertEqual(mock_attrs_read.call_count, 0)
+
+    @patch("dvfs_gpu_check_governors.test_sysfs_attrs_read")
+    def test_main_wrong_attrs(self, mock_attrs_read):
+        mock_attrs_read.return_value = 1
+        with patch("sys.argv", ["script_name", "mt8395"]):
+            with self.assertRaises(SystemExit):
+                dvfs.main()
+        self.assertEqual(mock_attrs_read.call_count, 1)

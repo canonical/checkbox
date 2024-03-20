873cccb241152bd634fd85a7a98c0b77aa250edf
rickwu666666 <98441647+rickwu666666@users.noreply.github.com>
2024-03-20 15:51:52 +0800

    [Checkbox-ce-oem] Modify job and script to support use x-test tool(Bugfix) (#1075)
    * Modify job and script too support use x-test tool
    
    * Modify the logic to prioritize the x-test snap
    
    * Modify the method to get xtest in gadget snap
    
    Modify the method to call python script
    
    * Separate look_up_xtest to another script

--- contrib/checkbox-provider-ce-oem/bin/parse_optee_test.py
+++ contrib/checkbox-provider-ce-oem/bin/parse_optee_test.py
@@ -2,32 +2,45 @@
 
 import json
 import argparse
+from look_up_xtest import look_up_xtest
 
 
-def parse_json_file(filename, filter=False):
-    with open(filename, 'r') as file:
+def parse_json_file(filepath, filter=False, xtest=None):
+    with open(filepath, 'r') as file:
         data = json.load(file)
     for test in data:
-        if (filter and test['suite'] == 'pkcs11') or \
-           (not filter and test['suite'] not in ['pkcs11']):
-            print_test_info(test)
+        if check_suite(test['suite'], filter):
+            print_test_info(test, xtest)
 
 
-def print_test_info(test):
+def check_suite(suite, filter):
+    if filter:
+        return suite == 'pkcs11'
+    else:
+        return suite != 'pkcs11'
+
+
+def print_test_info(test, xtest):
     print("suite: {}".format(test['suite']))
     print("test_id: {}".format(test['test_id']))
-    print("description: {}\n".format(test['test_description']))
+    print("description: {}".format(test['test_description']))
+    print("tool: {}\n".format(xtest))
 
 
 def main():
     parser = argparse.ArgumentParser(description='Parse a JSON file.')
-    parser.add_argument('filename', help='The name of the file to parse.')
+    parser.add_argument('filepath',
+                        help='The path of the file to parse.')
     parser.add_argument('-p', '--pkcs11',
-                        help='To filter out PKCS11 for the suite'
+                        help='To filter out PKCS11 for the suite.'
                         'field in JSON.',
                         action='store_true')
     args = parser.parse_args()
-    parse_json_file(args.filename, args.pkcs11)
+    try:
+        xtest = look_up_xtest()
+    except SystemExit:
+        xtest = None
+    parse_json_file(args.filepath, args.pkcs11, xtest)
 
 
 if __name__ == "__main__":

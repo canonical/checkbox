74d593bcd2a9461594bb1d880c16e81d114c0ab3
Bin Li <bin.li@canonical.com>
2024-03-23 00:04:39 +0800

    Add unittest for switch_power_mode.py

--- providers/resource/tests/test_platform_profile_resource.py
+++ providers/resource/tests/test_platform_profile_resource.py
@@ -1,12 +1,33 @@
+#!/usr/bin/env python3
+# Copyright 2024 Canonical Ltd.
+# Written by:
+#   Bin Li <bin.li@canonical.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# pylint: disable=import-error
+
+""" A unittest module for the platform_profile_resource module. """
 import unittest
 from unittest.mock import patch
 import platform_profile_resource
 
 
 class TestPlatformProfilesSupport(unittest.TestCase):
-    # Test the check_platform_profiles function
+    """ Test the platform profile support """
     @patch("builtins.print")
     def test_supported(self, mock_print):
+        """ Test the function when all paths exist"""
         with patch("pathlib.Path.exists") as mock_exists:
             # All paths exist
             mock_exists.side_effect = [True, True, True]
@@ -16,6 +37,7 @@ class TestPlatformProfilesSupport(unittest.TestCase):
 
     @patch("builtins.print")
     def test_unsupported(self, mock_print):
+        """ Test the function when some paths do not exist"""
         with patch("pathlib.Path.exists") as mock_exists:
             # First scenario: None of the paths exist
             mock_exists.side_effect = [False, False, False]
@@ -48,7 +70,6 @@ class TestPlatformProfilesSupport(unittest.TestCase):
 
     @patch("platform_profile_resource.check_platform_profiles")
     def test_main(self, mock_check_platform_profiles):
-        # Call the function
+        """ Test the main function """
         platform_profile_resource.main()
-        # Check the output
         mock_check_platform_profiles.assert_called()

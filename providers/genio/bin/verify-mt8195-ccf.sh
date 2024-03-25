41178aec65cfb9d7c0644fef5f1c8e328383324e
Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
2024-03-25 16:09:25 +0100

    Added copyright and license notice for scripts in genio provider (Infra) (#1110)
    Added copyright and license notice for scripts in genio provider

--- providers/genio/bin/verify-mt8195-ccf.sh
+++ providers/genio/bin/verify-mt8195-ccf.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+# This file is part of Checkbox.
+#
+# Copyright 2014 Canonical Ltd.
+# Authors:
+#   Amjad Ouled-Ameur <ouledameur.amjad@baylibre.com>
+#   Patrick Chang <patrick.chang@canonical.com>
+#   Fernando Bravo <daniel.manrique@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
-# Author: Amjad Ouled-Ameur <ouledameur.amjad@baylibre.com>
 
 CLK_TABLE=
 CLK_SUMMARY=

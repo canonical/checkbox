ece69a70e6ab2a39d377ea940338473c184007dd
Michael Reed <Michael.Reed@canonical.com>
2024-05-13 07:23:54 -0500

    Initial version of testing SRIOV on Intel Netork Devices
    Adding SRIOV container tests
    
    Add flake8 format errors.
    
    Adding sriov tests to testing framework
    
    Fix the virtualizaion function call with the correct parameters
    
    Add Unit tests
    Update minor changes found by the Unit Testa
    
    Updated the unit tests for flake8 format
    
    Removed the call to setup.   Setup is not called when the test runs on
    images less than 24.04

--- providers/base/tests/test_virtualization.py
+++ providers/base/tests/test_virtualization.py
@@ -20,7 +20,7 @@ import itertools
 from unittest import TestCase
 from unittest.mock import patch, MagicMock
 
-from virtualization import LXDTest_vm
+from virtualization import LXDTest_vm, LXDTest_sriov
 
 
 class TestLXDTest_vm(TestCase):
@@ -215,5 +215,187 @@ class TestLXDTest_vm(TestCase):
         self_mock.image_url = None
 
         setup_return = LXDTest_vm.setup(self_mock)
+
+        self.assertFalse(setup_return)
+
+
+class TestLXDTest_sriov(TestCase):
+    @patch("virtualization.logging")
+    @patch("virtualization.RunCommand")
+    def test_run_command_no_stderr(self, run_command_mock, logging_mock):
+        task = run_command_mock()
+        task.returncode = 0
+        task.stdout = "abc"
+        task.stderr = None
+
+        command_result = LXDTest_sriov.run_command(
+            MagicMock(), "command", log_stderr=True
+        )
+
+        self.assertTrue(logging_mock.debug.called)
+        self.assertTrue(command_result)
+
+    @patch("virtualization.logging")
+    @patch("virtualization.RunCommand")
+    def test_run_command_no_log_stderr(self, run_command_mock, logging_mock):
+        task = run_command_mock()
+        task.returncode = 1
+        task.stdout = "abc"
+        task.stderr = None
+
+        command_result = LXDTest_sriov.run_command(
+            MagicMock(), "command", log_stderr=False
+        )
+
+        self.assertFalse(command_result)
+
+    @patch("virtualization.logging")
+    @patch("virtualization.RunCommand")
+    def test_run_command_error(self, run_command_mock, logging_mock):
+        task = run_command_mock()
+        task.returncode = 1
+        task.stdout = "abc"
+        task.stderr = "some error"
+
+        command_result = LXDTest_sriov.run_command(
+            MagicMock(), "command", log_stderr=True
+        )
+
+        self.assertTrue(logging_mock.error.called)
+        self.assertFalse(command_result)
+
+    @patch("virtualization.logging")
+    @patch("virtualization.RunCommand")
+    def test_run_command_ok(self, run_command_mock, logging_mock):
+        task = run_command_mock()
+        task.returncode = 0
+        task.stdout = "abc"
+        task.stderr = "some error"
+
+        command_result = LXDTest_sriov.run_command(
+            MagicMock(), "command", log_stderr=True
+        )
+
+        self.assertTrue(logging_mock.debug.called)
+        self.assertTrue(command_result)
+
+    @patch("virtualization.logging")
+    @patch("virtualization.RunCommand")
+    def test_run_command_ok_no_stdout(self, run_command_mock, logging_mock):
+        task = run_command_mock()
+        task.returncode = 0
+        task.stdout = ""
+        task.stderr = "some error"
+
+        command_result = LXDTest_sriov.run_command(
+            MagicMock(), "command", log_stderr=True
+        )
+
+        self.assertTrue(logging_mock.debug.called)
+        self.assertTrue(command_result)
+
+    @patch("virtualization.logging")
+    def test_cleanup(self, logging_mock):
+        self_mock = MagicMock()
+        LXDTest_sriov.cleanup(self_mock)
+
+        self.assertTrue(self_mock.run_command.called)
+
+    @patch("virtualization.logging")
+    def test_start_sriov_fail_setup(self, logging_mock):
+        self_mock = MagicMock()
+        self_mock.setup.return_value = False
+
+        start_result = LXDTest_sriov.start_sriov(self_mock)
+        self.assertFalse(start_result)
+
+    @patch("virtualization.logging")
+    def test_start_sriov_fail_init_no_img_alias(self, logging_mock):
+        self_mock = MagicMock()
+        self_mock.setup.return_value = True
+        self_mock.image_url = None
+        self_mock.template_url = None
+        self_mock.network_name = "sriov_network"
+        self_mock.test_type = "vm"
+        self_mock.name = "testbed"
+        self_mock.run_command.side_effect = [False]
+
+        start_result = LXDTest_sriov.start_sriov(self_mock)
+        self.assertFalse(start_result)
+
+    @patch("virtualization.logging")
+    def test_start_sriov_fail_start(self, logging_mock):
+        self_mock = MagicMock()
+        self_mock.setup.return_value = True
+        self_mock.image_url = "image url"
+        self_mock.template_url = "template url"
+        self_mock.network_name = "sriov_network"
+        self_mock.test_type = "vm"
+        self_mock.name = "testbed"
+        self_mock.run_command.side_effect = [True, False]
+
+        start_result = LXDTest_sriov.start_sriov(self_mock)
+        self.assertFalse(start_result)
+
+    @patch("virtualization.logging")
+    def test_start_sriov_fail_list(self, logging_mock):
+        self_mock = MagicMock()
+        self_mock.setup.return_value = True
+        self_mock.image_url = "image url"
+        self_mock.template_url = "template url"
+        self_mock.network_name = "sriov_network"
+        self_mock.test_type = "vm"
+        self_mock.name = "testbed"
+        self_mock.run_command.side_effect = [True, True, False]
+
+        start_result = LXDTest_sriov.start_sriov(self_mock)
+        self.assertFalse(start_result)
+
+    @patch("time.sleep")
+    @patch("virtualization.logging")
+    def test_start_sriov_fail_exec(self, logging_mock, time_sleep_mock):
+        self_mock = MagicMock()
+        self_mock.setup.return_value = False
+        self_mock.image_url = "image url"
+        self_mock.template_url = "template url"
+        self_mock.network_name = "sriov_network"
+        self_mock.test_type = "vm"
+        self_mock.name = "testbed"
+        self_mock.run_command.side_effect = itertools.chain(
+            [True, True, True], itertools.repeat(False)
+        )
+
+        start_result = LXDTest_sriov.start_sriov(self_mock)
+        self.assertFalse(start_result)
+
+    @patch("time.sleep")
+    @patch("virtualization.print")
+    @patch("virtualization.logging")
+    def test_start_sriov_success(self, logging_mock, print_mock, time_sleep_mock):
+        self_mock = MagicMock()
+        self_mock.setup.return_value = True
+        self_mock.image_url = "image url"
+        self_mock.template_url = "template url"
+        self_mock.network_name = "sriov_network"
+        self_mock.test_type = "vm"
+        self_mock.name = "testbed"
+        self_mock.run_command.side_effect = itertools.chain(
+            [True, True, True], itertools.repeat(False)
+        )
+
+        start_result = LXDTest_sriov.start_sriov(self_mock)
+
+        self.assertFalse(start_result)
+        self.assertTrue(print_mock.called)
+
+    def test_setup_failure(self):
+        self_mock = MagicMock()
+        self_mock.run_command.return_value = False
+        self_mock.image_url = None
+        self_mock.template_url = None
+        self_mock.network_name = None
+        self_mock.test_type = None
+
+        setup_return = LXDTest_sriov.setup(self_mock)
 
         self.assertFalse(setup_return)

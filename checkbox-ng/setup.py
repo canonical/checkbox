16eaf7df8fc791ab19fbb70e2e88f201c5cc0e69
Hook25 <massimiliano.girardi@canonical.com>
2024-05-22 14:38:18 +0200

    Use version manually in setup.py

--- checkbox-ng/setup.py
+++ checkbox-ng/setup.py
@@ -1,6 +1,12 @@
 # This helps older pip version properly install editable version of checkbox
 # inside the venv with `pip install -e .`
 
+import os
 from setuptools import setup
 
-setup()
+# this version adoption is a no-op in non-debian build as setuptools-scm does
+# the same. Some pybuild versions are not triggering this behaviour correctly
+# so this makes it uniform. Note that if this variable is missing the fallback
+# to setuptools-scm is unchanged also in debian packages (so the package will
+# get the default version calculated from setuptools-scm)
+setup(version=os.getenv("SETUPTOOLS_SCM_PRETEND_VERSION"))

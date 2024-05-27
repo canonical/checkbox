b419424fb57a888595ce0603dd3cfd2d5326a844
hanhsuan <hanhsuan.lee@canonical.com>
2024-05-27 16:42:12 +0800

    move test data to the test_data folder

--- providers/base/tests/test_gl_support.py
+++ providers/base/tests/test_gl_support.py
@@ -25,48 +25,21 @@ class RemoveColorCode(unittest.TestCase):
     This function should remove color code
     """
 
-    unchanged = r"""
-OpenGL vendor string:   Intel
-OpenGL renderer string: Mesa Intel(R) Arc(tm) Graphics (MTL)
-OpenGL version string:4.6 (Compatibility Profile)Mesa 23.2.1-1ubuntu3.1~22.04.2
-
-Not software rendered:    [32;01myes[00m
-Not blacklisted:          [32;01myes[00m
-GLX fbconfig:             [32;01myes[00m
-GLX texture from pixmap:  [32;01myes[00m
-GL npot or rect textures: [32;01myes[00m
-GL vertex program:        [32;01myes[00m
-GL fragment program:      [32;01myes[00m
-GL vertex buffer object:  [32;01myes[00m
-GL framebuffer object:    [32;01myes[00m
-GL version is 1.4+:       [32;01myes[00m
-
-Unity 3D supported:       [32;01myes[00m
-"""
-    changed = r"""
-OpenGL vendor string:   Intel
-OpenGL renderer string: Mesa Intel(R) Arc(tm) Graphics (MTL)
-OpenGL version string:4.6 (Compatibility Profile)Mesa 23.2.1-1ubuntu3.1~22.04.2
-
-Not software rendered:    yes
-Not blacklisted:          yes
-GLX fbconfig:             yes
-GLX texture from pixmap:  yes
-GL npot or rect textures: yes
-GL vertex program:        yes
-GL fragment program:      yes
-GL vertex buffer object:  yes
-GL framebuffer object:    yes
-GL version is 1.4+:       yes
-
-Unity 3D supported:       yes
-"""
-
     @patch("subprocess.run")
     def test_succ(self, mock_run):
         gs = GLSupport()
-        rv = gs.remove_color_code(self.unchanged)
-        self.assertEqual(rv, self.changed)
+
+        with (
+            open("tests/test_data/gl_support_succ.txt", "r") as s,
+            open("tests/test_data/gl_support_succ_changed.txt", "r") as sc,
+            open("tests/test_data/gl_support_fail.txt", "r") as f,
+            open("tests/test_data/gl_support_fail_changed.txt", "r") as fc,
+        ):
+            s_data = s.read()
+            rv = gs.remove_color_code(s_data)
+            self.assertEqual(rv, sc.read())
+            rv = gs.remove_color_code(f.read())
+            self.assertEqual(rv, fc.read())
 
 
 class IsSupportOpenGLTests(unittest.TestCase):

76939c49b3beaf9cd49bd3c48d249f51d1dd86ce
rickwu666666 <98441647+rickwu666666@users.noreply.github.com>
2024-06-07 16:35:53 +0800

    Add disconnect function to snapd class (New) (#1179)
    * Add disconnect function to snapd class
    
    We intend to test the slots for SNAP, and the disconnect function will be a part of our test scenario
    
    * Increase unittest coverage
    
    Fix format to fit black
    
    * Add new function connect_or_disconnect to replace redundant code
    
    * Modify unittest
    
    * Re-formatted to black

--- checkbox-support/checkbox_support/snap_utils/tests/test_snapd.py
+++ checkbox-support/checkbox_support/snap_utils/tests/test_snapd.py
@@ -148,3 +148,155 @@ class TestSnapd(TestCase):
         Snapd.remove(mock_self, "test", revision="1")
         test_data = {"action": "remove", "revision": "1"}
         mock_self._post.assert_called_with(ANY, json.dumps(test_data))
+
+    def test_connect_success(self):
+        mock_self = MagicMock()
+        mock_self._poll_change = MagicMock()
+        mock_self._post.return_value = {
+            "type": "async",
+            "status": "Accepted",
+            "change": "1",
+        }
+        slot_snap = "test_slot_snap"
+        slot_slot = "test_slot"
+        plug_snap = "test_plug_snap"
+        plug_plug = "test_plug"
+
+        Snapd.connect_or_disconnect(
+            mock_self, slot_snap, slot_slot, plug_snap, plug_plug
+        )
+        mock_self._post.assert_called_once_with(
+            mock_self._interfaces,
+            json.dumps(
+                {
+                    "action": "connect",
+                    "slots": [{"snap": slot_snap, "slot": slot_slot}],
+                    "plugs": [{"snap": plug_snap, "plug": plug_plug}],
+                }
+            ),
+        )
+        mock_self._poll_change.assert_called_with("1")
+
+    def test_connect_fail(self):
+        mock_self = MagicMock()
+        mock_self._poll_change = MagicMock()
+        mock_self._post.return_value = {
+            "type": "async",
+            "status": "Not_Accepted",
+            "change": "1",
+        }
+        slot_snap = "test_slot_snap"
+        slot_slot = "test_slot"
+        plug_snap = "test_plug_snap"
+        plug_plug = "test_plug"
+
+        Snapd.connect_or_disconnect(
+            mock_self, slot_snap, slot_slot, plug_snap, plug_plug
+        )
+        mock_self._post.assert_called_once_with(
+            mock_self._interfaces,
+            json.dumps(
+                {
+                    "action": "connect",
+                    "slots": [{"snap": slot_snap, "slot": slot_slot}],
+                    "plugs": [{"snap": plug_snap, "plug": plug_plug}],
+                }
+            ),
+        )
+        mock_self._poll_change.assert_not_called()
+
+    def test_disconnect_success(self):
+        mock_self = MagicMock()
+        mock_self._poll_change = MagicMock()
+        mock_self._post.return_value = {
+            "type": "async",
+            "status": "Accepted",
+            "change": "1",
+        }
+        slot_snap = "test_slot_snap"
+        slot_slot = "test_slot"
+        plug_snap = "test_plug_snap"
+        plug_plug = "test_plug"
+
+        Snapd.connect_or_disconnect(
+            mock_self,
+            slot_snap,
+            slot_slot,
+            plug_snap,
+            plug_plug,
+            action="disconnect",
+        )
+        mock_self._post.assert_called_once_with(
+            mock_self._interfaces,
+            json.dumps(
+                {
+                    "action": "disconnect",
+                    "slots": [{"snap": slot_snap, "slot": slot_slot}],
+                    "plugs": [{"snap": plug_snap, "plug": plug_plug}],
+                }
+            ),
+        )
+        mock_self._poll_change.assert_called_with("1")
+
+    def test_disconnect_fail(self):
+        mock_self = MagicMock()
+        mock_self._poll_change = MagicMock()
+        mock_self._post.return_value = {
+            "type": "async",
+            "status": "Not_Accepted",
+            "change": "1",
+        }
+        slot_snap = "test_slot_snap"
+        slot_slot = "test_slot"
+        plug_snap = "test_plug_snap"
+        plug_plug = "test_plug"
+
+        Snapd.connect_or_disconnect(
+            mock_self,
+            slot_snap,
+            slot_slot,
+            plug_snap,
+            plug_plug,
+            action="disconnect",
+        )
+        mock_self._post.assert_called_once_with(
+            mock_self._interfaces,
+            json.dumps(
+                {
+                    "action": "disconnect",
+                    "slots": [{"snap": slot_snap, "slot": slot_slot}],
+                    "plugs": [{"snap": plug_snap, "plug": plug_plug}],
+                }
+            ),
+        )
+        mock_self._poll_change.assert_not_called()
+
+    @patch.object(Snapd, "connect_or_disconnect")
+    def test_connect_called(self, mock_connect_or_disconnect):
+        snapd = Snapd()
+        slot_snap = "test_slot_snap"
+        slot_slot = "test_slot"
+        plug_snap = "test_plug_snap"
+        plug_plug = "test_plug"
+        snapd.connect(slot_snap, slot_slot, plug_snap, plug_plug)
+
+        mock_connect_or_disconnect.assert_called_once_with(
+            slot_snap, slot_slot, plug_snap, plug_plug
+        )
+
+    @patch.object(Snapd, "connect_or_disconnect")
+    def test_disconnect_called(self, mock_connect_or_disconnect):
+        snapd = Snapd()
+        slot_snap = "test_slot_snap"
+        slot_slot = "test_slot"
+        plug_snap = "test_plug_snap"
+        plug_plug = "test_plug"
+        snapd.disconnect(slot_snap, slot_slot, plug_snap, plug_plug)
+
+        mock_connect_or_disconnect.assert_called_once_with(
+            slot_snap,
+            slot_slot,
+            plug_snap,
+            plug_plug,
+            action="disconnect",
+        )

3ddaf7f767a0bc54b4b10117f5266e3753a73ade
LiaoU3 <58060146+LiaoU3@users.noreply.github.com>
2024-06-24 14:48:26 +0800

    [checkbox-ce-oem] Add SPI Test (New) (#1298)
    * Add SPI Test
    
    * Remove comments for requires
    
    * Fix black
    
    * Update manifest name
    
    * Use shlex.split()
    
    * Add checking for correct spi device number
    
    * Raise exit when invalid argument
    
    * Fix black
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/units/spi/jobs.pxu
    
    Co-authored-by: Weichen Wu <32293177+weizhenwu@users.noreply.github.com>
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/units/spi/jobs.pxu
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/units/spi/testplan.pxu
    
    Co-authored-by: stanley31huang <stanley.huang@canonical.com>
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/units/spi/testplan.pxu
    
    ---------
    
    Co-authored-by: Weichen Wu <32293177+weizhenwu@users.noreply.github.com>
    Co-authored-by: stanley31huang <stanley.huang@canonical.com>

---
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/spidev_test.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import glob
+import argparse
+import subprocess
+import shlex
+
+
+def runcmd(command):
+    ret = subprocess.run(
+        shlex.split(command),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        encoding="utf-8",
+        timeout=1,
+    )
+    return ret
+
+
+def detect_spi_node(expected_number=1, exit_with_notice=False):
+    spi_devices = glob.glob("/dev/spidev*")
+    if spi_devices:
+        for device in spi_devices:
+            print("device: {}".format(device))
+            print()
+    if exit_with_notice:
+        if len(spi_devices) != expected_number:
+            raise SystemExit(
+                "Wrong number of SPI devices detected under /dev/!\n"
+                "Expected: {}\n"
+                "Actual: {}".format(expected_number, len(spi_devices))
+            )
+
+
+def test_spi_content_consistency(spi_path):
+    """
+    Test the consistency of the SPI communication.
+
+    Args:
+        spi_path (str): The path to the SPI device.
+
+    Raises:
+        SystemExit: If no output is reported or
+        if the content is not consistent.
+    """
+
+    # Construct the command to run the SPI test
+    cmd = "spidev_test -D {} -p 'Canonical SPI Test' -v".format(spi_path)
+    print(f"Run command: {cmd}\n")
+
+    # Run the command and capture the output
+    spi_ret = runcmd(cmd)
+    print(spi_ret.stdout)
+
+    # Split the output into packets
+    packets = spi_ret.stdout.split("\n")
+
+    # Check if any output is reported
+    if not len(packets):
+        raise SystemExit("ERROR: no any output be reported")
+
+    # Check if the content is consistent between TX and RX
+    for rx, tx in zip(packets[-2:-1], packets[-3:-2]):
+        tx_content = tx.split("|")[2]
+        rx_content = rx.split("|")[2]
+
+        if tx_content != rx_content:
+            raise SystemExit(
+                "ERROR: the content is not consistent between TX and RX"
+            )
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="SPI Testing Tool",
+        description="This is a tool to help you test "
+        "Serial Peripheral Interface (SPI)",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--detect",
+        "-d",
+        type=int,
+        nargs="?",
+        const=1,
+        default=None,
+        help="Detect SPI node in the system. Optionally specify a number.",
+    )
+    parser.add_argument(
+        "--list",
+        "-l",
+        action="store_true",
+        help="List SPI node in the system, will not raise error if not found",
+    )
+    parser.add_argument(
+        "--test",
+        "-t",
+        action="store_true",
+        help="Test SPI content consistency",
+    )
+    parser.add_argument(
+        "--path",
+        "-p",
+        action="store",
+        default="/dev/spidev0.0",
+        help="SPI path",
+    )
+    args = parser.parse_args()
+
+    if args.detect:
+        detect_spi_node(args.detect, exit_with_notice=True)
+    elif args.list:
+        detect_spi_node(exit_with_notice=False)
+    elif args.test:
+        test_spi_content_consistency(args.path)
+    else:
+        raise SystemExit("ERROR: Invalid arguments")
+
+
+if __name__ == "__main__":
+    main()

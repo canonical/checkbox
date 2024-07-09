b819e3ad14987e82b068af6a86e90fb072ad4d55
Zhongning Li <zhongning.li@canonical.com>
2024-07-09 14:03:03 +0800

    fix: update tests to reflect changes in reboot_check_test

--- providers/base/tests/test_reboot_check_test.py
+++ providers/base/tests/test_reboot_check_test.py
@@ -7,11 +7,14 @@ import os
 import typing as T
 
 
-def do_nothing_run_cmd(_: T.List[str]):
+def do_nothing(_: T.List[str]):
     return RCT.ShellResult(0, "", "")
 
 
 class UnitySupportParserTests(unittest.TestCase):
+    def setUp(self):
+        self.tester = RCT.HardwareRendererTester()
+
     def test_parse_ok_unity_support_string(self):
         OK_UNITY_STRING = """\
         OpenGL vendor string:   Intel
@@ -49,7 +52,7 @@ class UnitySupportParserTests(unittest.TestCase):
             "Unity 3D supported": "yes",
         }
 
-        actual = RCT.parse_unity_support_output(OK_UNITY_STRING)
+        actual = self.tester.parse_unity_support_output(OK_UNITY_STRING)
         self.assertDictEqual(expected, actual)
 
     def test_parse_bad_unity_support_string(self):
@@ -59,7 +62,7 @@ class UnitySupportParserTests(unittest.TestCase):
         OpenGL version string  4.6 (Compatibility Profile) Mesa 23.2.1-1ubuntu3.1~22.04.2
         GL version is 1.4+%       \x1B[033myes\x1B[0m
         """
-        actual = RCT.parse_unity_support_output(BAD_UNITY_STRING)
+        actual = self.tester.parse_unity_support_output(BAD_UNITY_STRING)
 
         expected = {
             "OpenGL renderer string": "Mesa Intel(R) UHD Graphics (ICL GT1)",
@@ -69,59 +72,99 @@ class UnitySupportParserTests(unittest.TestCase):
 
         ARBITRARY_STRING = "askjaskdnasdn"
         # should return empty dict if input string literally doesn't make sense
-        self.assertEqual(RCT.parse_unity_support_output(ARBITRARY_STRING), {})
+        self.assertEqual(
+            self.tester.parse_unity_support_output(ARBITRARY_STRING),
+            {},
+        )
 
 
 class DisplayConnectionTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.tester = RCT.HardwareRendererTester()
+
+    def create_side_effect(
+        self,
+        display_server_name: 'T.Literal["wayland", "x11", "tty"]',
+        happy_path=True,
+    ):
+        def side_effect(args: T.List[str]):
+            stdout = ""
+            if args[0] == "loginctl":
+                stdout = "Type={}".format(display_server_name)
+            if args[0] == "pgrep":
+                if happy_path:
+                    stdout = "75632 /usr/bin/Xwayland :0 -rootless -noreset -accessx -core -auth /run/user/1000/.mutter-Xwaylandauth.FFE5P2"
+                else:
+                    stdout = ""  # Xwayland is not guaranteed to exist
+            if args[0] == "w":
+                if happy_path:
+                    stdout = "ubuntu :0 :0 13:43 13:55m  0.01s  0.00s /usr/libexec/gdm-wayland-session en"
+                else:
+                    stdout = "ubuntu tty2 tty2 Mon13 35:51m  0.01s  0.00s /usr/libexec/gdm-wayland-session en"
+
+            return RCT.ShellResult(0, stdout, "")
+
+        return side_effect
+
     def test_display_check_happy_path(self):
-        with patch("os.listdir", return_value=["card0", "card1"]), patch(
+        with patch(
+            "os.listdir", return_value=["fakeCard0", "fakeCard1"]
+        ), patch(
             "builtins.open",
             new_callable=mock_open,
             read_data="connected",
         ):
-            self.assertTrue(RCT.has_display_connection())
+            self.assertTrue(self.tester.has_display_connection())
 
     def test_display_check_no_display_path(self):
         with patch("os.listdir", return_value=["version"]):
-            self.assertFalse(RCT.has_display_connection())
-        with patch("os.listdir", return_value=["card0", "card1"]), patch(
+            self.assertFalse(self.tester.has_display_connection())
+        with patch(
+            "os.listdir", return_value=["fakeCard0", "fakeCard1"]
+        ), patch(
             "builtins.open",
             new_callable=mock_open,
             read_data="not connected",
         ):
-            self.assertFalse(RCT.has_display_connection())
+            self.assertFalse(self.tester.has_display_connection())
 
-    def test_is_hardware_renderer_available(self):
-        self.assertTrue(RCT.is_hardware_renderer_available())
+    @patch("reboot_check_test.HardwareRendererTester.get_display_id")
+    @patch(
+        "reboot_check_test.HardwareRendererTester.parse_unity_support_output"
+    )
+    @patch("reboot_check_test.run_command")
+    def test_is_hardware_renderer_available(
+        self,
+        mock_run: MagicMock,
+        mock_parse: MagicMock,
+        mock_get_display_id: MagicMock,
+    ):
+        mock_run.side_effect = do_nothing
+        mock_get_display_id.return_value = ":0"
+        mock_parse.return_value = {
+            "Not software rendered": "yes",
+        }
+        tester = RCT.HardwareRendererTester()
+        self.assertTrue(tester.is_hardware_renderer_available())
 
     @patch("reboot_check_test.run_command")
     def test_get_display_id(self, mock_run: MagicMock):
         with patch.dict(os.environ, {"DISPLAY": ":0"}):
-            self.assertEqual(RCT.get_display_id(), ":0")
-
-        def create_side_effect(
-            display_server_name: 'T.Literal["wayland", "x11", "tty"]',
-        ):
-            def side_effect(args: T.List[str]):
-                stdout = ""
-                if args[0] == "loginctl":
-                    stdout = "Type={}".format(display_server_name)
-                if args[0] == "pgrep":
-                    stdout = "75632 /usr/bin/Xwayland :0 -rootless -noreset -accessx -core -auth /run/user/1000/.mutter-Xwaylandauth.FFE5P2"
-                if args[0] == "w":
-                    stdout = "ubuntu :0 :0 13:43 13:55m  0.01s  0.00s /usr/libexec/gdm-wayland-session en"
-
-                return RCT.ShellResult(0, stdout, "")
-
-            return side_effect
+            self.assertEqual(self.tester.get_display_id(), ":0")
 
         with patch.dict(os.environ, {"DISPLAY": ""}):
-            mock_run.side_effect = create_side_effect("wayland")
-            self.assertEqual(RCT.get_display_id(), ":0")
+            mock_run.side_effect = self.create_side_effect("wayland")
+            self.assertEqual(self.tester.get_display_id(), ":0")
 
-            mock_run.reset_mock()
-            mock_run.side_effect = create_side_effect("x11")
-            self.assertEqual(RCT.get_display_id(), ":0")
+            mock_run.side_effect = self.create_side_effect("wayland", False)
+            self.assertEqual(self.tester.get_display_id(), ":0")
+
+            mock_run.side_effect = self.create_side_effect("x11")
+            self.assertEqual(self.tester.get_display_id(), ":0")
+
+            mock_run.side_effect = self.create_side_effect("x11", False)
+            self.assertIsNone(self.tester.get_display_id())
 
 
 class InfoDumpTests(unittest.TestCase):
@@ -195,13 +238,14 @@ class InfoDumpTests(unittest.TestCase):
                 self.temp_comparison_dir, self.temp_output_dir
             )
         )
-    
+
         collector.dump(self.temp_comparison_dir)
 
         # optional
         with open(
             "{}/{}_log".format(
-                self.temp_comparison_dir, RCT.DeviceInfoCollector.Device.DRM.value
+                self.temp_comparison_dir,
+                RCT.DeviceInfoCollector.Device.DRM.value,
             ),
             "w",
         ) as f:
@@ -214,19 +258,45 @@ class InfoDumpTests(unittest.TestCase):
         )
 
 
+class FailedServiceCheckerTests(unittest.TestCase):
+
+    @patch("reboot_check_test.run_command")
+    def test_get_failed_services_happy_path(self, mock_run: MagicMock):
+        mock_run.return_value = RCT.ShellResult(0, "", "")
+        self.assertEqual(RCT.get_failed_services(), [])
+
+    @patch("reboot_check_test.run_command")
+    def test_get_failed_services_with_failed_services(
+        self, mock_run: MagicMock
+    ):
+        mock_run.return_value = RCT.ShellResult(
+            0,
+            "snap.checkbox.agent.service loaded failed failed Service for snap applictaion checkbox.agent",
+            "",
+        )
+        self.assertEqual(RCT.get_failed_services(), [mock_run.return_value.stdout])
+
+
 class MainFunctionTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.temp_directory = "{}/temporary_dump_directory".format(os.getcwd())
+        cls.temp_output_dir = "{}/temp_output_dir".format(os.getcwd())
+        cls.temp_comparison_dir = "{}/temp_comparison_dir".format(os.getcwd())
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_output_dir, ignore_errors=True)
+        shutil.rmtree(self.temp_comparison_dir, ignore_errors=True)        
 
     @patch("reboot_check_test.run_command")
     def test_partial_main(self, mock_run: MagicMock):
         # this test only validates the main function logic(if it picks out the correct tests to run)
-        mock_run.side_effect = do_nothing_run_cmd
+        mock_run.side_effect = do_nothing
 
         with patch(
             "sys.argv",
-            sh_split("reboot_check_test.py -d {}".format(self.temp_directory)),
+            sh_split(
+                "reboot_check_test.py -d {}".format(self.temp_output_dir)
+            ),
         ):
             RCT.main()
             self.assertEqual(
@@ -240,7 +310,7 @@ class MainFunctionTests(unittest.TestCase):
             "sys.argv",
             sh_split(
                 'reboot_check_test.py -d "{}" -c "{}"'.format(
-                    self.temp_directory, "some dir"
+                    self.temp_output_dir, self.temp_comparison_dir
                 )
             ),
         ), patch(
@@ -254,35 +324,38 @@ class MainFunctionTests(unittest.TestCase):
             )  # only lspci, lsusb, iw calls
             self.assertEqual(mock_compare.call_count, 1)
 
-    @patch("reboot_check_test.get_display_id")
+    @patch("reboot_check_test.HardwareRendererTester.get_display_id")
     @patch("reboot_check_test.run_command")
     def test_main_function_full(
         self, mock_run: MagicMock, mock_get_display_id: MagicMock
     ):
-        mock_run.side_effect = do_nothing_run_cmd
+        mock_run.side_effect = do_nothing
         mock_get_display_id.return_value = ":0"
         # Full suite
         with patch(
             "sys.argv",
             sh_split(
                 'reboot_check_test.py -d "{}" -c "{}" -f -s -g'.format(
-                    self.temp_directory, "some dir"
+                    self.temp_output_dir, self.temp_comparison_dir
                 )
             ),
         ), patch(
             "reboot_check_test.DeviceInfoCollector.compare_device_lists"
         ) as mock_compare, patch(
-            "reboot_check_test.is_fwts_supported"
-        ) as mock_fwts_support_check:
-            mock_fwts_support_check.return_value = True
+            "reboot_check_test.FwtsTester.is_fwts_supported"
+        ) as mock_is_fwts_supported:
+            mock_is_fwts_supported.return_value = True
+            mock_compare.return_value = True 
+
             RCT.main()
 
-            self.assertTrue(mock_compare.called)
-            self.assertTrue(mock_fwts_support_check.called)
+            # self.assertTrue(mock_compare.called)
+            self.assertTrue(mock_is_fwts_supported.called)
 
             expected_commands = {
                 "systemctl",
                 "sleep_test_log_check.py",
+                "fwts",
             }
 
             actual = set()
@@ -293,8 +366,6 @@ class MainFunctionTests(unittest.TestCase):
                 actual.add(call[0][0][0])
 
             # <= is an overloaded operator for sets that checks the isSubset relation
-            self.assertLessEqual(expected_commands, actual)
-
-    @classmethod
-    def tearDownClass(cls):
-        shutil.rmtree(cls.temp_directory)
+            self.assertLessEqual(
+                expected_commands, actual, "should be a subset"
+            )

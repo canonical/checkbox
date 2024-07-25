f96213d8c3242a9474afacdf4959063e9b3e14c0
Paolo Gentili <paolo.gentili@canonical.com>
2024-07-25 10:20:54 +0200

    Set extend mode after plugging a new monitor (BugFix) (#1286)
    * Change: replaced xrandr/gnome-randr with dbus signals for EDID test
    
    * Change: moved monitor config dbus to checkbox support
    
    * Fix: tests for edid cycle
    
    * fixup! Fix: tests for edid cycle
    
    * Add: reference to original script
    
    * Change: renamed Gnome Monitor class and files
    
    * Add: abstract class for monitor config handlers
    
    * Add: xrandr parser
    
    * Fix: gnome class name
    
    * Change: helper to return the proper monitor config
    
    * Add: tests for xrandr parser
    
    * Change: preferred mode for dbus as well
    
    * Change: moved named tuple back to xrandr
    
    * Fix: moved more zapper-related function under the CM
    
    * Fix: sorted dictionary
    
    * Fix: docstrings and another test
    
    * Fix: removed get max function

---
+++ checkbox-support/checkbox_support/dbus/tests/test_gnome_monitor.py
@@ -0,0 +1,164 @@
+import sys
+import unittest
+from unittest.mock import patch, Mock, MagicMock
+
+sys.modules["dbus"] = MagicMock()
+sys.modules["dbus.mainloop.glib"] = MagicMock()
+sys.modules["gi"] = MagicMock()
+sys.modules["gi.repository"] = MagicMock()
+
+from gi.repository import GLib, Gio
+from checkbox_support.dbus.gnome_monitor import MonitorConfigGnome
+
+
+class MonitorConfigGnomeTests(unittest.TestCase):
+    """This class provides test cases for the MonitorConfig DBus class."""
+
+    @patch("checkbox_support.dbus.gnome_monitor.Gio.DBusProxy")
+    def test_get_current_resolution(self, mock_dbus_proxy):
+        """
+        Test whether the function returns a dictionary of
+        monitor-id:resolution for any active monitors.
+        """
+
+        mock_proxy = Mock()
+        mock_dbus_proxy.new_for_bus_sync.return_value = mock_proxy
+
+        gnome_monitor = MonitorConfigGnome()
+        mock_proxy.call_sync.return_value = (
+            1,
+            [
+                (
+                    ("eDP-1", "LGD", "0x06b3", "0x00000000"),
+                    [
+                        (
+                            "1920x1200@59.950",
+                            1920,
+                            1200,
+                            59.950172424316406,
+                            1.0,
+                            [1.0, 2.0],
+                            {
+                                "is-current": GLib.Variant("b", True),
+                                "is-preferred": GLib.Variant("b", True),
+                            },
+                        )
+                    ],
+                    {
+                        "is-builtin": GLib.Variant("b", True),
+                        "display-name": GLib.Variant("s", "Built-in display"),
+                    },
+                ),
+                (
+                    ("HDMI-1", "LGD", "0x06b3", "0x00000000"),
+                    [
+                        (
+                            "2560x1440@59.950",
+                            2560,
+                            1440,
+                            59.950172424316406,
+                            1.0,
+                            [1.0, 2.0],
+                            {
+                                "is-current": GLib.Variant("b", True),
+                                "is-preferred": GLib.Variant("b", True),
+                            },
+                        )
+                    ],
+                    {
+                        "is-builtin": GLib.Variant("b", False),
+                        "display-name": GLib.Variant("s", "External Display"),
+                    },
+                ),
+            ],
+            [],
+            {},
+        )
+        resolutions = gnome_monitor.get_current_resolutions()
+        self.assertEqual(
+            resolutions, {"eDP-1": "1920x1200", "HDMI-1": "2560x1440"}
+        )
+
+    @patch("checkbox_support.dbus.gnome_monitor.Gio.DBusProxy")
+    def test_set_extended_mode(self, mock_dbus_proxy):
+        """
+        Test whether the function set the logical display
+        configuration to two screens at preferred resolution
+        placed horizontally.
+        """
+
+        mock_proxy = Mock()
+        mock_dbus_proxy.new_for_bus_sync.return_value = mock_proxy
+
+        gnome_monitor = MonitorConfigGnome()
+        mock_proxy.call_sync.return_value = (
+            1,
+            [
+                (
+                    ("eDP-1", "LGD", "0x06b3", "0x00000000"),
+                    [
+                        (
+                            "1920x1200@59.950",
+                            1920,
+                            1200,
+                            59.950172424316406,
+                            1.0,
+                            [1.0, 2.0],
+                            {
+                                "is-current": GLib.Variant("b", True),
+                                "is-preferred": GLib.Variant("b", True),
+                            },
+                        )
+                    ],
+                    {
+                        "is-builtin": GLib.Variant("b", True),
+                        "display-name": GLib.Variant("s", "Built-in display"),
+                    },
+                ),
+                (
+                    ("HDMI-1", "LGD", "0x06b3", "0x00000000"),
+                    [
+                        (
+                            "2560x1440@59.950",
+                            2560,
+                            1440,
+                            59.950172424316406,
+                            1.0,
+                            [1.0, 2.0],
+                            {
+                                "is-current": GLib.Variant("b", True),
+                                "is-preferred": GLib.Variant("b", True),
+                            },
+                        )
+                    ],
+                    {
+                        "is-builtin": GLib.Variant("b", False),
+                        "display-name": GLib.Variant("s", "External Display"),
+                    },
+                ),
+            ],
+            [],
+            {},
+        )
+        gnome_monitor.set_extended_mode()
+
+        logical_monitors = [
+            (0, 0, 1.0, 0, True, [("eDP-1", "1920x1200@59.950", {})]),
+            (1920, 0, 1.0, 0, False, [("HDMI-1", "2560x1440@59.950", {})]),
+        ]
+        expected_logical_monitors = GLib.Variant(
+            "(uua(iiduba(ssa{sv}))a{sv})",
+            (
+                1,
+                1,
+                logical_monitors,
+                {},
+            ),
+        )
+        mock_proxy.call_sync.assert_called_with(
+            method_name="ApplyMonitorsConfig",
+            parameters=expected_logical_monitors,
+            flags=Gio.DBusCallFlags.NONE,
+            timeout_msec=-1,
+            cancellable=None,
+        )

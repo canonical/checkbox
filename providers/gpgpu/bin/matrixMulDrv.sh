aca24be12fd0f1101d4058eb0da413c70f6286a7
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2024-07-29 17:52:49 -0500

    Build executables into `bin/` and `data/`
    This commit changes the gpu-setup script behaviour to build the cuda-samples
    and gpu-burn projects inside the `build/bin` directory, then copy them
    out into the `bin/` and (the necessary data files) into `data/`. For the
    cuda-samples executables to work, they need access to the data files,
    but they do not take the path to the data dir as an argument; to
    circumvent this limitation, I have made wrapper scripts that copy the
    necessary file into the temporary working directory that checkbox
    creates.
    
    Because of the change in build behaviour, the `gpu-setup` script now runs
    mostly as a regular user (to avoid permission issues when cleaning
    directories/builds). The expected operation now is to run `./manage.py build`
    instead of running the `gpu-setup.sh` script itself. This is more inline
    with what is done with the other providers.

---
+++ providers/gpgpu/bin/matrixMulDrv.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+cp $PLAINBOX_PROVIDER_DATA/matrixMul_kernel64.fatbin .
+matrixMulDrv
---
+++ providers/gpgpu/bin/vectorAddDrv.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+cp $PLAINBOX_PROVIDER_DATA/vectorAdd_kernel64.fatbin .
+vectorAddDrv

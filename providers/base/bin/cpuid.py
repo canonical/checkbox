1fc2d5bfa7bffa694931f5ee7e76862510c976c2
Hook25 <massimiliano.girardi@canonical.com>
2024-08-07 14:57:08 +0200

    Fixed missing id for cpuid turin
    Minor: Fix bug, print the id string, not the object, so that it is easier to fetch

--- providers/base/bin/cpuid.py
+++ providers/base/bin/cpuid.py
@@ -178,6 +178,7 @@ def cpuid_to_human_friendly(cpuid: str) -> str:
         "AMD Bergamo":      ['0xaa0f01'],
         "AMD Siena SP6":    ['0xaa0f02'],
         "AMD Raphael":      ['0xa60f12'],
+        "AMD Turin":        ['0xb00f21'],
         "Broadwell":        ['0x4067', '0x306d4', '0x5066', '0x406f'],
         "Canon Lake":       ['0x6066'],
         "Cascade Lake":     ['0x50655', '0x50656', '0x50657'],
@@ -231,7 +232,7 @@ def main():
             my_id, nice_name))
     except ValueError:
         raise SystemExit(
-            "Unable to determine CPU Family for this CPUID: {}".format(cpuid))
+            "Unable to determine CPU Family for this CPUID: {}".format(my_id))
 
 
 if __name__ == "__main__":

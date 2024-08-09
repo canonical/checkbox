b343b266df93bdaf34fa5a0fc12b12eeb25c17ae
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2024-08-09 16:39:45 -0500

    More `main` tests.

--- providers/base/tests/test_stress_ng_test.py
+++ providers/base/tests/test_stress_ng_test.py
@@ -18,6 +18,7 @@
 
 import unittest
 from unittest.mock import patch
+from subprocess import CalledProcessError, TimeoutExpired
 
 from stress_ng_test import main
 
@@ -33,3 +34,48 @@ class TestMainFunction(unittest.TestCase):
     @patch("stress_ng_test.sys.argv", ["stress_ng_test.py", "cpu"])
     def test_main_not_root(self, shutil_which_mock, os_geteuid_mock):
         self.assertEqual(main(), 1)
+
+    @patch("stress_ng_test.check_output", side_effect=FileNotFoundError)
+    @patch("stress_ng_test.os.geteuid", return_value=0)
+    @patch("stress_ng_test.shutil.which", return_value="/usr/bin/stress-ng")
+    @patch("stress_ng_test.sys.argv", ["stress_ng_test.py", "cpu"])
+    def test_main_stress_ng_not_found(
+        self, shutil_which_mock, os_geteuid_mock, check_output_mock
+    ):
+        self.assertEqual(main(), 1)
+
+    @patch("stress_ng_test.check_output", side_effect=TimeoutExpired(b"", 1))
+    @patch("stress_ng_test.os.geteuid", return_value=0)
+    @patch("stress_ng_test.shutil.which", return_value="/usr/bin/stress-ng")
+    @patch("stress_ng_test.sys.argv", ["stress_ng_test.py", "cpu"])
+    def test_main_timeout(
+        self, shutil_which_mock, os_geteuid_mock, check_output_mock
+    ):
+        self.assertEqual(main(), 1)
+
+    @patch("stress_ng_test.check_output", side_effect=KeyboardInterrupt)
+    @patch("stress_ng_test.os.geteuid", return_value=0)
+    @patch("stress_ng_test.shutil.which", return_value="/usr/bin/stress-ng")
+    @patch("stress_ng_test.sys.argv", ["stress_ng_test.py", "cpu"])
+    def test_main_keyboard_interrupt(
+        self, shutil_which_mock, os_geteuid_mock, check_output_mock
+    ):
+        self.assertEqual(main(), 1)
+
+    @patch("stress_ng_test.check_output", side_effect=CalledProcessError(1, b"", b""))
+    @patch("stress_ng_test.os.geteuid", return_value=0)
+    @patch("stress_ng_test.shutil.which", return_value="/usr/bin/stress-ng")
+    @patch("stress_ng_test.sys.argv", ["stress_ng_test.py", "cpu"])
+    def test_main_stress_ng_error(
+        self, shutil_which_mock, os_geteuid_mock, check_output_mock
+    ):
+        self.assertEqual(main(), 1)
+
+    @patch("stress_ng_test.check_output")
+    @patch("stress_ng_test.os.geteuid", return_value=0)
+    @patch("stress_ng_test.shutil.which", return_value="/usr/bin/stress-ng")
+    @patch("stress_ng_test.sys.argv", ["stress_ng_test.py", "cpu"])
+    def test_main_stress_cpu_success(
+        self, shutil_which_mock, os_geteuid_mock, check_output_mock
+    ):
+        self.assertEqual(main(), 0)

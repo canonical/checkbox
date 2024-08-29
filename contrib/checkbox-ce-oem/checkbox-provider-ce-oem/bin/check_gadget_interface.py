6bd49347738e02f005d952fe844de015d0fc4fbb
stanley31huang <stanley.huang@canonical.com>
2024-08-29 11:43:26 +0800

    add tests to validate defined interface in gadget
    add test cases to validate those plug and slot interface defined in
    gadget snap

---
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/check_gadget_interface.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+import sys
+import copy
+import logging
+import argparse
+from checkbox_support.snap_utils.snapd import Snapd
+from checkbox_support.snap_utils.system import get_gadget_snap
+
+
+def register_arguments():
+    parser = argparse.ArgumentParser(
+        description="Validate the slot and plug interface"
+    )
+    parser.add_argument(
+        "--type",
+        type=str,
+        required=True,
+        choices=["plug", "slot"],
+        help="the interface type of snap",
+    )
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        "--interface",
+        type=str,
+        required=True
+    )
+    parser.add_argument(
+        "--attrs",
+        type=str,
+    )
+    return parser.parse_args()
+
+
+def filter_gadget_interface(type):
+    gadget_snap = get_gadget_snap()
+    interface_key = "{}s".format(type)
+    system_snap_interfaces = Snapd().interfaces()[interface_key]
+
+    for interface in copy.deepcopy(system_snap_interfaces):
+        if interface["snap"] != gadget_snap:
+            system_snap_interfaces.remove(interface)
+
+    return system_snap_interfaces
+
+
+def varify_slot_interface(name, interface):
+    logging.info(
+        "[Expected Slot Interface] name: %s, type: %s",
+        name,
+        interface
+    )
+    gadget_interfaces = filter_gadget_interface("slot")
+    for sys_intf in gadget_interfaces:
+        if sys_intf["slot"] != name:
+            continue
+
+        logging.info(
+            "[Actual Slot Interface] name: %s, type: %s\n",
+            sys_intf["slot"],
+            sys_intf["interface"]
+        )
+        if sys_intf["interface"] == interface:
+            logging.info(
+                "The interface type of %s slot interface match",
+                name
+            )
+            exit(0)
+        else:
+            logging.error(
+                "The interface type of %s slot interface mismatch",
+                name)
+            exit(1)
+
+    logging.error("The %s slot interface is not defined in gadget", name)
+    exit(1)
+
+
+def varify_plug_interface(name, interface):
+    logging.info(
+        "[Expected Plug Interface] name: %s, type: %s",
+        name,
+        interface
+    )
+
+    gadget_interfaces = filter_gadget_interface("plug")
+
+    for sys_intf in gadget_interfaces:
+        if sys_intf["plug"] != name:
+            continue
+
+        logging.info(
+            "[Actual Plug Interface] name: %s, type: %s\n",
+            sys_intf["plug"],
+            sys_intf["interface"]
+        )
+        if sys_intf["interface"] == interface:
+            logging.info(
+                "The interface type of %s plug interface match",
+                name)
+            exit(0)
+        else:
+            logging.error(
+                "The interface type of %s plug interface mismatch",
+                name)
+            exit(1)
+
+    logging.error("The %s plug interface is not defined in gadget", name)
+    exit(1)
+
+
+def main():
+    args = register_arguments()
+
+    if args.type == "slot":
+        # AI: to verify the slot attrs
+        varify_slot_interface(args.name, args.interface)
+    elif args.type == "plug":
+        varify_plug_interface(args.name, args.interface)
+    else:
+        raise ValueError("Unsupported action")
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+        ]
+    )
+    logging.basicConfig(
+        level=logging.ERROR,
+        format="%(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stderr),
+        ]
+    )
+    main()
---
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/gadget_interface_utils.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+import logging
+import argparse
+from checkbox_support.snap_utils.snapd import Snapd
+from checkbox_support.snap_utils.system import get_gadget_snap
+
+
+DEFAULT_SCHEMA = "gadget_interface_schema.json"
+
+
+def register_arguments():
+    parser = argparse.ArgumentParser(
+        description=(
+            "This is a script has following method"
+            " 1. generate a gadget interface JSON file"
+            " 2. generate checkbox resource from gadget interface file"
+        )
+    )
+    parser.add_argument(
+        "--file",
+        type=str,
+        required=True,
+        help="The path with file name of the config file",
+    )
+    parser.add_argument(
+        "--action",
+        type=str,
+        choices=["generate", "dump"]
+    )
+    return parser.parse_args()
+
+
+def schema_validation(content: dict, schema_file: str = DEFAULT_SCHEMA):
+    import jsonschema
+
+    with open(schema_file, "r") as fp:
+        schema_content = json.load(fp)
+
+    jsonschema.validate(content, schema_content)
+
+
+def _generate_plug_data(interface: dict):
+    if interface.get("interface"):
+        return {
+            "name": interface["plug"],
+            "interface": interface["interface"],
+        }
+    else:
+        logging.error("Incorrect plug interface data: %s", interface)
+
+
+def _generate_slot_data(interface: dict):
+    if interface.get("interface"):
+        return {
+            "name": interface["slot"],
+            "interface": interface["interface"],
+            "attrs": interface["attrs"],
+        }
+    else:
+        logging.error("Incorrect slot interface data: %s", interface)
+
+
+def gadget_interface_generator(
+    file: str,
+    by_gadget: bool = True,
+    validate_by_schema: bool = False
+):
+    func_mapping = {
+        "plugs": _generate_plug_data,
+        "slots": _generate_slot_data
+    }
+    snap_interfaces = {"plugs": [], "slots": []}
+
+    if by_gadget:
+        gadget_snap = get_gadget_snap()
+        system_snap_interfaces = Snapd().interfaces()
+        for key, func in func_mapping.items():
+            for interface in system_snap_interfaces[key]:
+                if interface["snap"] != gadget_snap:
+                    continue
+                snap_interfaces[key].append(func(interface))
+    else:
+        pass
+
+    if validate_by_schema:
+        schema_validation(snap_interfaces)
+
+    with open(file, "w") as fp:
+        json.dump(snap_interfaces, fp, indent=4)
+
+    logging.info("Generate gadget slot and plug interface to %s", file)
+
+
+def _print_formatted_slot_data(interface):
+    required_keys = ["name", "interface", "attrs"]
+    for key in required_keys:
+        print("{}: {}".format(key, interface[key]))
+    print("type: slot")
+    print()
+
+
+def _print_formatted_plug_data(interface):
+    required_keys = ["name", "interface"]
+    for key in required_keys:
+        print("{}: {}".format(key, interface[key]))
+    print("type: plug")
+    print()
+
+
+def gadget_interface_parser(file: str, validate_by_schema: bool = False):
+    func_mapping = {
+        "plugs": _print_formatted_plug_data,
+        "slots": _print_formatted_slot_data
+    }
+    if not os.path.exists(file):
+        print("file: gadget file not found")
+        exit()
+
+    with open(file, "r") as fp:
+        snap_interfaces = json.load(fp)
+
+    if validate_by_schema:
+        schema_validation(snap_interfaces)
+
+    for key, func in func_mapping.items():
+        for interface in snap_interfaces[key]:
+            func(interface)
+
+
+def main():
+    args = register_arguments()
+    if args.action == "generate":
+        gadget_interface_generator(args.file)
+    elif args.action == "dump":
+        gadget_interface_parser(args.file)
+    else:
+        raise ValueError("Unsupported action")
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+        ]
+    )
+    logging.basicConfig(
+        level=logging.ERROR,
+        format="%(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stderr),
+        ]
+    )
+    main()

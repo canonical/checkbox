702b66f37272aeb0e5664af6e9148e7b42523410
stanley31huang <stanley.huang@canonical.com>
2024-09-02 13:37:01 +0800

    update schema file path
    update schema file path

--- contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/gadget_interface_utils.py
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/gadget_interface_utils.py
@@ -8,7 +8,10 @@ from checkbox_support.snap_utils.snapd import Snapd
 from checkbox_support.snap_utils.system import get_gadget_snap
 
 
-DEFAULT_SCHEMA = "gadget_interface_schema.json"
+DEFAULT_SCHEMA = os.path.join(
+    os.environ.get("PLAINBOX_PROVIDER_DATA", ""),
+    "gadget_interface_schema.json"
+)
 
 
 def register_arguments():

75ee35b865f00632ca82ddbe5ec89b647be93c33
stanley31huang <stanley.huang@canonical.com>
2024-09-02 16:12:33 +0800

    [checkbox-ce-oem] add test cases to validate those plug and slot interface defined in gadget snap (New) (#1439)
    * add tests to validate defined interface in gadget
    
    add test cases to validate those plug and slot interface defined in
    gadget snap
    
    * fix black8 issue
    
    fix format issue reported by black8
    
    * fix black8 issue
    
    fix black8 issue
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/units/gadget/manifest.pxu
    
    Co-authored-by: Pei Yao-Chang <baconYao@users.noreply.github.com>
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/check_gadget_interface.py
    
    Co-authored-by: Pei Yao-Chang <baconYao@users.noreply.github.com>
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/check_gadget_interface.py
    
    Co-authored-by: Pei Yao-Chang <baconYao@users.noreply.github.com>
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/check_gadget_interface.py
    
    Co-authored-by: Pei Yao-Chang <baconYao@users.noreply.github.com>
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/check_gadget_interface.py
    
    Co-authored-by: Pei Yao-Chang <baconYao@users.noreply.github.com>
    
    * Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/units/gadget/jobs.pxu
    
    * Move gadget_interface_schema.json to data folder
    
    move gadget_interface_schema.json to data folder
    
    * update schema file path
    
    update schema file path
    
    ---------
    
    Co-authored-by: Pei Yao-Chang <baconYao@users.noreply.github.com>

---
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/check_gadget_interface.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+import sys
+import copy
+import logging
+import argparse
+from checkbox_support.snap_utils.snapd import Snapd
+from checkbox_support.snap_utils.system import get_gadget_snap
+
+
+def register_arguments():
+    parser = argparse.ArgumentParser(
+        description="Validate the slot and plug interface"
+    )
+    parser.add_argument(
+        "--type",
+        type=str,
+        required=True,
+        choices=["plug", "slot"],
+        help="the interface type of snap",
+    )
+    parser.add_argument("--name", type=str, required=True)
+    parser.add_argument("--interface", type=str, required=True)
+    parser.add_argument("--attrs", type=str)
+    return parser.parse_args()
+
+
+def filter_gadget_interface(type):
+    gadget_snap = get_gadget_snap()
+    interface_key = "{}s".format(type)
+    system_snap_interfaces = Snapd().interfaces()[interface_key]
+
+    for interface in copy.deepcopy(system_snap_interfaces):
+        if interface["snap"] != gadget_snap:
+            system_snap_interfaces.remove(interface)
+
+    return system_snap_interfaces
+
+
+def verify_slot_interface(name, interface):
+    logging.info(
+        "[Expected Slot Interface] name: %s, type: %s", name, interface
+    )
+    gadget_interfaces = filter_gadget_interface("slot")
+    for sys_intf in gadget_interfaces:
+        if sys_intf["slot"] != name:
+            continue
+
+        logging.info(
+            "[Actual Slot Interface] name: %s, type: %s\n",
+            sys_intf["slot"],
+            sys_intf["interface"],
+        )
+        if sys_intf["interface"] == interface:
+            logging.info("The slot interface type of %s match", name)
+            exit(0)
+        else:
+            logging.error("The slot interface type of %s mismatch", name)
+            exit(1)
+
+    logging.error("The %s slot interface is not defined in gadget", name)
+    exit(1)
+
+
+def verify_plug_interface(name, interface):
+    logging.info(
+        "[Expected Plug Interface] name: %s, type: %s", name, interface
+    )
+
+    gadget_interfaces = filter_gadget_interface("plug")
+
+    for sys_intf in gadget_interfaces:
+        if sys_intf["plug"] != name:
+            continue
+
+        logging.info(
+            "[Actual Plug Interface] name: %s, type: %s\n",
+            sys_intf["plug"],
+            sys_intf["interface"],
+        )
+        if sys_intf["interface"] == interface:
+            logging.info("The plug interface type of %s match", name)
+            exit(0)
+        else:
+            logging.error("The plug interface type of %s mismatch", name)
+            exit(1)
+
+    logging.error("The %s plug interface is not defined in gadget", name)
+    exit(1)
+
+
+def main():
+    args = register_arguments()
+
+    if args.type == "slot":
+        # AI: to verify the slot attrs
+        verify_slot_interface(args.name, args.interface)
+    elif args.type == "plug":
+        verify_plug_interface(args.name, args.interface)
+    else:
+        raise ValueError("Unsupported action")
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stdout),
+        ],
+    )
+    logging.basicConfig(
+        level=logging.ERROR,
+        format="%(message)s",
+        handlers=[
+            logging.StreamHandler(sys.stderr),
+        ],
+    )
+    main()

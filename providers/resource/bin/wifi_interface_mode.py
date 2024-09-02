14f989b8215d7bea6fade29d60fc988c2e2b5fa4
rickwu666666 <98441647+rickwu666666@users.noreply.github.com>
2024-09-02 13:32:04 +0800

    Apply suggestions from code review
    Co-authored-by: Massimiliano <massimiliano.girardi@canonical.com>

--- providers/resource/bin/wifi_interface_mode.py
+++ providers/resource/bin/wifi_interface_mode.py
@@ -26,43 +26,46 @@ def get_interfaces() -> List[Tuple[str, str]]:
     raise SystemExit(0)
 
 
-def get_wiphy_info() -> List[Tuple[str, List[str]]]:
+def get_wiphy_info():
     # We can not use command "iw phy0 info" to get the infomation of sepcific
     # interface.
     # Because of "phy0" in command could be other pattern like "mwiphy0".
     # And such pattern as "mwiphy0" will only show in the output of command
     # "iw phy".
-    output = run_command(["iw", "phy"])
-    if output:
-        contents = re.split(r"Wiphy\s+", output)
-        index_pattern = r"phy(\d+)"
-        wiphy_info = []
-        for content in contents:
-            if content.strip():
-                match_index = re.search(
-                    index_pattern, content, re.DOTALL
-                ).group(1)
-                supported_mode_pattern = (
-                    r"Supported interface modes:\s*((?:\s*\*\s*[\w/-]+\s*)+)"
-                )
-                match_modes = re.search(
-                    supported_mode_pattern, content, re.DOTALL
-                )
-                if match_modes:
-                    modes = re.findall(r"\*\s*([\w/-]+)", match_modes.group(1))
+    output = check_output(["iw", "phy"], universal_newlines=True)
+    if not output:
+        return []
 
-                    wiphy_info.append((match_index, modes))
-        return wiphy_info
+    interfaces_info = (info.strip() for info in output.split("Wiphy"))
+    # drop empty sections
+    interfaces_info = filter(bool, interfaces_info)
+    index_re = re.compile(r"phy(\d+)")
+    wiphy_info = {}
+    supported_modes_re = re.compile(
+        r"Supported interface modes:\s*((?:\s*\*\s*[\w/-]+\s*)+)"
+    )
+    for interface_info in interfaces_info:
+        interface_id = index_re.search(interface_info).group(1)
+        match_modes = supported_modes_re.search(interface_info).group(1)
+
+        supported_modes = map(str.strip, match_modes.split("*"))
+        # remove first element because it is spaces before the first *
+        _ = next(supported_modes)
+        wiphy_info[interface_id] = list(supported_modes)
 
 
 def print_supported_modes() -> str:
     interfaces = get_interfaces()
     wiphy_info = get_wiphy_info()
-    for wiphy_index, modes in wiphy_info:
-        for device_index, interface in interfaces:
-            if wiphy_index == device_index:
-                for mode in modes:
-                    print("{}_{}: supported".format(interface, mode))
+    for wiphy_index, modes in wiphy_info.items():
+        interface = interfaces[wiphy_index]
+        for mode in modes:
+            print(
+                "{}_{}: supported".format(
+                    interface, mode.replace("-", "_").replace("/", "_")
+                )
+            )
+        print()
 
 
 def main():

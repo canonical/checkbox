2015736ad10fa4238905fd494b2c97117e49e43c
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2024-09-26 14:11:41 +0800

    fix: spelling

--- checkbox-support/checkbox_support/parsers/v4l2_compliance.py
+++ checkbox-support/checkbox_support/parsers/v4l2_compliance.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import re
+<<<<<<< HEAD
 import subprocess as sp
 import typing as T
 
@@ -163,6 +164,48 @@ def parse_v4l2_compliance(
         "failed": int(match_output.group(4)),
         "warnings": int(match_output.group(5)),
     }
+=======
+from shutil import which
+import subprocess as s
+from typing import Dict, List, Tuple, Union
+
+
+def get_test_name_from_line(line: str) -> str:
+    assert line.startswith("test"), "This line doesn't describe a test output"
+    return line.split("test ", maxsplit=1)[1].split(": ", maxsplit=1)[0]
+
+
+# see the summary dict literal for actual keys
+Summary = Dict[str, Union[int, str]]
+# see the details dict literal for actual keys
+Details = Dict[str, List[str]]
+
+
+def parse_v4l2_compliance() -> Tuple[Summary, Details]:
+    assert which("v4l2-compliance")
+    out = s.run(["v4l2-compliance"], stdout=s.PIPE)
+
+    lines = []  # type: list[str]
+    for line in out.stdout.decode().splitlines():
+        clean_line = line.strip()
+        if clean_line != "":
+            lines.append(clean_line)
+
+    pattern = (
+        r"Total for (.*): (.*), Succeeded: (.*), Failed: (.*), Warnings: (.*)"
+    )
+    match_output = re.match(pattern, lines[-1])
+
+    summary = {}
+    if match_output:
+        summary = {
+            "device_name": match_output.group(1),
+            "total": int(match_output.group(2)),
+            "succeeded": int(match_output.group(3)),
+            "failed": int(match_output.group(4)),
+            "warnings": int(match_output.group(5)),
+        }
+>>>>>>> b0d7c52af (fix: spelling)
 
     details = {
         "succeeded": [],
@@ -171,6 +214,7 @@ def parse_v4l2_compliance(
     }  # type: dict[str, list[str]]
 
     for line in lines:
+<<<<<<< HEAD
         if not summary["device_name"] and line.startswith(
             "Compliance test for"
         ):
@@ -202,3 +246,27 @@ def parse_v4l2_compliance(
                 details[result].append(ioctl_name)
 
     return summary, details
+=======
+        if line.endswith(": OK"):
+            details["succeeded"].append(get_test_name_from_line(line))
+        elif line.endswith(": OK (Not Supported)"):
+            details["not_supported"].append(get_test_name_from_line(line))
+        elif line.endswith(": FAIL"):
+            details["failed"].append(get_test_name_from_line(line))
+
+    assert (
+        len(details["succeeded"]) + len(details["not_supported"])
+        == summary["succeeded"]
+    )
+    assert len(details["failed"]) == summary["failed"]
+
+    return summary, details
+
+
+if __name__ == "__main__":
+    a, b = parse_v4l2_compliance()
+    print(a)
+    for k, v in b.items():
+        print(k, len(v))
+        print("\t", v)
+>>>>>>> b0d7c52af (fix: spelling)

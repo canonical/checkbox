478ce8970b15191a33185d272d0e7500688d5856
rickwu4444 <rick.wu@canonical.com>
2024-10-01 15:34:57 +0800

    Fix black format

--- contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/xtest_install_ta.py
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/xtest_install_ta.py
@@ -6,10 +6,9 @@ from subprocess import run, CalledProcessError
 
 def run_command(cmd, capture_output=True, text=True, check=True):
     try:
-        result = run(cmd,
-                     capture_output=capture_output,
-                     text=text,
-                     check=check)
+        result = run(
+            cmd, capture_output=capture_output, text=text, check=check
+        )
         return result.stdout.strip() if capture_output else None
     except CalledProcessError as e:
         raise SystemExit("Error: {}".format(e))
@@ -44,8 +43,12 @@ def enable_tee_supplicant(tee):
         print("Attempting to start tee-supplicant...", flush=True)
         cmd = ["snap", "start", tee]
         run_command(cmd)
-    print("tee-supplicant started, and PID is {}...".format(
-        find_tee_supplicant()), flush=True)
+    print(
+        "tee-supplicant started, and PID is {}...".format(
+            find_tee_supplicant()
+        ),
+        flush=True,
+    )
 
 
 def main():

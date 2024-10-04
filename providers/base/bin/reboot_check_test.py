502249342f5f56ea38ca39aa1d174f00ad086303
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2024-10-03 23:28:24 -0700

    Reboot check test python version (New) (#1338)
    * feat: initial migration
    
    * feat: created a wrapper for subprocess.run()
    
    * fix: incorrect main function logic
    
    * style: add more error messages
    
    * fix: no display connectio n-> skipped
    
    * refactor: move device comparison tests to a class
    
    * test: unit tests for reboot_check_test
    
    * fix: remove any direct calls to subprocess.run
    
    * test: add a test for the all-flags-enabled case
    
    * style: reorganize declarations
    
    * fix: missing .value when using enums
    
    * refactor: separate tests into more classes
    
    * refactor: add clenup methods
    
    * refactor: group tests into classes
    
    * fix: update tests to reflect changes in reboot_check_test
    
    * style: formatting, comments
    
    * fix: extra -g flag, add comments
    
    * fix: remove get_display_id due to too many edge cases
    
    * fix: extra wrapper for non-existent commands
    
    * style: fit everything within 80 char limit
    
    * test: update unit tests to reflect new method names
    
    * fix: assume display variable
    
    * test: update tests to reflect removal of get_display_id
    
    * fix: missing conditionals on certain logs
    
    * fix: let unity support infer environment variable
    
    * refactor: revert changes in boot.pxu to put in a separate PR
    
    * fix: flake8 linter errors
    
    * fix: increase test coverage
    
    * fix: combine dpkg check conditions
    
    * update: use the new desktop env detector
    
    * test: increase coverage
    
    * style: formatting
    
    * fix: remove subprocess.run wrapper
    
    * fix: use class attributes instead of enum to avoid writing .value
    
    * fix: reflect changes in unit tests
    
    * style: formatting

---
+++ providers/base/bin/reboot_check_test.py
@@ -0,0 +1,450 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import subprocess as sp
+import re
+import shutil
+import filecmp
+import sys
+import typing as T
+from checkbox_support.scripts.image_checker import has_desktop_environment
+
+
+# Checkbox could run in a snap container, so we need to prepend this root path
+RUNTIME_ROOT = os.getenv("CHECKBOX_RUNTIME", default="")
+# Snap mount point, see
+# https://snapcraft.io/docs/environment-variables#heading--snap
+SNAP = os.getenv("SNAP", default="")
+
+
+class DeviceInfoCollector:
+
+    class Device:
+        PCI = "pci"
+        WIRELESS = "wireless"
+        USB = "usb"
+        DRM = "drm"
+
+    DEFAULT_DEVICES = {
+        "required": [
+            Device.WIRELESS,
+            Device.PCI,
+            Device.USB,
+        ],  # these can fail the test case
+        "optional": [Device.DRM],  # these only produce warnings
+    }  # type: dict[str, list[str]]
+    # to modify, add more values in the enum
+    # and reference them in required/optional respectively
+
+    def get_drm_info(self) -> str:
+        return str(os.listdir("/sys/class/drm"))
+
+    def get_wireless_info(self) -> str:
+        iw_out = sp.run(
+            ["iw", "dev"], stdout=sp.PIPE, stderr=sp.PIPE, check=True
+        )
+        lines = iw_out.stdout.decode().splitlines()
+        lines_to_write = list(
+            filter(
+                lambda line: "addr" in line
+                or "Interface" in line
+                or "ssid" in line,
+                sorted(lines),
+            )
+        )
+        return "\n".join(map(lambda line: line.strip(), lines_to_write))
+
+    def get_usb_info(self) -> str:
+        return sp.run(
+            [
+                "checkbox-support-lsusb",
+                "-f",
+                '"{}"/var/lib/usbutils/usb.ids'.format(RUNTIME_ROOT),
+                "-s",
+            ],
+            check=True,
+        ).stdout.decode()
+
+    def get_pci_info(self) -> str:
+        return sp.run(
+            ["lspci", "-i", "{}/usr/share/misc/pci.ids".format(SNAP)],
+            check=True,
+        ).stdout.decode()
+
+    def compare_device_lists(
+        self,
+        expected_dir: str,
+        actual_dir: str,
+        devices: T.Dict[str, T.List[str]] = DEFAULT_DEVICES,
+    ) -> bool:
+        """Compares the list of devices in expected_dir against actual_dir
+
+        :param expected_dir: files containing the expected device list
+        :param actual_dir: files containing the actual device list
+        :param devices: what devices do we want to compare, see DEFAULT_DEVICES
+        :return: whether the device list matches
+        """
+        print(
+            "Comparing devices in (expected) {} against (actual) {}...".format(
+                expected_dir, actual_dir
+            )
+        )
+        for device in devices["required"]:
+            # file paths of the expected and actual device lists
+            expected = "{}/{}_log".format(expected_dir, device)
+            actual = "{}/{}_log".format(actual_dir, device)
+            if not filecmp.cmp(expected, actual):
+                print(
+                    "[ ERR ] The output of {} differs!".format(device),
+                    file=sys.stderr,
+                )
+                return False
+
+        for device in devices["optional"]:
+            expected = "{}/{}_log".format(expected_dir, device)
+            actual = "{}/{}_log".format(actual_dir, device)
+            if not filecmp.cmp(expected, actual):
+                print(
+                    "[ WARN ] Items under {} have changed.".format(actual),
+                    file=sys.stderr,
+                )
+
+        return True
+
+    def dump(
+        self,
+        output_directory: str,
+        devices: T.Dict[str, T.List[str]] = DEFAULT_DEVICES,
+    ) -> None:
+        os.makedirs(output_directory, exist_ok=True)
+        # add extra behavior if necessary
+        for device in devices["required"]:
+            with open(
+                "{}/{}_log".format(output_directory, device), "w"
+            ) as file:
+                file.write(self.dump_function[device]())
+
+        for device in devices["optional"]:
+            with open(
+                "{}/{}_log".format(output_directory, device), "w"
+            ) as file:
+                file.write(self.dump_function[device]())
+
+        os.sync()
+
+    def __init__(self) -> None:
+        self.dump_function = {
+            self.Device.PCI: self.get_pci_info,
+            self.Device.DRM: self.get_drm_info,
+            self.Device.USB: self.get_usb_info,
+            self.Device.WIRELESS: self.get_wireless_info,
+        }
+
+
+class FwtsTester:
+    def is_fwts_supported(self) -> bool:
+        return shutil.which("fwts") is not None
+
+    def fwts_log_check_passed(
+        self, output_directory: str, fwts_arguments=["klog", "oops"]
+    ) -> bool:
+        """
+        Check if fwts logs passes the checks specified in sleep_test_log_check
+        This script live in the same directory
+
+        :param output_directory: where the output of fwts should be written to
+        :type output_directory: str
+        :return: whether sleep_test_log_check.py returned 0 (success)
+        :rtype: bool
+        """
+        log_file_path = "{}/fwts_{}.log".format(
+            output_directory, "_".join(fwts_arguments)
+        )
+        sp.run(["fwts", "-r", log_file_path, *fwts_arguments])
+        result = sp.run(
+            [
+                "sleep_test_log_check.py",
+                "-v",
+                "--ignore-warning",
+                "-t",
+                "all",
+                log_file_path,
+            ],
+        )
+
+        return result.returncode == 0
+
+
+class HardwareRendererTester:
+
+    def has_display_connection(self) -> bool:
+        """
+        Checks if a display is connected by searching /sys/class/drm
+
+        :return: True if there's at least 1 node that is connected
+        """
+
+        # look for GPU file nodes first
+        DRM_PATH = "/sys/class/drm"
+        possible_gpu_nodes = [
+            directory
+            for directory in os.listdir(DRM_PATH)
+            if directory != "version"
+        ]
+        if len(possible_gpu_nodes) == 0:
+            # kernel doesn't see any GPU nodes
+            print(
+                "There's nothing under {}".format(DRM_PATH),
+                "if an external GPU is connected,"
+                "check if the connection is loose",
+            )
+            return False
+
+        print("These nodes", possible_gpu_nodes, "exist")
+
+        for gpu in possible_gpu_nodes:
+            # for each gpu, check for connection
+            # return true if anything is connected
+            try:
+                with open("{}/{}/status".format(DRM_PATH, gpu)) as status_file:
+                    if status_file.read().strip().lower() == "connected":
+                        print("{} is connected to display!".format(gpu))
+                        return True
+            except FileNotFoundError:
+                # this just means we don't have a status file
+                # => no connection, continue to the next
+                pass
+            except Exception as e:
+                print("Unexpected error: ", e, file=sys.stderr)
+
+        print(
+            "No display is connected. This case will be skipped.",
+            "Maybe the display cable is not connected?",
+            "If the device is not supposed to have a display,"
+            "then skipping is expected",
+        )
+        return False
+
+    def is_hardware_renderer_available(self) -> bool:
+        """
+        Checks if hardware rendering is being used.
+        THIS ASSUMES A DRM CONNECTION EXISTS
+        - self.has_display_connection() should be called first if unsure
+
+        :return: True if a hardware renderer is active, otherwise return False
+        :rtype: bool
+        """
+
+        DISPLAY = os.getenv("DISPLAY", "")
+
+        if DISPLAY == "":
+            print("$DISPLAY is not set, we will let unity_support infer this")
+        else:
+            print("Checking $DISPLAY={}".format(DISPLAY))
+
+        unity_support_output = sp.run(
+            ["{}/usr/lib/nux/unity_support_test".format(RUNTIME_ROOT), "-p"]
+        )
+        if unity_support_output.returncode != 0:
+            print(
+                "[ ERR ] unity support test returned {}".format(
+                    unity_support_output.returncode
+                ),
+                file=sys.stderr,
+            )
+            return False
+
+        is_hardware_rendered = (
+            self.parse_unity_support_output(
+                unity_support_output.stdout.decode()
+            ).get("Not software rendered")
+            == "yes"
+        )
+        if is_hardware_rendered:
+            print("[ OK ] This machine is using a hardware renderer!")
+            return True
+
+        print("[ ERR ] Software rendering detected", file=sys.stderr)
+        return False
+
+    def parse_unity_support_output(
+        self, unity_output_string: str
+    ) -> T.Dict[str, str]:
+        """
+        Parses the output of `unity_support_test` into a dictionary
+
+        :param output_string: the raw output from running unity_support_test -p
+        :type output_string: str
+        :return: string key-value pairs that mirror the output of unity_support
+        Left hand side of the first colon are the keys;
+        right hand side are the values.
+        :rtype: dict[str, str]
+        """
+
+        output = {}  # type: dict[str, str]
+        for line in unity_output_string.split("\n"):
+            # max_split=1 to prevent splitting the string after the 1st colon
+            words = line.split(":", maxsplit=1)
+            if len(words) == 2:
+                key = words[0].strip()
+                value = remove_color_code(words[1].strip())
+                output[key] = value
+
+        return output
+
+
+def get_failed_services() -> T.List[str]:
+    """
+    Counts the number of failed services listed in systemctl
+
+    :return: a list of failed services as they appear in systemctl
+    """
+    command = [
+        "systemctl",
+        "list-units",
+        "--system",
+        "--no-ask-password",
+        "--no-pager",
+        "--no-legend",
+        "--state=failed",
+        "--plain",  # plaintext, otherwise it includes color codes
+    ]
+
+    return sp.run(command).stdout.decode().splitlines()
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        prog="Reboot tests",
+        description="Collects device info and compares them across reboots",
+    )
+    parser.add_argument(
+        "-d",
+        "--dump-to",
+        required=False,
+        dest="output_directory",
+        help="Device info-dumps will be written here",
+    )
+    parser.add_argument(
+        "-c",
+        "--compare-to",
+        dest="comparison_directory",
+        help="Directory of ground-truth for device info comparison",
+    )
+    parser.add_argument(
+        "-s",
+        "--service-check",
+        default=False,
+        dest="do_service_check",
+        action="store_true",
+        help="If specified, check if all system services are running",
+    )
+    parser.add_argument(
+        "-f",
+        "--fwts-check",
+        default=False,
+        dest="do_fwts_check",
+        action="store_true",
+        help="If specified, look for fwts log errors",
+    )
+    parser.add_argument(
+        "-g",
+        "--graphics",
+        default=False,
+        dest="do_renderer_check",
+        action="store_true",
+        help="If specified, check if hardware rendering is being used",
+    )
+
+    return parser
+
+
+def remove_color_code(string: str) -> str:
+    """
+    Removes ANSI color escape sequences from string
+
+    :param string: the string that you would like to remove color code
+    credit: Hanhsuan Lee <hanhsuan.lee@canonical.com>
+    """
+    return re.sub(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])", "", string)
+
+
+def main() -> int:
+    """Main routine
+
+    :return: an return code for checkbox to consume, 1 = failed, 0 = success
+    :rtype: int
+    """
+
+    args = create_parser().parse_args()
+
+    # all 4 tests pass by default
+    # they only fail if their respective flags are specified
+    # if no flags are specified, calling this script is a no-op
+    fwts_passed = True
+    device_comparison_passed = True
+    renderer_test_passed = True
+    service_check_passed = True
+
+    if args.comparison_directory is not None:
+        if args.output_directory is None:
+            print(
+                "[ ERR ] Please specify an output directory with the -d flag.",
+                file=sys.stderr,
+            )
+            raise ValueError(
+                "Cmoparison directory is specified, but output directory isn't"
+            )
+        else:
+            collector = DeviceInfoCollector()
+            collector.dump(args.output_directory)
+            if collector.compare_device_lists(
+                args.comparison_directory, args.output_directory
+            ):
+                print("[ OK ] Devices match!")
+
+    # dump (no checks) if only output_directory is specified
+    if args.output_directory is not None and args.comparison_directory is None:
+        DeviceInfoCollector().dump(args.output_directory)
+
+    if args.do_fwts_check:
+        tester = FwtsTester()
+        if tester.is_fwts_supported() and not tester.fwts_log_check_passed(
+            args.output_directory
+        ):
+            fwts_passed = False
+        else:
+            print("[ OK ] fwts checks passed!")
+
+    if args.do_service_check:
+        failed_services = get_failed_services()
+        if len(failed_services) > 0:
+            print(
+                "These services failed: {}".format("\n".join(failed_services)),
+                file=sys.stderr,
+            )
+            service_check_passed = False
+        else:
+            print("[ OK ] Didn't find any failed system services!")
+
+    if args.do_renderer_check:
+        tester = HardwareRendererTester()
+        if has_desktop_environment() and tester.has_display_connection():
+            # skip renderer test if there's no display
+            renderer_test_passed = tester.is_hardware_renderer_available()
+
+    if (
+        fwts_passed
+        and device_comparison_passed
+        and renderer_test_passed
+        and service_check_passed
+    ):
+        return 0
+    else:
+        return 1
+
+
+if __name__ == "__main__":
+    return_code = main()
+    exit(return_code)
---
+++ providers/base/tests/test_reboot_check_test.py
@@ -0,0 +1,371 @@
+import shutil
+from shlex import split as sh_split
+from unittest.mock import MagicMock, mock_open, patch
+import reboot_check_test as RCT
+import unittest
+import os
+import typing as T
+import subprocess as sp
+
+
+def do_nothing(args: T.List[str], **kwargs):
+    return sp.CompletedProcess(args, 0, "".encode(), "".encode())
+
+
+class UnitySupportParserTests(unittest.TestCase):
+    def setUp(self):
+        self.tester = RCT.HardwareRendererTester()
+
+    def test_parse_ok_unity_support_string(self):
+        OK_UNITY_STRING = """\
+        OpenGL vendor string:   Intel
+        OpenGL renderer string: Mesa Intel(R) UHD Graphics (ICL GT1)
+        OpenGL version string:  4.6 (Compatibility Profile) Mesa 23.2.1
+
+        Not software rendered:    \x1B[033myes\x1B[0m
+        Not blacklisted:          \x1B[033myes\x1B[0m
+        GLX fbconfig:             \x1B[033myes\x1B[0m
+        GLX texture from pixmap:  \x1B[033myes\x1B[0m
+        GL npot or rect textures: \x1B[033myes\x1B[0m
+        GL vertex program:        \x1B[033myes\x1B[0m
+        GL fragment program:      \x1B[033myes\x1B[0m
+        GL vertex buffer object:  \x1B[033mno\x1B[0m
+        GL framebuffer object:    \x1B[033myes\x1B[0m
+        GL version is 1.4+:       \x1B[033myes\x1B[0m
+
+        Unity 3D supported:       \x1B[033myes\x1B[0m
+        """
+
+        expected = {
+            "OpenGL vendor string": "Intel",
+            "OpenGL renderer string": "Mesa Intel(R) UHD Graphics (ICL GT1)",
+            "OpenGL version string": "4.6 (Compatibility Profile) Mesa 23.2.1",
+            "Not software rendered": "yes",
+            "Not blacklisted": "yes",
+            "GLX fbconfig": "yes",
+            "GLX texture from pixmap": "yes",
+            "GL npot or rect textures": "yes",
+            "GL vertex program": "yes",
+            "GL fragment program": "yes",
+            "GL vertex buffer object": "no",
+            "GL framebuffer object": "yes",
+            "GL version is 1.4+": "yes",
+            "Unity 3D supported": "yes",
+        }
+
+        actual = self.tester.parse_unity_support_output(OK_UNITY_STRING)
+        self.assertDictEqual(expected, actual)
+
+    def test_parse_bad_unity_support_string(self):
+        BAD_UNITY_STRING = """
+        OpenGL vendor string   Intel
+        OpenGL renderer string: Mesa Intel(R) UHD Graphics (ICL GT1)
+        OpenGL version string  4.6 (Compatibility Profile) Mesa 23.2.1-1ubuntu
+        GL version is 1.4+%       \x1B[033myes\x1B[0m
+        """
+        actual = self.tester.parse_unity_support_output(BAD_UNITY_STRING)
+
+        expected = {
+            "OpenGL renderer string": "Mesa Intel(R) UHD Graphics (ICL GT1)",
+        }
+
+        self.assertDictEqual(expected, actual)
+
+        ARBITRARY_STRING = "askjaskdnasdn"
+        # should return empty dict if input string literally doesn't make sense
+        self.assertEqual(
+            self.tester.parse_unity_support_output(ARBITRARY_STRING),
+            {},
+        )
+
+
+class DisplayConnectionTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.tester = RCT.HardwareRendererTester()
+
+    def test_display_check_happy_path(self):
+        with patch(
+            "os.listdir", return_value=["fakeCard0", "fakeCard1"]
+        ), patch(
+            "builtins.open",
+            new_callable=mock_open,
+            read_data="connected",
+        ):
+            self.assertTrue(self.tester.has_display_connection())
+
+    def test_display_check_no_display_path(self):
+        with patch("os.listdir", return_value=["version"]):
+            self.assertFalse(self.tester.has_display_connection())
+        with patch(
+            "os.listdir", return_value=["fakeCard0", "fakeCard1"]
+        ), patch(
+            "builtins.open",
+            new_callable=mock_open,
+            read_data="not connected",
+        ):
+            self.assertFalse(self.tester.has_display_connection())
+
+    @patch(
+        "reboot_check_test.HardwareRendererTester.parse_unity_support_output"
+    )
+    @patch("subprocess.run")
+    def test_is_hardware_renderer_available(
+        self,
+        mock_run: MagicMock,
+        mock_parse: MagicMock,
+    ):
+        mock_run.side_effect = do_nothing
+        mock_parse.return_value = {
+            "Not software rendered": "yes",
+        }
+        tester = RCT.HardwareRendererTester()
+        self.assertTrue(tester.is_hardware_renderer_available())
+
+    @patch(
+        "reboot_check_test.HardwareRendererTester.parse_unity_support_output"
+    )
+    @patch("subprocess.run")
+    def test_is_hardware_renderer_available_fail(
+        self,
+        mock_run: MagicMock,
+        mock_parse: MagicMock,
+    ):
+
+        mock_run.side_effect = lambda _: sp.CompletedProcess(
+            [], 1, "".encode(), "".encode()
+        )
+        tester = RCT.HardwareRendererTester()
+        self.assertFalse(tester.is_hardware_renderer_available())
+
+        mock_run.reset_mock()
+        mock_run.side_effect = do_nothing
+        mock_parse.return_value = {
+            "Not software rendered": "no",
+        }
+        tester = RCT.HardwareRendererTester()
+        self.assertFalse(tester.is_hardware_renderer_available())
+
+
+class InfoDumpTests(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.temp_output_dir = "{}/temp_output_dir".format(os.getcwd())
+        cls.temp_comparison_dir = "{}/temp_comparison_dir".format(os.getcwd())
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_output_dir, ignore_errors=True)
+        shutil.rmtree(self.temp_comparison_dir, ignore_errors=True)
+
+    def mock_run(self, args: T.List[str], **_) -> sp.CompletedProcess:
+        stdout = ""
+        if args[0] == "iw":
+            stdout = """\
+                addr some address
+                Interface some interface
+                ssid some ssid
+                """
+        elif args[0] == "checkbox-support-lsusb":
+            stdout = """\
+                usb1
+                usb2
+                usb3
+                """
+        elif args[0] == "lspci":
+            stdout = """\
+                pci1
+                pci2
+                pci3
+                """
+        else:
+            raise Exception("Unexpected use of this mock")
+
+        return sp.CompletedProcess(args, 0, stdout.encode(), "".encode())
+
+    @patch("subprocess.run")
+    def test_info_dump_only_happy_path(self, mock_run: MagicMock):
+        # wrap over run's return value
+        mock_run.side_effect = self.mock_run
+        RCT.DeviceInfoCollector().dump(self.temp_output_dir)
+
+    @patch("subprocess.run")
+    def test_info_dump_and_comparison_happy_path(self, mock_run: MagicMock):
+        mock_run.side_effect = self.mock_run
+
+        collector = RCT.DeviceInfoCollector()
+
+        collector.dump(self.temp_comparison_dir)
+        collector.dump(self.temp_output_dir)
+
+        self.assertTrue(
+            collector.compare_device_lists(
+                self.temp_comparison_dir, self.temp_output_dir
+            )
+        )
+
+        # required
+        with open(
+            "{}/{}_log".format(
+                self.temp_comparison_dir,
+                RCT.DeviceInfoCollector.Device.WIRELESS,
+            ),
+            "w",
+        ) as f:
+            f.write("extra text that shouldn't be there")
+
+        self.assertFalse(
+            collector.compare_device_lists(
+                self.temp_comparison_dir, self.temp_output_dir
+            )
+        )
+
+        collector.dump(self.temp_comparison_dir)
+
+        # optional
+        with open(
+            "{}/{}_log".format(
+                self.temp_comparison_dir,
+                RCT.DeviceInfoCollector.Device.DRM,
+            ),
+            "w",
+        ) as f:
+            f.write("extra text that shouldn't be there")
+
+        self.assertTrue(
+            collector.compare_device_lists(
+                self.temp_comparison_dir, self.temp_output_dir
+            )
+        )
+
+
+class FailedServiceCheckerTests(unittest.TestCase):
+
+    @patch("subprocess.run")
+    def test_get_failed_services_happy_path(self, mock_run: MagicMock):
+        mock_run.return_value = sp.CompletedProcess(
+            [], 0, "".encode(), "".encode()
+        )
+        self.assertEqual(RCT.get_failed_services(), [])
+
+    @patch("subprocess.run")
+    def test_get_failed_services_with_failed_services(
+        self, mock_run: MagicMock
+    ):
+        mock_run.return_value = sp.CompletedProcess(
+            [],
+            0,
+            "snap.checkbox.agent.service loaded failed failed Service\
+                  for snap applictaion checkbox.agent".encode(),
+            "",
+        )
+        self.assertEqual(
+            RCT.get_failed_services(), [mock_run.return_value.stdout.decode()]
+        )
+
+
+class MainFunctionTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.tmp_output_dir = "{}/temp_output_dir".format(os.getcwd())
+        cls.tmp_comparison_dir = "{}/temp_comparison_dir".format(os.getcwd())
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_output_dir, ignore_errors=True)
+        shutil.rmtree(self.tmp_comparison_dir, ignore_errors=True)
+
+    @patch("subprocess.run")
+    def test_partial_main(self, mock_run: MagicMock):
+        # this test only validates the main function logic
+        # (if it picks out the correct tests to run)
+        mock_run.side_effect = do_nothing
+
+        with patch(
+            "sys.argv",
+            sh_split("reboot_check_test.py -d {}".format(self.tmp_output_dir)),
+        ):
+            RCT.main()
+            self.assertEqual(
+                mock_run.call_count,
+                len(RCT.DeviceInfoCollector.DEFAULT_DEVICES["required"]),
+            )
+
+        mock_run.reset_mock()
+
+        with patch(
+            "sys.argv",
+            sh_split(
+                'reboot_check_test.py -d "{}" -c "{}"'.format(
+                    self.tmp_output_dir, self.tmp_comparison_dir
+                )
+            ),
+        ), patch(
+            "reboot_check_test.DeviceInfoCollector.compare_device_lists"
+        ) as mock_compare:
+            RCT.main()
+
+            self.assertEqual(
+                mock_run.call_count,
+                len(RCT.DeviceInfoCollector.DEFAULT_DEVICES["required"]),
+            )  # only lspci, lsusb, iw calls
+            self.assertEqual(mock_compare.call_count, 1)
+
+    @patch("subprocess.run")
+    def test_main_function_full(self, mock_run: MagicMock):
+        mock_run.side_effect = do_nothing
+        # Full suite
+        with patch(
+            "sys.argv",
+            sh_split(
+                'reboot_check_test.py -d "{}" -c "{}" -f -s -g'.format(
+                    self.tmp_output_dir, self.tmp_comparison_dir
+                )
+            ),
+        ), patch(
+            "reboot_check_test.DeviceInfoCollector.compare_device_lists"
+        ) as mock_compare, patch(
+            "reboot_check_test.FwtsTester.is_fwts_supported"
+        ) as mock_is_fwts_supported:
+            mock_is_fwts_supported.return_value = True
+            mock_compare.return_value = True
+
+            RCT.main()
+
+            self.assertTrue(mock_is_fwts_supported.called)
+
+            expected_commands = {
+                "systemctl",
+                "sleep_test_log_check.py",
+                "fwts",
+            }
+
+            actual = set()
+            for call in mock_run.call_args_list:
+                # [0] takes the 1st from (args, kwargs, ) = call,
+                # then take tha actual list from args
+                # then take the 1st element, which is the command name
+                actual.add(call[0][0][0])
+
+            # <= is an overloaded operator for sets
+            # that checks the isSubset relation
+            self.assertLessEqual(
+                expected_commands, actual, "should be a subset"
+            )
+
+            with patch(
+                "reboot_check_test.get_failed_services"
+            ) as mock_get_failed_services:
+                mock_get_failed_services.return_value = [
+                    "failed service1",
+                    "failed service2",
+                ]
+                self.assertEqual(RCT.main(), 1)
+
+    def test_only_comparison_is_specified(self):
+        with patch(
+            "sys.argv",
+            sh_split(
+                'reboot_check_test.py -c "{}"'.format(self.tmp_output_dir)
+            ),
+        ), self.assertRaises(ValueError):
+            RCT.main()

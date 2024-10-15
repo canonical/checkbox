2226b2c3fc6289391e4e0de1d6097b9513ef0846
rickwu666666 <98441647+rickwu666666@users.noreply.github.com>
2024-10-15 13:36:14 +0800

    [ce-oem]Revise optee test script and job (Bugfix) (#1518)
    * Revise optee test script and job
    
    We can run the xtest from both gadget and other test apps
    
    ---------
    
    Co-authored-by: stanley31huang <stanley.huang@canonical.com>

--- contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/look_up_xtest.py
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/look_up_xtest.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python3
 
 from checkbox_support.snap_utils.snapd import Snapd
-from checkbox_support.snap_utils.system import get_gadget_snap
+import os
+
+
+class ExtendSanpd(Snapd):
+    _apps = "/v2/apps"
+
+    def __init__(self, task_timeout=30, poll_interval=1, verbose=False):
+        super().__init__(task_timeout, poll_interval, verbose)
+
+    def list_apps(self, snaps):
+        return self._get(self._apps, params={"names": snaps})
 
 
 def look_up_xtest():
-    if Snapd().list("x-test"):
-        return "x-test.xtest"
-    elif look_up_gadget() is not False:
-        return look_up_gadget()
+    """Lookup xtest and tee-supplicant apps."""
+    snap = os.environ.get("XTEST")
+    apps = ExtendSanpd().list_apps(snap)
+    results = {"xtest": None, "tee-supplicant": None}
+    xtest_apps = list(
+        {
+            (app["snap"], app["name"])
+            for app in apps["result"]
+            if app["name"] in ["tee-supplicant", "xtest"]
+        }
+    )
+    if xtest_apps:
+        if (
+            len(xtest_apps) > 2
+            or xtest_apps[0][0] != xtest_apps[1][0]
+            or xtest_apps[0][1] == xtest_apps[1][1]
+        ):
+            raise SystemError("Found multiple xtest snap in the system!")
     else:
-        raise SystemExit(1)
-
-
-def look_up_gadget():
-    gadget = get_gadget_snap()
-    snap = Snapd().list(gadget)
-    if "apps" in snap.keys():
-        for app in snap["apps"]:
-            if app["name"] == "xtest":
-                return ".".join([app["snap"], app["name"]])
-    return False
-
-
-def main():
-    print(look_up_xtest())
-
-
-if __name__ == "__main__":
-    main()
+        raise SystemError("Not found xtest snap in the system!")
+    for key in results.keys():
+        results[key] = ".".join([xtest_apps[0][0], key])
+    return results
---
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/xtest_install_ta.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+from look_up_xtest import look_up_xtest
+from subprocess import run, CalledProcessError
+import glob
+
+
+def run_command(cmd, capture_output=True, text=True, check=True):
+    try:
+        result = run(
+            cmd, capture_output=capture_output, text=text, check=check
+        )
+        return result.stdout.strip() if capture_output else None
+    except CalledProcessError as e:
+        raise SystemExit("Error: {}".format(e))
+
+
+def find_ta_path():
+    dir = "/var/snap/**/optee_armtz"
+    print("Looking for TA path...", flush=True)
+    ta_folder = glob.glob(dir, recursive=True)
+    if not ta_folder:
+        raise SystemError("Not able to find TA in the system!")
+    elif len(ta_folder) > 1:
+        raise SystemError(
+            "Found multiple TA sources."
+            "Please make sure only one exist in the system!"
+        )
+    return ta_folder[0]
+
+
+def install_ta(xtest, path):
+    cmd = ["timeout", "10", xtest, "--install-ta", path]
+    print("Attempting to install TA...", flush=True)
+    run_command(cmd)
+    print("TA install succeeded!", flush=True)
+
+
+def find_tee_supplicant():
+    cmd = ["pgrep", "-f", "tee-supplicant"]
+    return run_command(cmd, check=False)
+
+
+def enable_tee_supplicant(tee):
+    print("Looking for tee-supplicant process...", flush=True)
+    output = find_tee_supplicant()
+    if not output:
+        print("Not found tee-supplicant process...", flush=True)
+        print("Attempting to start tee-supplicant...", flush=True)
+        cmd = ["snap", "start", tee]
+        run_command(cmd)
+    print(
+        "tee-supplicant started, and PID is {}...".format(
+            find_tee_supplicant()
+        ),
+        flush=True,
+    )
+
+
+def main():
+    xtest = look_up_xtest()
+    enable_tee_supplicant(xtest["tee-supplicant"])
+    ta_path = find_ta_path()
+    install_ta(xtest["xtest"], ta_path)
+
+
+if __name__ == "__main__":
+    main()

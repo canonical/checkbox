b6d5bfadf66b5439e3722cb249ee3cea0a682b77
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2024-10-17 02:51:17 -0700

    Fix missing `subprocess.PIPE` in reboot_check_test.py (Bugfix) (#1546)
    * fix: missing subprocess.PIPE in run() calls
    
    * fix: replace .decode with universal_newlines
    
    * style: formatting
    
    * fix: update types in unit tests
    
    * fix: change byte -> str in mocks
    
    * style: formatting
    
    * fix: use check_output for systemctl calls

--- providers/base/bin/reboot_check_test.py
+++ providers/base/bin/reboot_check_test.py
@@ -41,10 +41,8 @@ class DeviceInfoCollector:
         return str(os.listdir("/sys/class/drm"))
 
     def get_wireless_info(self) -> str:
-        iw_out = sp.run(
-            ["iw", "dev"], stdout=sp.PIPE, stderr=sp.PIPE, check=True
-        )
-        lines = iw_out.stdout.decode().splitlines()
+        iw_out = sp.check_output(["iw", "dev"], universal_newlines=True)
+        lines = iw_out.splitlines()
         lines_to_write = list(
             filter(
                 lambda line: "addr" in line
@@ -56,21 +54,21 @@ class DeviceInfoCollector:
         return "\n".join(map(lambda line: line.strip(), lines_to_write))
 
     def get_usb_info(self) -> str:
-        return sp.run(
+        return sp.check_output(
             [
                 "checkbox-support-lsusb",
                 "-f",
                 '"{}"/var/lib/usbutils/usb.ids'.format(RUNTIME_ROOT),
                 "-s",
             ],
-            check=True,
-        ).stdout.decode()
+            universal_newlines=True,
+        )
 
     def get_pci_info(self) -> str:
-        return sp.run(
+        return sp.check_output(
             ["lspci", "-i", "{}/usr/share/misc/pci.ids".format(SNAP)],
-            check=True,
-        ).stdout.decode()
+            universal_newlines=True,
+        )
 
     def compare_device_lists(
         self,
@@ -244,7 +242,9 @@ class HardwareRendererTester:
             print("Checking $DISPLAY={}".format(DISPLAY))
 
         unity_support_output = sp.run(
-            ["{}/usr/lib/nux/unity_support_test".format(RUNTIME_ROOT), "-p"]
+            ["{}/usr/lib/nux/unity_support_test".format(RUNTIME_ROOT), "-p"],
+            stdout=sp.PIPE,
+            universal_newlines=True,
         )
         if unity_support_output.returncode != 0:
             print(
@@ -256,9 +256,9 @@ class HardwareRendererTester:
             return False
 
         is_hardware_rendered = (
-            self.parse_unity_support_output(
-                unity_support_output.stdout.decode()
-            ).get("Not software rendered")
+            self.parse_unity_support_output(unity_support_output.stdout).get(
+                "Not software rendered"
+            )
             == "yes"
         )
         if is_hardware_rendered:
@@ -311,7 +311,7 @@ def get_failed_services() -> T.List[str]:
         "--plain",  # plaintext, otherwise it includes color codes
     ]
 
-    return sp.run(command).stdout.decode().splitlines()
+    return sp.check_output(command, universal_newlines=True).splitlines()
 
 
 def create_parser():
--- providers/base/tests/test_reboot_check_test.py
+++ providers/base/tests/test_reboot_check_test.py
@@ -9,7 +9,10 @@ import subprocess as sp
 
 
 def do_nothing(args: T.List[str], **kwargs):
-    return sp.CompletedProcess(args, 0, "".encode(), "".encode())
+    if "universal_newlines" in kwargs:
+        return sp.CompletedProcess(args, 0, "", "")
+    else:
+        return sp.CompletedProcess(args, 0, "".encode(), "".encode())
 
 
 class UnitySupportParserTests(unittest.TestCase):
@@ -131,9 +134,8 @@ class DisplayConnectionTests(unittest.TestCase):
         mock_run: MagicMock,
         mock_parse: MagicMock,
     ):
-
-        mock_run.side_effect = lambda _: sp.CompletedProcess(
-            [], 1, "".encode(), "".encode()
+        mock_run.side_effect = lambda *args, **kwargs: sp.CompletedProcess(
+            [], 1, "", ""
         )
         tester = RCT.HardwareRendererTester()
         self.assertFalse(tester.is_hardware_renderer_available())
@@ -180,7 +182,7 @@ class InfoDumpTests(unittest.TestCase):
         else:
             raise Exception("Unexpected use of this mock")
 
-        return sp.CompletedProcess(args, 0, stdout.encode(), "".encode())
+        return sp.CompletedProcess(args, 0, stdout, "")
 
     @patch("subprocess.run")
     def test_info_dump_only_happy_path(self, mock_run: MagicMock):
@@ -242,9 +244,7 @@ class FailedServiceCheckerTests(unittest.TestCase):
 
     @patch("subprocess.run")
     def test_get_failed_services_happy_path(self, mock_run: MagicMock):
-        mock_run.return_value = sp.CompletedProcess(
-            [], 0, "".encode(), "".encode()
-        )
+        mock_run.return_value = sp.CompletedProcess([], 0, "", "")
         self.assertEqual(RCT.get_failed_services(), [])
 
     @patch("subprocess.run")
@@ -255,11 +255,11 @@ class FailedServiceCheckerTests(unittest.TestCase):
             [],
             0,
             "snap.checkbox.agent.service loaded failed failed Service\
-                  for snap applictaion checkbox.agent".encode(),
+                  for snap applictaion checkbox.agent",
             "",
         )
         self.assertEqual(
-            RCT.get_failed_services(), [mock_run.return_value.stdout.decode()]
+            RCT.get_failed_services(), [mock_run.return_value.stdout]
         )
 
 

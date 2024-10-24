aa87cdc3764da55e929f883d027394ce0c6501a3
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2024-10-24 17:53:07 +0200

    Install gpu-burn and cuda-samples in postinst
    The projects are downloaded to `/opt/` but the binaries are symlinked to `/usr/local/sbin`.
    
    Need to test if the symlinks are able to find the necessary data files.

--- providers/gpgpu/bin/matrixMulDrv.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-cp "$PLAINBOX_PROVIDER_DATA/matrixMul_kernel64.fatbin" .
-matrixMulDrv
-rm -f matrixMul_kernel64.fatbin
--- providers/gpgpu/bin/vectorAddDrv.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-cp -r "$PLAINBOX_PROVIDER_DATA/vectorAdd_kernel64.fatbin" .
-vectorAddDrv
-rm -f vectorAdd_kernel64.fatbin

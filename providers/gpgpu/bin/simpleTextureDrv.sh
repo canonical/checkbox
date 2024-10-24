4d33b77e7a1b68a9c697e13bd55e86f077acba4d
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2024-10-24 17:53:07 +0200

    Install gpu-burn and cuda-samples in postinst
    The projects are downloaded to `/opt/` but the binaries are symlinked to `/usr/local/sbin`.
    
    Need to test if the symlinks are able to find the necessary data files.

--- providers/gpgpu/bin/simpleTextureDrv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-cp "$PLAINBOX_PROVIDER_DATA/simpleTexture_kernel64.fatbin" .
-cp "$PLAINBOX_PROVIDER_DATA/ref_rotated.pgm" .
-cp "$PLAINBOX_PROVIDER_DATA/teapot512.pgm" .
-cp "$PLAINBOX_PROVIDER_DATA/teapot512_out.pgm" .
-simpleTextureDrv
-rm -f simpleTexture_kernel64.fatbin ref_rotated.pgm teapot512.pgm teapot512_out.pgm

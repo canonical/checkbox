89f93ed99925ae3a735e106ad910959242619750
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2024-10-30 14:41:38 +0100

    Test using snap GPGPU tools package

--- providers/gpgpu/bin/gpu_burn.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-CUDA_PATH=$(find /usr/local -maxdepth 1 -type d -iname "cuda*")/bin
-export PATH=$PATH:$CUDA_PATH
-gpu_burn 14400 | grep -v -e '^[[:space:]]*$' -e "errors:" -e "Summary at"

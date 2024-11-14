7c6b994a3048418eae96ed063d0a573c36f92c6b
Pedro Avalos <42501726+pedro-avalos@users.noreply.github.com>
2024-11-14 09:02:04 -0600

    Add LXD GPU passthrough tests (New) (#1577)
    * Add initial gpu_passthrough program
    
    The LXD and LXDVM classes may be useful in checkbox-support, as other tests may be able to benefit from these classes.
    
    * Add initial coverage tests
    
    * Fix parse_args function
    
    * Add Checkbox units
    
    * Fix typo in unit
    
    * Fix typo in LXD.launch
    
    * Force delete in cleanup
    
    Otherwise the test is not necessarily idempotent
    
    * Ensure insert_images is called
    
    * Fix nvidia repo url
    
    * Fix nvidia pinfile url
    
    * Fix symlink name
    
    * Pass NVIDIA runtime at launch
    
    * Format gpu_passthrough.py
    
    * Don't use dataclasses
    
    I guess these are newer than I thought...
    
    * Remove unsupported typehints
    
    * Document that parameters can be overwritten
    
    * Use cached property
    
    * Rewrite run function
    
    * Add retry decorator to download_image
    
    * Add type hint to launch
    
    * Rename jobs to gpgpu-passthrough
    
    * Update tests
    
    * Install mixbench snap
    
    * Make LXD and LXDVM context managers
    
    * Move setup to GPU_VENDORS fields
    
    * Update tests
    
    * Fix tests
    
    * Fix shlex join bug
    
    * No sudo needed
    
    Instance should be running as root
    
    * Make script a little more verbose
    
    * init_lxd is part of __enter__
    
    * Don't just sleep for system to be up
    
    * Ensure nvidia capabilities are passed through
    
    This ensures mixbench is able to find the right CUDA libraries from the host.
    
    * Make type hints more accurate
    
    * image_alias not image
    
    * Update tests
    
    * Update tox file requirements
    
    * Add libsystemd-dev to tox workflow
    
    * shlex.join available in 3.8+
    
    * Fix launch tests
    
    * Add more log messages
    
    * Increase wait for VM retries
    
    * Wait for VM to be up after adding GPU
    
    * Add wait until running function
    
    * Add test
    
    * Remove unused properties
    
    * Install gpgpu drivers on LXD vm
    
    This should at the very least help speed up the setup process since X11 is not needed in the vm
    
    * Add debug messages to run
    
    * Install linux-generic
    
    * remove todo message
    
    * Add options= to make it line clearer
    
    * Use default storage size for VM
    
    This is not needed anymore since CUDA Toolkit is not being installed on the
    VM
    
    * Auto-connect request granted
    
    * Compatibility with jammy and prior
    
    * Remove LXDVM passthrough test
    
    This is not working as intended, will add in a separate PR
    
    * Ensure nvidia driver is present
    
    * Add units to bootstrap_include

---
+++ providers/gpgpu/bin/gpu_passthrough.py
@@ -0,0 +1,472 @@
+#!/usr/bin/env python3
+"""Script to test virtualization GPU passthrough functionality.
+
+Copyright (C) 2024 Canonical Ltd.
+
+Authors
+  Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3,
+as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import argparse
+import logging
+import os
+import shlex
+import subprocess
+import time
+import urllib
+import uuid
+from typing import List, Optional
+from urllib.parse import urlparse
+
+from checkbox_support.helpers.retry import retry, run_with_retry
+from plainbox.impl.decorators import cached_property
+
+try:
+    import distro
+
+    if distro.id() == "ubuntu-core":
+        RELEASE = "{}.04".format(distro.version())
+        CODENAME = "focal"
+        if distro.version() == "18":
+            CODENAME = "bionic"
+        elif distro.version() == "16":
+            CODENAME = "xenial"
+    else:
+        RELEASE = distro.version()
+        CODENAME = distro.codename().split()[0].lower()
+except ImportError:
+    import lsb_release  # type: ignore
+
+    RELEASE = lsb_release.get_distro_information()["RELEASE"]
+    CODENAME = lsb_release.get_distro_information()["CODENAME"]
+
+ARCH = os.uname().machine
+
+
+GPU_VENDORS = {
+    "nvidia": {
+        "test": "mixbench.cuda",
+        "lxd": {
+            "launch_options": [
+                "-c",
+                "nvidia.runtime=true",
+                "-c",
+                "nvidia.driver.capabilities=all",
+            ]
+        },
+        "lxdvm": {
+            "launch_options": ["-c", "security.secureboot=false"],
+            "config_cmds": [
+                "apt-get -q update -y",
+                "apt-get -q upgrade -y",
+                "apt-get -q install -y linux-generic ubuntu-drivers-common",
+                "ubuntu-drivers install --gpgpu",
+            ],
+        },
+    },
+}
+"""Mapping of supported vendor names to test configuration."""
+
+GPU_RUNS = 20
+"""How many times to run the GPU test.
+
+This can be overwritten by the `--count` option or by setting the environment
+variable `LXD_GPU_RUNS`. With priority in that order.
+"""
+
+GPU_THRESHOLD_SEC = 12.0
+"""The threshold for the GPU test to pass.
+
+This can be overwritten by the `--threshold` option or by setting the
+environment variable `LXD_GPU_THRESHOLD`. With priority in that order.
+"""
+
+
+class LXD:
+    """This class represents a LXD instance."""
+
+    def __init__(
+        self,
+        template_url: Optional[str] = None,
+        image_url: Optional[str] = None,
+        name: str = "testbed",
+        remote: str = "ubuntu:",
+    ):
+        self.template_url = template_url
+        self.image_url = image_url
+        self.name = name
+        self.remote = remote
+        self.image_alias = uuid.uuid4()
+
+    @cached_property
+    def template(self) -> Optional[str]:
+        """Gets path to template tarball."""
+        if not self.template_url:
+            return None
+        targetfile = urlparse(self.template_url).path.split("/")[-1]
+        filename = os.path.join("/tmp", targetfile)
+        self.download_image(self.template_url, filename)
+        return filename
+
+    @cached_property
+    def image(self) -> Optional[str]:
+        """Gets path to image tarball."""
+        if not self.image_url:
+            return None
+        targetfile = urlparse(self.image_url).path.split("/")[-1]
+        filename = os.path.join("/tmp", targetfile)
+        self.download_image(self.image_url, filename)
+        return filename
+
+    def run(
+        self,
+        cmd: str,
+        log_stderr: bool = True,
+        on_guest: bool = False,
+        ignore_errors: bool = False,
+    ):
+        """Runs a command on the host or instance."""
+        if on_guest:
+            cmd = "lxc exec {} -- {}".format(self.name, cmd)
+        stderr_pipe = subprocess.STDOUT if log_stderr else subprocess.DEVNULL
+        try:
+            logging.debug("Command: %s", cmd)
+            out = subprocess.check_output(
+                shlex.split(cmd),
+                stderr=stderr_pipe,
+                stdin=subprocess.DEVNULL,
+                universal_newlines=True,
+            )
+            logging.debug(" STDOUT: %s", out)
+        except subprocess.CalledProcessError as e:
+            logging.error("Command failed: %s", cmd)
+            logging.info(" STDOUT: %s", e.stdout)
+            if not ignore_errors:
+                raise
+
+    @retry(5, 2)
+    def download_image(self, url, filename):
+        """Downloads LXD files for same release as host machine."""
+        if os.path.isfile(filename):
+            return
+        logging.debug("Attempting download of %s from %s", filename, url)
+        urllib.request.urlretrieve(url, filename)
+        if not os.path.isfile(filename):
+            raise FileNotFoundError(filename)
+
+    def insert_images(self):
+        """Insert LXD template and image images."""
+        if self.template and self.image:
+            logging.debug("Importing images into LXD")
+            self.run(
+                "lxc image import {} rootfs {} --alias {}".format(
+                    self.template, self.image, self.image_alias.hex
+                )
+            )
+        else:
+            logging.debug("No local images, attempting import from remote")
+            run_with_retry(
+                self.run,
+                5,
+                2,
+                "lxc image copy {}{} local: --alias {}".format(
+                    self.remote, RELEASE, self.image_alias.hex
+                ),
+            )
+
+    def init_lxd(self):
+        """Initializes LXD."""
+        try:
+            self.run("lxd waitready --timeout=5")
+        except subprocess.CalledProcessError:
+            self.run("lxd init --auto")
+        self.insert_images()
+
+    def cleanup(self):
+        """Cleans up instance."""
+        self.run(
+            "lxc image delete {}".format(self.image_alias.hex),
+            ignore_errors=True,
+        )
+        self.run("lxc delete --force {}".format(self.name), ignore_errors=True)
+
+    def launch(self, options: Optional[List[str]] = None):
+        """Sets up and creates the instance."""
+        cmd = ["lxc", "launch", self.image_alias.hex, self.name]
+        if options:
+            cmd += options
+        self.run(" ".join(cmd))
+
+    def stop(self, force: bool = False):
+        """Stops LXD instance."""
+        cmd = "lxc stop {}".format(self.name)
+        if force:
+            cmd += " --force"
+        self.run(cmd)
+
+    def start(self):
+        """Starts LXD instance."""
+        self.run("lxc start {}".format(self.name))
+
+    def restart(self):
+        """Restarts LXD instance."""
+        self.run("lxc restart {}".format(self.name))
+
+    @retry(10, 10)
+    def wait_until_running(self):
+        """Waits for the instance to be up and running."""
+        self.run("systemctl is-system-running --wait", on_guest=True)
+
+    def add_device(
+        self,
+        device: str,
+        device_type: str,
+        options: Optional[List[str]] = None,
+    ):
+        """Adds a device to the LXD instance."""
+        if not options:
+            options = []
+        self.run(
+            "lxc config device add {} {} {} {}".format(
+                self.name, device, device_type, " ".join(options)
+            )
+        )
+
+    def __enter__(self):
+        self.init_lxd()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+        self.cleanup()
+
+
+class LXDVM(LXD):
+    """This class represents a LXD VM instance."""
+
+    def insert_images(self):
+        if self.template and self.image:
+            logging.debug("Importing images into LXD")
+            self.run(
+                "lxc image import {} {} --alias {}".format(
+                    self.template, self.image, self.image_alias.hex
+                )
+            )
+
+    def launch(self, options: Optional[List[str]] = None):
+        logging.debug("Initializing virtual machine")
+        cmd = ["lxc", "init"]
+        if not self.image and not self.template:
+            logging.debug("No local image, importing from remote")
+            cmd += ["{}{}".format(self.remote, RELEASE)]
+        else:
+            cmd += [self.image_alias.hex]
+        cmd += [self.name, "--vm"]
+        if options:
+            cmd += options
+        self.run(" ".join(cmd))
+
+        logging.debug("Starting virtual machine")
+        self.run("lxc start {}".format(self.name))
+
+    def add_device(self, device: str, device_type: str, options=None):
+        # Hot plugging is only supported on containers
+        self.stop(force=True)
+        super().add_device(device, device_type, options)
+        self.start()
+
+
+def run_gpu_test(
+    instance: LXD,
+    cmd: str,
+    run_count: int = GPU_RUNS,
+    threshold_sec: float = GPU_THRESHOLD_SEC,
+):
+    """Executes GPU passthrough test."""
+    logging.info("Running GPU passthrough test %d times", run_count)
+    total_runtime_sec = 0.0
+    for i in range(run_count):
+        tic = time.time()
+        instance.run(cmd, on_guest=True)
+        toc = time.time()
+        runtime_sec = toc - tic
+        total_runtime_sec += runtime_sec
+        logging.debug("Runtime #%d (sec): %f", i, runtime_sec)
+
+    avg_runtime_sec = total_runtime_sec / run_count
+    logging.info("Average runtime (sec): %f", avg_runtime_sec)
+    if avg_runtime_sec >= threshold_sec:
+        logging.error(
+            "Average runtime %fs greater than threshold %fs",
+            avg_runtime_sec,
+            threshold_sec,
+        )
+        raise SystemExit(1)
+
+
+def test_lxd_gpu(args):
+    """Tests GPU passthrough with a LXD container."""
+    logging.info("Executing LXD GPU passthrough test")
+
+    instance = LXD(args.template, args.rootfs)
+    with LXD(args.template, args.rootfs) as instance:
+        logging.info("Launching container: %s", instance.name)
+        instance.launch(
+            options=GPU_VENDORS[args.vendor]["lxd"].get("launch_options")
+        )
+
+        logging.info("Passing GPU %s through to %s", args.pci, instance.name)
+        instance.add_device("gpu", "gpu", options=["pci={}".format(args.pci)])
+
+        logging.info("Waiting for %s to be up", instance.name)
+        instance.wait_until_running()
+
+        logging.info("Installing mixbench snap")
+        instance.run("snap install mixbench", on_guest=True)
+
+        run_gpu_test(
+            instance,
+            GPU_VENDORS[args.vendor]["test"],
+            args.count,
+            args.threshold,
+        )
+
+
+def test_lxdvm_gpu(args):
+    """Tests GPU passthrough with a LXD virtual machine."""
+    logging.info("Executing LXD VM GPU passthrough test")
+
+    with LXDVM(args.template, args.image) as instance:
+        logging.info("Launching virtual machine: %s", instance.name)
+        instance.launch(
+            options=GPU_VENDORS[args.vendor]["lxdvm"].get("launch_options")
+        )
+
+        logging.info("Waiting for %s to be up", instance.name)
+        instance.wait_until_running()
+
+        logging.info("Passing GPU %s through to %s", args.pci, instance.name)
+        instance.add_device("gpu", "gpu", options=["pci={}".format(args.pci)])
+
+        logging.info("Waiting for %s to be up", instance.name)
+        instance.wait_until_running()
+
+        logging.info("Configuring %s", instance.name)
+        for cmd in GPU_VENDORS[args.vendor]["lxdvm"].get("config_cmds", []):
+            instance.run(cmd, on_guest=True)
+
+        logging.info("Restarting %s", instance.name)
+        instance.restart()
+
+        logging.info("Waiting for %s to be up", instance.name)
+        instance.wait_until_running()
+
+        logging.info("Installing mixbench snap")
+        instance.run("snap install mixbench", on_guest=True)
+
+        run_gpu_test(
+            instance,
+            GPU_VENDORS[args.vendor]["test"],
+            args.count,
+            args.threshold,
+        )
+
+
+def parse_args():
+    """Parses command-line arguments."""
+    parser = argparse.ArgumentParser(description="GPU passthrough tests")
+    subparsers = parser.add_subparsers()
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        dest="log_level",
+        action="store_const",
+        default=logging.INFO,
+        const=logging.DEBUG,
+        help="Increase logging level",
+    )
+
+    test_group = parser.add_argument_group("test")
+    test_group.add_argument(
+        "--threshold",
+        type=float,
+        default=float(os.getenv("LXD_GPU_THRESHOLD") or GPU_THRESHOLD_SEC),
+        help="Threshold (sec) for GPU test",
+    )
+    test_group.add_argument(
+        "--count",
+        type=int,
+        default=int(os.getenv("LXD_GPU_RUNS") or GPU_RUNS),
+        help="Times to run GPU test",
+    )
+
+    gpu_group = parser.add_argument_group("gpu")
+    gpu_group.add_argument(
+        "--pci", type=str, help="PCI address of GPU", required=True
+    )
+    gpu_group.add_argument(
+        "--vendor",
+        type=str,
+        choices=GPU_VENDORS.keys(),
+        help="GPU vendor",
+        required=True,
+    )
+
+    lxd_subparser = subparsers.add_parser("lxd", help="Run on LXD container")
+    lxd_subparser.add_argument(
+        "--template",
+        type=str,
+        default=os.getenv("LXD_TEMPLATE"),
+        help="URL to template",
+    )
+    lxd_subparser.add_argument(
+        "--rootfs",
+        type=str,
+        default=os.getenv("LXD_ROOTFS"),
+        help="URL to rootfs image",
+    )
+    lxd_subparser.set_defaults(func=test_lxd_gpu)
+
+    lxdvm_subparser = subparsers.add_parser("lxdvm", help="Run on LXD VM")
+    lxdvm_subparser.add_argument(
+        "--template",
+        type=str,
+        default=os.getenv("LXD_TEMPLATE"),
+        help="URL to template",
+    )
+    lxdvm_subparser.add_argument(
+        "--image",
+        type=str,
+        default=os.getenv("KVM_IMAGE"),
+        help="URL to image",
+    )
+    lxdvm_subparser.set_defaults(func=test_lxdvm_gpu)
+
+    return parser.parse_args()
+
+
+def main():
+    """Main entrypoint to the program."""
+    args = parse_args()
+
+    logging.basicConfig(level=args.log_level)
+    logging.getLogger("requests").setLevel(logging.WARNING)
+    logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()

cdb82d0d3519d4504966ce94b3a48fd5df1b4df5
Abdullah <5238256+motjuste@users.noreply.github.com>
2024-11-18 11:40:11 +0100

    Refactor DSS provider tests (New) (#1601)
    The tests for DSS provider in contrib have been refactored so that they use individual scripts now available in the relevant bin folder instead of scripts written inline in the jobs' command. The tests end up checking the same things, but are now better implemented.
    
    The shell scripts were extracted out into one of multiple ones that roughly group related tests, including those generic to DSS, those for setting up Intel GPUs, those for using ITEX, and those for using IPEX. Furthermore, larger Python scripts that were previously written in-line have been factored out into individual ones. Please note that these Python scripts are not meant to be run by the jobs directly, instead they are submitted via the job to DSS to be run in the appropriate Kubernetes Pods, hence the Python dependencies of these scripts are not relevant to the provider itself.

---
+++ contrib/checkbox-dss-validation/checkbox-provider-dss/bin/check_ipex.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+check_ipex_can_be_imported() {
+    echo "Starting ipex import test"
+    script="import intel_extension_for_pytorch as ipex; import torch; import jupyter"
+    if microk8s.kubectl -n dss exec "$1" -- python3 -c "$script"; then
+        echo "PASS: Found module"
+        exit 0
+    else
+        >&2 echo "FAIL: Did not find IPEX python module"
+        exit 1
+    fi
+}
+
+check_pytorch_can_use_xpu() {
+    echo "Starting ipex GPU check test"
+    script="$(cat "$SCRIPT_DIR/pytorch_can_use_xpu.py")"
+    gpu_grep_out=$(microk8s.kubectl -n dss exec "$1" -- python3 -c "$script" | grep "dev_type=.gpu" 2>&1)
+    if [[ -z ${gpu_grep_out} ]]; then
+        >&2 echo "FAIL: No GPU found"
+        exit 1
+    else
+        echo "PASS: GPU found"
+        exit 0
+    fi
+}
+
+help_function() {
+    echo "This script is used for tests related to IPEX"
+    echo "Usage: check_dss.sh <test_case>"
+    echo
+    echo "Test cases currently implemented:"
+    echo -e "\t<can_be_imported>: check_itex_can_be_imported"
+    echo -e "\t<pytorch_can_use_xpu>: check_pytorch_can_use_xpu"
+}
+
+main() {
+    pod=$(microk8s.kubectl get pods -n dss --field-selector=status.phase==Running -o=jsonpath='{.items..metadata.name}' | grep -o 'ipex-2120-notebook\S*')
+    echo "Found PyTorch pod: ${pod}"
+    case ${1} in
+    can_be_imported) check_ipex_can_be_imported "$pod" ;;
+    pytorch_can_use_xpu) check_pytorch_can_use_xpu "$pod" ;;
+    *) help_function ;;
+    esac
+}
+
+main "$@"
---
+++ contrib/checkbox-dss-validation/checkbox-provider-dss/bin/tensorflow_can_use_xpu.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import intel_extension_for_tensorflow as itex
+import tensorflow as tf
+import jupyter
+
+
+devices = tf.config.experimental.list_physical_devices()
+xpu_found = False
+for device_str in devices:
+    if "XPU" in device_str:
+        xpu_found = True
+        break
+
+assert xpu_found, "XPU not found"

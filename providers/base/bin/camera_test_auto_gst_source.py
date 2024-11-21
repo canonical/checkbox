9b59700d59772b2c70ee841880300150138f4c92
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2024-11-21 14:06:06 +0800

    fix: missing handling for deocdebin linking

--- providers/base/bin/camera_test_auto_gst_source.py
+++ providers/base/bin/camera_test_auto_gst_source.py
@@ -1,6 +1,10 @@
 #! /usr/bin/python3
 
+# import time
+# from checkbox_support.helpers.timeout import timeout
+from enum import Enum
 import sys
+import time
 import gi
 from argparse import ArgumentParser
 import typing as T
@@ -18,6 +22,58 @@ Gst.init(None)
 main_loop = GLib.MainLoop.new(  # type: GLib.MainLoop
     None, False  # type: ignore
 )
+
+
+class ElementPrinter:
+    included_properties = {
+        "capsfilter": ("caps",),
+        "multifilesink": ("location",),
+    }
+    global_exclude_keys = ("parent", "client-name")
+    simple_elements = ("videoconvert", "decodebin", "videorate", "jpegenc")
+
+    @staticmethod
+    def print(element: Gst.Element):
+        properties = element.list_properties()  # list[GObject.GParamSpec]
+        element_name = element.get_factory().get_name()
+
+        if element_name in ElementPrinter.simple_elements:
+            return element_name
+
+        print_full = element_name not in ElementPrinter.included_properties
+
+        prop_strings = []  # type: list[str]
+        for prop in properties:
+            if (
+                not print_full
+                and prop.name
+                not in ElementPrinter.included_properties[element_name]
+            ):
+                continue
+            if prop.name in ElementPrinter.global_exclude_keys:
+                continue
+            prop_value = element.get_property(prop.name)
+            if hasattr(prop_value, "to_string"):
+                # sometimes we have a nice to_string method, prioritize this
+                prop_strings.append(
+                    "{}={}".format(prop.name, prop_value.to_string())
+                )
+            elif prop.name == "caps":
+                # need to double quote the caps
+                prop_strings.append(
+                    '{}="{}"'.format(prop.name, prop_value.to_string())
+                )
+            elif type(prop_value) is Enum:
+                prop_strings.append(
+                    "{}={}".format(prop.name, prop_value.value)
+                )
+            else:
+                prop_strings.append(
+                    "{}={}".format(prop.name, str(prop_value))
+                )  # handle native python types
+        return "{} {}".format(
+            element_name, " ".join(prop_strings)
+        )  # libcamerasrc name=cam_name location=p.jpeg
 
 
 def get_devices() -> T.List[Gst.Device]:
@@ -51,55 +107,54 @@ def get_all_fixated_caps(caps: Gst.Caps) -> T.List[Gst.Caps]:
     return fixated_caps
 
 
-def pipeline_add_many(pipeline: Gst.Pipeline, *elements: Gst.Element):
+def pipeline_add_many(
+    pipeline: Gst.Pipeline, elements: T.Iterable[Gst.Element]
+):
     for elem in elements:
         pipeline.add(elem)
 
 
-def element_link_many(*elements: Gst.Element):
+def element_link_many(elements: T.Iterable[Gst.Element]):
     elem_list = list(elements)
     for i in range(len(elem_list) - 1):
-        assert Gst.Element.link(elem_list[i], elem_list[i + 1]), "not linked!"
+        e1, e2 = elem_list[i], elem_list[i + 1]
+        is_linked = Gst.Element.link(e1, e2)
+        if not is_linked:
+            if e1.get_factory().get_name() == "decodebin":
+                e2_copy = e2  # force a reference copy
 
+                def on_pad_added(
+                    decodebin: Gst.Element, decodebin_src: Gst.Pad
+                ):
+                    nonlocal e2_copy
+                    print("\n\ndecode bin pad added\n\n")
+                    e2_name = e2_copy.get_factory().get_name()
+                    e2_sink = e2_copy.get_static_pad("sink")
+                    assert e2_sink, "Null sink"
+                    assert decodebin.link(e2_copy)
+                    print("Linked decodebin to {}".format(e2_name))
 
-def element_to_str(
-    element: Gst.Element,
-    exclude=["parent"],
-    simple_elem=["jpegdec", "videoconvert", "videorate", "multifilesink"],
-) -> str:
-    """Stringifies the given element
-
-    :param element: which element to convert to str
-    :param exclude: which property names to exclude, defaults to ["parent"]
-    :return: string usable in gst-launch-1.0
-    """
-    properties = element.list_properties()  # list[GObject.GParamSpec]
-    element_name = element.get_factory().get_name()
-
-    if element_name in simple_elem:
-        return element_name
-
-    prop_strings = []  # type: list[str]
-    for prop in properties:
-        if prop.name in exclude:
-            continue
-        prop_value = element.get_property(prop.name)
-        if hasattr(prop_value, "to_string"):
-            # sometimes we have a nice to_string method, prioritize this
-            prop_strings.append(
-                "{}={}".format(prop.name, prop_value.to_string())
-            )
-        else:
-            prop_strings.append(
-                "{}={}".format(prop.name, str(prop_value))
-            )  # handle native python types
-    return "{} {}".format(
-        element_name, " ".join(prop_strings)
-    )  # libcamerasrc name=cam_name location=p.jpeg
+                e1.connect("pad-added", on_pad_added)
+            else:
+                raise RuntimeError(
+                    "{} and {} could not be linked!".format(
+                        ElementPrinter.print(e1),
+                        ElementPrinter.print(e2),
+                    )
+                )
 
 
 def parse_args():
-    parser = ArgumentParser()
+    p = ArgumentParser()
+    p.add_argument(
+        "-s",
+        "--wait-seconds",
+        type=int,
+        help="Number of seconds to keep the pipeline running "
+        "before taking the photo. Default = 1",
+        default=1,
+    )
+    return p.parse_args()
 
 
 def take_photo(
@@ -109,6 +164,15 @@ def take_photo(
     filename="/home/fgfg/photo",
     delay_seconds=0,
 ):
+    """Take a photo using the source element
+
+    :param source: The camera source element
+    :param caps: Which capability to use for the source
+        - If none, no caps filter will be inserted between source and decoder
+    :param filename: the path to the photo
+    :param delay_seconds: number of seconds to keep the pipeline running
+        before taking the photo
+    """
     # key is the name, value is the element. Ordered
     elements = {
         "source-capsfilter": Gst.ElementFactory.make(
@@ -131,18 +195,15 @@ def take_photo(
     )
 
     if caps:
-        assert (
-            caps.is_fixed()
-        ), '"{}" is not fixed. If caps.fixate was called before, then this is a bug in GStreamer'.format(
-            caps.to_string()
-        )
+        assert caps.is_fixed(), '"{}" is not fixed.'.format(caps.to_string())
         elements["source-capsfilter"].set_property("caps", caps)
         # structure 0 is guaranteed to exist
         mime_type = caps.get_structure(0).get_name()  # type: str
         if mime_type == "image/jpeg":
+            # decodebin has funny clock problem with image/jpeg
             elements["decoder"] = Gst.ElementFactory.make("jpegdec")
-            assert elements["decoder"] is not None
-        elif mime_type == "video/x-raw":
+        assert elements["decoder"] is not None
+        if mime_type == "video/x-raw":
             # don't need a decoder for raw
             del elements["decoder"]
         # else case is using decodebin as a fallback
@@ -166,9 +227,10 @@ def take_photo(
             num_buffers = delay_seconds * int(framerate_match.group(1))
             source.set_property("num-buffers", num_buffers)
             print(
-                "[ INFO ] Dynamically computed num-buffers={} to delay {} seconds".format(
+                "[ INFO ] Dynamically computed",
+                "num-buffers={} to delay {} seconds".format(
                     num_buffers, delay_seconds
-                )
+                ),
             )
         else:
             source.set_property("num-buffers", 60)
@@ -185,14 +247,15 @@ def take_photo(
         del elements["video-rate-capsfilter"]
 
     # link elements and create pipeline
-    pipeline = Gst.Pipeline()  # type: Gst.Pipeline
+    final_elements = (source, *elements.values())
+    pipeline = Gst.Pipeline()
     # Gst.Pipeline.add_many and Gst.Element.link_many
     # do not exist in default ubuntu gst python binding
-    pipeline_add_many(pipeline, source, *elements.values())
-    element_link_many(source, *elements.values())
+    pipeline_add_many(pipeline, final_elements)
+    element_link_many(final_elements)
 
     print("Created pipeline: ")
-    print(" ! ".join(element_to_str(e) for e in (source, *elements.values())))
+    print(" ! ".join(ElementPrinter.print(e) for e in final_elements))
 
     def eos_handler(_, message: Gst.Message):
         nonlocal pipeline
@@ -202,6 +265,10 @@ def take_photo(
             # none of which can be the pipeline
             pipeline.set_state(Gst.State.NULL)
             main_loop.quit()
+        if message.type == Gst.MessageType.ELEMENT:
+            print(("Element message from bus:"), bus.name)
+            message_string = message.get_structure().to_string()  # type: ignore
+            print(message_string)
 
     bus = pipeline.get_bus()
     bus.add_signal_watch()
@@ -209,13 +276,14 @@ def take_photo(
 
     print("Setting playing state")
     pipeline.set_state(Gst.State.PLAYING)
-
+    print("Playing")
     main_loop.run()
     while main_loop.is_running():
         pass
 
 
 def main():
+    args = parse_args()
     devices = get_devices()
     if len(devices) == 0:
         print(
@@ -229,19 +297,21 @@ def main():
         caps = device.get_caps()
         for cap_i, capability in enumerate(get_all_fixated_caps(caps)):
             print(
-                "Testing",
+                "[ INFO ] Testing",
                 capability.to_string(),
                 "for device",
                 device.get_display_name(),
             )
             take_photo(
                 device.create_element(),
-                delay_seconds=1,
+                delay_seconds=args.wait_seconds,
                 caps=capability,
                 filename="/home/zhongning/fgfg/photo_dev{}_cap{}".format(
                     dev_i, cap_i
                 ),
             )
+            break
+        break
 
 
 if __name__ == "__main__":

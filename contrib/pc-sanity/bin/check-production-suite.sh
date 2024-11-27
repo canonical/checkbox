4b06588f34e68543bb70ae833fe95cbd94e746bc
Artur <40683252+Artur-at-work@users.noreply.github.com>
2024-11-27 11:36:38 +0800

    pc-sanity: Update platform archive checks test for 24.04 (BugFix) (#1624)
    * pc-sanity: check-production-suite should be run for any later versions (BugFix)
    
    * pc-sanity: Update platform archive checks for Ubuntu 24.04 and 22.04 (BugFix)
    
    ---------
    
    Co-authored-by: Yao Wei (魏銘廷) <yao.wei@canonical.com>

--- contrib/pc-sanity/bin/check-production-suite.sh
+++ contrib/pc-sanity/bin/check-production-suite.sh
@@ -3,6 +3,7 @@
 codename=$(lsb_release -cs)
 oemcodename=$(get-oem-info.sh --oem-codename)
 platform=$(get-oem-info.sh --platform-codename)
+release_number=$(lsb_release -rs)
 
 if [ -z "$codename" ] || [ -z "$oemcodename" ] || [ -z "$platform" ]; then
     echo "Can't get oem info from the platform"
@@ -40,10 +41,20 @@ if ! [[ "$components" == *"$oemcodename"* ]]; then
     exit 1
 fi
 
-if [ "$oemcodename" = "somerville" ]; then
+if [ "$(echo "$release_number >= 24.04" | bc -l)" -eq 1 ]; then
+    # For versions 24.04 and newer, check platform archives for all OEMs
     if ! [[ "$components" == *"$oemcodename-$platform"* ]]; then
         echo "platform archive is not ready"
         echo "$components"
         exit 1
+    fi
+elif [ "$(echo "$release_number == 22.04" | bc -l)" -eq 1 ]; then
+    # For 22.04, only check somerville
+    if [ "$oemcodename" = "somerville" ]; then
+        if ! [[ "$components" == *"$oemcodename-$platform"* ]]; then
+            echo "platform archive is not ready"
+            echo "$components"
+            exit 1
+        fi
     fi
 fi

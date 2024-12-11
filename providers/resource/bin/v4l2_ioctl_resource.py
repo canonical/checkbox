ebd9286e3fdfdbc75089ec4cbcffddcd8edd8da1
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2024-12-11 12:00:46 +0800

    feat: add resource job

---
+++ providers/resource/bin/v4l2_ioctl_resource.py
@@ -0,0 +1,12 @@
+from checkbox_support.parsers.v4l2_compliance import TEST_NAME_TO_IOCTL_MAP
+
+
+def main():
+    for ioctl_names in TEST_NAME_TO_IOCTL_MAP.values():
+        for name in ioctl_names:
+            print("name: {}".format(name))
+            print()  # empty line to mark end of list item
+
+
+if __name__ == "__main__":
+    main()

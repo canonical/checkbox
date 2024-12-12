57f4b6d40de1d3b9483b8095c3fcc11b5942c2c0
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2024-12-12 15:05:56 +0800

    feat: generate tests for each device

--- providers/resource/bin/v4l2_ioctl_resource.py
+++ providers/resource/bin/v4l2_ioctl_resource.py
@@ -1,13 +1,25 @@
 #! /usr/bin/python3
 
 from checkbox_support.parsers.v4l2_compliance import TEST_NAME_TO_IOCTL_MAP
+import subprocess as sp
 
 
 def main():
-    for ioctl_names in TEST_NAME_TO_IOCTL_MAP.values():
-        for name in ioctl_names:
-            print("ioctl_name: {}".format(name))
-            print()  # empty line to mark end of list item
+    try:
+        udev_out = sp.check_output(
+            "udev_resource.py -f CAPTURE", universal_newlines=True, shell=True
+        )
+        lines = udev_out.splitlines()
+    except Exception as e:
+        lines = [f"name: broken {str(e)}"]
+
+    for line in lines:
+        if line.startswith("name:"):
+            for ioctl_names in TEST_NAME_TO_IOCTL_MAP.values():
+                for name in ioctl_names:
+                    print(line)
+                    print("ioctl_name:", format(name))
+                    print()  # empty line to mark end of list item
 
 
 if __name__ == "__main__":

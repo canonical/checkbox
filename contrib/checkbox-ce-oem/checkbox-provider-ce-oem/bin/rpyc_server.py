ed274c044051f9fc3228912120ab115d40532ad7
stanley31huang <stanley.huang@canonical.com>
2025-01-06 18:14:11 +0800

    refactor rpyc server
    refactor rpyc server

--- contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/rpyc_server.py
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/rpyc_server.py
@@ -37,6 +37,22 @@ LIBS = {
         ),
         "function": "perform_ping_test",
     },
+    "serial_check": {
+        "source": __file__,
+        "function": "serial_check",
+    },
+    "ethernet_check": {
+        "source": __file__,
+        "function": "ethernet_check",
+    },
+    "configure_local_network": {
+        "source": __file__,
+        "function": "configure_local_network",
+    },
+    "usb_storage_test": {
+        "source": __file__,
+        "function": "usb_storage_test",
+    },
 }
 dynamic_integrate_funcs = [LIBS[k]["function"] for k in LIBS.keys()]
 
@@ -73,100 +89,101 @@ def _load_method_from_file(name, file, func):
         logger.debug(e)
 
 
+def append_method_to_service(cls):
+    for key, value in LIBS.items():
+        func = _load_method_from_file(
+            key, value["source"], value["function"]
+        )
+        if func:
+            setattr(cls, key, capture_io_logs(func))
+
+    return cls
+
+
+# Method for testing
+def _get_ethernet_ifaces():
+    return set([i.name for i in Path("/sys/class/net").iterdir()])
+
+
+def _get_serial_ifaces():
+    return set([i.name for i in Path("/dev/serial/by-id").iterdir()])
+
+
+def _device_node_detect(func, device_type=""):
+    starting_ifaces = func()
+
+    attempts = 20
+    while attempts > 0:
+        now_ifaces = func()
+        # check if something disappeared
+        if not starting_ifaces == now_ifaces & starting_ifaces:
+            raise SystemExit(
+                "Interface(s) disappeared: {}".format(
+                    ", ".join(list(starting_ifaces - now_ifaces))
+                )
+            )
+        new_ifaces = now_ifaces - starting_ifaces
+        if new_ifaces:
+            print()
+            print(
+                "New interface(s) detected: {}".format(
+                    ", ".join(list(new_ifaces))
+                )
+            )
+            return new_ifaces
+        time.sleep(1)
+        print(".", end="", flush=True)
+        attempts -= 1
+    print()
+    raise SystemExit(
+        "Failed to detect new {} interface".format(device_type)
+    )
+
+
+def serial_check():
+    return _device_node_detect(_get_serial_ifaces, "serial")
+
+
+def ethernet_check():
+    return _device_node_detect(_get_ethernet_ifaces, "ethernet")
+
+
+def configure_local_network(interface, net_info):
+    logger.info("set %s to %s interface on RPYC", net_info, interface)
+    subprocess.check_output(
+        "ip addr add {} dev {}".format(net_info, interface),
+        shell=True,
+        text=True,
+    )
+
+
+def usb_storage_test(usb_type):
+    logger.info("%s drive read/write testing on RPYC", usb_type)
+    usb_read_write.REPETITION_NUM = 2
+    watcher = run_watcher.USBStorage(usb_type)
+    mounted_partition = watcher.run_insertion()
+    logger.info(
+        "%s drive been mounted to %s on RPYC", usb_type, mounted_partition
+    )
+    watcher.run_storage(mounted_partition)
+    # usb_read_write been imported in run_watcher
+    # the temporary mount point been created while import it and been removed in gen_random_file function
+    # thus, we need to reload it to create temporary mount point in every testing cycle
+    reload(usb_read_write)
+
+
 class RpycTestService(rpyc.Service):
 
     def __init__(self):
         super().__init__()
         self.logs = ""
-        for key, value in LIBS.items():
-            func = _load_method_from_file(
-                key, value["source"], value["function"]
-            )
-            if func:
-                setattr(RpycTestService, key, capture_io_logs(func))
-
-    def on_connect(self, conn):
-        # code that runs when a connection is created
-        # (to init the service, if needed)
-        pass
-
-    def on_disconnect(self, conn):
-        # code that runs after the connection has already closed
-        # (to finalize the service, if needed)
-        pass
-
-    def _get_ethernet_ifaces(self):
-        return set([i.name for i in Path("/sys/class/net").iterdir()])
-
-    def _get_serial_ifaces(self):
-        return set([i.name for i in Path("/dev/serial/by-id").iterdir()])
-
-    def _device_node_detect(self, func, device_type=""):
-        starting_ifaces = func()
-
-        attempts = 20
-        while attempts > 0:
-            now_ifaces = func()
-            # check if something disappeared
-            if not starting_ifaces == now_ifaces & starting_ifaces:
-                raise SystemExit(
-                    "Interface(s) disappeared: {}".format(
-                        ", ".join(list(starting_ifaces - now_ifaces))
-                    )
-                )
-            new_ifaces = now_ifaces - starting_ifaces
-            if new_ifaces:
-                print()
-                print(
-                    "New interface(s) detected: {}".format(
-                        ", ".join(list(new_ifaces))
-                    )
-                )
-                return new_ifaces
-            time.sleep(1)
-            print(".", end="", flush=True)
-            attempts -= 1
-        print()
-        raise SystemExit(
-            "Failed to detect new {} interface".format(device_type)
-        )
-
-    @capture_io_logs
-    def serial_check(self):
-        return self._device_node_detect(self._get_serial_ifaces, "serial")
-
-    @capture_io_logs
-    def ethernet_check(self):
-        return self._device_node_detect(self._get_ethernet_ifaces, "ethernet")
-
-    @capture_io_logs
-    def configure_local_network(self, interface, net_info):
-        logger.info("set %s to %s interface on RPYC", net_info, interface)
-        subprocess.check_output(
-            "ip addr add {} dev {}".format(net_info, interface),
-            shell=True,
-            text=True,
-        )
-
-    @capture_io_logs
-    def usb_storage_test(self, usb_type):
-        logger.info("%s drive read/write testing on RPYC", usb_type)
-        usb_read_write.REPETITION_NUM = 2
-        watcher = run_watcher.USBStorage(usb_type)
-        mounted_partition = watcher.run_insertion()
-        logger.info(
-            "%s drive been mounted to %s on RPYC", usb_type, mounted_partition
-        )
-        watcher.run_storage(mounted_partition)
-        # usb_read_write been imported in run_watcher
-        # the temporary mount point been created while import it and been removed in gen_random_file function
-        # thus, we need to reload it to create temporary mount point in every testing cycle
-        reload(usb_read_write)
 
 
 def main():
+    rpyc_service = append_method_to_service(RpycTestService())
+
     t = ThreadedServer(
-        RpycTestService,
+        rpyc_service,
         port=60000,
         logger=logger,
         protocol_config={

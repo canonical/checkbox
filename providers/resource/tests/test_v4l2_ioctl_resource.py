cb311e29419fe5fd7a3db62fe8ddffdcee50135f
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-01-17 14:09:47 +0800

    test: update unittests

--- providers/resource/tests/test_v4l2_ioctl_resource.py
+++ providers/resource/tests/test_v4l2_ioctl_resource.py
@@ -6,37 +6,15 @@ from checkbox_support.parsers.v4l2_compliance import TEST_NAME_TO_IOCTL_MAP
 
 class TestV4L2IoctlResource(ut.TestCase):
     @patch("v4l2_ioctl_resource.sp.check_output")
-    def test_all_ioctl_names_are_generated(self, mock_check_output: MagicMock):
-        mock_check_output.return_value = "\n".join(
-            [
-                "path: /devices/pci0000:00/0000:00:14.0/usb3/"
-                + "3-9/3-9:1.0/video4linux/video0",
-                "name: video0",
-                "bus: video4linux",
-                "category: CAPTURE",
-                "driver: uvcvideo",
-                "product_id: 22782",
-                "vendor_id: 3034",
-                "product: Integrated_Webcam_HD: Integrate",
-                "vendor: CN0PW36V8LG009BQA0YWA00",
-                "product_slug: Integrated_Webcam_HD__Integrate",
-                "vendor_slug: CN0PW36V8LG009BQA0YWA00",
-                "",
-                "path: /devices/pci0000:00/0000:00:14.0/usb3/"
-                + "3-9/3-9:1.0/video4linux/video1",
-                "name: video1",
-                "bus: video4linux",
-                "category: CAPTURE",
-                "driver: uvcvideo",
-                "product_id: 2312312",
-                "vendor_id: 12213",
-                "product: Integrated_Webcam_HD: Integrate",
-                "vendor: CN0PW36V3444438LG009BQA0YWA00",
-                "product_slug: Integrated_Webcam_HD__Integrate",
-                "vendor_slug: CN0PW36V8LG009BQA0YWA00",
-            ]
-        )
-
+    @patch("v4l2_ioctl_resource.UdevadmParser")
+    def test_all_ioctl_names_are_generated(
+        self, mock_udev: MagicMock, mock_check_output: MagicMock
+    ):
+        mock_dev_list = [MagicMock(), MagicMock()]
+        for i, dev in enumerate(mock_dev_list):
+            dev.name = "video{}".format(i)
+            dev.category = "CAPTURE"
+        mock_udev.return_value.run.return_value = mock_dev_list
         with patch("builtins.print") as mock_print:
             main_under_test()
             expected_calls = []

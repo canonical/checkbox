832464bdfac82a5730abf0304444c8286e927de7
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-01-20 13:18:44 +0800

    style: header comments

--- checkbox-support/checkbox_support/camera_pipelines.py
+++ checkbox-support/checkbox_support/camera_pipelines.py
@@ -1,3 +1,22 @@
+# This file is part of Checkbox.
+#
+# Copyright 2025 Canonical Ltd.
+# Written by:
+#   Zhongning Li <zhongning.li@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from enum import Enum
 import gi
 import typing as T

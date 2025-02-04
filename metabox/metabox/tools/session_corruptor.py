0950afc9d094ac56216e26ef430956891b6ebc93
Hook25 <massimiliano.girardi@canonical.com>
2025-02-04 17:30:27 +0100

    Forgotten tool to corrupt sessions

---
+++ metabox/metabox/tools/session_corruptor.py
@@ -0,0 +1,24 @@
+import gzip
+import json
+
+from pathlib import Path
+
+
+def corrupt(path):
+    with gzip.open(str(path)) as f:
+        session = json.load(f)
+    session["session"]["desired_job_list"].append(
+        "@ invalid id - intentionally corrupted session"
+    )
+    with gzip.open(str(path), "wt") as f:
+        json.dump(session, f)
+
+
+def main():
+    for session in Path("/var/tmp/checkbox-ng/sessions").glob("*/session"):
+        print("Corrupting session", str(session))
+        corrupt(session)
+
+
+if __name__ == "__main__":
+    main()

6b1ec69c831bc85f94ba7de5ae5e53e25ecc0e30
Abdullah <abdullah.abdullah@canonical.com>
2025-02-12 17:44:03 +0530

    add creating parser with checks

--- contrib/checkbox-dss-validation/checkbox-provider-dss/bin/_common.py
+++ contrib/checkbox-dss-validation/checkbox-provider-dss/bin/_common.py
@@ -8,7 +8,35 @@ Authors:
     - Abdullah (@motjuste) <abdullah.abdullah@canonical.com>
 """
 
+import argparse
+import inspect
 import subprocess
+import typing as t
+
+
+def create_parser_with_checks_as_commands(
+    checks: t.List[t.Callable], **kwargs
+) -> argparse.ArgumentParser:
+    assert len(checks) > 0, "must provide at least one check"
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter, **kwargs
+    )
+    sub_parsers = parser.add_subparsers(required=True)
+
+    for check in checks:
+        command_parser = sub_parsers.add_parser(
+            check.__name__, description=check.__doc__, help=check.__doc__
+        )
+        command_parser.set_defaults(func=check)
+        for name, param in inspect.signature(check).parameters.items():
+            assert param.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD
+            assert param.annotation is not bool
+            assert param.default is inspect._empty
+
+            command_parser.add_argument(name, type=param.annotation)
+
+    return parser
 
 
 def run_command(*command: str, **kwargs) -> str:
--- contrib/checkbox-dss-validation/checkbox-provider-dss/tests/test_common.py
+++ contrib/checkbox-dss-validation/checkbox-provider-dss/tests/test_common.py
@@ -8,6 +8,7 @@ Authors:
     - Abdullah (@motjuste) <abdullah.abdullah@canonical.com>
 """
 
+import argparse
 import subprocess
 import unittest
 from unittest import mock
@@ -66,3 +67,55 @@ class TestRunCommand(unittest.TestCase):
         with self.assertRaises(SystemExit) as caught:
             _common.run_command("testing")
         assert caught.exception.__cause__ == causing_exception
+
+
+def check_1():
+    """check 1"""
+    return 1
+
+
+def check_2():
+    """check 2"""
+    return 2
+
+
+def check_p2(n: int):
+    """check p2"""
+    return n**2
+
+
+class TestCreateParserWithChecks(unittest.TestCase):
+    def test_accepts_at_least_one_check(self):
+        with self.subTest("no checks"):
+            with self.assertRaises(AssertionError):
+                _common.create_parser_with_checks_as_commands([])
+        with self.subTest("1 check"):
+            _common.create_parser_with_checks_as_commands([check_1])
+        with self.subTest("2 checks"):
+            _common.create_parser_with_checks_as_commands([check_1, check_2])
+        with self.subTest("3 checks"):
+            _common.create_parser_with_checks_as_commands([check_1, check_2, check_p2])
+
+    def test_catches_duplicate_checks(self):
+        with self.assertRaises(argparse.ArgumentError) as caught:
+            _common.create_parser_with_checks_as_commands(
+                [check_1, check_2, check_p2, check_1]
+            )
+        assert "conflicting subparser: check_1" in caught.exception.message
+
+    def test_creates_expected_sub_parsesr_funcs(self):
+        parser = _common.create_parser_with_checks_as_commands(
+            [check_1, check_2, check_p2]
+        )
+        for check, args in [
+            (check_1, ["check_1"]),
+            (check_2, ["check_2"]),
+            (check_p2, ["check_p2", "2"]),
+        ]:
+            with self.subTest(check):
+                parsed = parser.parse_args(args)
+                assert parsed.func == check
+
+    # XXX:@motjuste: argparse provides very limited introspecting for sub-parsers?
+    #   Otherwise, tests should be added about setting appropriate help, type, and
+    #   description for the sub-parsers

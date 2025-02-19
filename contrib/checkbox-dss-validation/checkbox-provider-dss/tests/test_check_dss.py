073b67a3388f559aa020603dbac0b6e15d91e35b
Abdullah <abdullah.abdullah@canonical.com>
2025-02-19 20:23:46 +0530

    Fix copyright info in Python scripts

--- contrib/checkbox-dss-validation/checkbox-provider-dss/tests/test_check_dss.py
+++ contrib/checkbox-dss-validation/checkbox-provider-dss/tests/test_check_dss.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
-# Copyright 2025 Canonical Ltd.
-# All rights reserved.
 #
-"""Tests for `check_dss.py`
-
-Authors:
-    - Abdullah (@motjuste) <abdullah.abdullah@canonical.com>
-"""
+# This file is part of Checkbox.
+#
+# Copyright 2022 Canonical Ltd.
+#
+# Authors:
+#     Abdullah (@motjuste) <abdullah.abdullah@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+#
+"""Tests for `check_dss.py`"""
 
 import os
 import subprocess

76c6e0e3c4b7610ea004780d7836517157c65359
Abdullah <abdullah.abdullah@canonical.com>
2025-02-19 21:05:14 +0530

    Fix copyright info in scripts

--- contrib/checkbox-dss-validation/checkbox-provider-dss/bin/enable_intel.sh
+++ contrib/checkbox-dss-validation/checkbox-provider-dss/bin/enable_intel.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
-# Copyright 2025 Canonical Ltd.
-# All rights reserved.
 #
-# Enable the Intel GPU plugin in the Kubernetes cluster
+# This file is part of Checkbox.
+#
+# Copyright 2022 Canonical Ltd.
 #
 # Authors:
-#   - Abdullah (@motjuste) <abdullah.abdullah@canonical.com>
+#     Abdullah (@motjuste) <abdullah.abdullah@canonical.com>
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
+#
 set -eo pipefail
 
 VERSION="$1"

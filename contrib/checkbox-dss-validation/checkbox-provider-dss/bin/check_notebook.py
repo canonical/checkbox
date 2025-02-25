7e663514bac9348249fe44d1290dc16bac0e484f
Abdullah <abdullah.abdullah@canonical.com>
2025-02-25 16:46:42 +0530

    Add new arg parser and main for checking notebooks

--- contrib/checkbox-dss-validation/checkbox-provider-dss/bin/check_notebook.py
+++ contrib/checkbox-dss-validation/checkbox-provider-dss/bin/check_notebook.py
@@ -21,7 +21,11 @@
 #
 """Check notebooks in DSS"""
 
+import argparse
+import os
+from pathlib import Path
 import subprocess
+import textwrap
 import typing as t
 
 
@@ -52,8 +56,54 @@ def get_notebook_pod(notebook_name: str) -> str:
     )
 
 
+def create_parser(checks_names: t.Sequence[str]) -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description=textwrap.dedent(
+            """
+           Check notebooks in DSS by running Python scripts in their pods.
+
+           Possible scripts to run must be placed in the data directory,
+           and named as `<check_name>.py` for them to be available as a
+           check named `<check_name>`.  The data directory is NOT searched
+           recursively.
+
+           If no scripts are found in the data directory, an error is raised.
+
+           `./data` is used as the data directory by default, but it can
+           be overridden by setting `PLAINBOX_PROVIDER_DATA` in the env.
+           """
+        ),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "notebook_name",
+        type=str,
+        help="Name of the notebook in DSS to run the check in",
+    )
+    parser.add_argument(
+        "check_name",
+        type=str,
+        choices=checks_names,
+        help="Name of check to perform in the notebook",
+    )
+    return parser
+
+
+def get_available_checks() -> t.Dict[str, Path]:
+    data_dir = Path(os.getenv("PLAINBOX_PROVIDER_DATA", "data"))
+    checks = {script.stem: script for script in data_dir.glob("*.py")}
+    if len(checks) == 0:
+        raise ValueError(f"no Python scripts found in directory {data_dir}")
+    return checks
+
+
 def main(args: t.List[str] | None = None) -> None:
-    raise NotImplementedError
+    checks = get_available_checks()
+    parser = create_parser(sorted(checks.keys()))
+    parsed = parser.parse_args(args)
+
+    script = checks[parsed.check_name].read_text()
+    run_script_in_notebook(parsed.notebook_name, script)
 
 
 if __name__ == "__main__":  # pragma: no cover
--- contrib/checkbox-dss-validation/checkbox-provider-dss/tests/test_check_notebook.py
+++ contrib/checkbox-dss-validation/checkbox-provider-dss/tests/test_check_notebook.py
@@ -21,17 +21,132 @@
 #
 """Tests for `check_notebook.py`"""
 
+import argparse
+import contextlib
+import os
+from pathlib import Path
 import subprocess
+import tempfile
 import unittest
 from unittest import mock
 
 import check_notebook
 
 
-class TestArgumentParsing(unittest.TestCase): ...
+class TestCreateParser(unittest.TestCase):
+    check_names = ["check1", "check_2"]
+
+    def test_has_help(self):
+        parser = check_notebook.create_parser(self.check_names)
+        with self.assertRaises(SystemExit) as raised:
+            parser.parse_args(["--help"])
+        self.assertEqual(raised.exception.code, 0)
+
+    def test_normal_success(self):
+        parser = check_notebook.create_parser(self.check_names)
+        parsed = parser.parse_args(["notebook_1", "check1"])
+        with self.subTest("notebook name"):
+            self.assertEqual(parsed.notebook_name, "notebook_1")
+        with self.subTest("check name"):
+            self.assertEqual(parsed.check_name, "check1")
+
+    def test_accepts_all_check_names(self):
+        parser = check_notebook.create_parser(self.check_names)
+        for check in self.check_names:
+            with self.subTest(check):
+                parsed = parser.parse_args(["notebook", check])
+            self.assertEqual(parsed.check_name, check)
+
+    def test_exits_with_code_2_on_unknown_check_name(self):
+        parser = check_notebook.create_parser(self.check_names)
+        with self.assertRaises(SystemExit) as raised:
+            parser.parse_args(["notebook", "not a check"])
+        self.assertEqual(raised.exception.code, 2)
+
+    def test_exits_with_code_2_on_missing_notebook_name(self):
+        parser = check_notebook.create_parser(self.check_names)
+        with self.assertRaises(SystemExit) as raised:
+            parser.parse_args([self.check_names[0]])
+        self.assertEqual(raised.exception.code, 2)
+
+    def test_exits_with_code_2_on_missing_check_name(self):
+        parser = check_notebook.create_parser(self.check_names)
+        with self.assertRaises(SystemExit) as raised:
+            parser.parse_args(["notebook"])
+        self.assertEqual(raised.exception.code, 2)
+
+    def test_exits_with_code_2_on_too_many_args(self):
+        parser = check_notebook.create_parser(self.check_names)
+        with self.assertRaises(SystemExit) as raised:
+            parser.parse_args(
+                [
+                    "notebook",
+                    self.check_names[0],
+                    self.check_names[1],
+                ]
+            )
+        self.assertEqual(raised.exception.code, 2)
 
 
-class TestMain(unittest.TestCase): ...
+class TestAvailableChecks(unittest.TestCase):
+    def test_picks_python_scripts_from_env_var(self):
+        check_names = ["check1", "check_2"]
+        script_names = [f"{name}.py" for name in check_names]
+
+        with tempfile.TemporaryDirectory() as d:
+            os.environ["PLAINBOX_PROVIDER_DATA"] = d
+            for script in script_names:
+                (Path(d) / script).touch()
+            actual = check_notebook.get_available_checks()
+
+        with self.subTest("picks check names"):
+            self.assertListEqual(sorted(actual.keys()), check_names)
+        for check, script in zip(check_names, script_names):
+            with self.subTest(f"script path for {check}"):
+                actual_script_path = actual[check]
+                self.assertEqual(actual_script_path, Path(d) / script)
+
+    def test_raises_error_when_data_dir_has_no_scripts(self):
+        with tempfile.TemporaryDirectory() as d:
+            os.environ["PLAINBOX_PROVIDER_DATA"] = d
+            (Path(d) / "something.txt").touch()
+            with self.assertRaises(ValueError):
+                check_notebook.get_available_checks()
+
+
+class TestMain(unittest.TestCase):
+    check_names = ["check1", "check_2"]
+    script_names = [f"{name}.py" for name in check_names]
+    script_contents = [f"print({name})" for name in check_names]
+
+    @contextlib.contextmanager
+    def data_dir_setup(self):
+        with tempfile.TemporaryDirectory() as d:
+            os.environ["PLAINBOX_PROVIDER_DATA"] = d
+            for script, content in zip(
+                self.script_names,
+                self.script_contents,
+            ):
+                (Path(d) / script).write_text(content)
+            yield
+
+    @mock.patch(
+        "check_notebook.create_parser",
+        return_value=argparse.ArgumentParser(),
+    )
+    def test_creates_parser_with_available_checks(self, mocked):
+        with self.data_dir_setup():
+            with self.assertRaises(SystemExit):
+                check_notebook.main(["--help"])
+        mocked.assert_called_once_with(self.check_names)
+
+    @mock.patch("check_notebook.run_script_in_notebook")
+    def test_runs_script_content_notebook(self, mocked):
+        with self.data_dir_setup():
+            for check, content in zip(self.check_names, self.script_contents):
+                check_notebook.main(["notebook", check])
+                mocked.assert_called_once_with("notebook", content)
+                mocked.reset_mock()
 
 
 class TestRunScriptInNotebook(unittest.TestCase):

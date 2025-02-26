5618016e9abb8cc0ad05d774317fd703b00f2f4c
Pierre Equoy <pierre.equoy@canonical.com>
2025-02-26 14:25:30 +0100

    Slugify helper function should replace hyphens (bugfix) (#1754)
    * Slugify helper function should replace hyphens
    
    The slugify helper function is used in resource jobs for attributes that
    are then used for some job requirements. These attributes should be
    Python-compatible, meaning they cannot include hyphens.
    
    Modifying the slugify function to replace hyphens with an underscore.
    
    Fix #1745
    
    * Slugify dots as well
    
    * Prefix string with underscore if it starts with a digit
    
    * Better digit check

--- checkbox-support/checkbox_support/helpers/slugify.py
+++ checkbox-support/checkbox_support/helpers/slugify.py
@@ -20,8 +20,16 @@ import string
 
 
 def slugify(_string):
-    """Transform any string to one that can be used in filenames."""
+    """
+    Slugify a string
+
+    Transform any string to one that can be used in filenames and Python
+    identifers.
+    """
     valid_chars = frozenset(
-        "-_.{}{}".format(string.ascii_letters, string.digits)
+        "_{}{}".format(string.ascii_letters, string.digits)
     )
+    # Python identifiers cannot start with a digit
+    if _string[0].isdigit():
+        _string = "_" + _string
     return "".join(c if c in valid_chars else "_" for c in _string)

4dcbc98c09c792b926e8026fbd1f61397c061add
Gabriel Zhi Chen <gabriel.chen@canonical.com>
2025-03-06 15:42:12 +0800

    A test case for randomly generate input events

---
+++ providers/base/tests/test_mouse_key.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import patch
+import mouse
+
+class TestMouse(unittest.TestCase):
+    def test_main_successful(self):
+        mouse.main()
+
+    @patch("mouse.rand_mouse_moves", side_effect=Exception())
+    def test_main_failure(self):
+        mouse.main()

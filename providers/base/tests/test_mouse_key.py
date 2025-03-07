575451135ddb718c50f82ad77e1ed47076294879
Gabriel Zhi Chen <gabriel.chen@canonical.com>
2025-03-07 15:11:48 +0800

    Modify the test_mouse_key.py, correct the typo.

--- providers/base/tests/test_mouse_key.py
+++ providers/base/tests/test_mouse_key.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import unittest
-from unittest.mock import patch
-import mouse_keyboard
+from unittest.mock import patch, MagicMock
+import sys
+sys.modules["evdev"] = MagicMock()
+from mouse_keyboard import *
 
 
 class TestMouse(unittest.TestCase):
     def test_main_successful(self):
-        mouse_keyboard.main()
+        main()
 
     @patch("mouse_keyboard.rand_mouse_moves", side_effect=Exception())
-    def test_main_failure(self):
-        mouse_keyboard.main()
+    def test_main_failure(self, mock_mouse):
+        main()

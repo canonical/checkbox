9e51d1a1e4adeb31ee0eb2025b57a549e76cd1cf
Michael Reed <Michael.Reed@canonical.com>
2025-04-14 10:39:39 -0500

    Updates made to address the PR comments

--- providers/base/bin/sriov.py
+++ providers/base/bin/sriov.py
@@ -23,8 +23,6 @@ import argparse
 import logging
 import os
 import sys
-import distro
-
 from checkbox_support.lxd_support import LXD, LXDVM
 
 # Map vendor IDs to vendor names
@@ -38,23 +36,32 @@ VENDOR_INFO = {
 NUM_OF_VIRTUAL_IFACES = 1
 
 
+def get_release_to_test():
+    try:
+        import distro
+
+        if distro.id() == "ubuntu-core":
+            return "{}.04".format(distro.version())
+        return distro.version()
+    except (ImportError):
+        import lsb_release
+
+        return lsb_release.get_distro_information()["RELEASE"]
+
+
 def check_ubuntu_version():
     logging.info("Check for 24.04 or greater")
     try:
-        # Get distro information
-        version = distro.version()
+        version = get_release_to_test()
 
-        # Check if the OS is Ubuntu and the version is greater than 24.04
         if float(version) < 24.04:
-            logging.info(
-                "24.04 or greater is required, this is {}".format(version)
-            )
-            sys.exit(1)
+            raise ValueError("Ubuntu 24.04 or greater is required, \
+                             but found {}.".format(version))
         else:
             logging.info("The system is 24.04 or greater, proceed")
-    except Exception as e:
-        logging.info("An error occurred: {}".format(e))
-        sys.exit(1)
+
+    except (Exception, ValueError) as e:
+        logging.error("Error checking Ubuntu version: {}".format(e))
 
 
 def check_interface_vendor(interface):
@@ -64,30 +71,25 @@ def check_interface_vendor(interface):
     vendor_id_path = "/sys/class/net/{}/device/vendor".format(interface)
 
     try:
-        if os.path.exists(vendor_id_path):
-            with open(vendor_id_path, "r") as file:
-                vendor_id = file.read().strip()
+        if not os.path.exists(vendor_id_path):
+            raise FileNotFoundError(
+                "Vendor ID path {} not found".format(vendor_id_path))
 
-        if vendor_id in VENDOR_INFO:
-            vendor_name = VENDOR_INFO[vendor_id]
-            if vendor_name != "Broadcom":
-                logging.info(
-                    "The interface {} is a(n) {} NIC".format(
-                        interface, vendor_name
-                    )
-                )
-            else:
-                logging.info(
-                    "Broadcom SRIOV testing is not supported at this time"
-                )
-                sys.exit(1)
-        else:
-            logging.info(
-                "{} has an unknown vendor  ID {}".format(interface, vendor_id)
-            )
-            sys.exit(1)
+        with open(vendor_id_path, "r", encoding="utf-8") as file:
+            vendor_id = file.read().strip()
 
-    except Exception as e:
+        if vendor_id not in VENDOR_INFO:
+            raise ValueError("{} has an unknown vendor ID {}".
+                             format(interface, vendor_id))
+
+        vendor_name = VENDOR_INFO[vendor_id]
+        if vendor_name == "Broadcom":
+            raise NotImplementedError(
+                "Broadcom SRIOV testing is not supported at this time")
+
+        logging.info("The interface %s is a(n) %s NIC", interface, vendor_name)
+
+    except (FileNotFoundError, ValueError, NotImplementedError) as e:
         logging.info("An error occurred: {}".format(e))
         sys.exit(1)
 
@@ -104,25 +106,22 @@ def is_sriov_capable(interface):
         # Check if the interface supports SR-IOV
         logging.info("checking if sriov_numvfs exits")
         if not os.path.exists(sriov_path):
-            logging.error(
-                "SR-IOV not supported or interface {} \
-                          does not exist.".format(
+            raise FileNotFoundError(
+                "SR-IOV not supported or interface {} does not exist.".format(
                     interface
                 )
             )
         else:
-            logging.info(
-                "SR-IOV before change {} VFs on interface {}.".format(
-                    num_vfs, interface
-                )
-            )
+            logging.info("SR-IOV before change {} VFs on interface {}."
+                         .format(num_vfs, interface))
             # First, disable VFs before changing the number to avoid issues
-            logging.info("setting numvfs to zero")
-            with open(sriov_path, "w") as f:
+            logging.info("Setting numvfs to zero")
+            with open(sriov_path, "w", encoding="utf-8") as f:
                 f.write("0")
 
             # Set the desired number of VFs
-            with open(sriov_path, "w") as f:
+            logging.info("Setting numvfs to %d", num_vfs)
+            with open(sriov_path, "w", encoding="utf-8") as f:
                 f.write(str(num_vfs))
 
             logging.info(
@@ -152,7 +151,6 @@ def test_lxd_sriov(args):
     check_interface_vendor(args.interface)
     is_sriov_capable(args.interface)
 
-    instance = LXD(args.template, args.rootfs)
     with LXD(args.template, args.rootfs) as instance:
         logging.info("Create sriov network for lxc")
         instance.run("lxc network delete lab_sriov", ignore_errors=True)
@@ -247,21 +245,13 @@ def main():
 
     args = parser.parse_args()
 
-    try:
-        logging.basicConfig(level=args.log_level)
-    except AttributeError:
-        pass
+    logging.basicConfig(level=args.log_level)
 
     # silence normal output from requests module
     logging.getLogger("requests").setLevel(logging.WARNING)
     logging.getLogger("urllib3").setLevel(logging.WARNING)
 
-    # Verify args
-    try:
-        args.func(args)
-    except AttributeError:
-        parser.print_help()
-        return 1
+    args.func(args)
 
 
 if __name__ == "__main__":

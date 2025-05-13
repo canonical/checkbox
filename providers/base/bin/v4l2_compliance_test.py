be582973169e12b56e28b416b79defdfc2dfce37
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-05-13 16:25:01 +0200

    Test Jobs for V4L2 Compliance (New) (#1653)
    * feat: initial impl of v4l2-compliance test parser
    
    * fix: spelling
    
    * fix: naming
    
    * style: rename
    
    * feat: add test name to ioctl map
    
    * fix: raise an error if the device can't be opened
    
    * feat: v4l2-compliance test case
    
    * feat: add resource job
    
    * feat: test jobs for each ioctl
    
    * feat: generate tests for each device
    
    * fix: remove unused
    
    * test: add initial unit tests
    
    * test: add unit tests
    
    * test: unit test for the actual job
    
    * fix: flake8
    
    * fix: consistent file name
    
    * fix: use udevadm
    
    * test: update unittests
    
    * style: cleaner condition
    
    * style: formatting
    
    * fix: apply Fernando's suggestions
    
    * test: update unit tests
    
    * fix (job.pxu): apply Fernando's suggestion
    
    * test: increase coverage
    
    * fix: make the test script more generic
    
    * test: reflect changes in tests
    
    * feat: Apply Fernando's suggestion and simplify the job
    
    * fix: update job definitions
    
    * fix: include after suspend jobs
    
    * style: output style changes
    
    * fix: remove git conflict arrows
    
    * cleanup: remove unused test
    
    * fix: v4l2src doesn't use that many ioctls for video
    
    * style: minor style changes
    
    * test: update unit test to reflect changes
    
    * test: increase coverage
    
    * style: minor name tweaks and remove unused import
    
    * style: names
    
    * doc: comment
    
    * fix: apply Fernando's suggestion
    
    * fix: unused import
    
    * fix: 3.5 f string
    
    * fix: tmp workaround for 22.04
    
    * style: formatting
    
    * fix: tmp workaround for 18.04
    
    * test: increase coverage
    
    * fix: wrong bracket type

---
+++ providers/base/bin/v4l2_compliance_test.py
@@ -0,0 +1,122 @@
+#! /usr/bin/env python3
+
+# Copyright 2025 Canonical Ltd.
+# Written by:
+#   Zhongning Li <zhongning.li@canonical.com>
+#   Fernando Bravo Hernández<fernando.bravo.hernandez@canonical.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import argparse
+from checkbox_support.parsers.v4l2_compliance import (
+    parse_v4l2_compliance,
+    IOCTL_USED_BY_V4L2SRC,
+    TEST_NAME_TO_IOCTL_MAP,
+)
+from checkbox_support.helpers.release_info import get_release_info
+
+
+# add more or exclude blockers here
+BLOCKERS = IOCTL_USED_BY_V4L2SRC
+
+
+def parse_args():
+    """
+    class Input:
+        device: str
+        ioctl_selection: Literal['blockers', 'non-blockers', 'all']
+        treat_unsupported_as_fail: bool
+    """
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--device",
+        help="The device to test, usually /dev/video0",
+        type=str,
+        required=True,
+    )
+    parser.add_argument(
+        "--ioctl-selection",
+        help="Which set of ioctls to test, blockers/non-blockers/all",
+        choices=["blockers", "non-blockers", "all"],
+        required=True,
+    )
+    parser.add_argument(
+        "--treat-unsupported-as-fail",
+        action="store_true",
+        help="If specified, and if any of the ioctls are unsupported, "
+        "they are treated as failures and will fail the test case",
+        default=False,
+    )
+    return parser.parse_args()  # type: ignore
+
+
+def main():
+    args = parse_args()
+    _, details = parse_v4l2_compliance(args.device)
+
+    # Gather all known IOCTLs (flattening the map)
+    all_ioctls = {
+        ioctl
+        for ioctl_group in TEST_NAME_TO_IOCTL_MAP.values()
+        for ioctl in ioctl_group
+    }
+
+    # Pick which IOCTLs to test based on user selection
+    if args.ioctl_selection == "blockers":
+        codename = get_release_info()["codename"]
+        if codename in ("jammy", "bionic"):
+            # temp workaround for 22.04 & 18.04
+            ioctls_to_check = BLOCKERS - set(["VIDIOC_REQBUFS"])
+        else:
+            ioctls_to_check = BLOCKERS
+    elif args.ioctl_selection == "non-blockers":
+        ioctls_to_check = all_ioctls - BLOCKERS
+    else:  # "all"
+        ioctls_to_check = all_ioctls
+
+    # Check if the IOCTLs are in the details and categorize them into succeeded
+    # failed, and not supported
+    results = {
+        "succeeded": [],
+        "failed": [],
+        "not_supported": [],
+    }
+
+    for result_type in results.keys():
+        results[result_type] = [
+            ioctl for ioctl in ioctls_to_check if ioctl in details[result_type]
+        ]
+        if not results[result_type]:
+            print("No {} IOCTLs detected".format(result_type))
+        else:
+            print("{} IOCTLs:".format(result_type))
+            for item in results[result_type]:
+                print(" - {}".format(item))
+
+    if results["failed"]:
+        raise SystemExit(
+            "The following IOCTLs failed: {}".format(results["failed"])
+        )
+
+    if args.treat_unsupported_as_fail and results["not_supported"]:
+        raise SystemExit(
+            "The following IOCTLs are not supported: {}".format(
+                results["not_supported"]
+            )
+        )
+
+
+if __name__ == "__main__":
+    main()

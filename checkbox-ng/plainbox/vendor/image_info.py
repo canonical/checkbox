8cca552b448ec850154bbb261c9890d3d31ed31c
Artur Pak <artur.pak@canonical.com>
2025-05-21 16:21:01 +0800

    fix: py35 compatibility

--- checkbox-ng/plainbox/vendor/image_info.py
+++ checkbox-ng/plainbox/vendor/image_info.py
@@ -157,10 +157,14 @@ def dcd_string_to_info_iot(dcd_string):
 
 
 def dcd_info():
-    if DCD_FILE_IOT.is_file():
-        dcd_string = DCD_FILE_IOT.read_text().strip()
-        print(f"Found IoT dcd string: {dcd_string}", file=sys.stderr)
-        return dcd_string_to_info_iot(dcd_string)
+    try:
+        if DCD_FILE_IOT.is_file():
+            with DCD_FILE_IOT.open("r", encoding="utf-8") as f:
+                dcd_string = f.read().strip()
+            print("Found IoT dcd string: {}".format(dcd_string), file=sys.stderr)
+            return dcd_string_to_info_iot(dcd_string)
+    except (IOError, OSError):
+        print("IoT dcd file not found. Assuming PC platform", file=sys.stderr)
 
     ubuntu_report = parse_ubuntu_report()
     print(

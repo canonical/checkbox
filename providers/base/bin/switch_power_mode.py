bebf70bf73ae020830604a7add65470727f5f2ea
Bin Li <binli@ubuntu.com>
2025-06-13 11:08:19 +0800

    Support quiet, cool and balanced_performance. https://gitlab.freedesktop.org/upower/power-profiles-daemon/-/commit/6b565b0b05a2bc92e3c546a567b394f415937265

--- providers/base/bin/switch_power_mode.py
+++ providers/base/bin/switch_power_mode.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 #
 # This file is part of Checkbox.
 #
@@ -48,9 +48,17 @@ def set_power_profile(profile):
     Raises:
         SystemExit: If the power profile could not be set.
     """
-    # In sys file the modes are low-power, balanced, or performance
+    # In sys file the modes are quiet, low-power, cool, balanced or performance
     # but powerprofilesctl only accepts power-saver, balanced or performance
-    profile = "power-saver" if profile == "low-power" else profile
+    profile = (
+        "power-saver"
+        if profile in ["low-power", "quiet"]
+        else (
+            "balanced"
+            if profile in ["cool", "balanced_performance"]
+            else profile
+        )
+    )
     try:
         subprocess.check_call(["powerprofilesctl", "set", profile])
     except subprocess.CalledProcessError as e:

e7671e14bf6b24d114ae9793d7d54fe08319d3f8
Hook25 <massimiliano.girardi@canonical.com>
2025-07-14 14:24:26 +0200

    Check that we aren't setting the recepie to buidl for outadted

--- tools/release/lp_update_recipe.py
+++ tools/release/lp_update_recipe.py
@@ -29,19 +29,44 @@ import sys
 import textwrap
 import argparse
 
-from utils import get_source_build_recipe
+from utils import get_source_build_recipe, get_launchpad_client
 
-# this is hardcoded because supporting a new release requires a bit of manual
-# effort (copying support packages to the new series and doing the first build
-# dance of building packages in dependency order)
-active_series = [
-    "https://api.launchpad.net/devel/ubuntu/bionic",
-    "https://api.launchpad.net/devel/ubuntu/focal",
-    "https://api.launchpad.net/devel/ubuntu/jammy",
-    "https://api.launchpad.net/devel/ubuntu/noble",
-    "https://api.launchpad.net/devel/ubuntu/plucky",
-    "https://api.launchpad.net/devel/ubuntu/questing",
-]
+
+def get_active_series():
+    # this is hardcoded because supporting a new release requires a bit of manual
+    # effort (copying support packages to the new series and doing the first build
+    # dance of building packages in dependency order)
+    desired_series = {
+        "bionic",
+        "focal",
+        "jammy",
+        "noble",
+        "plucky",
+        "questing",
+    }
+    lp = get_launchpad_client()
+    active_series = {
+        series.name for series in lp.projects["ubuntu"].series if series.active
+    }
+
+    outdated_series = desired_series - active_series
+    if outdated_series:
+        print(
+            "The following series were desired but are now outdated:\n- ",
+            end="",
+        )
+        print("\n- ".join(outdated_series))
+
+    undesired_active = active_series - desired_series
+    if undesired_active:
+        print(
+            "The following series weren't desired but are active:\n- ", end=""
+        )
+        print("\n- ".join(undesired_active))
+
+    possible_series = desired_series & active_series
+    api_formatter = "https://api.launchpad.net/devel/ubuntu/{}"
+    return [api_formatter.format(x) for x in possible_series]
 
 
 def get_build_path(recipe_name: str) -> str:
@@ -85,7 +110,7 @@ def update_build_recipe(
     # this is important because while debugging, one may inadvertedly leave a
     # not-yet-working daily build enabled, this makes the daily builds fail for
     # no reason
-    lp_recipe.distroseries = active_series
+    lp_recipe.distroseries = get_active_series()
     lp_recipe.lp_save()
     print(f"Updated build recipe: {lp_recipe.web_link}")
     print(new_recipe)

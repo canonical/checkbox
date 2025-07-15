039459088d2a2d2792a109e5644fb367887031b5
Massimiliano <massimiliano.girardi@canonical.com>
2025-07-15 09:59:33 +0200

    Copy all packages and catch those that couldn't be copied (infra) (#2017)
    * Copy all packages and catch those that couldn't be copied
    
    * Something is wrong in my black sigh
    
    * Check that we aren't setting the recepie to buidl for outadted

--- tools/release/lp_update_recipe.py
+++ tools/release/lp_update_recipe.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # This file is part of Checkbox.
 #
-# Copyright 2023 Canonical Ltd.
+# Copyright 2023-2025 Canonical Ltd.
 # Written by:
 #   Sylvain Pineau <sylvain.pineau@canonical.com>
 #   Massimiliano Girardi <massimiliano.girardi@canonical.com>
@@ -29,7 +29,44 @@ import sys
 import textwrap
 import argparse
 
-from utils import get_source_build_recipe
+from utils import get_source_build_recipe, get_launchpad_client
+
+
+def get_active_series():
+    # this is hardcoded because supporting a new release requires a bit of manual
+    # effort (copying support packages to the new series and doing the first build
+    # dance of building packages in dependency order)
+    desired_series = {
+        "bionic",
+        "focal",
+        "jammy",
+        "noble",
+        "plucky",
+        "questing",
+    }
+    lp = get_launchpad_client()
+    active_series = {
+        series.name for series in lp.projects["ubuntu"].series if series.active
+    }
+
+    outdated_series = desired_series - active_series
+    if outdated_series:
+        print(
+            "The following series were desired but are now outdated:\n- ",
+            end="",
+        )
+        print("\n- ".join(outdated_series))
+
+    undesired_active = active_series - desired_series
+    if undesired_active:
+        print(
+            "The following series weren't desired but are active:\n- ", end=""
+        )
+        print("\n- ".join(undesired_active))
+
+    possible_series = desired_series & active_series
+    api_formatter = "https://api.launchpad.net/devel/ubuntu/{}"
+    return [api_formatter.format(x) for x in possible_series]
 
 
 def get_build_path(recipe_name: str) -> str:
@@ -70,6 +107,10 @@ def update_build_recipe(
     lp_recipe = get_source_build_recipe(project_name, recipe_name)
     new_recipe = get_updated_build_recipe(recipe_name, version, revision)
     lp_recipe.recipe_text = new_recipe
+    # this is important because while debugging, one may inadvertedly leave a
+    # not-yet-working daily build enabled, this makes the daily builds fail for
+    # no reason
+    lp_recipe.distroseries = get_active_series()
     lp_recipe.lp_save()
     print(f"Updated build recipe: {lp_recipe.web_link}")
     print(new_recipe)
--- tools/release/test_lp_update_recipe.py
+++ tools/release/test_lp_update_recipe.py
@@ -38,6 +38,10 @@ class TestUpdateBuildRecipe(unittest.TestCase):
 
     @patch("lp_update_recipe.get_source_build_recipe")
     @patch("lp_update_recipe.print")
+    @patch(
+        "lp_update_recipe.get_active_series",
+        new=MagicMock(return_value="noble"),
+    )
     def test_main(self, print_mock, get_build_path_mock):
         lp_recipe = get_build_path_mock()
 
@@ -56,3 +60,26 @@ class TestUpdateBuildRecipe(unittest.TestCase):
         self.assertIn("checkbox-support", lp_recipe.recipe_text)
         self.assertIn("abcd", lp_recipe.recipe_text)
         self.assertTrue(lp_recipe.lp_save.called)
+
+    @patch("lp_update_recipe.print")
+    @patch("lp_update_recipe.get_launchpad_client")
+    def test_get_active_series(self, get_launchpad_client_mock, print_mock):
+        lp_mock = get_launchpad_client_mock()
+        # if this broke, 10+yrs went by and this is still in use. Good :)
+        # Update the following line with the latest LTS name
+        active_release_mock = MagicMock(active=True)
+        active_release_mock.name = "noble"
+        active_undesired_mock = MagicMock(active=True)
+        active_undesired_mock.name = " <this release is undesired> "
+        lp_mock.projects["ubuntu"].series = [
+            active_release_mock,
+            active_undesired_mock,
+        ]
+        returned = lp_update_recipe.get_active_series()
+        self.assertTrue(any(active_release_mock.name in x for x in returned))
+        log = "\n".join(str(x) for x in print_mock.call_args_list)
+        # active but undesired was reported
+        self.assertIn(active_undesired_mock.name, log)
+        # this assumes more than 1 distro is active, if this fails you are
+        # desiring only 1 distro which is the latest lts from before
+        self.assertIn("outdated", log)

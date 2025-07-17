1cef1bff00bea462142deb3e49c1b2bf153b2251
Hook25 <massimiliano.girardi@canonical.com>
2025-07-17 16:13:41 +0200

    Fix pep8 linelen in docstring

--- providers/base/bin/pulse_active_port_change.py
+++ providers/base/bin/pulse_active_port_change.py
@@ -105,21 +105,21 @@ class AudioPlugDetection:
     @classmethod
     def main(cls):
         """
-        This script checks if the active port on either sinks (speakers or headphones)
-        or sources (microphones, webcams) is changed after an appropriate device is
-        plugged into the DUT. The script is fully automatic and either times out after
-        30 seconds or returns as soon as the change is detected.
+        This script checks if the active port on either sinks (speakers or
+        headphones) or sources (microphones, webcams) is changed after an
+        appropriate device is plugged into the DUT. The script is fully
+        automatic and either times out after 30 seconds or returns as soon as
+        the change is detected.
 
 
-        The script monitors pulse audio events with `pactl subscribe`. Any changes to
-        sinks (or sources, depending on the mode) are treated as a possible match. A
-        match is verified by running `pactl list sinks` (or `pactl list sources`) and
-        constructing a set of tuples (sink-source-name, sink-source-active-port,
-        sink-source-availability). Any change to the computed set, as compared to the
-        initially computed set, is considered a match.
-
-        Due to the algorithm used, it will also detect things like USB headsets, HDMI
-        monitors/speakers, webcams, etc.
+        The script monitors pulse audio events with `pactl subscribe`. Any
+        changes to sinks (or sources, depending on the mode) are treated as a
+        possible match. A match is verified by running `pactl list sinks` (or
+        `pactl list sources`) and constructing a set of tuples
+        (sink-source-name, sink-source-active-port, sink-source-availability).
+        Any change to the computed set, as compared to the initially computed
+        set, is considered a match. Due to the algorithm used, it will also
+        detect things like USB headsets, HDMI monitors/speakers, webcams, etc.
         """
         parser = argparse.ArgumentParser(
             description=cls.main.__doc__.split("\n\n", 1)[0],

6f01c4fa474e9a860d2864261f6470b6de9bb878
Shane McKee <shane.mckee@canonical.com>
2025-07-28 21:26:05 -0600

    Move Vulkan tests into Python and use sibling tests

---
+++ contrib/checkbox-gfx/checkbox-provider-gfx/bin/run-vulkan-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import subprocess
+import argparse
+
+def run_vk_test(test_file):
+    result = subprocess.run(["/usr/local/checkbox-gfx/VK-GL-CTS/build/external/vulkancts/modules/vulkan/deqp-vk",
+                    "--deqp-caselist-file=%s" % test_file],
+                    capture_output=True,
+                    text=True)
+    print(result.stdout + result.stderr)
+    exit(result.returncode)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run a vulkan test from VK-GL-CTS")
+    parser.add_argument('--test_file', type=str, required=True, help='Path to VK-GL-CTS test file')
+    args = parser.parse_args()
+
+    run_vk_test(args.test_file)

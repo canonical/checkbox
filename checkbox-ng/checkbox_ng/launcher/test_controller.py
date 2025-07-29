3ae654ae85edd864fe4ee2c270a2912a4bfc967e
Hook25 <massimiliano.girardi@canonical.com>
2025-07-29 17:42:47 +0200

    Check that all states have a connection_strategy

--- checkbox-ng/checkbox_ng/launcher/test_controller.py
+++ checkbox-ng/checkbox_ng/launcher/test_controller.py
@@ -32,6 +32,11 @@ from checkbox_ng.launcher.controller import is_hostname_a_loopback
 
 
 class ControllerTests(TestCase):
+    def test_connection_strategy_comprehensive(self):
+        connection_strategy = RemoteController.connection_strategy()
+        for state in RemoteSessionStates:
+            connection_strategy[state]
+
     @mock.patch("checkbox_ng.launcher.controller.is_hostname_a_loopback")
     @mock.patch("time.time")
     @mock.patch("builtins.print")
@@ -1042,7 +1047,7 @@ class ControllerTests(TestCase):
         RemoteController.automatically_start_via_launcher(self_mock)
 
         self.assertTrue(self_mock.select_test_plan.called)
-        self.assertTrue(self_mock.select_jobs.called)
+        self.assertTrue(self_mock.bootstrap_and_continue.called)
 
     def test_automatically_resume_last_session(self):
         self_mock = mock.MagicMock()

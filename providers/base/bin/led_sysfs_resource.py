e546311864e9c56ef8b7b38e22a89b1b7df48e36
stanley31huang <stanley.huang@canonical.com>
2025-08-07 16:02:51 +0800

    revised the led_sysfs_resource.py
    revised the led_sysfs_resource.py

--- providers/base/bin/led_sysfs_resource.py
+++ providers/base/bin/led_sysfs_resource.py
@@ -1,61 +1,76 @@
 #!/usr/bin/env python3
 import argparse
-from enum import Enum
 
 
-UNDEFINED = "undefined"
+USAGE = """
+led_sysfs_resource.py resource
+
+Note:
+    format of led_patterns:
+    {name1}|{path1}|{color_type} {name2}|{path2}|{color_type} ...
+
+    path under "/sys/class/leds/{path}"
+    e.g.,
+        DL1|beat-yel-led|single DL2|rgb:status|multi
+    Note: DONT include any space character in the name field
+"""
+SupportedColorType = ["single", "multi"]
 
 
-class SupportedColorTypeEnum(Enum):
-    SINGLE = "single"
-    MULTIPLE = "multi"
+def check_environment(resource):
+    if not resource:
+        raise SystemExit("SYS_LEDS is not defined")
+
+    for led_data in resource.split(" "):
+        tmp_data = led_data.split("|")
+        if len(tmp_data) != 3:
+            raise SystemExit("Incorrect led data: {}".format(led_data))
+
+        if tmp_data[2] not in SupportedColorType:
+            raise SystemExit("Unexpected color type: {}".format(tmp_data[2]))
+
+    print("the format of SYS_LEDS is correct")
 
 
 def parse_sysfs_led_resource(resource):
-    # Usage of parameter:
-    # SYS_LEDS={name1}|{path1}|{color_type} {name2}|{path2}|{color_type} ...
-    #
-    # path under "/sys/class/leds/{path}"
-    # e.g.,
-    #   SYS_LEDS=DL1|beat-yel-led|single DL2|rgb:status|multi
-    # Note: DONT include any space character in the name field
     for led_data in resource.split(" "):
         tmp_data = led_data.split("|")
-        led_phys = tmp_data[0]
-        sysfs_name = tmp_data[1] if len(tmp_data) >= 2 else UNDEFINED
-        color_type = tmp_data[2] if len(tmp_data) == 3 else UNDEFINED
+        if len(tmp_data) != 3:
+            continue
 
-        try:
-            color_type = SupportedColorTypeEnum(color_type).value
-        except ValueError:
-            pass
+        led_phys, sysfs_name, color_type = tmp_data
+        if color_type not in SupportedColorType:
+            continue
 
         print("name: {}".format(led_phys))
         print("path: {}".format(sysfs_name))
         print("color_type: {}".format(color_type))
         print()
 
+def main():
+    args = register_arguments()
+
+    if args.validate:
+        check_environment(args.resource)
+    else:
+        parse_sysfs_led_resource(args.resource)
+
 
 def register_arguments():
     parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="Sysfs LED Resource parser",
+        description="Sysfs LED Resource parser", usage=USAGE
     )
-    parser.add_argument("resource", type=str)
+    parser.add_argument("resource", type=str, default="")
+    parser.add_argument(
+        "--validate",
+        action="store_true",
+        default=False,
+        help="check if the format of led_patterns correctly"
+    )
 
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
-
-    args = register_arguments()
-    if not args.resource:
-        print(
-            "name: {}\npath: {}\ncolor_type: {}".format(
-                UNDEFINED, UNDEFINED, UNDEFINED
-            )
-        )
-        # DO NOT return exit code 1 due to this is use for resource job
-    else:
-        parse_sysfs_led_resource(args.resource)
+    main()
--- providers/base/tests/test_led_sysfs_resource.py
+++ providers/base/tests/test_led_sysfs_resource.py
@@ -6,13 +6,13 @@ from unittest.mock import patch, call, Mock
 import led_sysfs_resource
 
 
-class TestSupportedColorEnum(unittest.TestCase):
+class TestSupportedColorType(unittest.TestCase):
 
-    def test_single_color(self):
+    def test_color_definition(self):
 
         self.assertEqual(
-            led_sysfs_resource.SupportedColorTypeEnum.SINGLE,
-            led_sysfs_resource.SupportedColorTypeEnum("single"),
+            led_sysfs_resource.SupportedColorType,
+            ["single", "multi"]
         )
 
 
@@ -34,40 +34,38 @@ class TestLEDParser(unittest.TestCase):
         mock_print.assert_has_calls(calls)
 
     @patch("builtins.print")
+    def test_parse_with_invalid_led_pattern(self, mock_print):
+        pattern = "LED1"
+        led_sysfs_resource.parse_sysfs_led_resource(pattern)
+        mock_print.assert_not_called()
+
+    def test_validate_led_pattern_passed(self):
+        pattern = "LED1|path1|single LED2|path2|multi"
+        led_sysfs_resource.check_environment(pattern)
+
+    def test_validate_led_pattern_invalid_format(self):
+        pattern = "LED1|path1 LED2|path2"
+        with self.assertRaises(SystemExit):
+            led_sysfs_resource.check_environment(pattern)
+
+    def test_validate_led_pattern_invalid_color_type(self):
+        pattern = "LED1|path1|dual"
+        with self.assertRaises(SystemExit):
+            led_sysfs_resource.check_environment(pattern)
+
+
+    @patch("builtins.print")
     def test_parse_invalid_color_type(self, mock_print):
         pattern = "LED1|path1|error"
         led_sysfs_resource.parse_sysfs_led_resource(pattern)
-        calls = [
-            call("name: LED1"),
-            call("path: path1"),
-            call("color_type: error"),
-            call(),
-        ]
-        mock_print.assert_has_calls(calls)
+        mock_print.assert_not_called()
 
     @patch("builtins.print")
     def test_parse_undefined_color_type(self, mock_print):
         pattern = "LED1|path1"
         led_sysfs_resource.parse_sysfs_led_resource(pattern)
-        calls = [
-            call("name: LED1"),
-            call("path: path1"),
-            call("color_type: {}".format(led_sysfs_resource.UNDEFINED)),
-            call(),
-        ]
-        mock_print.assert_has_calls(calls)
+        mock_print.assert_not_called()
 
-    @patch("builtins.print")
-    def test_parse_led_name_only(self, mock_print):
-        pattern = "LED1"
-        led_sysfs_resource.parse_sysfs_led_resource(pattern)
-        calls = [
-            call("name: LED1"),
-            call("path: {}".format(led_sysfs_resource.UNDEFINED)),
-            call("color_type: {}".format(led_sysfs_resource.UNDEFINED)),
-            call(),
-        ]
-        mock_print.assert_has_calls(calls)
 
 
 class TestArgumentParser(unittest.TestCase):
@@ -78,3 +76,11 @@ class TestArgumentParser(unittest.TestCase):
         args = led_sysfs_resource.register_arguments()
 
         self.assertEqual(args.resource, pattern)
+        self.assertFalse(args.validate, False)
+
+    def test_parser_with_validate_arg(self):
+        sys.argv = ["led_sysfs_resource.py", "data", "--validate"]
+        args = led_sysfs_resource.register_arguments()
+
+        self.assertEqual(args.resource, "data")
+        self.assertTrue(args.validate, True)

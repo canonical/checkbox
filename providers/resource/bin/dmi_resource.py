c9c4c17bf17dee94481541d1d636ed7bf60db86e
Massimiliano <massimiliano.girardi@canonical.com>
2025-08-08 16:08:17 +0200

    Dmi resource sane_product to avoid `in` duplication (bugfix) (#2046)
    * Add sane_product key
    
    * Move all hardcoded consts to new sane_product
    
    * Test sane_product translator
    
    * Also test the unknown

--- providers/resource/bin/dmi_resource.py
+++ providers/resource/bin/dmi_resource.py
@@ -2,12 +2,11 @@
 #
 # This file is part of Checkbox.
 #
-# Copyright 2011 Canonical Ltd.
+# Copyright 2011-2025 Canonical Ltd.
 #
 # Checkbox is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3,
 # as published by the Free Software Foundation.
-
 #
 # Checkbox is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -25,6 +24,38 @@ from checkbox_support.parsers.dmidecode import DmidecodeParser
 
 # Command to retrieve dmi information.
 COMMAND = "dmidecode"
+
+
+def sane_product(og_product: str) -> str:
+    """
+    Transform the product key into a usable form
+
+    The product key is basically free-form text. In order to make it more
+    usable in resource expressions, which usually want to know if a device is
+    portable (a laptop/tablet) or not, this cleans up the key to a "canonical"
+    answer, either `non-portable` or `portable`
+    """
+    cleaned = og_product.lower().replace(" ", "-")
+    if cleaned in [
+        "desktop",
+        "low-profile-desktop",
+        "tower",
+        "mini-tower",
+        "space-saving",
+        "all-in-one",
+        "aio",
+    ]:
+        return "not-portable"
+    elif cleaned in [
+        "notebook",
+        "laptop",
+        "portable",
+        "convertible",
+        "tablet",
+        "detachable",
+    ]:
+        return "portable"
+    return "unknown"
 
 
 class DmiResult:
@@ -45,7 +76,9 @@ class DmiResult:
         for attribute in self.attributes:
             value = getattr(device, attribute, None)
             if value is not None:
-                print("%s: %s" % (attribute, value))
+                print("{}: {}".format(attribute, value))
+            if attribute == "product" and value:
+                print("{}: {}".format("sane_product", sane_product(value)))
 
         print()
 

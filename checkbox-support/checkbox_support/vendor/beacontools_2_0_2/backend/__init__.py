17199c9fd0d909f67e661e4d321d098b689d85fc
stanley31huang <stanley.huang@canonical.com>
2025-08-27 16:12:17 +0800

    Fixed eddystone_scanner fallback issue (New) (#1967)
    * Fixed eddystone_scanner fallback issue
    
    fixed eddystone_scanner fallback issue
    
    * revised scripts and unit tests
    
    revicsed scripts and unit tests
    
    * update unit tests
    
    update unit tests
    
    * revised unit test
    
    revised unit test
    
    * revised unit test
    
    revised unit test
    
    * revised scripts and unit tests
    
    revised scripts and unit tests
    
    * revised unit test
    
    revised unit test
    
    * revised unit tests to fit python3.5
    
    revised unit tests to fit python3.5
    
    * revised scanner.py
    
    revised scanner.py
    
    * updated command status code
    
    update the index of the command status code
    
    * Revised scripts and unit tests
    
    revised scripts and unit tests
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * Update checkbox-support/checkbox_support/vendor/beacontools/scanner.py
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>
    
    * update logging function and unittest
    
    update logging function and unittest
    
    ---------
    
    Co-authored-by: Fernando Bravo <39527354+fernando79513@users.noreply.github.com>

--- checkbox-support/checkbox_support/vendor/beacontools_2_0_2/backend/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Load a different backend depending on the OS."""
-import sys
-
-if sys.platform.startswith("linux"):
-    from .linux import *
-elif sys.platform.startswith("freebsd"):
-    from .freebsd import *
-else:
-    raise NotImplementedError("Scanning not supported on this platform")

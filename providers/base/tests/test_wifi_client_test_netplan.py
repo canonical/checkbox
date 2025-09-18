a11f0be973f0d021ebaf9d23e59390a45582558e
Pierre Ducroquet <pierre.ducroquet@canonical.com>
2025-09-18 14:08:39 +0200

    wifi test: fix checking of ping results (#2116)

--- providers/base/tests/test_wifi_client_test_netplan.py
+++ providers/base/tests/test_wifi_client_test_netplan.py
@@ -780,6 +780,33 @@ class WifiClientTestNetplanTests(TestCase):
         self.assertIn("Got gateway address: ", captured_output.getvalue())
 
 
+@patch("wifi_client_test_netplan.ping")
+@patch("wifi_client_test_netplan.get_gateway")
+@patch("wifi_client_test_netplan.sp.check_output")
+class TestPerformPingTest(TestCase):
+    def test_perform_ping_test_success(
+        self, mock_check_output, mock_gateway, mock_ping
+    ):
+        mock_gateway.return_value = "127.1"
+        mock_ping.return_value = {
+            "transmitted": 5,
+            "received": 5,
+            "pct_loss": 0,
+        }
+        self.assertTrue(perform_ping_test("wlan0", "networkd"))
+
+    def test_perform_ping_test_failure(
+        self, mock_check_output, mock_gateway, mock_ping
+    ):
+        mock_gateway.return_value = "127.1"
+        mock_ping.return_value = {
+            "transmitted": 5,
+            "received": 0,
+            "pct_loss": 0,
+        }
+        self.assertFalse(perform_ping_test("wlan0", "networkd"))
+
+
 class TestMain(TestCase):
 
     @patch("wifi_client_test_netplan.parse_args")

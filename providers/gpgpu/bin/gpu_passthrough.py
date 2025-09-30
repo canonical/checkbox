bcef58ce7c723615df53546cf63ad4199a8444c9
Pedro Avalos Jimenez <pedro.avalosjimenez@canonical.com>
2025-09-30 12:36:23 -0400

    feat: simplify gpu_passthrough.py
    the mixbench program is less so a stress test and more an informational program that stresses at different levels depending on the GPU resources. Running it once and succeeding should be enough.

--- providers/gpgpu/bin/gpu_passthrough.py
+++ providers/gpgpu/bin/gpu_passthrough.py
@@ -50,20 +50,6 @@ GPU_VENDORS = {
 }
 """Mapping of supported vendor names to test configuration."""
 
-GPU_RUNS = 20
-"""How many times to run the GPU test.
-
-This can be overwritten by the `--count` option or by setting the environment
-variable `LXD_GPU_RUNS`. With priority in that order.
-"""
-
-GPU_THRESHOLD_SEC = 12.0
-"""The threshold for the GPU test to pass.
-
-This can be overwritten by the `--threshold` option or by setting the
-environment variable `LXD_GPU_THRESHOLD`. With priority in that order.
-"""
-
 QEMU_OPTS = ""
 """Any custom QEMU options required for passthrough on your platform
 
@@ -86,39 +72,22 @@ environment variable `VM_CPUS`. With priority in that order.
 """
 
 
-def run_gpu_test(
-    instance: LXD,
-    cmd: str,
-    run_count: int = GPU_RUNS,
-    threshold_sec: float = GPU_THRESHOLD_SEC,
-):
+def run_gpu_test(instance: LXD, cmd: str):
     """Executes GPU passthrough test."""
-    logging.info("Running GPU passthrough test %d times", run_count)
+    logging.info("Running GPU passthrough test")
     total_runtime_sec = 0.0
-    for i in range(run_count):
-        tic = time.time()
-        instance.run(cmd, on_guest=True)
-        toc = time.time()
-        runtime_sec = toc - tic
-        total_runtime_sec += runtime_sec
-        logging.debug("Runtime #%d (sec): %f", i, runtime_sec)
-
-    avg_runtime_sec = total_runtime_sec / run_count
-    logging.info("Average runtime (sec): %f", avg_runtime_sec)
-    if avg_runtime_sec >= threshold_sec:
-        logging.error(
-            "Average runtime %fs greater than threshold %fs",
-            avg_runtime_sec,
-            threshold_sec,
-        )
-        raise SystemExit(1)
+    tic = time.time()
+    instance.run(cmd, on_guest=True)
+    toc = time.time()
+    runtime_sec = toc - tic
+    total_runtime_sec += runtime_sec
+    logging.debug("Runtime: %f sec", runtime_sec)
 
 
 def test_lxd_gpu(args):
     """Tests GPU passthrough with a LXD container."""
     logging.info("Executing LXD GPU passthrough test")
 
-    instance = LXD(args.template, args.rootfs)
     with LXD(args.template, args.rootfs) as instance:
         logging.info("Launching container: %s", instance.name)
         instance.launch(
@@ -134,12 +103,7 @@ def test_lxd_gpu(args):
         logging.info("Installing mixbench snap")
         instance.run("snap install mixbench", on_guest=True)
 
-        run_gpu_test(
-            instance,
-            GPU_VENDORS[args.vendor]["test"],
-            args.count,
-            args.threshold,
-        )
+        run_gpu_test(instance, GPU_VENDORS[args.vendor]["test"])
 
 
 def test_lxdvm_gpu(args):
@@ -200,12 +164,7 @@ def test_lxdvm_gpu(args):
         logging.info("Installing mixbench snap")
         instance.run("snap install mixbench", on_guest=True)
 
-        run_gpu_test(
-            instance,
-            GPU_VENDORS[args.vendor]["test"],
-            args.count,
-            args.threshold,
-        )
+        run_gpu_test(instance, GPU_VENDORS[args.vendor]["test"])
 
 
 def parse_args():
@@ -221,20 +180,6 @@ def parse_args():
         default=logging.INFO,
         const=logging.DEBUG,
         help="Increase logging level",
-    )
-
-    test_group = parser.add_argument_group("test")
-    test_group.add_argument(
-        "--threshold",
-        type=float,
-        default=float(os.getenv("LXD_GPU_THRESHOLD") or GPU_THRESHOLD_SEC),
-        help="Threshold (sec) for GPU test",
-    )
-    test_group.add_argument(
-        "--count",
-        type=int,
-        default=int(os.getenv("LXD_GPU_RUNS") or GPU_RUNS),
-        help="Times to run GPU test",
     )
 
     gpu_group = parser.add_argument_group("gpu")

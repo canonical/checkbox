f1504d7ad44f39afdd22e5f187b0fdebcb2a7ade
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-10-03 11:03:08 +0800

    feat: also check kernel version

--- providers/base/bin/wifi_7_resource.py
+++ providers/base/bin/wifi_7_resource.py
@@ -1,15 +1,17 @@
+import os
 import subprocess as sp
 
 
-def main():
+def get_wpa_supplicant_version() -> "tuple[int, int]":
     """
     (manually verified on ubuntu16 ~ 24 including ubuntu core)
     The output of "wpa_supplicant -v" always looks like the following
 
     wpa_supplicant v2.10
     Copyright (c) 2003-2022, Jouni Malinen <j@w1.fi> and contributors
-    """
 
+    :return: the sys.version style tuple like (2, 10)
+    """
     wpa_supplicant_version_output = sp.check_output(
         ["wpa_supplicant", "-v"], universal_newlines=True
     )
@@ -18,8 +20,36 @@ def main():
     clean_version_str = wpa_supplicant_version_output.splitlines()[0].split()[
         1
     ][1:]
+    # Example: clean_version_str == '2.11'
+    # Now convert to a sys.version style tuple
+    version_tuple = tuple(
+        map(
+            int,
+            clean_version_str.split(".", maxsplit=1),
+        ),
+    )
+    assert len(version_tuple) == 2
 
-    print("wpa_supplicant_version: {}".format(clean_version_str))
+    return version_tuple
+
+
+def get_kernel_version() -> "tuple[int, int]":
+    version_str = os.uname().release
+    # '6.14.0-32-generic'
+    # '6.14.0-1012-oem'
+    major_minor_patch = version_str.split("-", maxsplit=1)[0].split(".")
+    return (int(major_minor_patch[0]), int(major_minor_patch[1]))
+
+
+def main():
+    # have to produce true/false here, pxu files only support string comparison
+    # geq = greater than or equal to
+    print(
+        "wpa_supplicant_geq_2_11: {}".format(
+            get_wpa_supplicant_version() >= (2, 11)
+        )
+    )
+    print("kernel_geq_6_14: {}".format(get_kernel_version() >= (6, 14)))
 
 
 if __name__ == "__main__":

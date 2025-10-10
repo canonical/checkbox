16d4fb6aafc7beefcf9e6a2d5fa7e532083e53ae
Hook25 <massimiliano.girardi@canonical.com>
2025-10-10 16:14:56 +0200

    Cover modification to execution

--- checkbox-ng/plainbox/impl/test_execution.py
+++ checkbox-ng/plainbox/impl/test_execution.py
@@ -16,9 +16,12 @@
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
+import contextlib
+
 from plainbox.impl.execution import (
     UnifiedRunner,
     get_execution_command_systemd_unit,
+    get_execution_command_subshell,
 )
 from plainbox.impl.unit.job import InvalidJob
 
@@ -104,3 +107,128 @@ class UnifiedRunnerTests(TestCase):
         self.assertIn("test_command", result)
         # don't log in root commands
         self.assertNotIn("-pam", result)
+
+    @mock.patch(
+        "plainbox.impl.execution.get_differential_execution_environment"
+    )
+    @mock.patch("plainbox.impl.execution.on_ubuntucore")
+    def test_get_execution_command_subshell_with_user(
+        self, mock_on_ubuntucore, mock_get_diff_env
+    ):
+        job = mock.Mock(shell="bash", command="test_command")
+        mock_on_ubuntucore.return_value = False
+        mock_get_diff_env.return_value = {"TEST_VAR": "test_value"}
+
+        result = get_execution_command_subshell(
+            job, {}, "test_session", "/tmp/nest", "ubuntu", None
+        )
+        result = " ".join(result)
+
+        self.assertIn("sudo", result)
+        self.assertIn("TEST_VAR=test_value", result)
+        self.assertIn("test_command", result)
+        self.assertNotIn("aa-exec", result)
+
+    @mock.patch("plainbox.impl.execution.get_execution_environment")
+    @mock.patch("plainbox.impl.execution.on_ubuntucore")
+    def test_get_execution_command_subshell_no_user(
+        self, mock_on_ubuntucore, mock_get_env
+    ):
+        job = mock.Mock(shell="bash", command="test_command")
+        mock_on_ubuntucore.return_value = False
+        mock_get_env.return_value = {"TEST_VAR": "test_value"}
+
+        result = get_execution_command_subshell(
+            job, {}, "test_session", "/tmp/nest", None, None
+        )
+        result = " ".join(result)
+
+        self.assertNotIn("sudo", result)
+        self.assertIn("TEST_VAR=test_value", result)
+        self.assertIn("test_command", result)
+        self.assertNotIn("aa-exec", result)
+
+    @mock.patch("plainbox.impl.execution.get_execution_environment")
+    @mock.patch("plainbox.impl.execution.on_ubuntucore")
+    def test_get_execution_command_subshell_on_core(
+        self, mock_on_ubuntucore, mock_get_env
+    ):
+        job = mock.Mock(shell="bash", command="test_command")
+        mock_on_ubuntucore.return_value = True
+        mock_get_env.return_value = {"TEST_VAR": "test_value"}
+
+        result = get_execution_command_subshell(
+            job, {}, "test_session", "/tmp/nest", None, None
+        )
+        result = " ".join(result)
+
+        self.assertNotIn("sudo", result)
+        self.assertIn("TEST_VAR=test_value", result)
+        self.assertIn("test_command", result)
+        self.assertIn("aa-exec -p unconfined", result)
+
+    @mock.patch("plainbox.impl.execution.get_execution_environment")
+    @mock.patch("plainbox.impl.execution.get_execution_command_subshell")
+    @mock.patch("plainbox.impl.execution.get_execution_command_systemd_unit")
+    @mock.patch("getpass.getuser")
+    def test_execute_job_subshell(
+        self,
+        getuser_mock,
+        get_execution_command_systemd_unit_mock,
+        get_execution_command_subshell_mock,
+        get_execution_environment_mock,
+    ):
+        @contextlib.contextmanager
+        def configured_filesystem_mock(self, *args, **kwargs):
+            yield
+
+        self_mock = mock.Mock(configured_filesystem=configured_filesystem_mock)
+        self_mock._user_provider.return_value = None
+
+        job_mock = mock.Mock(user="ubuntu")
+        job_mock.get_flag_set.return_value = {"preserve-cwd"}
+        getuser_mock.return_value = "ubuntu"
+
+        get_execution_command_subshell_mock.side_effect = Exception("subshell")
+        get_execution_command_systemd_unit_mock.side_effect = Exception(
+            "systemd"
+        )
+
+        with self.assertRaises(Exception) as e:
+            UnifiedRunner.execute_job(self_mock, job_mock, {}, mock.Mock())
+
+        self.assertEqual(str(e.exception), "subshell")
+
+    @mock.patch("plainbox.impl.execution.get_execution_environment")
+    @mock.patch("plainbox.impl.execution.get_execution_command_subshell")
+    @mock.patch("plainbox.impl.execution.get_execution_command_systemd_unit")
+    @mock.patch("getpass.getuser")
+    def test_execute_job_systemd(
+        self,
+        getuser_mock,
+        get_execution_command_systemd_unit_mock,
+        get_execution_command_subshell_mock,
+        get_execution_environment_mock,
+    ):
+        @contextlib.contextmanager
+        def configured_filesystem_mock(self, *args, **kwargs):
+            yield
+
+        self_mock = mock.Mock(configured_filesystem=configured_filesystem_mock)
+        self_mock._user_provider.return_value = None
+
+        job_mock = mock.Mock(user="ubuntu")
+        job_mock.get_flag_set.return_value = {"preserve-cwd"}
+        getuser_mock.return_value = "ubuntu"
+
+        get_execution_command_subshell_mock.side_effect = Exception("subshell")
+        get_execution_command_systemd_unit_mock.side_effect = Exception(
+            "systemd"
+        )
+
+        with self.assertRaises(Exception) as e:
+            UnifiedRunner.execute_job(
+                self_mock, job_mock, {}, mock.Mock(), systemd_unit=True
+            )
+
+        self.assertEqual(str(e.exception), "systemd")

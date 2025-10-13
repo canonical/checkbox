49a095a5496f0821109d22c4a1026956011f3430
Pierre Equoy <pierre.equoy@canonical.com>
2025-10-13 09:32:37 +0200

    Dropping patch/mock from unit test

--- checkbox-support/checkbox_support/parsers/tests/test_udevadm.py
+++ checkbox-support/checkbox_support/parsers/tests/test_udevadm.py
@@ -18,7 +18,6 @@ import json
 
 from io import StringIO
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
 from textwrap import dedent
 
 try:
@@ -62,7 +61,6 @@ class UdevadmDataMixIn(object):
             return None
 
 
-@patch("checkbox_support.parsers.udevadm.check_output", MagicMock())
 class TestUdevadmParser(TestCase, UdevadmDataMixIn):
 
     def parse(self, name, with_lsblk=False, with_partitions=False):

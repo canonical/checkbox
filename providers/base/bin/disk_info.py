10662e066b509cd651ebc4f81684656146c9da6d
Pierre Equoy <pierre.equoy@canonical.com>
2025-10-27 22:07:38 +0100

    Update disk_info to use the JSON output of lsblk
    This script is calling find_pkname_is_root_mountpoint, which has
    recently been modified to use the JSON output of lsblk. It is therefore
    modified to fit the same model.
    
    A simple unit test is added, since there was none previously (!). It is
    using contextlib.redirect_stdout() to check the printed output, as
    explained in this article:
    
    https://adamj.eu/tech/2025/08/29/python-unittest-capture-stdout-stderr/

--- providers/base/bin/disk_info.py
+++ providers/base/bin/disk_info.py
@@ -4,12 +4,13 @@
 #
 # Copyright (C) 2010-2013 by Cloud Computing Center for Mobile Applications
 # Industrial Technology Research Institute
-# Copyright 2016 Canonical Ltd.
+# Copyright 2016-2025 Canonical Ltd.
 #
 # Authors:
 #   Nelson Chu <Nelson.Chu@itri.org.tw>
 #   Jeff Lane <jeff@ubuntu.com>
 #   Sylvain Pineau <sylvain.pineau@canonical.com>
+#   Pierre Equoy <pierre.equoy@canonical.com>
 #
 # Checkbox is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 3,
@@ -24,6 +25,7 @@
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 """disk_info utility."""
 
+import json
 import re
 import sys
 from subprocess import check_output, CalledProcessError
@@ -31,63 +33,52 @@ from subprocess import check_output, CalledProcessError
 from checkbox_support.parsers.udevadm import find_pkname_is_root_mountpoint
 
 
-def main():
+def get_lsblk_json():
+    lsblk = check_output(
+        [
+            "lsblk",
+            "-i",
+            "-n",
+            "--json",
+            "-o",
+            "KNAME,TYPE,SIZE,MODEL,MOUNTPOINT",
+        ],
+        universal_newlines=True,
+    )
+    return json.loads(lsblk)
+
+
+def main(lsblk):
     """
     disk_info.
 
     Uses lsblk to gather information about disks seen by the OS.
     Outputs kernel name, model and size data
     """
-    pattern = re.compile(
-        r'KNAME="(?P<KNAME>.*)" '
-        r'TYPE="(?P<TYPE>.*)" '
-        r'SIZE="(?P<SIZE>.*)" '
-        r'MODEL="(?P<MODEL>.*)" '
-        r'MOUNTPOINT="(?P<MOUNTPOINT>.*)"'
-    )
-    try:
-        lsblk = check_output(
-            [
-                "lsblk",
-                "-i",
-                "-n",
-                "-P",
-                "-o",
-                "KNAME,TYPE,SIZE,MODEL,MOUNTPOINT",
-            ],
-            universal_newlines=True,
-        )
-    except CalledProcessError as e:
-        sys.exit(e)
-
     disks = 0
-    for line in lsblk.splitlines():
-        m = pattern.match(line)
-        if not m or m.group("TYPE") not in ("disk", "crypt"):
+    for blkdev in lsblk.get("blockdevices", []):
+        if blkdev["type"] not in ("disk", "crypt"):
             continue
         # Only consider MMC block devices if one of their mounted partitions is
         # root (/)
-        if m.group("KNAME").startswith(
+        if blkdev["kname"].startswith(
             "mmcblk"
-        ) and not find_pkname_is_root_mountpoint(m.group("KNAME"), lsblk):
+        ) and not find_pkname_is_root_mountpoint(blkdev["kname"], lsblk):
             continue
         # Don't consider any block dev mounted as snapd save partition
-        if "snapd/save" in m.group("MOUNTPOINT"):
+        if blkdev["mountpoint"] and "snapd/save" in blkdev["mountpoint"]:
             continue
         disks += 1
-        model = m.group("MODEL")
+        model = blkdev["model"]
         if not model:
             model = "Unknown"
-        print("Name: /dev/{}".format(m.group("KNAME")))
+        print("Name: /dev/{}".format(blkdev["kname"]))
         print("\t{:7}\t{}".format("Model:", model))
-        print("\t{:7}\t{}".format("Size:", m.group("SIZE")))
-
+        print("\t{:7}\t{}".format("Size:", blkdev["size"]))
     if not disks:
-        print("No disk information discovered.")
-        return 10
-
-    return 0
+        raise SystemExit("No disk information discovered.")
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    lsblk = get_lsblk_json()
+    sys.exit(main(lsblk))

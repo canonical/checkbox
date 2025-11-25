eab56d862ef2df2fd18d631a3cdd9b46b42f34f7
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-11-25 10:17:49 +0800

    fix: change how envs are patched

--- providers/base/tests/test_gl_support.py
+++ providers/base/tests/test_gl_support.py
@@ -30,19 +30,15 @@ class TestGLSupportTests(ut.TestCase):
     def setUp(self) -> None:
         gl_support.CHECKBOX_RUNTIME = None
 
+    @patch.dict("os.environ", {"DISPLAY": ":0", "XDG_SESSION_TYPE": "wayland"})
     @patch("sys.argv", ["gl_support_test.py"])
     @patch("platform.uname")
     @patch("subprocess.check_output")
-    @patch("os.getenv")
     def test_happy_path(
         self,
-        mock_getenv: MagicMock,
         mock_check_output: MagicMock,
         mock_uname: MagicMock,
     ):
-        mock_getenv.side_effect = lambda key: (
-            ":0" if key == "DISPLAY" else "wayland"
-        )
         for arch in "x86_64", "aarch64":
             mock_uname().machine = arch
 
@@ -50,65 +46,47 @@ class TestGLSupportTests(ut.TestCase):
                 mock_check_output.return_value = f.read()
                 gl_support.main()
 
+    @patch.dict("os.environ", {"DISPLAY": ":0", "XDG_SESSION_TYPE": "wayland"})
     @patch("sys.argv", ["gl_support_test.py"])
     @patch("subprocess.check_output")
-    @patch("os.getenv")
     def test_happy_path_es2(
         self,
-        mock_getenv: MagicMock,
         mock_check_output: MagicMock,
     ):
-        mock_getenv.side_effect = lambda key: (
-            ":0" if key == "DISPLAY" else "x11"
-        )
 
         with (TEST_DATA_DIR / "glmark2_es2_ok.txt").open() as f:
             mock_check_output.return_value = f.read()
             gl_support.main()
 
+    @patch.dict("os.environ", {"DISPLAY": ":0", "XDG_SESSION_TYPE": "wayland"})
     @patch("sys.argv", ["gl_support_test.py"])
     @patch("subprocess.check_output")
-    @patch("os.getenv")
     def test_llvmpipe_path(
         self,
-        mock_getenv: MagicMock,
         mock_check_output: MagicMock,
     ):
-        mock_getenv.side_effect = lambda key: (
-            ":0" if key == "DISPLAY" else "x11"
-        )
-
         with (TEST_DATA_DIR / "glmark2_llvmpipe.txt").open() as f:
             mock_check_output.return_value = f.read()
             self.assertRaises(SystemExit, gl_support.main)
 
+    @patch.dict("os.environ", {"DISPLAY": ":0", "XDG_SESSION_TYPE": "wayland"})
     @patch("sys.argv", ["gl_support_test.py"])
     @patch("subprocess.check_output")
-    @patch("os.getenv")
     def test_llvmpipe_path_es2(
         self,
-        mock_getenv: MagicMock,
         mock_check_output: MagicMock,
     ):
-        mock_getenv.side_effect = lambda key: (
-            ":0" if key == "DISPLAY" else "x11"
-        )
-
         with (TEST_DATA_DIR / "glmark2_es2_llvmpipe.txt").open() as f:
             mock_check_output.return_value = f.read()
             self.assertRaises(SystemExit, gl_support.main)
 
+    @patch.dict("os.environ", {"DISPLAY": ":0", "XDG_SESSION_TYPE": "wayland"})
     @patch("sys.argv", ["gl_support_test.py"])
     @patch("subprocess.check_output")
-    @patch("os.getenv")
     def test_version_too_old_path_x86(
         self,
-        mock_getenv: MagicMock,
         mock_check_output: MagicMock,
     ):
-        mock_getenv.side_effect = lambda key: (
-            ":0" if key == "DISPLAY" else "x11"
-        )
 
         with (TEST_DATA_DIR / "glmark2_version_too_old.txt").open() as f:
             mock_check_output.return_value = f.read()
@@ -125,56 +103,56 @@ class TestGLSupportTests(ut.TestCase):
     @patch("os.unlink")
     @patch("os.symlink")
     @patch("subprocess.check_output")
-    @patch("os.getenv")
     def test_cleanup_glmark2_data_symlink(
         self,
-        mock_getenv: MagicMock,
-        mock_check_output: MagicMock,
+        _: MagicMock,
         mock_symlink: MagicMock,
         mock_unlink: MagicMock,
         mock_islink: MagicMock,
         mock_path_exists: MagicMock,
         mock_pick_glmark2_executable: MagicMock,
     ):
-        def custom_env(key: str, is_snap: bool) -> str:
-            if key == "CHECKBOX_RUNTIME":
-                return "/snap/runtime/path/" if is_snap else ""
-
-            raise Exception("unexpected use of this mock")
-
         mock_pick_glmark2_executable.return_value = "glmark2"
-
         tester = gl_support.GLSupportTester()
 
         for is_snap in (True, False):
-            mock_getenv.side_effect = lambda k: custom_env(k, is_snap)
-            gl_support.CHECKBOX_RUNTIME = PosixPath(
-                custom_env("CHECKBOX_RUNTIME", is_snap)
-            )
-            # RCT.SNAP = custom_env("SNAP", is_snap)
-            mock_islink.return_value = is_snap
-            # deb case, the file actually exists
-            mock_path_exists.return_value = not is_snap
+            with patch.dict(
+                "os.environ",
+                {
+                    "CHECKBOX_RUNTIME": (
+                        "/snap/runtime/path/" if is_snap else ""
+                    ),
+                    "DISPLAY": ":0",
+                    "XDG_SESSION_TYPE": "wayland",
+                },
+            ):
+                mock_islink.return_value = is_snap
+                # deb case, the file actually exists
+                mock_path_exists.return_value = not is_snap
+                # hack for this unittest, since we import gl_support early
+                # so the variable assignment was already done during import
+                gl_support.CHECKBOX_RUNTIME = PosixPath("/snap/runtime/path/")
 
-            tester.call_glmark2_validate()
+                tester.call_glmark2_validate()
 
-            if is_snap:
-                mock_symlink.assert_called_once_with(
-                    gl_support.CHECKBOX_RUNTIME / gl_support.GLMARK2_DATA_PATH,
-                    PosixPath("/usr/share/glmark2"),
-                    target_is_directory=True,
-                )
+                if is_snap:
+                    mock_symlink.assert_called_once_with(
+                        gl_support.CHECKBOX_RUNTIME
+                        / gl_support.GLMARK2_DATA_PATH,
+                        PosixPath("/usr/share/glmark2"),
+                        target_is_directory=True,
+                    )
 
-                mock_unlink.assert_called_once_with(
-                    PosixPath("/usr/share/glmark2")
-                )
-            else:
-                mock_symlink.assert_not_called()
-                mock_unlink.assert_not_called()
+                    mock_unlink.assert_called_once_with(
+                        PosixPath("/usr/share/glmark2")
+                    )
+                else:
+                    mock_symlink.assert_not_called()
+                    mock_unlink.assert_not_called()
 
-            mock_symlink.reset_mock()
-            mock_unlink.reset_mock()
-        gl_support.CHECKBOX_RUNTIME = None
+                mock_symlink.reset_mock()
+                mock_unlink.reset_mock()
+            gl_support.CHECKBOX_RUNTIME = None
 
     @patch("subprocess.run")
     @patch("os.environ")
@@ -188,20 +166,16 @@ class TestGLSupportTests(ut.TestCase):
             SystemExit, gl_support.GLSupportTester().call_glmark2_validate
         )
 
+    @patch.dict("os.environ", {"DISPLAY": ":0", "XDG_SESSION_TYPE": "wayland"})
     @patch("shutil.which")
     @patch("gl_support.GLSupportTester.pick_glmark2_executable")
     @patch("subprocess.check_output")
-    @patch("os.getenv")
     def test_glmark2_cmd_override(
         self,
-        mock_getenv: MagicMock,
         mock_check_output: MagicMock,
         mock_pick_exec: MagicMock,
         mock_which: MagicMock,
     ):
-        mock_getenv.side_effect = lambda key: (
-            ":0" if key == "DISPLAY" else "x11"
-        )
         mock_which.return_value = None
         self.assertRaises(
             SystemExit,

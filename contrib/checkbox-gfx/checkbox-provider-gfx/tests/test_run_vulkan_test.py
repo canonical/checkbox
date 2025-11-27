97878e202bd0424a3c213d182837604d2b6ca765
Antoine Lassagne <antonie.lassagne@canonical.com>
2025-11-27 16:58:11 +0000

    Add the gfx provider in the CI

--- contrib/checkbox-gfx/checkbox-provider-gfx/tests/test_run_vulkan_test.py
+++ contrib/checkbox-gfx/checkbox-provider-gfx/tests/test_run_vulkan_test.py
@@ -4,22 +4,25 @@ import unittest
 from unittest.mock import patch, MagicMock
 import run_vulkan_cts
 
+
 class MockProcess:
     """Simulates a running process for Popen."""
+
     def __init__(self, stdout_lines, stderr_output, return_code):
         # Simulate stdout as a file-like object that returns lines
         self.stdout = MagicMock()
-        self.stdout.readline.side_effect = stdout_lines + ['']
-        
+        self.stdout.readline.side_effect = stdout_lines + [""]
+
         # Simulate stderr as a file-like object
         self.stderr = MagicMock()
         self.stderr.read.return_value = stderr_output
-        
+
         # Define the return code
         self.return_code = return_code
 
     def wait(self):
         return self.return_code
+
 
 class TestRunVulkanCTS(unittest.TestCase):
 
@@ -34,9 +37,7 @@ class TestRunVulkanCTS(unittest.TestCase):
         ]
 
         mock_result = MockProcess(
-            stdout_lines=mock_output, 
-            stderr_output="", 
-            return_code=0
+            stdout_lines=mock_output, stderr_output="", return_code=0
         )
 
         # mock_result.stdout = "Vulkan test passed"
@@ -49,7 +50,6 @@ class TestRunVulkanCTS(unittest.TestCase):
         mock_run.assert_called_once()
         mock_exit.assert_called_once_with(0)
 
-
     @patch("run_vulkan_cts.sys.stdout.write")
     @patch("run_vulkan_cts.subprocess.Popen")
     @patch("run_vulkan_cts.exit")
@@ -61,9 +61,7 @@ class TestRunVulkanCTS(unittest.TestCase):
         ]
 
         mock_result = MockProcess(
-            stdout_lines=mock_output, 
-            stderr_output="", 
-            return_code=1
+            stdout_lines=mock_output, stderr_output="", return_code=1
         )
 
         mock_run.return_value = mock_result
@@ -73,14 +71,13 @@ class TestRunVulkanCTS(unittest.TestCase):
         mock_run.assert_called_once()
         mock_exit.assert_called_once_with(1)
 
-
     # @patch("run_vulkan_cts.subprocess.run")
     # @patch("run_vulkan_cts.exit")
     # def test_run_vk_test_failure(self, mock_exit, mock_run):
     #     # Mock a failed subprocess result
     #     mock_result = MockProcess(
-    #         stdout_lines=mock_output, 
-    #         stderr_output="", 
+    #         stdout_lines=mock_output,
+    #         stderr_output="",
     #         return_code=0
     #     )
     #     mock_result.stdout = ""

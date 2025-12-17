f54a82fc3b307e124fc5f3a4811f943a06ff9f0e
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2025-12-17 10:57:31 +0800

    fix: copilot suggestions

--- checkbox-support/checkbox_support/scripts/fwts_test.py
+++ checkbox-support/checkbox_support/scripts/fwts_test.py
@@ -473,7 +473,7 @@ def main(args=sys.argv[1:]):
         tests = requested_tests
         iteration_results = (
             {}
-        )  #  # type: dict[int, tuple[float | str, float|str]]
+        )  # type: dict[int, tuple[float | str, float | str]]
         print("=" * 20 + " Test Results " + "=" * 20)
         progress_indicator_process = None
         if detect_progress_indicator():

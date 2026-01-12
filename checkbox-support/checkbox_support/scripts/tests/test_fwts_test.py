0133613e93e9ba4e88ba54af48857aecf94b5dd6
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-01-12 13:21:02 +0800

    fix: f str, type annotation

--- checkbox-support/checkbox_support/scripts/tests/test_fwts_test.py
+++ checkbox-support/checkbox_support/scripts/tests/test_fwts_test.py
@@ -26,7 +26,7 @@ from checkbox_support.scripts.fwts_test import (
     print_log,
     get_sleep_test_command,
 )
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from pathlib import Path
 
 
@@ -35,7 +35,7 @@ class LogPrinterTest(unittest.TestCase):
         self.logfile = NamedTemporaryFile(delete=False)
 
     @patch("sys.stdout", new_callable=StringIO)
-    def test_logfile_with_encoding_error(self, mock_stdout):
+    def test_logfile_with_encoding_error(self, mock_stdout: MagicMock):
         with open(self.logfile.name, "wb") as f:
             f.write(b"Cannot read PCI config for device 0000:00:\xa1")
             f.write(b"<85>)PNP0B00:00\n")
@@ -74,14 +74,14 @@ class TestGetSleepTestCommand(unittest.TestCase):
         },
     )
     @patch("checkbox_support.scripts.fwts_test.Path.exists")
-    def test_snap_environment_success(self, mock_exists):
+    def test_snap_environment_success(self, mock_exists: MagicMock):
         """Test the snap behavior when the FWTS data directory exists."""
         mock_exists.return_value = True
 
         result = get_sleep_test_command(self.log_path, self.tests)
 
         expected_dir = "/snap/test-snap/checkbox-runtime/share/fwts"
-        expected_cmd = f"fwts -j {expected_dir} -q --stdout-summary -r /tmp/results.log s3 s4"
+        expected_cmd = "fwts -j {} -q --stdout-summary -r /tmp/results.log s3 s4".format(expected_dir)
         self.assertEqual(result, expected_cmd)
 
     @patch.dict(
@@ -92,7 +92,7 @@ class TestGetSleepTestCommand(unittest.TestCase):
         },
     )
     @patch("checkbox_support.scripts.fwts_test.Path.exists")
-    def test_snap_environment_missing_dir(self, mock_exists):
+    def test_snap_environment_missing_dir(self, mock_exists: MagicMock):
         """Test that SystemExit is raised if the FWTS directory is missing."""
         mock_exists.return_value = False
 

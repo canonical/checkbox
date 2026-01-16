f88da8c0a325daac6249954b5ec3863e1a9b5e2e
rene-oromtz <157750458+rene-oromtz@users.noreply.github.com>
2026-01-16 14:34:52 -0700

    Add Intel Granite Rapids-D CPUID (Bugfix) (#2290)
    * Add Intel Granite Rapids CPUID (Bugfix)
    
    * add cpuid to its own friendly name

--- providers/base/bin/cpuid.py
+++ providers/base/bin/cpuid.py
@@ -210,6 +210,7 @@ def cpuid_to_human_friendly(cpuid: str) -> str:
         "Whisky Lake":      ['0x806eb', '0x806ec'],
         "Sierra Forest":    ['0xa06f3'],
         "Granite Rapids":   ['0xa06e0', '0xa06d0', '0xa06d1'],
+        "Granite Rapids-D": ['0xa06e1'],
         "Meteor Lake":      ['0xa06a4'],
         "Arrow Lake":       ['0xc0660', '0xc0652'],
         "Lunar Lake":       ["0xb06d1"]

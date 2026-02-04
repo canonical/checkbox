3b7f65d606a1483077f8015fff6448163f157f3e
Luiz Sampaio <luiz.sampaio@canonical.com>
2026-02-04 20:42:49 +0000

    Create OEM provider and provider snap (New)
    This commit creates the checkbox-oem provider and adds the necessary
    files to be able to pack it as a snap. Add a GitHub Actions workflow to
    build amd64/arm64 snaps and publish them to the edge channel on every
    merge to contrib/checkbox-oem.

---
+++ contrib/checkbox-oem/checkbox-provider-oem/manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from plainbox.provider_manager import setup, N_
+
+setup(
+    name="checkbox-provider-oem",
+    namespace="com.canonical.contrib",
+    version="0.5.7",
+    description=N_("The checkbox oem provider"),
+    gettext_domain="checkbox-provider-oem",
+)

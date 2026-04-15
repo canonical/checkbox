e2953b11dc7da8584cb577852565251ed1e60fa3
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-04-15 14:25:23 +0800

    fix: 3.5

---
+++ providers/base/bin/detect_vrr.py
@@ -0,0 +1,121 @@
+import ctypes
+import ctypes.util
+import os
+from pathlib import Path
+
+# this should find "libdrm.so.2"
+libdrm_path = ctypes.util.find_library("drm")
+if not libdrm_path:
+    raise ImportError("libdrm not found")
+drm = ctypes.CDLL(libdrm_path)
+
+
+# https://github.com/CPFL/drm/blob/6f90b77ea903756c87ae614c093e3d816ebb26fc/xf86drmMode.h#L72
+DRM_MODE_PROP_NAME_LEN = 32
+
+
+class drmModePropertyRes(ctypes.Structure):
+    _fields_ = [
+        ("prop_id", ctypes.c_uint32),
+        ("flags", ctypes.c_uint32),
+        ("name", ctypes.c_char * DRM_MODE_PROP_NAME_LEN),
+        ("count_values", ctypes.c_uint32),
+        ("values", ctypes.POINTER(ctypes.c_uint64)),
+        ("count_enums", ctypes.c_uint32),
+        ("enums", ctypes.c_void_p),
+        ("count_blobs", ctypes.c_uint32),
+        ("blob_ids", ctypes.POINTER(ctypes.c_uint32)),
+    ]
+
+
+class drmModeConnector(ctypes.Structure):
+    _fields_ = [
+        ("connector_id", ctypes.c_uint32),
+        ("encoder_id", ctypes.c_uint32),
+        ("connector_type", ctypes.c_uint32),
+        ("connector_type_id", ctypes.c_uint32),
+        ("connection", ctypes.c_uint32),
+        ("mmWidth", ctypes.c_uint32),
+        ("mmHeight", ctypes.c_uint32),
+        ("subpixel", ctypes.c_uint32),
+        ("count_modes", ctypes.c_int),
+        ("modes", ctypes.c_void_p),
+        ("count_props", ctypes.c_int),
+        ("props", ctypes.POINTER(ctypes.c_uint32)),
+        ("prop_values", ctypes.POINTER(ctypes.c_uint64)),
+        ("count_encoders", ctypes.c_int),
+        ("encoders", ctypes.POINTER(ctypes.c_uint32)),
+    ]
+
+
+class drmModeRes(ctypes.Structure):
+    _fields_ = [
+        ("count_fbs", ctypes.c_int),
+        ("fbs", ctypes.c_void_p),
+        ("count_crtcs", ctypes.c_int),
+        ("crtcs", ctypes.c_void_p),
+        ("count_connectors", ctypes.c_int),
+        ("connectors", ctypes.POINTER(ctypes.c_uint32)),
+        ("count_encoders", ctypes.c_int),
+        ("encoders", ctypes.c_void_p),
+        ("min_width", ctypes.c_uint32),
+        ("max_width", ctypes.c_uint32),
+        ("min_height", ctypes.c_uint32),
+        ("max_height", ctypes.c_uint32),
+    ]
+
+
+drm.drmModeGetResources.restype = ctypes.POINTER(drmModeRes)
+drm.drmModeGetConnector.restype = ctypes.POINTER(drmModeConnector)
+drm.drmModeGetProperty.restype = ctypes.POINTER(drmModePropertyRes)
+
+
+def get_vrr_capable(dri_card: Path):
+    """Checks if a /dev/dri/cardN is vrr capable
+    A monitor must be connected and active for the result to be accurate
+
+    :param dri_card: _description_
+    :raises RuntimeError: Failed to open /dev/driN
+    :raises RuntimeError: drmModeGetResources returned nullptr
+    """
+    with dri_card.open() as f:
+        fd = f.fileno()
+        if fd < 0:
+            raise RuntimeError("Could not open DRM device")
+
+        res_ptr = drm.drmModeGetResources(fd)
+        if not res_ptr:
+            raise RuntimeError("Failed to get DRM resources")
+
+        res = res_ptr.contents
+        for i in range(res.count_connectors):
+            conn_id = res.connectors[i]
+            conn_ptr = drm.drmModeGetConnector(fd, conn_id)
+            if not conn_ptr:
+                continue
+
+            conn = conn_ptr.contents
+            if conn.connection != 1:  # ignore disconnected connectors
+                continue
+
+            for j in range(conn.count_props):
+                prop_ptr = drm.drmModeGetProperty(fd, conn.props[j])
+                if prop_ptr:
+                    prop_name = prop_ptr.contents.name.decode()
+                    print(prop_name)
+                    if prop_name == "vrr_capable":
+                        val = conn.prop_values[j]
+                        vrr_capable = val == 1
+                        print("Connector", conn_id, "capable", vrr_capable)
+                    drm.drmModeFreeProperty(prop_ptr)
+
+            drm.drmModeFreeConnector(conn_ptr)
+        drm.drmModeFreeResources(res_ptr)
+
+
+if __name__ == "__main__":
+    for path in Path("/dev/dri").iterdir():
+        if os.path.basename(str(path)).startswith("card"):
+            print("Testing", path)
+            get_vrr_capable(path)
+            print("=" * 10)

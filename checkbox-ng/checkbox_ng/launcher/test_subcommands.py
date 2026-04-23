6d4741e7b88389bda4b2631dcc020a9db31e08cc
Hook25 <massimiliano.girardi@canonical.com>
2026-04-23 15:53:38 +0200

    Fix get_record_value call

--- checkbox-ng/checkbox_ng/launcher/test_subcommands.py
+++ checkbox-ng/checkbox_ng/launcher/test_subcommands.py
@@ -984,43 +984,46 @@ class TestLauncherReturnCodes(TestCase):
         self.assertEqual(self.launcher.invoked(self.ctx), 1)
 
 
-class TestLListBootstrapped(TestCase):
+class TestListBootstrapped(TestCase):
     def setUp(self):
         self.launcher = ListBootstrapped()
         self.ctx = Mock()
         self.ctx.args = Mock(TEST_PLAN="", format="")
+
+        def identity(self, name):
+            return self._raw_data[name]
+
+        job_1 = Mock(
+            _raw_data={
+                "id": "namespace1::test-job1",
+                "summary": "fake-job1",
+                "plugin": "manual",
+                "description": "fake-description1",
+                "certification_status": "non-blocker",
+            },
+            id="namespace1::test-job1",
+            partial_id="test-job1",
+        )
+        job_2 = Mock(
+            _raw_data={
+                "id": "namespace2::test-job2",
+                "summary": "fake-job2",
+                "plugin": "shell",
+                "command": "ls",
+                "certification_status": "non-blocker",
+            },
+            id="namespace2::test-job2",
+            partial_id="test-job2",
+        )
+        job_1.get_record_value = partial(identity, job_1)
+        job_2.get_record_value = partial(identity, job_2)
         self.ctx.sa = Mock(
             start_new_session=Mock(),
             get_test_plans=Mock(return_value=["test-plan1", "test-plan2"]),
             select_test_plan=Mock(),
             bootstrap=Mock(),
             get_static_todo_list=Mock(return_value=["test-job1", "test-job2"]),
-            get_job=Mock(
-                side_effect=[
-                    Mock(
-                        _raw_data={
-                            "id": "namespace1::test-job1",
-                            "summary": "fake-job1",
-                            "plugin": "manual",
-                            "description": "fake-description1",
-                            "certification_status": "non-blocker",
-                        },
-                        id="namespace1::test-job1",
-                        partial_id="test-job1",
-                    ),
-                    Mock(
-                        _raw_data={
-                            "id": "namespace2::test-job2",
-                            "summary": "fake-job2",
-                            "plugin": "shell",
-                            "command": "ls",
-                            "certification_status": "non-blocker",
-                        },
-                        id="namespace2::test-job2",
-                        partial_id="test-job2",
-                    ),
-                ]
-            ),
+            get_job=Mock(side_effect=[job_1, job_2]),
             get_job_state=Mock(
                 return_value=Mock(effective_certification_status="blocker")
             ),

32f5194bd7dec1d99d1a19633a8b8714adc0a41f
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-05-27 16:38:57 +0800

    fix: formatting

--- providers/base/tests/test_detect_vrr.py
+++ providers/base/tests/test_detect_vrr.py
@@ -110,7 +110,9 @@ class TestGetVrrCapableMonitors(unittest.TestCase):
 
         self.assertFalse(result)
         # still need to free the pointer
-        self.mock_drm.drmModeFreeConnector.assert_called_once_with(self.mock_drm.drmModeGetConnector.return_value )
+        self.mock_drm.drmModeFreeConnector.assert_called_once_with(
+            self.mock_drm.drmModeGetConnector.return_value
+        )
 
     def test_skips_null_connector_pointer(self):
         self.mock_drm.drmModeGetResources.return_value = _ptr(_make_res([10]))

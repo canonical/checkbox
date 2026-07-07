5b0bd78e55da0ccad767897f27862d89875a285f
stanley31 <stanley.huang@canonical.com>
2026-07-07 17:04:21 +0800

    fix shell check issue
    fix shell check issue

--- contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/dragonwing_fastrpc_test.sh
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/dragonwing_fastrpc_test.sh
@@ -3,7 +3,7 @@
 # Run the fastrpc test on the given domains, signed or unsigned.
 
 calculator() {
-    cd /home/ubuntu/calculator
+    cd /home/ubuntu/calculator || return 1
     output="$(LD_PRELOAD=./libcalculator.so DSP_LIBRARY_PATH=. ./calculator "${@}" -n 1000)"
     echo "${output}"
     echo "${output}" | grep -q Success
@@ -11,15 +11,21 @@ calculator() {
 }
 
 if [ -x /usr/bin/fastrpc_test ]; then
-    cmd=fastrpc_test
+    use_fastrpc_test=true
+    cmd_name=fastrpc_test
 else
-    cmd=calculator
+    use_fastrpc_test=false
+    cmd_name=calculator
 fi
 
 exit_code=0
 while [ "${#}" -ne 0 ]; do
-    printf 'Testing: %s -d %s -U %s\n' "${cmd}" "${1%:*}" "${1#*:}"
-    "${cmd}" -d "${1%:*}" -U "${1#*:}" || exit_code=1
+    printf 'Testing: %s -d %s -U %s\n' "${cmd_name}" "${1%:*}" "${1#*:}"
+    if "${use_fastrpc_test}"; then
+        fastrpc_test -d "${1%:*}" -U "${1#*:}" || exit_code=1
+    else
+        calculator -d "${1%:*}" -U "${1#*:}" || exit_code=1
+    fi
     shift
 done
 exit "${exit_code}"

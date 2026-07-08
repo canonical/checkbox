58cb0d97ac6abc0a033a1681eac205fc16d6f6fa
Hook25 <massimiliano.girardi@canonical.com>
2026-07-08 15:00:51 +0200

    Inject before clause in jobs with siblings after suspend

--- checkbox-ng/plainbox/impl/session/state.py
+++ checkbox-ng/plainbox/impl/session/state.py
@@ -1278,7 +1278,28 @@ class SessionState:
             siblings.append(overrides)
         return siblings
 
+    def _get_job_data_directly_required_suspend(self, job_data):
+        to_r = set()
+
+        def pxu_compatible_in(con):
+            if isinstance(con, list):
+                return con
+            # this is not perfect but it is good enough for what we need here
+            return con.split()
+
+        for suspend_id in [Suspend.AUTO_JOB_ID, Suspend.MANUAL_JOB_ID]:
+            if suspend_id in pxu_compatible_in(job_data.get("depends", [])):
+                to_r.add(suspend_id)
+            elif suspend_id in pxu_compatible_in(job_data.get("after", [])):
+                to_r.add(suspend_id)
+        return to_r
+
     def _add_job_siblings_unit(self, job, recompute, via):
+        job_requiring_suspend = set(
+            self._get_job_data_directly_required_suspend(job._data)
+        )
+        siblings_requiring_suspend = set()
+
         siblings = job.siblings or []
         siblings += self._get_flags_siblings(job)
         for overrides in siblings:
@@ -1300,6 +1321,24 @@ class SessionState:
                 recompute,
                 via,
             )
+            siblings_requiring_suspend |= (
+                self._get_job_data_directly_required_suspend(data)
+            )
+        sibling_required_not_job = (
+            siblings_requiring_suspend - job_requiring_suspend
+        )
+        # when a job doesn't require to be after suspend but has a sibling that
+        # wants to be the job is always before suspend
+        if not sibling_required_not_job:
+            return
+        # Note: intentionally avoid accessing before directly as it is a
+        #       cached property
+        before = job._data.get("before", [])
+        if isinstance(before, list):
+            before += list(sibling_required_not_job)
+        else:
+            before += " " + " ".join(sibling_required_not_job)
+        job._data["before"] = before
 
     def remove_unit(self, unit, *, recompute=True):
         """

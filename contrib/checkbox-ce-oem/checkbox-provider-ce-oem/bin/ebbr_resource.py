8fdfa6ded9b8ec393728cae316ea63e3f5b7e63d
baconYao <patrick.chang@canonical.com>
2026-07-09 16:13:30 +0800

    Fix black checking
    Signed-off-by: baconYao <patrick.chang@canonical.com>

--- contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/ebbr_resource.py
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/ebbr_resource.py
@@ -10,25 +10,25 @@ def runcmd(command):
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         encoding="utf-8",
-        timeout=1
+        timeout=1,
     )
     return ret
 
 
 def main():
-    fwts_ret = runcmd(['fwts --ebbr --show-tests'])
-    fwts_line_split = fwts_ret.stdout.split('\n')
-    ebbr_tests_idx = fwts_line_split.index('EBBR tests:')
+    fwts_ret = runcmd(["fwts --ebbr --show-tests"])
+    fwts_line_split = fwts_ret.stdout.split("\n")
+    ebbr_tests_idx = fwts_line_split.index("EBBR tests:")
 
-    for i in range(ebbr_tests_idx+1, len(fwts_line_split)):
+    for i in range(ebbr_tests_idx + 1, len(fwts_line_split)):
         line = fwts_line_split[i]
-        if line == '':
+        if line == "":
             return
         c = line.split()
         case_name = c[0]
-        description = ' '.join(c[1:-1])
-        print(f'case: {case_name}')
-        print(f'description: {description}\n')
+        description = " ".join(c[1:-1])
+        print(f"case: {case_name}")
+        print(f"description: {description}\n")
 
 
 if __name__ == "__main__":

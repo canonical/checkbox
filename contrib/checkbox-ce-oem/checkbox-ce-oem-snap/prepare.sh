2d6ee52a7d6069a4bcc8be801ccf52c5e52b46b8
stanley31 <stanley.huang@canonical.com>
2026-07-22 12:59:24 +0800

    append commit hash in checkbox-ce-oem snap version
    append commit hash to the version of checkboc ce-oem snap

--- contrib/checkbox-ce-oem/checkbox-ce-oem-snap/prepare.sh
+++ contrib/checkbox-ce-oem/checkbox-ce-oem-snap/prepare.sh
@@ -18,3 +18,27 @@ fi
 
 echo "Copying over providers to $series"
 rsync -r --links ../checkbox-provider-ce-oem "$series/"
+
+snapcraft_file="$series/snap/snapcraft.yaml"
+provider_commit_short_hash=$(git log -1 --format='%h' -- ../checkbox-provider-ce-oem)
+
+if [ ! -f "$snapcraft_file" ]; then
+	echo "$snapcraft_file not found"
+	exit 1
+fi
+
+current_version=$(sed -n "s/^version:[[:space:]]*'\([^']*\)'/\1/p" "$snapcraft_file")
+if [ -z "$current_version" ]; then
+	echo "Could not read version from $snapcraft_file"
+	exit 1
+fi
+
+if [[ "$current_version" == *-"$provider_commit_short_hash" ]]; then
+	new_version="$current_version"
+else
+	new_version="${current_version}-${provider_commit_short_hash}"
+fi
+
+sed -i "s/^version:.*/version: '${new_version}'/" "$snapcraft_file"
+echo "Updated $snapcraft_file version:"
+grep '^version:' "$snapcraft_file"

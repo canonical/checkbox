2f25238f94318bc0d502a50c66a97e64b1f1e01b
stanley31huang <stanley.huang@canonical.com>
2026-08-25 08:38:42 +0800

    Update contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/resource_gpio_buzzer_mapping.sh
    Co-authored-by: Pei Yao-Chang <baconYao@users.noreply.github.com>

--- contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/resource_gpio_buzzer_mapping.sh
+++ contrib/checkbox-ce-oem/checkbox-provider-ce-oem/bin/resource_gpio_buzzer_mapping.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 awk '{
-    split($0, record, " ")
-    for (i in record) {
-        split(record[i], data, ":")
-        printf "name: %s\nport: %s\nenable_value: %s\n", data[1], data[2], data[3]
-    }
-}' <<< "$GPIO_BUZZER"
+        count = split($0, record, " ")
+        for (i = 1; i <= count; i++) {
+            split(record[i], data, ":")
+            if (length(data) == 2) {
+                # Example: BUZZER_EN:1
+                #   BUZZER_EN is the GPIO line name
+                #   You can find the name via /sys/kernel/debug/gpio or the gpioinfo command
+                port = data[1]
+                enable_value = data[2]
+            } else if (length(data) == 3) {
+                # Example: buzzer1:498:0
+                port = data[2]
+                enable_value = data[3]
+            }
+            printf "name: %s\nport: %s\nenable_value: %s\n\n", data[1], port, enable_value
+        }
+    }' <<< "$GPIO_BUZZER"

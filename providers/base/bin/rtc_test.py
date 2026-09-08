73a970ac6d12719b3409431fe0c8a367302de659
zy <zihyu23@gmail.com>
2026-09-08 09:45:54 +0800

    add copyright header

--- providers/base/bin/rtc_test.py
+++ providers/base/bin/rtc_test.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
-# RTC (Real Time Clock) test utility for Checkbox.
+# This file is part of Checkbox.
 #
-# Consolidates the shell logic previously embedded in the
-# rtc/battery, rtc/rtc_number, rtc_list, rtc/rtc_read_{rtc},
-# rtc/rtc_alarm_{rtc} and rtc/rtc_clock_{rtc} jobs into a single
-# script with subcommands, following the argparse + dispatch-table
-# pattern used by other checkbox-support scripts.
+# Copyright 2025 Canonical Ltd.
+#
+# Checkbox is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3,
+# as published by the Free Software Foundation.
+#
+# Checkbox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Checkbox. If not, see <http://www.gnu.org/licenses/>.
 
 import argparse
 import glob

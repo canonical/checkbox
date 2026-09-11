034581b4ff8eb63019df4538a63c914a8f1fa5e4
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-09-11 16:50:08 +0800

    fix: space

--- providers/base/bin/graphics_max_resolution.py
+++ providers/base/bin/graphics_max_resolution.py
@@ -107,7 +107,7 @@ def main():
                 "[ ERR ]",
                 msg_prefix,
                 "is not using its maximum resolution.",
-                f"Expected {max_w}x{max_h},"
+                f"Expected {max_w}x{max_h},",
                 f"but got {curr.width}x{curr.height}",
                 file=sys.stderr,
             )

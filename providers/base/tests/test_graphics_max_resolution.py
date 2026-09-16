7cfd78c90f10f74e110ce40037e34f11ec8bb3fb
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-09-16 14:17:30 +0800

    fix: unused import

--- providers/base/tests/test_graphics_max_resolution.py
+++ providers/base/tests/test_graphics_max_resolution.py
@@ -19,7 +19,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Checkbox.  If not, see <http://www.gnu.org/licenses/>.
 
-from contextlib import ExitStack
 import sys
 import unittest
 from io import StringIO

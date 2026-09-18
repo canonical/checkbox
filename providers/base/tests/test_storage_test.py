01023ca4007a53bc1e2c6c49960be65061e58d44
Zhongning Li <60045212+tomli380576@users.noreply.github.com>
2026-09-18 16:56:23 +0800

    fix: types

--- providers/base/tests/test_storage_test.py
+++ providers/base/tests/test_storage_test.py
@@ -1,6 +1,6 @@
 import json
 import unittest
-from pathlib import Path
+from pathlib import Path, PosixPath
 from unittest.mock import MagicMock, patch
 
 from storage_test import (
@@ -151,7 +151,15 @@ class TestRunBonnie(unittest.TestCase):
     def test_run_bonnie(self, mock_check_call, mock_free_space, mock_memory):
         run_bonnie(Path("/test_dir"))
         mock_check_call.assert_called_once_with(
-            ["bonnie++", "-d", "/test_dir", "-u", "root", "-r", "8000"]
+            [
+                "bonnie++",
+                "-d",
+                PosixPath("/test_dir"),
+                "-u",
+                "root",
+                "-r",
+                "8000",
+            ]
         )
 
     @patch("storage_test.memory", return_value=8000)
@@ -162,7 +170,15 @@ class TestRunBonnie(unittest.TestCase):
     ):
         run_bonnie(Path("/test_dir"))
         mock_check_call.assert_called_once_with(
-            ["bonnie++", "-d", "/test_dir", "-u", "root", "-r", "250.0"]
+            [
+                "bonnie++",
+                "-d",
+                PosixPath("/test_dir"),
+                "-u",
+                "root",
+                "-r",
+                "250.0",
+            ]
         )
 
 
